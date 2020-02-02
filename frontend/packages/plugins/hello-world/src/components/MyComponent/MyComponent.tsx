de721255640fe866cf79d41bf0a50f3f1ba689d3
Patrik Oldsberg <patriko@spotify.com>
2020-02-03 00:08:22 +0100

    frontend/packages: add material-ui

--- frontend/packages/plugins/hello-world/src/components/MyComponent/MyComponent.tsx
+++ frontend/packages/plugins/hello-world/src/components/MyComponent/MyComponent.tsx
@@ -1,7 +1,16 @@
 import React, { FC } from 'react';
+import Button from '@material-ui/core/Button';
 
 const MyComponent: FC<{}> = () => {
-  return <div>Hello!</div>;
+  return (
+    <Button
+      variant="contained"
+      color="primary"
+      onClick={() => window.location.reload()}
+    >
+      Hello!
+    </Button>
+  );
 };
 
 export default MyComponent;
