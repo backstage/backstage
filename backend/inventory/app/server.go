bce7bdb73b8ea7326685f8f7facc4a24a1ce909f
Jefferson Girão <jefferson@girao.net>
2020-02-04 10:37:32 +0100

    style: put server in its own package

---
+++ backend/inventory/app/server.go
@@ -0,0 +1,16 @@
+package app
+
+import (
+	"context"
+
+	pb "github.com/spotify/backstage/inventory/protos"
+)
+
+// Server is the inventory GRPC server
+type Server struct {
+}
+
+// GetEntity returns an inventory Entitity with the selected facts
+func (s *Server) GetEntity(ctx context.Context, req *pb.GetEntityRequest) (*pb.GetEntityReply, error) {
+	return &pb.GetEntityReply{}, nil
+}
--- backend/inventory/main.go
+++ backend/inventory/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"context"
 	"log"
 	"net"
 
+	"github.com/spotify/backstage/inventory/app"
 	pb "github.com/spotify/backstage/inventory/protos"
 	"google.golang.org/grpc"
 )
@@ -13,19 +13,12 @@ const (
 	port = ":50051"
 )
 
-type inventoryServer struct {
-}
-
-func (s *inventoryServer) GetEntity(ctx context.Context, req *pb.GetEntityRequest) (*pb.GetEntityReply, error) {
-	return &pb.GetEntityReply{}, nil
-}
-
 func main() {
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	grpcServer := grpc.NewServer()
-	pb.RegisterInventoryServer(grpcServer, &inventoryServer{})
+	pb.RegisterInventoryServer(grpcServer, &app.Server{})
 	grpcServer.Serve(lis)
 }
