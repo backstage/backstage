926732ce6cc2762892a7be1c9a547639d3a4c59c
Fredrik Adelöw <freben@spotify.com>
2020-02-06 10:49:00 +0100

    Persist login (for now - due to constant page reloads)

--- frontend/packages/app/src/login/MockCurrentUser.ts
+++ frontend/packages/app/src/login/MockCurrentUser.ts
@@ -1,17 +1,27 @@
 import Observable from 'zen-observable';
 import { LoginState } from './types';
 
+const LOCAL_STORAGE_KEY = 'mock_current_user';
+
 export class MockCurrentUser {
-  private listeners: ZenObservable.SubscriptionObserver<LoginState>[] = [];
-  private currentUser: string | undefined = undefined;
+  private listeners: ZenObservable.SubscriptionObserver<LoginState>[];
+  private currentUser: string | undefined;
+
+  constructor() {
+    this.listeners = [];
+    this.currentUser = undefined;
+    this.loadPrevious();
+  }
 
   login(user: string) {
     this.currentUser = user;
+    this.saveCurrent();
     this.notify();
   }
 
   logout() {
     this.currentUser = undefined;
+    this.saveCurrent();
     this.notify();
   }
 
@@ -23,6 +33,25 @@ export class MockCurrentUser {
         this.listeners = this.listeners.filter(x => x !== subscriber);
       };
     });
+  }
+
+  private loadPrevious() {
+    const user = localStorage.getItem(LOCAL_STORAGE_KEY);
+    if (user) {
+      try {
+        this.currentUser = JSON.parse(user);
+      } catch (e) {
+        localStorage.removeItem(LOCAL_STORAGE_KEY);
+      }
+    }
+  }
+
+  private saveCurrent() {
+    if (!this.currentUser) {
+      localStorage.removeItem(LOCAL_STORAGE_KEY);
+    } else {
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(this.currentUser));
+    }
   }
 
   private notify() {
