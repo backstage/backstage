b69ee27d9efb28b9072f8acb862182b11d42876b
Patrik Oldsberg <poldsberg@gmail.com>
2020-02-06 20:33:03 +0100

    front/app: move AppShell things into home-page plugin

--- frontend/packages/plugins/home-page/src/components/HomePage/HomePage.tsx
+++ frontend/packages/plugins/home-page/src/components/HomePage/HomePage.tsx
@@ -1,20 +1,45 @@
-import { EntityLink, InfoCard } from '@backstage/core';
-import { Typography } from '@material-ui/core';
+import { EntityLink, InfoCard, Header, Page, theme } from '@backstage/core';
+import { Typography, makeStyles, Theme } from '@material-ui/core';
 import React, { FC } from 'react';
+import HomePageTimer from '../HomepageTimer';
+
+const useStyles = makeStyles<Theme>(theme => ({
+  mainContentArea: {
+    overflowX: 'hidden',
+    overflowY: 'auto',
+  },
+  pageBody: {
+    padding: theme.spacing(2),
+  },
+  avatarButton: {
+    padding: theme.spacing(2),
+  },
+}));
 
 const HomePage: FC<{}> = () => {
+  const classes = useStyles();
+
   return (
-    <InfoCard title="Home Page">
-      <Typography variant="body1">Welcome to Backstage!</Typography>
-      <div>
-        <EntityLink kind="service" id="backstage-backend">
-          Backstage Backend
-        </EntityLink>
-        <EntityLink uri="entity:service:backstage-lb" subPath="ci-cd">
-          Backstage LB CI/CD
-        </EntityLink>
+    <Page theme={theme.home}>
+      <div className={classes.mainContentArea}>
+        <Header title="This is Backstage!">
+          <HomePageTimer />
+        </Header>
+        <div className={classes.pageBody}>
+          <InfoCard title="Home Page">
+            <Typography variant="body1">Welcome to Backstage!</Typography>
+            <div>
+              <EntityLink kind="service" id="backstage-backend">
+                Backstage Backend
+              </EntityLink>
+              <EntityLink uri="entity:service:backstage-lb" subPath="ci-cd">
+                Backstage LB CI/CD
+              </EntityLink>
+            </div>
+          </InfoCard>
+        </div>
       </div>
-    </InfoCard>
+    </Page>
   );
 };
 
