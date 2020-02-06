c52d6cbb19dc146303a047b63945da9e86ae905e
Nicola Corti <corti.nico@gmail.com>
2020-02-06 14:50:44 +0100

    Include the /main folder for the App Template

---
+++ backend/scaffolder/templates/android-kotlin-app-template/{{cookiecutter.app_name}}/app/src/main/java/{{cookiecutter.app_id}}/MainActivity.kt
@@ -0,0 +1,12 @@
+package {{cookiecutter.app_id}}
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
