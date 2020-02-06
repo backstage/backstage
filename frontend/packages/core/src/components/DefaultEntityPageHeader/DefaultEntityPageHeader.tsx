c62f92d9126dff8f7e872f462263f7311d71b8a3
Patrik Oldsberg <poldsberg@gmail.com>
2020-02-06 20:39:56 +0100

    front/core/DefaultEntityPageHeader: provide hard-coded service theme for now

--- frontend/packages/core/src/components/DefaultEntityPageHeader/DefaultEntityPageHeader.tsx
+++ frontend/packages/core/src/components/DefaultEntityPageHeader/DefaultEntityPageHeader.tsx
@@ -1,11 +1,18 @@
 import React, { FC } from 'react';
-import { Header, useEntity, useEntityConfig } from '../..';
+import { Header, useEntity, useEntityConfig, theme } from '../..';
 import { EntityPageHeaderProps } from '../../api/entityView/types';
+import { Theme } from '../../layout/Page/Page';
 
 const DefaultEntityPageHeader: FC<EntityPageHeaderProps> = () => {
   const { id } = useEntity();
   const config = useEntityConfig();
-  return <Header title={`${config.title} - ${id}`} />;
+
+  // TODO(rugvip): provide theme through entity config
+  return (
+    <Theme.Provider value={theme.service}>
+      <Header title={`${config.title} - ${id}`} />
+    </Theme.Provider>
+  );
 };
 
 export default DefaultEntityPageHeader;
