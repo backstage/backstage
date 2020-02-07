bb450005af9a852de37d5b6ec22c476c7dda7203
Patrik Oldsberg <poldsberg@gmail.com>
2020-02-07 13:27:43 +0100

    front/core: add plugin api hooks and output for widgets

--- frontend/packages/core/src/api/widgetView/WidgetViewBuilder.tsx
+++ frontend/packages/core/src/api/widgetView/WidgetViewBuilder.tsx
@@ -1,12 +1,18 @@
 import React, { ComponentType } from 'react';
 import { App, AppComponentBuilder } from '../app/types';
 import { Widget } from './types';
+import BackstagePlugin from '../plugin/Plugin';
 import DefaultWidgetView from '../../components/DefaultWidgetView';
 
-type WidgetViewRegistration = {
-  type: 'component';
-  widget: Widget;
-};
+type WidgetViewRegistration =
+  | {
+      type: 'component';
+      widget: Widget;
+    }
+  | {
+      type: 'plugin';
+      plugin: BackstagePlugin;
+    };
 
 export default class WidgetViewBuilder extends AppComponentBuilder {
   private readonly registrations = new Array<WidgetViewRegistration>();
@@ -17,19 +23,41 @@ export default class WidgetViewBuilder extends AppComponentBuilder {
     return this;
   }
 
+  register(plugin: BackstagePlugin): WidgetViewBuilder {
+    this.registrations.push({ type: 'plugin', plugin });
+    return this;
+  }
+
   build(_app: App): ComponentType<any> {
     if (this.output) {
       return this.output;
     }
 
-    const widgets = this.registrations.map(reg => {
+    const widgets = new Array<Widget>();
+
+    for (const reg of this.registrations) {
       switch (reg.type) {
         case 'component':
-          return reg.widget;
+          widgets.push(reg.widget);
+          break;
+        case 'plugin':
+          let added = false;
+          for (const output of reg.plugin.output()) {
+            if (output.type === 'widget') {
+              widgets.push(output.widget);
+              added = true;
+            }
+          }
+          if (!added) {
+            throw new Error(
+              `Plugin ${reg.plugin} was registered as widget provider, but did not provide any widgets`,
+            );
+          }
+          break;
         default:
           throw new Error(`Unknown WidgetViewBuilder registration`);
       }
-    });
+    }
 
     this.output = () => <DefaultWidgetView widgets={widgets} />;
     return this.output;
