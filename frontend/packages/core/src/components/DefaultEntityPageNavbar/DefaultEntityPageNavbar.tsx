58ad9cd3228b5d74c3ef8255d20d524769f462cd
Patrik Oldsberg <poldsberg@gmail.com>
2020-02-07 09:27:04 +0100

    front/core/DefaultEntityPageNavbar: stole styling from alunds PR

--- frontend/packages/core/src/components/DefaultEntityPageNavbar/DefaultEntityPageNavbar.tsx
+++ frontend/packages/core/src/components/DefaultEntityPageNavbar/DefaultEntityPageNavbar.tsx
@@ -1,21 +1,35 @@
 import React, { FC } from 'react';
 import { EntityPageNavbarProps } from '../../api/entityView/types';
-import { useEntityUri, EntityLink } from '../..';
-import { List, ListItem } from '@material-ui/core';
+import { useEntityUri } from '../..';
+import { List, makeStyles, Theme } from '@material-ui/core';
+import NavbarItem from './NavbarItem';
+
+const useStyles = makeStyles<Theme>({
+  nav: {
+    gridArea: 'pageNav',
+    width: 220,
+    transition: 'width 0.07s, height 0s',
+    transitionTimingFunction: 'ease-in',
+    backgroundColor: '#eeeeee',
+    boxShadow: '0px 0 4px 0px rgba(0,0,0,0.35)',
+  },
+  list: {
+    padding: 0,
+  },
+});
 
 const DefaultEntityPageNavbar: FC<EntityPageNavbarProps> = ({ navItems }) => {
+  const classes = useStyles();
   const entityUri = useEntityUri();
 
   return (
-    <List>
-      {navItems.map(({ title, target }) => (
-        <ListItem key={target}>
-          <EntityLink uri={entityUri} subPath={target}>
-            {title}
-          </EntityLink>
-        </ListItem>
-      ))}
-    </List>
+    <nav className={classes.nav}>
+      <List className={classes.list}>
+        {navItems.map((navItem, index) => (
+          <NavbarItem key={index} navItem={navItem} entityUri={entityUri} />
+        ))}
+      </List>
+    </nav>
   );
 };
 
