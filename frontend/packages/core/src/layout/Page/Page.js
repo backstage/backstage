da8895abace089bf15cf7354aba7caf2223cd195
Patrik Oldsberg <poldsberg@gmail.com>
2020-02-07 11:05:49 +0100

    front/core/layout/Page: remove page grid

--- frontend/packages/core/src/layout/Page/Page.js
+++ frontend/packages/core/src/layout/Page/Page.js
@@ -1,18 +1,5 @@
 import React, { Component } from 'react';
-import { withStyles } from '@material-ui/core';
 import { theme } from './PageThemeProvider';
-
-const styles = theme => ({
-  root: {
-    display: 'grid',
-    gridTemplateAreas:
-      "'pageHeader pageHeader pageHeader' 'pageSubheader pageSubheader pageSubheader' 'pageNav pageContent pageSidebar'",
-    gridTemplateRows: 'auto auto 1fr',
-    gridTemplateColumns: 'auto 1fr auto',
-    minHeight: '100%',
-    paddingBottom: theme.spacing(3),
-  },
-});
 
 export const Theme = React.createContext({});
 
@@ -22,16 +9,10 @@ class Page extends Component {
   };
 
   render() {
-    const { theme, backgroundColor, classes, children, styles = {}, ...otherProps } = this.props;
+    const { theme, children } = this.props;
 
-    return (
-      <Theme.Provider value={theme}>
-        <div style={{ backgroundColor, ...styles }} className={classes.root} {...otherProps}>
-          {children}
-        </div>
-      </Theme.Provider>
-    );
+    return <Theme.Provider value={theme}>{children}</Theme.Provider>;
   }
 }
 
-export default withStyles(styles)(Page);
+export default Page;
