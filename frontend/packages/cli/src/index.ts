3e3e428d55dde9ce3da031eae50b3bc5ba55d5a4
Patrik Oldsberg <poldsberg@gmail.com>
2020-02-27 10:54:37 +0100

    packages/cli: add broken servePlugin command

--- frontend/packages/cli/src/index.ts
+++ frontend/packages/cli/src/index.ts
@@ -1,11 +1,21 @@
 import program from 'commander';
 import createPluginCommand from './commands/createPlugin';
+import servePlugin from './commands/servePlugin';
+
+process.on('unhandledRejection', err => {
+  throw err;
+});
 
 const main = (argv: string[]) => {
   program
     .command('create-plugin')
     .description('Creates a new plugin in the current repository')
     .action(createPluginCommand);
+
+  program
+    .command('serve-plugin')
+    .description('Serves a plugin dev folder')
+    .action(servePlugin);
 
   program.on('command:*', () => {
     console.error(
