c3febb492f5c83446d27cdee1a64ac6284cdac99
Patrik Oldsberg <poldsberg@gmail.com>
2020-03-06 16:30:37 +0100

    cli: properly forward status code in plugin commands

--- packages/cli/src/commands/plugin/lint.ts
+++ packages/cli/src/commands/plugin/lint.ts
@@ -9,6 +9,7 @@ export default async () => {
     if (result.error) {
       throw result.error;
     }
+    process.exit(result.status ?? 0);
   } catch (error) {
     process.stderr.write(`${chalk.red(error.message)}\n`);
     process.exit(1);
--- packages/cli/src/commands/plugin/test.ts
+++ packages/cli/src/commands/plugin/test.ts
@@ -14,6 +14,7 @@ export default async (cmd: Command) => {
     if (result.error) {
       throw result.error;
     }
+    process.exit(result.status ?? 0);
   } catch (error) {
     process.stderr.write(`${chalk.red(error.message)}\n`);
     process.exit(1);
