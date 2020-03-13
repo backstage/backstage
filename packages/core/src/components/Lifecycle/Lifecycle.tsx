72dca3b1a50024a2d4c88a0a9e305484a3a284c6
Fredrik Adelöw <freben@gmail.com>
2020-03-13 11:58:41 +0100

    Just some utilities converted to ts

---
+++ packages/core/src/components/Lifecycle/Lifecycle.tsx
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2020 Spotify AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React, { FC } from 'react';
+import CSS from 'csstype';
+import { makeStyles } from '@material-ui/core';
+
+type Props = CSS.Properties & {
+  isShorthand?: boolean;
+};
+
+const useStyles = makeStyles({
+  alpha: {
+    color: '#d00150',
+    fontFamily: 'serif',
+    fontWeight: 'normal',
+    fontStyle: 'italic',
+  },
+  beta: {
+    color: '#4d65cc',
+    fontFamily: 'serif',
+    fontWeight: 'normal',
+    fontStyle: 'italic',
+  },
+});
+
+export const AlphaLabel: FC<Props> = props => {
+  const classes = useStyles(props);
+  const { isShorthand } = props;
+  return isShorthand ? (
+    <span className={classes.alpha} style={{ fontSize: '120%' }}>
+      &alpha;
+    </span>
+  ) : (
+    <span className={classes.alpha}>Alpha</span>
+  );
+};
+
+export const BetaLabel: FC<Props> = props => {
+  const classes = useStyles(props);
+  const { isShorthand } = props;
+  return isShorthand ? (
+    <span className={classes.beta} style={{ fontSize: '120%' }}>
+      &beta;
+    </span>
+  ) : (
+    <span className={classes.beta}>Beta</span>
+  );
+};
---
+++ packages/core/src/components/Progress/Progress.tsx
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2020 Spotify AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React, { FC, useState, useEffect } from 'react';
+import { LinearProgress, LinearProgressProps } from '@material-ui/core';
+
+const Progress: FC<LinearProgressProps> = props => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handle = setTimeout(() => setIsVisible(true), 250);
+    return () => clearTimeout(handle);
+  }, []);
+
+  return isVisible ? (
+    <LinearProgress {...props} data-testid="progress" />
+  ) : (
+    <div style={{ display: 'none' }} data-testid="progress" />
+  );
+};
+
+export default Progress;
