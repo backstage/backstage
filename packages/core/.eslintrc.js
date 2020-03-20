512e5cb54dc0d562f3dae4e91ecfa96e37b69ef3
Patrik Oldsberg <poldsberg@gmail.com>
2020-03-20 12:13:58 +0100

    cli: don't enforce prop-types for plugins

--- packages/core/.eslintrc.js
+++ packages/core/.eslintrc.js
@@ -1,6 +1,14 @@
 module.exports = {
+  overrides: [
+    {
+      files: ['**/*.ts?(x)'],
+      rules: {
+        // TODO: add prop types and set to 1
+        'react/prop-types': 0,
+      },
+    },
+  ],
   rules: {
-    'react/prop-types': 0,
     'jest/expect-expect': 0,
   },
 };
