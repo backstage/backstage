4609c8f4e008bcf0d2e61512032fccf9a26c6847
Muhammad Rivki <Muhammad.Rivki@live.com>
2020-03-21 21:55:31 +0700

    feat: integration with storybook for @core package

---
+++ packages/core/.storybook/main.js
@@ -0,0 +1,19 @@
+module.exports = {
+  stories: [
+    '../src/layout/**/*.stories.tsx',
+    '../src/components/**/*.stories.tsx',
+  ],
+  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: require.resolve('ts-loader'),
+        },
+      ],
+    });
+    config.resolve.extensions.push('.ts', '.tsx');
+    return config;
+  },
+};
