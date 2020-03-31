7217b56825aef89088c8d66f65e084b4e53ebf3f
Jose Balanza Martinez <braulio.balanza@gmail.com>
2020-03-31 15:13:31 -0600

    Fixed @bakcstage name in test

--- packages/cli/src/commands/remove-plugin/removePlugin.test.ts
+++ packages/cli/src/commands/remove-plugin/removePlugin.test.ts
@@ -1,44 +1,50 @@
 // Test Suite for removePlugin command.
 
-import fse from 'fs-extra'
-import path from 'path'
-import {// removeExportStatementFromPlugins,
-    removePluginDependencyFromApp,
-    //removePluginDirectory,
-} from './removePlugin'
+import fse from 'fs-extra';
+import path from 'path';
+import {
+  // removeExportStatementFromPlugins,
+  removePluginDependencyFromApp,
+  //removePluginDirectory,
+} from './removePlugin';
 
 const rootDir = fse.realpathSync(process.cwd().replace('/cli', ''));
+const BACKSTAGE = `@backstage`;
 
 // test remove export statement
 describe('removePlugin', () => {
-    describe('Remove Plugin Dependencies', () => {
-        // Set up test
-        // Copy contents of package file for test
-        const packageFile = path.join(rootDir, 'app', 'package.json');
-        const testFilePath = path.join(rootDir, 'app', 'test.json');
-        const testPluginName = 'yarn-test-package';
-        const testPluginPackage = `@spotify-backstage/plugin-${testPluginName}`;
+  describe('Remove Plugin Dependencies', () => {
+    // Set up test
+    // Copy contents of package file for test
+    const packageFile = path.join(rootDir, 'app', 'package.json');
+    const testFilePath = path.join(rootDir, 'app', 'test.json');
+    const testPluginName = 'yarn-test-package';
+    const testPluginPackage = `${BACKSTAGE}/plugin-${testPluginName}`;
 
-        let packageFileContents = JSON.parse(fse.readFileSync(packageFile, 'utf8'));
-        packageFileContents.dependencies[testPluginPackage] = "0.1.0";
+    let packageFileContents = JSON.parse(fse.readFileSync(packageFile, 'utf8'));
+    packageFileContents.dependencies[testPluginPackage] = '0.1.0';
 
-        it('should remove plugin dependency from /packages/app/package.json', async () => {
-            fse.createFileSync(testFilePath);
-            fse.writeFileSync(
-                testFilePath,
-                `${JSON.stringify(packageFileContents, null, 2)}\n`,
-                'utf8');
-            console.log(JSON.parse(fse.readFileSync(testFilePath, 'utf8')));
-            try {
-                await removePluginDependencyFromApp(testFilePath, testPluginName);
-                expect(JSON.parse(fse.readFileSync(testFilePath, 'utf8')).hasOwnProperty(testPluginPackage)).toBe(false);
-            } finally {
-                fse.removeSync(testFilePath);
-            }
-        });
+    it('should remove plugin dependency from /packages/app/package.json', async () => {
+      fse.createFileSync(testFilePath);
+      fse.writeFileSync(
+        testFilePath,
+        `${JSON.stringify(packageFileContents, null, 2)}\n`,
+        'utf8',
+      );
+      console.log(JSON.parse(fse.readFileSync(testFilePath, 'utf8')));
+      try {
+        await removePluginDependencyFromApp(testFilePath, testPluginName);
+        expect(
+          JSON.parse(fse.readFileSync(testFilePath, 'utf8')).hasOwnProperty(
+            testPluginPackage,
+          ),
+        ).toBe(false);
+      } finally {
+        fse.removeSync(testFilePath);
+      }
     });
+  });
 });
-
 
 // test remove plugin dependency from app
 
