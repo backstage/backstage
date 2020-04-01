8fffc427013ced8820a54e8a88d0d99f9e4b0cd6
nikek <nikek@spotify.com>
2020-04-01 10:33:53 +0200

    remove unnecessary comment

--- packages/cli/src/commands/plugin/rollup.config.ts
+++ packages/cli/src/commands/plugin/rollup.config.ts
@@ -15,7 +15,7 @@
  */
 
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import typescript from 'rollup-plugin-typescript2'; // @rollup/plugin-typescript
+import typescript from 'rollup-plugin-typescript2';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import postcss from 'rollup-plugin-postcss';
