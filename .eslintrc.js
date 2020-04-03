32072ad4295a804c389ec09c18aa705589fc3024
Paul Marbach <pmarbach@spotify.com>
2020-04-03 11:50:49 -0400

    chore: use eslint-config-oss

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,6 @@
-const path = require('path');
-const base = require('@backstage/cli/config/eslint');
-
 module.exports = {
-  ...base,
-  rules: {
-    ...base.rules,
-    'notice/notice': [
-      'error',
-      {
-        templateFile: path.resolve(__dirname, 'scripts/copyright.js'),
-      },
-    ],
-  },
+  extends: [
+    require.resolve('@backstage/cli/config/eslint.js'),
+    '@spotify/eslint-config-oss',
+  ],
 };
