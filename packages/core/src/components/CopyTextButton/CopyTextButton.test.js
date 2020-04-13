223a752bc0a9fce4296e0a48f286aaa0b9da2eb8
Stefan Ålund <alund@spotify.com>
2020-04-13 17:41:13 +0200

    Add HorizontalProgress

--- packages/core/src/components/CopyTextButton/CopyTextButton.test.js
+++ packages/core/src/components/CopyTextButton/CopyTextButton.test.js
@@ -17,7 +17,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { wrapInThemedTestApp } from '@backstage/test-utils';
-import CopyTextButton from 'shared/components/CopyTextButton';
+import CopyTextButton from './CopyTextButton';
 
 const props = {
   text: 'mockText',
