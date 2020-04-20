fc5398921ab8aacc87c4b67b6428c5c1b4bf72d6
Sebastian Qvarfordt <s.qvarfordt@gmail.com>
2020-04-20 10:30:28 +0200

    Initial table component with storybook example

---
+++ packages/core/src/components/Table/Table.stories.tsx
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2020 Spotify AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from 'react';
+import Table from './Table';
+
+export default {
+  title: 'Table',
+  component: Table,
+};
+
+const generateTestData: (number) => Array<{}> = (rows = 20) => {
+  const data: Array<{}> = [];
+  while (data.length <= rows) {
+    data.push({
+      col1: `Some value ${data.length}`,
+      col2: `More data ${data.length}`,
+      number: Math.floor(Math.random() * 1000),
+      date: new Date(Math.random() * 10000000000000),
+    });
+  }
+
+  return data;
+};
+
+const testColumns = [
+  {
+    Header: 'Column 1',
+    accessor: 'col1',
+  },
+  {
+    Header: 'Column 2',
+    accessor: 'col2',
+  },
+  {
+    Header: 'Numeric value',
+    accessor: 'number',
+    type: 'numeric', // || align: 'right',
+    sortType: 'basic',
+  },
+  {
+    Header: 'A Date',
+    accessor: 'date',
+    sortType: 'datetime',
+    Cell: ({ value }) => value.toLocaleDateString(),
+  },
+];
+
+const testData100 = generateTestData(100);
+
+export const DefaultTable = () => {
+  const columns = React.useMemo(() => testColumns, []);
+  const data = React.useMemo(() => testData100, []);
+
+  return (
+    <>
+      <Table data={data} columns={columns} />
+    </>
+  );
+};
---
+++ packages/core/src/components/Table/Table.tsx
@@ -0,0 +1,177 @@
+/*
+ * Copyright 2020 Spotify AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { makeStyles } from '@material-ui/core';
+import { BackstageTheme } from '@backstage/theme';
+import React, { FC, useCallback } from 'react';
+import { useTable, useGlobalFilter, useSortBy } from 'react-table';
+
+import { FixedSizeList } from 'react-window';
+import MUITable from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+
+import TextField from '@material-ui/core/TextField';
+
+//const useStyles = makeStyles<typeof BackstageTheme>(theme => ({ }));
+type GlobalFilterProps = {
+  setGlobalFilter: any;
+  preGlobalFilteredRows: any;
+  globalFilter: any;
+};
+
+const GlobalFilter: FC<GlobalFilterProps> = ({
+  setGlobalFilter,
+  preGlobalFilteredRows,
+  globalFilter,
+}) => {
+  const count = preGlobalFilteredRows.length;
+
+  return (
+    <span>
+      <TextField
+        value={globalFilter || ''}
+        placeholder={`${count} records...`}
+        id="standard-basic"
+        label="Search"
+        onChange={e => {
+          setGlobalFilter(e.target.value || undefined); // Set undefined to remove the filter entirely
+        }}
+      />
+    </span>
+  );
+};
+
+type TableProps = {
+  data: Array<{}>;
+  columns: Array<{}>;
+};
+
+const Table: FC<TableProps> = ({ columns, data }) => {
+  0;
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    preGlobalFilteredRows,
+    setGlobalFilter,
+    state,
+  } = useTable(
+    {
+      columns,
+      data,
+    },
+    useGlobalFilter,
+    useSortBy,
+  );
+
+  //Used for react-window rendering. Which means not at all right now.
+  const renderRow = useCallback(
+    ({ index, style }) => {
+      const row = rows[index];
+      prepareRow(row);
+      return (
+        <TableRow style={style} {...row.getRowProps()}>
+          {row.cells.map(cell => {
+            return (
+              <TableCell {...cell.getCellProps()}>
+                {cell.render('Cell')}
+              </TableCell>
+            );
+          })}
+        </TableRow>
+      );
+    },
+    [prepareRow, rows],
+  );
+
+  const renderTableColumn = (type, column) => {
+    switch (type) {
+      default:
+        return (
+          <TableCell
+            align={type === 'numeric' ? 'right' : 'left'}
+            {...column.getHeaderProps(column.getSortByToggleProps())}
+          >
+            <TableSortLabel
+              active={column.isSorted}
+              direction={column.isSortedDesc ? 'desc' : 'asc'}
+            >
+              {column.render('Header')}
+            </TableSortLabel>
+          </TableCell>
+        );
+    }
+  };
+
+  return (
+    <>
+      <GlobalFilter
+        preGlobalFilteredRows={preGlobalFilteredRows}
+        setGlobalFilter={setGlobalFilter}
+        globalFilter={state.globalFilter}
+      />
+      <TableContainer>
+        <MUITable {...getTableProps()}>
+          <TableHead>
+            {headerGroups.map(headerGroup => (
+              <TableRow {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map(column =>
+                  renderTableColumn(column.type, column),
+                )}
+              </TableRow>
+            ))}
+          </TableHead>
+          <TableBody>
+            {rows.map((row, i) => {
+              prepareRow(row);
+              return (
+                <TableRow {...row.getRowProps()}>
+                  {row.cells.map(cell => {
+                    return (
+                      <TableCell
+                        align={
+                          cell.column.type === 'numeric' ? 'right' : 'left'
+                        }
+                        {...cell.getCellProps()}
+                      >
+                        {cell.render('Cell')}
+                      </TableCell>
+                    );
+                  })}
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </MUITable>
+      </TableContainer>
+    </>
+  );
+};
+
+/*
+<FixedSizeList height={400} itemCount={rows.length} itemSize={35}>
+  {renderRow}
+</FixedSizeList>
+*/
+
+export default Table;
