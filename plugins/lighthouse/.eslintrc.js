a8b68a9595f99d557ff80b328f8259543c3a1ac5
Jose Balanza Martinez <braulio.balanza@gmail.com>
2020-04-22 23:55:04 -0500

    Add eslint-react-hooks

--- plugins/lighthouse/.eslintrc.js
+++ plugins/lighthouse/.eslintrc.js
@@ -1,3 +1,10 @@
 module.exports = {
+  plugins: [
+  "react-hooks"
+  ],
+  rules: {
+    "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+    "react-hooks/exhaustive-deps": "warn" // Checks effect dependencies
+  },
   extends: [require.resolve('@backstage/cli/config/eslint.js')],
 };
