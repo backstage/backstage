b6eb26575dccf0ece01a0070f43e6a2210546114
Jose Balanza Martinez <braulio.balanza@gmail.com>
2020-04-24 21:34:42 -0500

    Remove eslint-plugin-react-hooks

--- plugins/lighthouse/.eslintrc.js
+++ plugins/lighthouse/.eslintrc.js
@@ -1,10 +1,3 @@
 module.exports = {
-  plugins: [
-  "react-hooks"
-  ],
-  rules: {
-    "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
-    "react-hooks/exhaustive-deps": "warn" // Checks effect dependencies
-  },
   extends: [require.resolve('@backstage/cli/config/eslint.js')],
 };
