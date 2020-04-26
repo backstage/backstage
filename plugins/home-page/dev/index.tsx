e62bd2b4313dec417322b35b635b7005249f6987
Patrik Oldsberg <poldsberg@gmail.com>
2020-04-26 14:22:58 +0200

    plugins/home-page: use dev-utils to set up dev app

--- plugins/home-page/dev/index.tsx
+++ plugins/home-page/dev/index.tsx
@@ -14,41 +14,9 @@
  * limitations under the License.
  */
 
-import React, { FC } from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import HomeIcon from '@material-ui/icons/Home';
-import { ThemeProvider, CssBaseline } from '@material-ui/core';
-import {
-  createApp,
-  SidebarPage,
-  Sidebar,
-  SidebarItem,
-  SidebarSpacer,
-} from '@backstage/core';
-import { lightTheme } from '@backstage/theme';
+import { createDevApp } from '@backstage/dev-utils';
 import { plugin } from '../src/plugin';
 
-const app = createApp();
-app.registerPlugin(plugin);
-const AppComponent = app.build();
-
-const App: FC<{}> = () => {
-  return (
-    <ThemeProvider theme={lightTheme}>
-      <CssBaseline>
-        <BrowserRouter>
-          <SidebarPage>
-            <Sidebar>
-              <SidebarSpacer />
-              <SidebarItem icon={HomeIcon} to="/home" text="Home" />
-            </Sidebar>
-            <AppComponent />
-          </SidebarPage>
-        </BrowserRouter>
-      </CssBaseline>
-    </ThemeProvider>
-  );
-};
-
-ReactDOM.render(<App />, document.getElementById('root'));
+createDevApp()
+  .registerPlugin(plugin)
+  .render();
