516e105bd850820032c0701f929b2e42151d2bab
Stefan Ålund <alund@spotify.com>
2020-04-27 22:00:05 +0200

    Add padding for all Stories (#663)

--- packages/storybook/.storybook/config.js
+++ packages/storybook/.storybook/config.js
@@ -3,9 +3,12 @@ import { addDecorator } from '@storybook/react';
 import { lightTheme, darkTheme } from '@backstage/theme';
 import { CssBaseline, ThemeProvider } from '@material-ui/core';
 import { useDarkMode } from 'storybook-dark-mode';
+import { Content } from '@backstage/core';
 
 addDecorator(story => (
   <ThemeProvider theme={useDarkMode() ? darkTheme : lightTheme}>
-    <CssBaseline>{story()}</CssBaseline>
+    <CssBaseline>
+      <Content>{story()}</Content>
+    </CssBaseline>
   </ThemeProvider>
 ));
