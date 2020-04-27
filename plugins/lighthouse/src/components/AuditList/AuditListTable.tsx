ea880db04a1eb799f0e3fe05598fd9c6c864e052
Jose Balanza Martinez <62359443+braulio-balanza@users.noreply.github.com>
2020-04-27 09:59:52 -0500

    Use useInterval() hook to refetch audit status in lighthouse plugin WIP (#623)
    * Fix error when using yarn lint
    
    * Update logic for create app
    
    * Add eslint-react-hooks
    
    * Move common utils to utils.ts
    
    * Add AuditListRow that refetches the audit
    
    * Fixed Audit state using a react component
    
    * Change AuditListRow to AuditRow
    
    * Fixed some eslint-react-hooks warnings
    
    * Change AuditListRow to AuditRow
    
    * Merge
    
    * Fixes tests in AuditListTable.test.tsx
    
    * Refactored based on freben's review
    
    * Remove eslint-plugin-react-hooks

--- plugins/lighthouse/src/components/AuditList/AuditListTable.tsx
+++ plugins/lighthouse/src/components/AuditList/AuditListTable.tsx
@@ -15,7 +15,6 @@
  */
 import React, { FC, useMemo } from 'react';
 import {
-  Link,
   Table,
   TableBody,
   TableCell,
@@ -24,31 +23,11 @@ import {
   TableRow,
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import { TrendLine } from '@backstage/core';
-
 import {
-  Audit,
-  AuditCompleted,
-  LighthouseCategoryId,
   Website,
 } from '../../api';
-import { formatTime } from '../../utils';
-import AuditStatusIcon from '../AuditStatusIcon';
-
-export const CATEGORIES: LighthouseCategoryId[] = [
-  'accessibility',
-  'performance',
-  'seo',
-  'best-practices',
-];
-
-export const CATEGORY_LABELS: Record<LighthouseCategoryId, string> = {
-  accessibility: 'Accessibility',
-  performance: 'Performance',
-  seo: 'SEO',
-  'best-practices': 'Best Practices',
-  pwa: 'Progressive Web App',
-};
+import { CATEGORIES, CATEGORY_LABELS, SparklinesDataByCategory, buildSparklinesDataForItem } from '../../utils';
+import Audit from '../Audit'
 
 const useStyles = makeStyles(theme => ({
   table: {
@@ -66,34 +45,13 @@ const useStyles = makeStyles(theme => ({
   sparklinesCell: { minWidth: 120 },
 }));
 
-type SparklinesDataByCategory = Record<LighthouseCategoryId, number[]>;
-function buildSparklinesDataForItem(item: Website): SparklinesDataByCategory {
-  return item.audits
-    .filter(
-      (audit: Audit): audit is AuditCompleted => audit.status === 'COMPLETED',
-    )
-    .reduce((scores, audit) => {
-      Object.values(audit.categories).forEach(category => {
-        scores[category.id] = scores[category.id] || [];
-        scores[category.id].unshift(category.score);
-      });
-
-      // edge case: if only one audit exists, force a "flat" sparkline
-      Object.values(scores).forEach(arr => {
-        if (arr.length === 1) arr.push(arr[0]);
-      });
-
-      return scores;
-    }, {} as SparklinesDataByCategory);
-}
-
 export const AuditListTable: FC<{ items: Website[] }> = ({ items }) => {
   const classes = useStyles();
   const categorySparklines: Record<string, SparklinesDataByCategory> = useMemo(
     () =>
       items.reduce(
         (res, item) => ({
-          ...res,
+           ...res,
           [item.url]: buildSparklinesDataForItem(item),
         }),
         {},
@@ -118,34 +76,11 @@ export const AuditListTable: FC<{ items: Website[] }> = ({ items }) => {
         </TableHead>
         <TableBody>
           {items.map(website => (
-            <TableRow key={website.url}>
-              <TableCell>
-                <Link
-                  className={classes.link}
-                  href={`/lighthouse/audit/${website.lastAudit.id}`}
-                >
-                  {website.url}
-                </Link>
-              </TableCell>
-              {CATEGORIES.map(category => (
-                <TableCell
-                  key={`${website.url}|${category}`}
-                  className={classes.sparklinesCell}
-                >
-                  <TrendLine
-                    title={`trendline for ${CATEGORY_LABELS[category]} category of ${website.url}`}
-                    data={categorySparklines[website.url][category] || []}
-                  />
-                </TableCell>
-              ))}
-              <TableCell className={classes.statusCell}>
-                <AuditStatusIcon audit={website.lastAudit} />{' '}
-                <span className={classes.status}>
-                  {website.lastAudit.status.toLowerCase()}
-                </span>
-              </TableCell>
-              <TableCell>{formatTime(website.lastAudit.timeCreated)}</TableCell>
-            </TableRow>
+            <Audit
+              key={website.url}
+              website={website}
+              categorySparkline={categorySparklines[website.url]}
+            />
           ))}
         </TableBody>
       </Table>
