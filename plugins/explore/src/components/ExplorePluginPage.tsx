cafd47ab086fdcc003722105fef34d951589d0a3
Stefan Ålund <alund@spotify.com>
2020-04-30 11:42:00 +0200

    Real conent

--- plugins/explore/src/components/ExplorePluginPage.tsx
+++ plugins/explore/src/components/ExplorePluginPage.tsx
@@ -55,20 +55,21 @@ const useStyles = makeStyles<BackstageTheme>(theme => ({
 const toolsCards = [
   {
     title: 'Tech Radar',
-    description: 'Something something',
-    url: 'http://spotify.com/',
+    description:
+      'Tech Radar is a list of technologies, complemented by an assessment result, called ring assignment.',
+    url: '/tech-radar',
     image:
       'https://storage.googleapis.com/wf-blogs-engineering-media/2018/09/fe13bb32-wf-tech-radar-hero-1024x597.png',
-    tags: ['tag1', 'tag2'],
+    tags: ['standards', 'landscape'],
   },
   {
     title: 'GraphiQL',
     description:
       'Integrates GraphiQL as a tool to browse GraphiQL endpoints inside Backstage.',
-    url: 'http://spotify.com/',
+    url: 'graphiql',
     image:
       'https://camo.githubusercontent.com/517398c3fbe0687d3d4dcbe05da82970b882e75a/68747470733a2f2f64337676366c703535716a6171632e636c6f756466726f6e742e6e65742f6974656d732f33413061324e314c3346324f304c3377326e316a2f477261706869514c382e706e673f582d436c6f75644170702d56697369746f722d49643d3433363432',
-    tags: ['tag1', 'tag2'],
+    tags: ['graphql', 'dev'],
   },
 ];
 
