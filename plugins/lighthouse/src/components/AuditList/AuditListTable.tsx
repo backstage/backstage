425cf93c08d8190412922bf10c8bcd0b3f14c561
Sebastian Qvarfordt <s.qvarfordt@gmail.com>
2020-05-06 12:58:34 +0200

    Replacing SortableTable and a few MUI tables with new Table component (#722)
    * Befind work of replacing tables with new Table component
    
    * Removed test plugins and fixed test
    
    * Fixed lint issue
    
    * Removed toolbar from lighthouse audit page
    
    * Moved column out of auditstables render function, no no interation with useInterval

--- plugins/lighthouse/src/components/AuditList/AuditListTable.tsx
+++ plugins/lighthouse/src/components/AuditList/AuditListTable.tsx
@@ -13,81 +13,111 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import React, { FC, useMemo } from 'react';
+import React, { FC, useState } from 'react';
+import { Table, TableColumn, TrendLine, useApi } from '@backstage/core';
+import { Website, lighthouseApiRef } from '../../api';
+import { useInterval } from 'react-use';
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-} from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import { Website } from '../../api';
-import {
+  formatTime,
   CATEGORIES,
   CATEGORY_LABELS,
-  SparklinesDataByCategory,
   buildSparklinesDataForItem,
 } from '../../utils';
-import Audit from '../Audit';
+import { Link } from '@material-ui/core';
+import AuditStatusIcon from '../AuditStatusIcon';
 
-const useStyles = makeStyles(theme => ({
-  table: {
-    minWidth: 650,
+const columns: TableColumn[] = [
+  {
+    title: 'Website URL',
+    field: 'websiteUrl',
   },
-  status: {
-    textTransform: 'capitalize',
+  ...CATEGORIES.map((category) => ({
+    title: CATEGORY_LABELS[category],
+    field: category,
+  })),
+  {
+    title: 'Last Report',
+    field: 'lastReport',
+    cellStyle: {
+      whiteSpace: 'nowrap',
+    },
   },
-  link: {
-    paddingTop: theme.spacing(2),
-    paddingBottom: theme.spacing(2),
-    display: 'inline-block',
+  {
+    title: 'Last Audit Triggered',
+    field: 'lastAuditTriggered',
+    cellStyle: {
+      minWidth: 120,
+    },
   },
-  statusCell: { whiteSpace: 'nowrap' },
-  sparklinesCell: { minWidth: 120 },
-}));
+];
 
 export const AuditListTable: FC<{ items: Website[] }> = ({ items }) => {
-  const classes = useStyles();
-  const categorySparklines: Record<string, SparklinesDataByCategory> = useMemo(
-    () =>
-      items.reduce(
-        (res, item) => ({
-          ...res,
-          [item.url]: buildSparklinesDataForItem(item),
-        }),
-        {},
-      ),
-    [items],
+  const [websiteState, setWebsiteState] = useState(items);
+  const lighthouseApi = useApi(lighthouseApiRef);
+
+  const runRefresh = (websites: Website[]) => {
+    websites.forEach(async (website) => {
+      const response = await lighthouseApi.getWebsiteForAuditId(
+        website.lastAudit.id,
+      );
+      const auditStatus = response.lastAudit.status;
+      if (auditStatus === 'COMPLETED' || auditStatus === 'FAILED') {
+        const newWebsiteData = websiteState.slice(0);
+        newWebsiteData[
+          newWebsiteData.findIndex((w) => w.url === response.url)
+        ] = response;
+        setWebsiteState(newWebsiteData);
+      }
+    });
+  };
+
+  const runningWebsiteAudits = websiteState
+    ? websiteState.filter((website) => website.lastAudit.status === 'RUNNING')
+    : [];
+
+  useInterval(
+    () => runRefresh(runningWebsiteAudits),
+    runningWebsiteAudits.length > 0 ? 5000 : null,
   );
 
+  const data = websiteState.map((website) => {
+    const trendlineData = buildSparklinesDataForItem(website);
+    const trendlines: any = {};
+    CATEGORIES.forEach((category) => {
+      trendlines[category] = (
+        <TrendLine
+          title={`trendline for ${CATEGORY_LABELS[category]} category of ${website.url}`}
+          data={trendlineData[category] || []}
+        />
+      );
+    });
+
+    return {
+      websiteUrl: (
+        <Link href={`/lighthouse/audit/${website.lastAudit.id}`}>
+          {website.url}
+        </Link>
+      ),
+      ...trendlines,
+      lastReport: (
+        <>
+          <AuditStatusIcon audit={website.lastAudit} />{' '}
+          <span>{website.lastAudit.status.toUpperCase()}</span>
+        </>
+      ),
+      lastAuditTriggered: formatTime(website.lastAudit.timeCreated),
+    };
+  });
+
   return (
-    <TableContainer>
-      <Table className={classes.table} size="small" aria-label="a dense table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Website URL</TableCell>
-            {CATEGORIES.map(category => (
-              <TableCell key={`${category}-label`}>
-                {CATEGORY_LABELS[category]}
-              </TableCell>
-            ))}
-            <TableCell>Last Report</TableCell>
-            <TableCell>Last Audit Triggered</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {items.map(website => (
-            <Audit
-              key={website.url}
-              website={website}
-              categorySparkline={categorySparklines[website.url]}
-            />
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <Table
+      options={{
+        paging: false,
+        toolbar: false,
+      }}
+      columns={columns}
+      data={data}
+    />
   );
 };
 
