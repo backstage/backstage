7fed67b8768a188a149e8700b78887600d4837e0
Stefan Ålund <alund@spotify.com>
2020-05-08 12:08:56 +0200

    Adjust header label spacing

--- plugins/explore/src/components/ExplorePluginPage.tsx
+++ plugins/explore/src/components/ExplorePluginPage.tsx
@@ -59,7 +59,7 @@ const toolsCards = [
     title: 'GraphiQL',
     description:
       'Integrates GraphiQL as a tool to browse GraphiQL endpoints inside Backstage.',
-    url: 'graphiql',
+    url: '/graphiql',
     image:
       'https://camo.githubusercontent.com/517398c3fbe0687d3d4dcbe05da82970b882e75a/68747470733a2f2f64337676366c703535716a6171632e636c6f756466726f6e742e6e65742f6974656d732f33413061324e314c3346324f304c3377326e316a2f477261706869514c382e706e673f582d436c6f75644170702d56697369746f722d49643d3433363432',
     tags: ['graphql', 'dev'],
