f3c42578c4c484338fb72655d298cfb953158825
Nikita Nek Dudnik <nikdudnik@gmail.com>
2020-05-08 15:59:50 +0200

    Fix linting

--- packages/app/src/setupProxy.js
+++ packages/app/src/setupProxy.js
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+// eslint-disable-next-line
 const createProxyMiddleware = require('http-proxy-middleware');
 
 // FIXME: somehow get it from within the plugin itself?
