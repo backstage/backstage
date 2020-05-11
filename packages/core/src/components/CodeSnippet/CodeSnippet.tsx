8b2dd21fd0dc3783f41bef81944e07aaa472f4d6
Nathan Calvank <nwcalvank@gmail.com>
2020-05-11 07:28:20 -0700

    Issue 660: CodeSnippet Reuseable component (#803)
    * Initial CodeSnippet setup in Storybook
    
    * Add support for line numbers
    
    * Pseudo-code some basic tests and leverage useTheme hook
    
    The test suite cannot currently be run, as the react-syntax-highlighter
    appears to be using ESM, which are not supported by Jest out-of-the-box.
    
    This should only require a simple Babel loader to remedy, but I want to
    make sure first that I have not misconfigured something, as configuring
    Babel is likely a notable scope increase for this relatively small
    issue.
    
    * Fix ESM Jest issue and update tests
    
    It would be ideal to have snapshot tests to cover the lightmode vs
    darkmode rendering. I started experimenting with directly testing the
    styling, and it quickly became very sloppy.
    
    * Provide more examples in stories
    
    * Add showLineNumbers to PropType validation

---
+++ packages/core/src/components/CodeSnippet/CodeSnippet.tsx
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2020 Spotify AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React, { FC } from 'react';
+import PropTypes from 'prop-types';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { docco, dark } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
+import { useTheme } from '@material-ui/core';
+import { BackstageTheme } from '@backstage/theme';
+
+type Props = {
+  text: string;
+  language: string;
+  showLineNumbers?: boolean;
+};
+
+const defaultProps = {
+  showLineNumbers: false,
+};
+
+const CodeSnippet: FC<Props> = props => {
+  const { text, language, showLineNumbers } = {
+    ...defaultProps,
+    ...props,
+  };
+
+  const theme = useTheme<BackstageTheme>();
+  const mode = theme.palette.type === 'dark' ? dark : docco;
+
+  return (
+    <SyntaxHighlighter
+      language={language}
+      style={mode}
+      showLineNumbers={showLineNumbers}
+    >
+      {text}
+    </SyntaxHighlighter>
+  );
+};
+
+// Type check for the JS files using this core component
+CodeSnippet.propTypes = {
+  text: PropTypes.string.isRequired,
+  language: PropTypes.string.isRequired,
+  showLineNumbers: PropTypes.bool,
+};
+
+export default CodeSnippet;
