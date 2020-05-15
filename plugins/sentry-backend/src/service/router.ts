f8ee87ce1d6b33c9b93f97a7f40f3a457bd674f7
Wojciech Adaszynski <wojciecha@spotify.com>
2020-05-15 11:29:27 +0200

    Add Sentry plugin backend API (forwarding proxy)

---
+++ plugins/sentry-backend/src/service/router.ts
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2020 Spotify AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { Logger } from 'winston';
+import Router from 'express-promise-router';
+import express from 'express';
+import { SentryApiForwarder } from './sentry-api';
+
+export async function createRouter(
+  rootLogger: Logger,
+): Promise<express.Router> {
+  const router = Router();
+  const sentryForwarder = new SentryApiForwarder('');
+  const logger = rootLogger.child({ plugin: 'sentry' });
+
+  router.get('*', (req, res) => sentryForwarder.fowardRequest(req, res));
+
+  const app = express();
+  app.set('logger', logger);
+  app.use('/', router);
+
+  return app;
+}
