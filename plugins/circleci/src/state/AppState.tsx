f3da9cd7eff33d3f1b6810606773e52c838d8ffe
Ivan Shmidt <bahoc47@gmail.com>
2020-05-18 18:52:41 +0200

    fix: type imports

--- plugins/circleci/src/state/AppState.tsx
+++ plugins/circleci/src/state/AppState.tsx
@@ -15,7 +15,7 @@
  */
 import React, { FC, useReducer, Dispatch, Reducer } from 'react';
 import { circleCIApiRef } from '../api';
-import { State, Action, SettingsState } from './types';
+import type { State, Action, SettingsState } from './types';
 export { SettingsState };
 
 export const AppContext = React.createContext<[State, Dispatch<Action>]>(
