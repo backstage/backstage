659e904f55843ed98da9e8e503b04bd7fd23163b
Patrik Oldsberg <poldsberg@gmail.com>
2020-05-22 13:39:39 +0200

    packages/core: export auth APIs

--- packages/core/src/api/apis/definitions/index.ts
+++ packages/core/src/api/apis/definitions/index.ts
@@ -20,6 +20,8 @@
 //
 // If you think some API definition is missing, please open an Issue or send a PR!
 
+export * from './auth';
+
 export * from './AlertApi';
 export * from './AppThemeApi';
 export * from './ErrorApi';
