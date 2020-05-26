0cae5dea3a4e4aa499187c4469c254d1650e980b
Ivan Shmidt <bahoc47@gmail.com>
2020-05-26 16:44:49 +0200

    fix: conflict resolution leftover

--- plugins/catalog-backend/src/catalog/DatabaseLocationsCatalog.ts
+++ plugins/catalog-backend/src/catalog/DatabaseLocationsCatalog.ts
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 
-import { AddLocation, Location, LocationsCatalog } from './types';
 import { LocationReader } from '../ingestion';
 import { Database, DatabaseLocationUpdateLogEvent } from '../database';
 import {
