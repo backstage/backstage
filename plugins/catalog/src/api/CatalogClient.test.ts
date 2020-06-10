a6ec92a50df86978ce5c5d3be910adc2db9e5562
Fredrik Adelöw <freben@gmail.com>
2020-06-10 16:49:10 +0200

    Use URLSearchParams

---
+++ plugins/catalog/src/api/CatalogClient.test.ts
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2020 Spotify AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { CatalogClient } from './CatalogClient';
+import mockFetch from 'jest-fetch-mock';
+
+describe('CatalogClient', () => {
+  it('builds entity search filters properly', async () => {
+    mockFetch.mockResponse('[]');
+    const client = new CatalogClient({ apiOrigin: '', basePath: '' });
+    const entities = await client.getEntities({ a: '1', ö: '=' });
+    expect(entities).toEqual([]);
+    expect(mockFetch).toBeCalledWith('/entities?a=1&%C3%B6=%3D');
+  });
+});
