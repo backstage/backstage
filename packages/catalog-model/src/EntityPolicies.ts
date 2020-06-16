ea6cc764473c00436959612c03e14c39c72ac4ee
blam <ben@blam.sh>
2020-06-16 14:30:40 +0200

    feat(catalog): adding kind:Template validation so that we can add templates into the service catalog

--- packages/catalog-model/src/EntityPolicies.ts
+++ packages/catalog-model/src/EntityPolicies.ts
@@ -25,6 +25,7 @@ import {
 import {
   ComponentEntityV1alpha1Policy,
   LocationEntityV1alpha1Policy,
+  TemplateEntityV1alpha1Policy,
 } from './kinds';
 import { EntityPolicy } from './types';
 
@@ -74,6 +75,7 @@ export class EntityPolicies implements EntityPolicy {
       EntityPolicies.anyOf([
         new ComponentEntityV1alpha1Policy(),
         new LocationEntityV1alpha1Policy(),
+        new TemplateEntityV1alpha1Policy(),
       ]),
     ]);
   }
---
+++ packages/catalog-model/src/kinds/TemplateEntityV1alpha1.ts
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2020 Spotify AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as yup from 'yup';
+import type { Entity } from '../entity/Entity';
+import type { EntityPolicy } from '../types';
+
+const API_VERSION = ['backstage.io/v1alpha1', 'backstage.io/v1beta1'] as const;
+const KIND = 'Template' as const;
+
+export interface TemplateEntityV1alpha1 extends Entity {
+  apiVersion: typeof API_VERSION[number];
+  kind: typeof KIND;
+  spec: {
+    type: string;
+  };
+}
+
+export class TemplateEntityV1alpha1Policy implements EntityPolicy {
+  private schema: yup.Schema<any>;
+
+  constructor() {
+    this.schema = yup.object<Partial<TemplateEntityV1alpha1>>({
+      apiVersion: yup.string().required().oneOf(API_VERSION),
+      kind: yup.string().required().equals([KIND]),
+      spec: yup
+        .object({
+          type: yup.string().required().min(1),
+        })
+        .required(),
+    });
+  }
+
+  async enforce(envelope: Entity): Promise<Entity> {
+    return await this.schema.validate(envelope, { strict: true });
+  }
+}
