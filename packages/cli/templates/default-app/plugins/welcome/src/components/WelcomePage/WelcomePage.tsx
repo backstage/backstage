d197a41ccca8aa9c2a387ac0fc474fe926e6fd91
Stefan Ålund <alund@spotify.com>
2020-06-16 09:21:39 +0200

    Revert "Rm from package.json"
    This reverts commit 579fefad5ba29622d96456fe8198f55e1a9a42d0.

--- packages/cli/templates/default-app/plugins/welcome/src/components/WelcomePage/WelcomePage.tsx
+++ packages/cli/templates/default-app/plugins/welcome/src/components/WelcomePage/WelcomePage.tsx
@@ -1,20 +1,5 @@
-/*
- * Copyright 2020 Spotify AB
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import React, { FC } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
 import {
   Typography,
   Grid,
@@ -88,8 +73,12 @@ const WelcomePage: FC<{}> = () => {
                 We suggest you either check out the documentation for{' '}
                 <Link href="https://github.com/spotify/backstage/blob/master/docs/getting-started/create-a-plugin.md">
                   creating a plugin
-                </Link>
-                .
+                </Link>{' '}
+                or have a look in the code for the{' '}
+                <Link component={RouterLink} to="/home">
+                  Home Page
+                </Link>{' '}
+                in the directory "plugins/home-page/src".
               </Typography>
             </InfoCard>
           </Grid>
