a133bf542a50a8f19922054ecd57dfe14eec03bd
Bilawal Hameed <bil@spotify.com>
2020-06-23 15:07:12 +0200

    Update setup.py

--- plugins/techdocs/mkdocs/container/techdocs-core/setup.py
+++ plugins/techdocs/mkdocs/container/techdocs-core/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mkdocs-techdocs-core',
-    version='0.1.0',
+    version='0.0.1',
     description='A Mkdocs package that contains TechDocs defaults',
     long_description='',
     keywords='mkdocs',
