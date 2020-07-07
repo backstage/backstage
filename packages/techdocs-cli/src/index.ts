56b63396f5f10095573d874c6deba96d5d2c3f90
Sebastian Qvarfordt <s.qvarfordt@gmail.com>
2020-07-07 17:32:13 +0200

    Initial techdocs-cli

--- packages/techdocs-cli/src/index.ts
+++ packages/techdocs-cli/src/index.ts
@@ -13,5 +13,65 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import program from 'commander';
+import { version } from './lib/version';
+// import chalk from 'chalk';
+import { spawn } from 'child_process';
 
-export const techDocsCli = () => {};
+const run = (name: string, args: string[] = []) => {
+  const child = spawn(name, args, {
+    stdio: ['inherit', 'inherit', 'inherit'],
+    shell: true,
+    env: {
+      ...process.env,
+      FORCE_COLOR: 'true',
+    },
+  });
+
+  child.once('error', error => {
+    console.error(error);
+  });
+  child.once('exit', code => {
+    console.log('exited!', code);
+  });
+};
+
+const main = (argv: string[]) => {
+  program.name('techdocs-cli').version(version);
+
+  program
+    .command('serve')
+    .description('Serve a documentation project locally')
+    .action(() => {
+      run('docker', [
+        'run',
+        '-it',
+        '-w',
+        '/content',
+        '-v',
+        '$(pwd):/content',
+        '-p',
+        '8000:8000',
+        'mkdocs:local-dev',
+        'serve',
+        '-a',
+        '0.0.0.0:8000',
+      ]);
+    });
+
+  program
+    .command('do-thing')
+    .description('Does the thing')
+    .option(
+      '--skip-install',
+      'Skip the install and builds steps after creating the app',
+    )
+    .action(() => console.log('testing thing'));
+  /* .action(
+        lazyAction(() => import('./commands/create-app/createApp'), 'default'),
+      );*/
+
+  program.parse(argv);
+};
+
+main(process.argv);
