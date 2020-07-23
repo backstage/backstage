dd9cc6a666df8ef972e43dfe02ce8a60d06e091c
blam <ben@blam.sh>
2020-07-23 10:42:55 +0200

    chore(cli): polyfill whatwg-fetch in tests

--- packages/cli/templates/default-backend-plugin/src/setupTests.ts
+++ packages/cli/templates/default-backend-plugin/src/setupTests.ts
@@ -13,7 +13,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-import '@testing-library/jest-dom';
-
-require('jest-fetch-mock').enableMocks();
+require('whatwg-fetch');
