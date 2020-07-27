1fb616c5f5398c905936df23f2431a08f7305882
blam <ben@blam.sh>
2020-07-27 15:36:28 +0200

    feat(gql): added some experimentation around graphql modules

--- plugins/graphql/src/service/router.ts
+++ plugins/graphql/src/service/router.ts
@@ -20,7 +20,10 @@ import Router from 'express-promise-router';
 import { Logger } from 'winston';
 import fs from 'fs';
 import path from 'path';
+import { GraphQLModule } from '@graphql-modules/core';
 import { ApolloServer } from 'apollo-server-express';
+import { createModule as createCatalogModule } from '@backstage/plugin-catalog-graphql';
+import { createModule as createBogusModule } from '@backstage/plugin-bogus-graphql';
 
 export interface RouterOptions {
   logger: Logger;
@@ -34,7 +37,14 @@ export async function createRouter(
     'utf-8',
   );
 
-  const server = new ApolloServer({ typeDefs, logger: options.logger });
+  const catalogModule = await createCatalogModule(options);
+  const bogusModule = await createBogusModule(options);
+  const { schema } = new GraphQLModule({
+    imports: [catalogModule, bogusModule],
+    typeDefs,
+  });
+
+  const server = new ApolloServer({ schema, logger: options.logger });
   const router = Router();
 
   const apolloMiddlware = server.getMiddleware({ path: '/' });
