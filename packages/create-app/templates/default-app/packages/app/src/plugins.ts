4c5c6e3c123a249a3b72d6c79fd0b12e194df3f6
Rémi DOREAU <remi.d45@gmail.com>
2020-08-09 12:35:27 +0200

    feat(create-app): add catalog and scaffolder plugins

--- packages/create-app/templates/default-app/packages/app/src/plugins.ts
+++ packages/create-app/templates/default-app/packages/app/src/plugins.ts
@@ -1,1 +1,3 @@
 export { plugin as WelcomePlugin } from 'plugin-welcome';
+export { plugin as CatalogPlugin } from '@backstage/plugin-catalog';
+export { plugin as ScaffolderPlugin } from '@backstage/plugin-scaffolder';
