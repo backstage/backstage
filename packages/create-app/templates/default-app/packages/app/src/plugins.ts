953e55330b135c80db12e09800986cf4a48251df
Patrik Oldsberg <poldsberg@gmail.com>
2020-08-13 12:35:57 +0200

    create-app: remove welcome plugin

--- packages/create-app/templates/default-app/packages/app/src/plugins.ts
+++ packages/create-app/templates/default-app/packages/app/src/plugins.ts
@@ -1,3 +1,2 @@
-export { plugin as WelcomePlugin } from 'plugin-welcome';
 export { plugin as CatalogPlugin } from '@backstage/plugin-catalog';
 export { plugin as ScaffolderPlugin } from '@backstage/plugin-scaffolder';
