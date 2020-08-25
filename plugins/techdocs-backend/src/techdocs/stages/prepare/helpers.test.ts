af2eed82efef18bc98a2e1bb963ef63cab7227a5
Sebastian Qvarfordt <s.qvarfordt@gmail.com>
2020-08-25 16:31:01 +0200

    Added missing tests and make sure we merge latest changes before building docs from a repo

---
+++ plugins/techdocs-backend/src/techdocs/stages/prepare/helpers.test.ts
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2020 Spotify AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import fs from 'fs-extra';
+import path from 'path';
+import { clearGithubRepositoryCache, checkoutGitRepository } from './helpers';
+
+describe('github repo checkout', () => {
+  beforeEach(() => {
+    clearGithubRepositoryCache();
+  });
+
+  it('should check out git repo and return a tmp dir', async () => {
+    jest.setTimeout(30000);
+
+    const resultDir = await checkoutGitRepository(
+      'https://github.com/spotify/backstage.git',
+    );
+
+    expect(fs.existsSync(resultDir)).toBeTruthy();
+    expect(
+      fs.existsSync(path.join(resultDir, '/catalog-info.yaml')),
+    ).toBeTruthy();
+  });
+});
