c908fd61d3c68611e1220fdc327a7376f843cc7e
Emma Indal <emmai@spotify.com>
2020-08-28 18:48:34 +0200

    add docs to sidebar in default app

--- packages/create-app/templates/default-app/packages/app/src/sidebar.tsx
+++ packages/create-app/templates/default-app/packages/app/src/sidebar.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import HomeIcon from '@material-ui/icons/Home';
+import LibraryBooks from '@material-ui/icons/LibraryBooks';
 import CreateComponentIcon from '@material-ui/icons/AddCircleOutline';
 import {
   Sidebar,
@@ -17,6 +18,7 @@ export const AppSidebar = () => (
     <SidebarDivider />
     {/* Global nav, not org-specific */}
     <SidebarItem icon={HomeIcon} to="./" text="Home" />
+    <SidebarItem icon={LibraryBooks} to="/docs" text="Docs" />
     <SidebarItem icon={CreateComponentIcon} to="create" text="Create..." />
     {/* End global nav */}
     <SidebarDivider />
