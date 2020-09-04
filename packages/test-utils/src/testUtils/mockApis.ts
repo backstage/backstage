a5741ff0aa4242ad7d3e9a5916ec82f4a85a825f
Patrik Oldsberg <poldsberg@gmail.com>
2020-09-04 17:33:55 +0200

    test-utils: switch to new auto DI api

--- packages/test-utils/src/testUtils/mockApis.ts
+++ packages/test-utils/src/testUtils/mockApis.ts
@@ -14,14 +14,14 @@
  * limitations under the License.
  */
 
-import { ApiTestRegistry } from '@backstage/core-api';
+import {
+  storageApiRef,
+  errorApiRef,
+  createApiFactory,
+} from '@backstage/core-api';
 import { MockErrorApi, MockStorageApi } from './apis';
 
-export function createMockApiRegistry(): ApiTestRegistry {
-  const registry = new ApiTestRegistry();
-
-  registry.register(MockErrorApi.factory);
-  registry.register(MockStorageApi.factory);
-
-  return registry;
-}
+export const mockApis = [
+  createApiFactory(errorApiRef, new MockErrorApi()),
+  createApiFactory(storageApiRef, MockStorageApi.create()),
+];
