fe5bb1ee319f0d87d602a06328ce8e58e072c7a1
Stefan Ålund <alund@spotify.com>
2020-09-07 09:49:22 +0200

    Update InfoCard.stories.tsx

--- packages/core/src/layout/InfoCard/InfoCard.stories.tsx
+++ packages/core/src/layout/InfoCard/InfoCard.stories.tsx
@@ -37,7 +37,7 @@ const text = (
 
 const Wrapper: FC<{}> = ({ children }) => (
   <Grid container spacing={4}>
-    <Grid item xs="4">
+    <Grid item xs={4}>
       {children}
     </Grid>
   </Grid>
