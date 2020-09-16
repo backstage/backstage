76fd218171f746359f47d343b723cd7d5b886570
ebarrios <esteban.barrios@trivago.com>
2020-09-16 10:11:00 +0200

    Added repo_deployment permision to scaffolder template

--- plugins/scaffolder/src/components/TemplatePage/TemplatePage.tsx
+++ plugins/scaffolder/src/components/TemplatePage/TemplatePage.tsx
@@ -91,7 +91,7 @@ export const TemplatePage = () => {
   const [jobId, setJobId] = useState<string | null>(null);
   const handleClose = () => setJobId(null);
   const githubAuthApi = useApi(githubAuthApiRef);
-  const tokenPromise = githubAuthApi.getAccessToken('repo');
+  const tokenPromise = githubAuthApi.getAccessToken('repo, repo_deployment');
 
   const handleCreate = async () => {
     const token = await tokenPromise;
