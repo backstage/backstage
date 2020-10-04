26c9c9ec0ab01deb3df3a99db80dbab19ce5bd0d
Laurent RICHARD <esylo@gmail.com>
2020-10-04 19:31:15 +0200

    revert to tools.d.ts

--- plugins/tech-stack/src/tools.d.ts
+++ plugins/tech-stack/src/tools.d.ts
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export interface Tools {
+interface Tools {
   type: string;
   src?: string;
   caption?: string | undefined;
