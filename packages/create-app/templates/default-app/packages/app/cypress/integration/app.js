1f4f856cd4ae8d62d45d374b82a04f18fccf8d34
althaf.hameez <althaf.hameez@grabtaxi.com>
2020-10-15 16:25:15 +0800

    Revert

--- packages/create-app/templates/default-app/packages/app/cypress/integration/app.js
+++ packages/create-app/templates/default-app/packages/app/cypress/integration/app.js
@@ -1,6 +1,6 @@
 describe('App', () => {
   it('should render the catalog', () => {
     cy.visit('/');
-    cy.contains('Spotify Service Catalog');
+    cy.contains('Backstage Service Catalog');
   });
 });
