a749b791a522d995f43b9fbad4d6d0996aadc19b
Sutorus,Joseph M <sutorus.joseph@heb.com>
2020-10-16 15:02:51 -0500

    removed custom name from oauth2 provider, conflicts tests

--- plugins/user-settings/src/components/AuthProviders/DefaultProviderSettings.tsx
+++ plugins/user-settings/src/components/AuthProviders/DefaultProviderSettings.tsx
@@ -20,8 +20,6 @@ import {
   oauth2ApiRef,
   oktaAuthApiRef,
   microsoftAuthApiRef,
-  useApi,
-  configApiRef,
 } from '@backstage/core';
 import Star from '@material-ui/icons/Star';
 import React from 'react';
@@ -31,59 +29,55 @@ type Props = {
   configuredProviders: string[];
 };
 
-export const DefaultProviderSettings = ({ configuredProviders }: Props) => {
-  const configApi = useApi(configApiRef);
-  const organizationName = configApi.getString('organization.name');
-  return (
-    <>
-      {configuredProviders.includes('google') && (
-        <ProviderSettingsItem
-          title="Google"
-          description={googleAuthApiRef.description}
-          apiRef={googleAuthApiRef}
-          icon={Star}
-        />
-      )}
-      {configuredProviders.includes('microsoft') && (
-        <ProviderSettingsItem
-          title="Microsoft"
-          description={microsoftAuthApiRef.description}
-          apiRef={microsoftAuthApiRef}
-          icon={Star}
-        />
-      )}
-      {configuredProviders.includes('github') && (
-        <ProviderSettingsItem
-          title="Github"
-          description={githubAuthApiRef.description}
-          apiRef={githubAuthApiRef}
-          icon={Star}
-        />
-      )}
-      {configuredProviders.includes('gitlab') && (
-        <ProviderSettingsItem
-          title="Gitlab"
-          description={gitlabAuthApiRef.description}
-          apiRef={gitlabAuthApiRef}
-          icon={Star}
-        />
-      )}
-      {configuredProviders.includes('okta') && (
-        <ProviderSettingsItem
-          title="Okta"
-          description={oktaAuthApiRef.description}
-          apiRef={oktaAuthApiRef}
-          icon={Star}
-        />
-      )}
-      {configuredProviders.includes('oauth2') && (
-        <ProviderSettingsItem
-          title={organizationName || ''}
-          description={oauth2ApiRef.description}
-          apiRef={oauth2ApiRef}
-          icon={Star}
-        />
-      )}
-    </>
-  );
-};
+export const DefaultProviderSettings = ({ configuredProviders }: Props) => (
+  <>
+    {configuredProviders.includes('google') && (
+      <ProviderSettingsItem
+        title="Google"
+        description={googleAuthApiRef.description}
+        apiRef={googleAuthApiRef}
+        icon={Star}
+      />
+    )}
+    {configuredProviders.includes('microsoft') && (
+      <ProviderSettingsItem
+        title="Microsoft"
+        description={microsoftAuthApiRef.description}
+        apiRef={microsoftAuthApiRef}
+        icon={Star}
+      />
+    )}
+    {configuredProviders.includes('github') && (
+      <ProviderSettingsItem
+        title="Github"
+        description={githubAuthApiRef.description}
+        apiRef={githubAuthApiRef}
+        icon={Star}
+      />
+    )}
+    {configuredProviders.includes('gitlab') && (
+      <ProviderSettingsItem
+        title="Gitlab"
+        description={gitlabAuthApiRef.description}
+        apiRef={gitlabAuthApiRef}
+        icon={Star}
+      />
+    )}
+    {configuredProviders.includes('okta') && (
+      <ProviderSettingsItem
+        title="Okta"
+        description={oktaAuthApiRef.description}
+        apiRef={oktaAuthApiRef}
+        icon={Star}
+      />
+    )}
+    {configuredProviders.includes('oauth2') && (
+      <ProviderSettingsItem
+        title="YourOrg"
+        description={oauth2ApiRef.description}
+        apiRef={oauth2ApiRef}
+        icon={Star}
+      />
+    )}
+  </>
+);
