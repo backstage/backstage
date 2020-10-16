3fa86af62dfc272c875158623c004b125947f981
blam <ben@blam.sh>
2020-10-16 14:25:36 +0200

    feat(linkerd): make communication with the linkerd api

--- plugins/linkerd-backend/src/service/router.ts
+++ plugins/linkerd-backend/src/service/router.ts
@@ -18,18 +18,70 @@ import { errorHandler } from '@backstage/backend-common';
 import express from 'express';
 import Router from 'express-promise-router';
 import { Logger } from 'winston';
+import { Config } from '@backstage/config';
+import fetch from 'cross-fetch';
+import WebSocket from 'isomorphic-ws';
 
 export interface RouterOptions {
   logger: Logger;
+  config: Config;
 }
+
+// TODO - Need to work out better authentication using the CA of the k8s api cluster
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 export async function createRouter(
   options: RouterOptions,
 ): Promise<express.Router> {
   const { logger } = options;
-
   const router = Router();
-  router.use(express.json());
+
+  const cluster = (options.config.getConfigArray(
+    'kubernetes.clusters',
+  ) as any)[0].data;
+
+  const baseUrl = cluster.url;
+  const Authorization = `Bearer ${cluster.serviceAccountToken}`;
+
+  const makeRequest = (url: string) => {
+    const k8sProxyUrl = `/api/v1/namespaces/linkerd/services/linkerd-web:8084/proxy${url}`;
+
+    return fetch(`${baseUrl}${k8sProxyUrl}`, {
+      headers: { Authorization },
+    }).then(r => r.json());
+  };
+
+  const Socket = new WebSocket(
+    'wss://127.0.0.1:59436/api/v1/namespaces/linkerd/services/linkerd-web:8084/proxy/api/tap',
+    [],
+    { headers: { Authorization }, rejectUnauthorized: false },
+  );
+
+  Socket.onmessage = console.warn;
+  Socket.onclose = console.warn;
+  Socket.onopen = () => {
+    Socket.send(
+      JSON.stringify({
+        id: 'top-web',
+        resource: 'deployment/emoji',
+        namespace: 'emojivoto',
+        maxRps: 0,
+      }),
+    );
+  };
+  Socket.onerror = console.warn;
+
+  router.get(
+    '/deployment/:namespace/:deployment',
+    async ({ params: { namespace, deployment } }, response) => {
+      const podRequest = await makeRequest(`/api/pods?namespace=${namespace}`);
+      response.send(
+        podRequest.pods.filter(
+          p => p.deployment === `${namespace}/${deployment}`,
+        ),
+      );
+    },
+  );
 
   router.get('/health', (_, response) => {
     logger.info('PONG!');
