ff0dc1be98984e642b49b4255764c25c09f1cd0d
Ryan Vazquez <ryanv@spotify.com>
2020-10-26 17:46:18 -0400

    export root as page

--- plugins/cost-insights/src/components/CostInsightsPage/index.ts
+++ plugins/cost-insights/src/components/CostInsightsPage/index.ts
@@ -14,5 +14,4 @@
  * limitations under the License.
  */
 
-export { CostInsightsPage } from './CostInsightsPage';
-export { CostInsightsPageRoot } from './CostInsightsPageRoot';
+export { CostInsightsPageRoot as CostInsightsPage } from './CostInsightsPageRoot';
