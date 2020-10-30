801e5e55cc2668fd7979a8b885ab404804a9da6a
Abhishek Jakhar <abhishekjakhar.11@gmail.com>
2020-10-30 14:46:53 +0530

    chore: remove redundant functions from microsite footer (#3174)

--- microsite/core/Footer.js
+++ microsite/core/Footer.js
@@ -17,19 +17,6 @@
 const React = require('react');
 
 class Footer extends React.Component {
-  docUrl(doc, language) {
-    const baseUrl = this.props.config.baseUrl;
-    const docsUrl = this.props.config.docsUrl;
-    const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
-    const langPart = `${language ? `${language}/` : ''}`;
-    return `${baseUrl}${docsPart}${langPart}${doc}`;
-  }
-
-  pageUrl(doc, language) {
-    const baseUrl = this.props.config.baseUrl;
-    return baseUrl + (language ? `${language}/` : '') + doc;
-  }
-
   render() {
     return (
       <footer className="nav-footer" id="footer">
