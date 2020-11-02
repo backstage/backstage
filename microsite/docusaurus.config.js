41b9ab5ec2fe6e9fbbfc19647e34b3bed818bf2a
Abhishek Jakhar <abhishekjakhar.11@gmail.com>
2020-11-02 14:03:09 +0530

    feat: update the docusaurus config

--- microsite/docusaurus.config.js
+++ microsite/docusaurus.config.js
@@ -1,102 +1,108 @@
+/*
+ * Copyright 2020 Spotify AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 module.exports = {
-  title: 'My Site',
-  tagline: 'The tagline of my site',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  title: "Backstage",
+  tagline: "An open platform for building developer portals",
+  url: "https://backstage.io",
+  baseUrl: "/",
+  favicon: "img/favicon.ico",
+  onBrokenLinks: "throw",
+  organizationName: "Spotify", // Usually your GitHub org/user name.
+  projectName: "backstage", // Usually your repo name.
+  stylesheets: [
+    "https://fonts.googleapis.com/css?family=IBM+Plex+Mono:500,700&display=swap",
+  ],
+  scripts: [
+    "https://platform.twitter.com/widgets.js",
+    "https://buttons.github.io/buttons.js",
+  ],
   themeConfig: {
+    twitterImage: "logo_assets/png/Backstage_Identity_Assets_Artwork_RGB_04_Icon_Teal.png",
+    ogImage: "logo_assets/png/Backstage_Identity_Assets_Artwork_RGB_04_Icon_Teal.png",
+    algolia: {
+      apiKey: "8d115c9875ba0f4feaee95bab55a1645",
+      indexName: "backstage",
+    },
+    colorMode: {
+      defaultMode: "dark",
+      disableSwitch: true,
+    },
+    googleAnalytics: {
+      trackingID: 'UA-163836834-5',
+    },
+    image:
+      "logo_assets/png/Backstage_Identity_Assets_Artwork_RGB_04_Icon_Teal.png",
     navbar: {
-      title: 'My Site',
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        alt: "Backstage Logo",
+        src: "img/logo.svg",
       },
       items: [
         {
-          to: 'docs/',
-          activeBasePath: 'docs',
-          label: 'Docs',
-          position: 'left',
+          href: "https://github.com/spotify/backstage",
+          label: "GitHub",
+          position: "left",
         },
-        {to: 'blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
+          to: "/docs/overview/what-is-backstage",
+          activeBasePath: "docs",
+          label: "Docs",
+          position: "left",
+        },
+        {
+          to: "/plugins",
+          label: "Plugins",
+          position: "left",
+        },
+        {
+          to: "/blog",
+          label: "Blog",
+          position: "left",
+        },
+        {
+          to: "/demos",
+          label: "Demos",
+          position: "left",
+        },
+        {
+          href: "https://mailchi.mp/spotify/backstage-community",
+          label: "Newsletter",
+          position: "left",
         },
       ],
     },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Style Guide',
-              to: 'docs/',
-            },
-            {
-              label: 'Second Doc',
-              to: 'docs/doc2/',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: 'blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    prism: {
+      theme: require("prism-react-renderer/themes/github"),
+      darkTheme: require("prism-react-renderer/themes/dracula"),
     },
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+          path: '../docs',
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/spotify/backstage/edit/master/docs",
         },
         blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+          postsPerPage: 4,
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
