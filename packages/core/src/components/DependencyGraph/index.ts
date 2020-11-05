fa0cc6bc7f5cf5075341cbcea589863996a6b02b
Mikaela Grundin <mikaelag@spotify.com>
2020-11-05 15:00:25 +0100

    fix types export

--- packages/core/src/components/DependencyGraph/index.ts
+++ packages/core/src/components/DependencyGraph/index.ts
@@ -14,5 +14,7 @@
  * limitations under the License.
  */
 
+import * as DependencyGraphTypes from './types';
+
 export { DependencyGraph } from './DependencyGraph';
-export * as DependencyGraphTypes from './types';
+export { DependencyGraphTypes };
