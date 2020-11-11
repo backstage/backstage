4a9d666a329056eea954455215bea93511b1f565
Fredrik Adelöw <freben@gmail.com>
2020-11-11 13:31:08 +0100

    catalog: more info about refresh errors (#3272)
    We sometimes get errors like `Cannot read property 'toLowerCase' of undefined` which really doesn't help debugging. So log the error stack instead of just the message, and also make the rules enforcer (which is the very first code that's hit by newly read data) more defensive. The latter is needed because no validation has been performed on the entity data yet, so if the user mistakenly inserted bad data, the code will crash instead of just rejecting the data as invalid.

--- plugins/catalog-backend/src/ingestion/HigherOrderOperations.ts
+++ plugins/catalog-backend/src/ingestion/HigherOrderOperations.ts
@@ -130,7 +130,7 @@ export class HigherOrderOperations implements HigherOrderOperation {
         await this.locationsCatalog.logUpdateSuccess(location.id, undefined);
       } catch (e) {
         this.logger.warn(
-          `Failed to refresh location ${location.type}:${location.target}, ${e}`,
+          `Failed to refresh location ${location.type}:${location.target}, ${e.stack}`,
         );
         await this.locationsCatalog.logUpdateFailure(location.id, e);
       }
@@ -152,7 +152,7 @@ export class HigherOrderOperations implements HigherOrderOperation {
 
     for (const item of readerOutput.errors) {
       this.logger.warn(
-        `Failed item in location ${item.location.type}:${item.location.target}, ${item.error}`,
+        `Failed item in location ${item.location.type}:${item.location.target}, ${item.error.stack}`,
       );
     }
 
