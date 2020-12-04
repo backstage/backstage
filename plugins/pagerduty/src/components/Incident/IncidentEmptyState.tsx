b7a5d90c83f3266d64e00ab3dac34933be4d7617
Samira Mokaram <samiram@spotify.com>
2020-12-04 14:56:53 +0100

    fix import

--- plugins/pagerduty/src/components/Incident/IncidentEmptyState.tsx
+++ plugins/pagerduty/src/components/Incident/IncidentEmptyState.tsx
@@ -16,7 +16,7 @@
 
 import React from 'react';
 import { Grid, Typography } from '@material-ui/core';
-import EmptyStateImage from '../../assets/emptyState.svg';
+import EmptyStateImage from '../../assets/emptystate.svg';
 
 export const IncidentsEmptyState = () => {
   return (
