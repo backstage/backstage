49435447993b0b5f11257b37c11b6f79cabfa10f
AlleyWei <64219471+AlleyWei@users.noreply.github.com>
2020-12-05 11:55:45 -0500

    Update Footer.js

--- microsite/core/Footer.js
+++ microsite/core/Footer.js
@@ -57,11 +57,9 @@ class Footer extends React.Component {
               Open Source @ {this.props.config.organizationName}
             </a>
       
-      <a href="https://engineering.atspotify.com/">Spotify Engineering Blog</a>
-      </a>
-
-      <a href="https://developer.spotify.com/">Spotify for Developers</a>
-</a>
+            <a href="https://engineering.atspotify.com/">Spotify Engineering Blog</a>
+            <a href="https://developer.spotify.com/">Spotify for Developers</a>
+            </a>
       
             <a href={this.props.config.repoUrl}>GitHub</a>
             <a
