a04b28ac4b38ddf4e385cf366eecaf7dcb9a00cb
blam <ben@blam.sh>
2020-12-15 21:04:15 +0100

    feat: do better file detection to check that the universal file is added to the diff too

--- packages/cli/src/lib/diff/handlers.ts
+++ packages/cli/src/lib/diff/handlers.ts
@@ -61,11 +61,32 @@ class PackageJsonHandler {
       await this.syncField('main:src');
     }
     await this.syncField('types');
-
+    await this.syncFiles();
     await this.syncScripts();
     await this.syncPublishConfig();
     await this.syncDependencies('dependencies');
     await this.syncDependencies('devDependencies');
+  }
+
+  private async syncFiles() {
+    const hasUniversalFile = this.targetPkg.files.includes('universal.js');
+    const hasConfigTypeScriptFile =
+      typeof this.targetPkg.configSchema === 'string';
+
+    if (hasUniversalFile || hasConfigTypeScriptFile) {
+      const files = [...this.pkg.files];
+      if (hasUniversalFile) {
+        files.push('universal.js');
+      }
+
+      if (hasConfigTypeScriptFile) {
+        files.push(this.targetPkg.configSchema);
+      }
+
+      await this.syncField('files', { files });
+    } else {
+      await this.syncField('files');
+    }
   }
 
   // Make sure a field inside package.json is in sync. This mutates the targetObj and writes package.json on change.
