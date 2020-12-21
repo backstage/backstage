9838c202d3ffbd14cb9bdfb57fe77f4a262ea1cf
Patrik Oldsberg <poldsberg@gmail.com>
2020-12-21 18:14:25 +0100

    core-api: implement mount point gathering for entity page tabs
    Co-authored-by: blam <ben@blam.sh>

--- packages/core-api/src/routing/collectors.tsx
+++ packages/core-api/src/routing/collectors.tsx
@@ -32,25 +32,48 @@ function getMountPoint(node: ReactElement): RouteRef | undefined {
 
 export const routePathCollector = createCollector(
   () => new Map<RouteRef, string>(),
-  (acc, node, parent) => {
+  (acc, node, parent, ctxPath: string | undefined) => {
+    // The context path is used during mount point gathering to assign the same path
+    // to all discovered mount points
+    let currentCtxPath = ctxPath;
+
     if (parent?.props.element === node) {
-      return;
+      return currentCtxPath;
+    }
+
+    // Start gathering mount points when we encounter a mount point gathering flag
+    if (getComponentData<boolean>(node, 'core.gatherMountPoints')) {
+      const path: string | undefined = node.props?.path;
+      if (!path) {
+        throw new Error('Mount point gatherer must have a path');
+      }
+      currentCtxPath = path;
     }
 
     const routeRef = getMountPoint(node);
     if (routeRef) {
-      const path: string | undefined = node.props?.path;
+      let path: string | undefined = node.props?.path;
+      // If we're gathering mount points we use the context path as out path, unless
+      // the element has its own path, in which case we use that instead and stop gathering
+      if (currentCtxPath) {
+        if (path) {
+          currentCtxPath = undefined;
+        } else {
+          path = currentCtxPath;
+        }
+      }
       if (!path) {
         throw new Error('Mounted routable extension must have a path');
       }
       acc.set(routeRef, path);
     }
+    return currentCtxPath;
   },
 );
 
 export const routeParentCollector = createCollector(
   () => new Map<RouteRef, RouteRef | undefined>(),
-  (acc, node, parent, parentRouteRef?: RouteRef) => {
+  (acc, node, parent, parentRouteRef?: RouteRef | { sticky: RouteRef }) => {
     if (parent?.props.element === node) {
       return parentRouteRef;
     }
@@ -59,8 +82,27 @@ export const routeParentCollector = createCollector(
 
     const routeRef = getMountPoint(node);
     if (routeRef) {
-      acc.set(routeRef, parentRouteRef);
-      nextParent = routeRef;
+      // "sticky" route ref is when we've encountered a mount point gatherer, and we want a
+      // mount points beneath it to have the same parent, regardless of internal structure
+      if (parentRouteRef && 'sticky' in parentRouteRef) {
+        acc.set(routeRef, parentRouteRef.sticky);
+
+        // When we encounter a mount point with an explicit path, we stop gathering
+        // mount points withing the children and remove the sticky state
+        if (node.props?.path) {
+          nextParent = routeRef;
+        } else {
+          nextParent = parentRouteRef;
+        }
+      } else {
+        acc.set(routeRef, parentRouteRef);
+        nextParent = routeRef;
+      }
+    }
+
+    // Mount point gatherers are marked as "sticky"
+    if (getComponentData<boolean>(node, 'core.gatherMountPoints')) {
+      return { sticky: nextParent };
     }
 
     return nextParent;
