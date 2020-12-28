63f40032ca03049f9f7a942e76c565d60993e6ab
Patrik Oldsberg <poldsberg@gmail.com>
2020-12-28 11:13:50 +0100

    Update plugins/catalog/src/hooks/useEntity.ts
    Co-authored-by: Fredrik Adelöw <freben@gmail.com>

--- plugins/catalog/src/hooks/useEntity.ts
+++ plugins/catalog/src/hooks/useEntity.ts
@@ -55,7 +55,7 @@ export const useEntityFromUrl = (): EntityLoadingStatus => {
 };
 
 /**
- * Grab Entity from the context and it's current loading state.
+ * Grab Entity from the context and its current loading state.
  */
 export const useEntity = () => {
   const { entity, loading, error } = useContext<{
