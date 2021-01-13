10b22d6c706a456a9f60e672c6f4a62d2e3276c2
Kiran <kiranpatel11@gmail.com>
2021-01-13 02:28:24 +0000

    Revert "feat(create-app): synch backend/scaffolder.js with master"
    This reverts commit 087e90cf8baeeff60d4cee7d503a84a88ee87fd1.

--- packages/create-app/templates/default-app/packages/backend/src/plugins/scaffolder.ts
+++ packages/create-app/templates/default-app/packages/backend/src/plugins/scaffolder.ts
@@ -1,13 +1,21 @@
 import {
   CookieCutter,
   createRouter,
+  FilePreparer,
+  GithubPreparer,
+  GitlabPreparer,
   Preparers,
   Publishers,
+  GithubPublisher,
+  GitlabPublisher,
   CreateReactAppTemplater,
   Templaters,
+  RepoVisibilityOptions,
   CatalogEntityClient,
 } from '@backstage/plugin-scaffolder-backend';
 import { SingleHostDiscovery } from '@backstage/backend-common';
+import { Octokit } from '@octokit/rest';
+import { Gitlab } from '@gitbeaker/node';
 import type { PluginEnvironment } from '../types';
 import Docker from 'dockerode';
 
@@ -21,8 +29,74 @@ export default async function createPlugin({
   templaters.register('cookiecutter', cookiecutterTemplater);
   templaters.register('cra', craTemplater);
 
-  const preparers = await Preparers.fromConfig(config, { logger });
-  const publishers = await Publishers.fromConfig(config, { logger });
+  const filePreparer = new FilePreparer();
+  const githubPreparer = new GithubPreparer();
+  const gitlabPreparer = new GitlabPreparer(config);
+  const preparers = new Preparers();
+
+  preparers.register('file', filePreparer);
+  preparers.register('github', githubPreparer);
+  preparers.register('gitlab', gitlabPreparer);
+  preparers.register('gitlab/api', gitlabPreparer);
+
+  const publishers = new Publishers();
+
+  const githubConfig = config.getOptionalConfig('scaffolder.github');
+
+  if (githubConfig) {
+    try {
+      const repoVisibility = githubConfig.getString(
+        'visibility',
+      ) as RepoVisibilityOptions;
+
+      const githubToken = githubConfig.getString('token');
+      const githubHost = githubConfig.getOptionalString('host');
+      const githubClient = new Octokit({ auth: githubToken, baseUrl: githubHost });
+      const githubPublisher = new GithubPublisher({
+        client: githubClient,
+        token: githubToken,
+        repoVisibility,
+      });
+      publishers.register('file', githubPublisher);
+      publishers.register('github', githubPublisher);
+    } catch (e) {
+      const providerName = 'github';
+      if (process.env.NODE_ENV !== 'development') {
+        throw new Error(
+          `Failed to initialize ${providerName} scaffolding provider, ${e.message}`,
+        );
+      }
+
+      logger.warn(
+        `Skipping ${providerName} scaffolding provider, ${e.message}`,
+      );
+    }
+  }
+
+  const gitLabConfig = config.getOptionalConfig('scaffolder.gitlab.api');
+  if (gitLabConfig) {
+    try {
+      const gitLabToken = gitLabConfig.getString('token');
+      const gitLabClient = new Gitlab({
+        host: gitLabConfig.getOptionalString('baseUrl'),
+        token: gitLabToken,
+      });
+      const gitLabPublisher = new GitlabPublisher(gitLabClient, gitLabToken);
+      publishers.register('gitlab', gitLabPublisher);
+      publishers.register('gitlab/api', gitLabPublisher);
+    } catch (e) {
+      const providerName = 'gitlab';
+      if (process.env.NODE_ENV !== 'development') {
+        throw new Error(
+          `Failed to initialize ${providerName} scaffolding provider, ${e.message}`,
+        );
+      }
+
+      logger.warn(
+        `Skipping ${providerName} scaffolding provider, ${e.message}`,
+      );
+    }
+  }
 
   const dockerClient = new Docker();
 
