d1fa548bd1a20f1c5d61476503d7b3b85936b1b5
Johan Haals <johan.haals@gmail.com>
2021-01-19 08:56:13 +0100

    Rename azure/api to azure

--- plugins/scaffolder-backend/src/service/router.test.ts
+++ plugins/scaffolder-backend/src/service/router.test.ts
@@ -47,7 +47,7 @@ describe('createRouter - working directory', () => {
     const mockPreparer = {
       prepare: mockPrepare,
     };
-    mockPreparers.register('azure/api', mockPreparer);
+    mockPreparers.register('azure', mockPreparer);
   });
 
   beforeEach(() => {
@@ -65,7 +65,7 @@ describe('createRouter - working directory', () => {
     kind: 'Template',
     metadata: {
       annotations: {
-        'backstage.io/managed-by-location': 'azure/api:dev.azure.com',
+        'backstage.io/managed-by-location': 'azure:dev.azure.com',
       },
     },
     spec: {
