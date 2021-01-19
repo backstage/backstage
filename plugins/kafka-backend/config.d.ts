9c3eda7c6fa471e2b0eeffd1ea4905b0ba6a74fd
Nir Gazit <nir.gzt@gmail.com>
2021-01-19 21:49:27 +0200

    Added docs to config.d.ts

--- plugins/kafka-backend/config.d.ts
+++ plugins/kafka-backend/config.d.ts
@@ -15,8 +15,21 @@
  */
 export interface Config {
   kafka?: {
+    /**
+     * Client ID used to Backstage uses to identify when connecting to the Kafka cluster.
+     */
     clientId: string;
+    /**
+     * List of brokers in the Kafka cluster to connect to.
+     */
     brokers: string[];
+
+    /**
+     * Optional SSL connection parameters to connect to the cluster. Passed directly to Node tls.connect.
+     * See https://nodejs.org/dist/latest-v8.x/docs/api/tls.html#tls_tls_createsecurecontext_options
+     *
+     * @visibility secret
+     */
     ssl?: {
       ca: string[];
       key: string;
