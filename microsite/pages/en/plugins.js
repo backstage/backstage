0703ff9fbb9738d7d42807707b57d9961d1d3c5b
Adam Harvey <adam.harvey@dxc.com>
2021-01-20 22:29:58 -0500

    Remove extra spacing

--- microsite/pages/en/plugins.js
+++ microsite/pages/en/plugins.js
@@ -97,14 +97,14 @@ const Plugins = () => (
           </div>
           <Container className="PluginCardFooter">
             <p>
-              See what plugins are already{' '}
+              See what plugins are already
               <a href="https://github.com/backstage/backstage/issues?q=is%3Aissue+is%3Aopen+label%3Aplugin+sort%3Areactions-%2B1-desc">
                 in progress
-              </a>{' '}
-              and 👍. Missing a plugin for your favorite tool? Please{' '}
+              </a>
+              and 👍. Missing a plugin for your favorite tool? Please
               <a href="https://github.com/backstage/backstage/issues/new?labels=plugin&template=plugin_template.md&title=%5BPlugin%5D+THE+PLUGIN+NAME">
                 suggest
-              </a>{' '}
+              </a>
               a new one.
             </p>
           </Container>
