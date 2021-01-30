59c234ed2ed3254164defcd8677170bee7a269f9
blam <ben@blam.sh>
2021-01-30 04:38:32 +0100

    chore: push for taras

--- packages/e2e-test/src/bigtest/index.ts
+++ packages/e2e-test/src/bigtest/index.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Spotify AB
+ * Copyright 2021 Spotify AB
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,12 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import { Page, test } from 'bigtest';
 
-import { CommanderStatic } from 'commander';
-import { run } from './run';
-import { run as bigtest } from './bigtest';
-
-export function registerCommands(program: CommanderStatic) {
-  program.command('run').description('Run e2e tests').action(run);
-  program.command('bigtest').description('Run e2e bigtests').action(bigtest);
-}
+export default test('bigtest todomvc').step(Page.visit('/catalog'));
