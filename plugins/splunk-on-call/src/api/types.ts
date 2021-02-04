5001b3f7647f685c213f4c9e9939f01bd65b0ca8
Remi <remi.d45@gmail.com>
2021-02-04 23:41:34 +0100

    feat(splunk-on-call-plugin): add api

---
+++ plugins/splunk-on-call/src/api/types.ts
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2020 Spotify AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+  EscalationPolicyInfo,
+  Incident,
+  OnCall,
+  Service,
+  User,
+} from '../components/types';
+import { DiscoveryApi } from '@backstage/core';
+
+export enum TargetType {
+  User = 'User',
+  EscalationPolicy = 'EscalationPolicy',
+}
+
+export type IncidentTarget = {
+  type: TargetType;
+  slug: string;
+};
+
+export type TriggerAlarmRequest = {
+  targets: IncidentTarget[];
+  details: string;
+  summary: string;
+  userName: string;
+  isMultiResponder?: boolean;
+};
+
+export interface SplunkOnCallApi {
+  /**
+   * Fetches a list of incidents
+   *
+   */
+  getIncidents(): Promise<Incident[]>;
+
+  /**
+   * Fetches the list of users in an escalation policy.
+   *
+   */
+  getOnCallUsers(): Promise<OnCall[]>;
+
+  /**
+   * Triggers an incident to whoever is on-call.
+   */
+  triggerAlarm(request: TriggerAlarmRequest): Promise<Response>;
+
+  /**
+   * Resolves an incident.
+   */
+  resolveIncident(request: PatchIncidentRequest): Promise<Response>;
+
+  /**
+   * Acknowledge an incident to whoever is on-call.
+   */
+  acknowledgeIncident(request: PatchIncidentRequest): Promise<Response>;
+
+  /**
+   * Get a list of users for your organization.
+   */
+  getUsers(): Promise<User[]>;
+
+  /**
+   * Get a list of users for your organization.
+   */
+  getEscalationPolicies(): Promise<EscalationPolicyInfo[]>;
+}
+
+export type PatchIncidentRequest = {
+  userName: string;
+  incidentNames: string[];
+  message?: string;
+};
+
+export type EscalationPolicyResponse = {
+  policies: EscalationPolicyInfo[];
+};
+
+export type ListUserResponse = {
+  users: User[];
+  _selfUrl?: string;
+};
+
+export type ServicesResponse = {
+  services: Service[];
+};
+
+export type IncidentsResponse = {
+  incidents: Incident[];
+};
+
+export type OnCallsResponse = {
+  teamsOnCall: OnCall[];
+};
+
+export type ClientApiConfig = {
+  username: string | null;
+  discoveryApi: DiscoveryApi;
+};
+
+export type RequestOptions = {
+  method: string;
+  headers: HeadersInit;
+  body?: BodyInit;
+};
