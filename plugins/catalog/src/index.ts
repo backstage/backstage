0cd7081d8da99f088c1dd76ad786dfc046b1e0c0
Adam Harvey <adam.harvey@dxc.com>
2021-02-11 20:35:57 -0500

    Add export
    Signed-off-by: Adam Harvey <adam.harvey@dxc.com>

--- plugins/catalog/src/index.ts
+++ plugins/catalog/src/index.ts
@@ -17,6 +17,7 @@
 export { AboutCard } from './components/AboutCard';
 export { EntityLayout } from './components/EntityLayout';
 export { EntityPageLayout } from './components/EntityPageLayout';
+export { SystemDiagram } from './components/SystemDiagram';
 export * from './components/EntitySwitch';
 export { Router } from './components/Router';
 export {
