75cd8ac6c56d4b2b7958a3d0b05d49095f4307de
tudi2d <phugenroth@googlemail.com>
2021-02-12 11:49:16 +0100

    Add basic breadcrumbs test

--- packages/core/src/layout/Breadcrumbs/Breadcrumbs.test.tsx
+++ packages/core/src/layout/Breadcrumbs/Breadcrumbs.test.tsx
@@ -13,3 +13,42 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+import { renderInTestApp } from '@backstage/test-utils';
+import { Typography } from '@material-ui/core';
+import { fireEvent } from '@testing-library/react';
+import React from 'react';
+import { Link } from '../..';
+import { Breadcrumbs } from './Breadcrumbs';
+
+describe('<Breadcrumbs/>', () => {
+  it('should render', async () => {
+    const rendered = await renderInTestApp(
+      <Breadcrumbs>
+        <Link to="/">General Page</Link>
+        <Typography>Current Page</Typography>
+      </Breadcrumbs>,
+    );
+    expect(rendered.getByLabelText('breadcrumb')).toBeVisible();
+    expect(rendered.getByText('General Page')).toBeVisible();
+    expect(rendered.getByText('Current Page')).toBeVisible();
+  });
+
+  it('should render hidden breadcrumbs', async () => {
+    const rendered = await renderInTestApp(
+      <Breadcrumbs>
+        <Link to="/">General Page</Link>
+        <Link to="/">Second Page</Link>
+        <Link to="/">Third Page</Link>
+        <Link to="/">Fourth Page</Link>
+        <Typography>Current page</Typography>
+      </Breadcrumbs>,
+    );
+    expect(rendered.getByText('...')).toBeVisible();
+    expect(rendered.queryByText('Third Page')).not.toBeInTheDocument();
+    expect(rendered.queryByText('Fourth Page')).not.toBeInTheDocument();
+    fireEvent.click(rendered.getByText('...'));
+    expect(rendered.getByText('Third Page')).toBeVisible();
+    expect(rendered.getByText('Fourth Page')).toBeVisible();
+  });
+});
