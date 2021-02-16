f6c81bb8d54f4039ba3b3f9bb9beddfb0770dc19
Johan Haals <johan.haals@gmail.com>
2021-02-16 11:33:29 +0100

    create-app: Update scaffolder routes

--- packages/create-app/templates/default-app/packages/app/src/App.tsx
+++ packages/create-app/templates/default-app/packages/app/src/App.tsx
@@ -10,7 +10,11 @@ import { apis } from './apis';
 import * as plugins from './plugins';
 import { AppSidebar } from './sidebar';
 import { Route, Navigate } from 'react-router';
-import { Router as CatalogRouter } from '@backstage/plugin-catalog';
+import {
+  catalogPlugin,
+  CatalogIndexPage,
+  CatalogEntityPage,
+} from '@backstage/plugin-catalog';
 import { Router as DocsRouter } from '@backstage/plugin-techdocs';
 import { CatalogImportPage } from '@backstage/plugin-catalog-import';
 import { Router as TechRadarRouter } from '@backstage/plugin-tech-radar';
@@ -18,10 +22,16 @@ import { SearchPage as SearchRouter } from '@backstage/plugin-search';
 import { Router as SettingsRouter } from '@backstage/plugin-user-settings';
 
 import { EntityPage } from './components/catalog/EntityPage';
+import { scaffolderPlugin, ScaffolderPage } from '@backstage/plugin-scaffolder';
 
 const app = createApp({
   apis,
   plugins: Object.values(plugins),
+  bindRoutes({ bind }) {
+    bind(catalogPlugin.externalRoutes, {
+      createComponent: scaffolderPlugin.routes.root,
+    });
+  }
 });
 
 const AppProvider = app.getProvider();
@@ -37,11 +47,15 @@ const App = () => (
         <AppSidebar />
         <FlatRoutes>
           <Navigate key="/" to="/catalog" />
+          <Route path="/catalog" element={<CatalogIndexPage />} />
           <Route
-            path="/catalog"
-            element={<CatalogRouter EntityPage={EntityPage} />}
-          />
+            path="/catalog/:namespace/:kind/:name"
+            element={<CatalogEntityPage />}
+          >
+            <EntityPage />
+          </Route>
           <Route path="/docs" element={<DocsRouter />} />
+          <Route path="/create" element={<ScaffolderPage />} />
           <Route
             path="/tech-radar"
             element={<TechRadarRouter width={1500} height={800} />}
