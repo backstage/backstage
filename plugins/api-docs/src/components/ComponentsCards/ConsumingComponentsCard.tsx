a3e23a5547c5b5426b44b077559b199da1449a65
Adam Harvey <adam.harvey@dxc.com>
2021-02-23 11:37:44 -0500

    Cleanup formatting
    Signed-off-by: Adam Harvey <adam.harvey@dxc.com>
    Signed-off-by: ebarrios <esteban.barrios@trivago.com>

--- plugins/api-docs/src/components/ComponentsCards/ConsumingComponentsCard.tsx
+++ plugins/api-docs/src/components/ComponentsCards/ConsumingComponentsCard.tsx
@@ -72,7 +72,7 @@ export const ConsumingComponentsCard = ({ variant = 'gridItem' }: Props) => {
       variant={variant}
       emptyContent={
         <div style={{ textAlign: 'center' }}>
-          <Typography variant="h6">No component consumes this API. </Typography>
+          <Typography variant="h6">No component consumes this API.</Typography>
           <Typography variant="body2">
             <Link to="https://backstage.io/docs/features/software-catalog/descriptor-format#specconsumesapis-optional">
               Learn how to change this.
