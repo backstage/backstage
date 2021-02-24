b5869387f6e7e5758866b2889b7544c20baf8dbd
erdoganoksuz <erdoganoksuz95@gmail.com>
2021-02-24 12:10:29 +0300

    console
    Signed-off-by: Mert Can Bilgiç <mert.bilgic@trendyol.com>
    Signed-off-by: ebarrios <esteban.barrios@trivago.com>

--- packages/techdocs-common/src/stages/publish/awsS3.ts
+++ packages/techdocs-common/src/stages/publish/awsS3.ts
@@ -150,7 +150,6 @@ export class AwsS3Publish implements PublisherBase {
    * Directory structure used in the bucket is - entityNamespace/entityKind/entityName/index.html
    */
   async publish({ entity, directory }: PublishRequest): Promise<void> {
-    console.log(entity, directory, 'Publish hey');
     try {
       // Note: OpenStack Swift manages creation of parent directories if they do not exist.
       // So collecting path of only the files is good enough.
@@ -211,8 +210,6 @@ export class AwsS3Publish implements PublisherBase {
   async fetchTechDocsMetadata(
     entityName: EntityName,
   ): Promise<TechDocsMetadata> {
-    console.log(entityName, 'fetchTechDocsMetadata hey');
-
     try {
       return await new Promise<TechDocsMetadata>(async (resolve, reject) => {
         const entityRootDir = `${entityName.namespace}/${entityName.kind}/${entityName.name}`;
@@ -286,8 +283,6 @@ export class AwsS3Publish implements PublisherBase {
    */
   async hasDocsBeenGenerated(entity: Entity): Promise<boolean> {
     try {
-      console.log(entity, 'hasDocsBeenGenerated hey');
-
       const entityRootDir = `${entity.metadata.namespace}/${entity.kind}/${entity.metadata.name}`;
 
       return new Promise(res => {
