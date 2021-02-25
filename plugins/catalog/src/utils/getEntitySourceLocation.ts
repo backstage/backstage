b586c2393cca0777974a30bbe124a79788d7c7d8
Oliver Sand <oliver.sand@sda-se.com>
2021-02-25 18:39:53 +0100

    Move logic for generating URLs for the view, edit and source links of catalog entities from the catalog frontend into the backend
    Signed-off-by: Oliver Sand <oliver.sand@sda-se.com>
    Signed-off-by: mclarke <mclarke@spotify.com>

---
+++ plugins/catalog/src/utils/getEntitySourceLocation.ts
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2020 Spotify AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Entity, SOURCE_LOCATION_ANNOTATION } from '@backstage/catalog-model';
+import { ConfigApi } from '@backstage/core';
+import { ScmIntegrations } from '@backstage/integration';
+
+export type EntitySourceLocation = {
+  url: string;
+  type?: string;
+};
+
+export function getEntitySourceLocation(
+  entity: Entity,
+  config: ConfigApi,
+): EntitySourceLocation | undefined {
+  const sourceLocation =
+    entity.metadata.annotations?.[SOURCE_LOCATION_ANNOTATION];
+
+  if (!sourceLocation) {
+    return undefined;
+  }
+
+  const scmIntegrations = ScmIntegrations.fromConfig(config);
+  const integration = scmIntegrations.byUrl(sourceLocation);
+
+  return {
+    url: sourceLocation,
+    type: integration?.type,
+  };
+}
