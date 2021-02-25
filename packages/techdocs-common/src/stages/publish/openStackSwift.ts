e36ef00fd0ee961e386e224362bbf1ae70040602
erdoganoksuz <erdoganoksuz95@gmail.com>
2021-02-25 11:35:39 +0300

    router case added and hasDocsBeenGenerated fixed
    Signed-off-by: Mert Can Bilgiç <mert.bilgic@trendyol.com>

--- packages/techdocs-common/src/stages/publish/openStackSwift.ts
+++ packages/techdocs-common/src/stages/publish/openStackSwift.ts
@@ -107,6 +107,7 @@ export class OpenStackSwiftPublish implements PublisherBase {
    * Directory structure used in the bucket is - entityNamespace/entityKind/entityName/index.html
    */
   async publish({ entity, directory }: PublishRequest): Promise<void> {
+    this.logger.info(`Publish Called hey`);
     try {
       // Note: OpenStack Swift manages creation of parent directories if they do not exist.
       // So collecting path of only the files is good enough.
@@ -167,6 +168,7 @@ export class OpenStackSwiftPublish implements PublisherBase {
   async fetchTechDocsMetadata(
     entityName: EntityName,
   ): Promise<TechDocsMetadata> {
+    this.logger.info(`fetchTechDocsMetadata Called hey`);
     try {
       return await new Promise<TechDocsMetadata>(async (resolve, reject) => {
         const entityRootDir = `${entityName.namespace}/${entityName.kind}/${entityName.name}`;
@@ -204,6 +206,7 @@ export class OpenStackSwiftPublish implements PublisherBase {
    */
   docsRouter(): express.Handler {
     return async (req, res) => {
+      this.logger.info(`docsRouter Called hey`);
       // Trim the leading forward slash
       // filePath example - /default/Component/documented-component/index.html
 
@@ -240,18 +243,21 @@ export class OpenStackSwiftPublish implements PublisherBase {
    */
   async hasDocsBeenGenerated(entity: Entity): Promise<boolean> {
     try {
+      this.logger.info(`hasDocsBeenGenerated Called hey`);
       const entityRootDir = `${entity.metadata.namespace}/${entity.kind}/${entity.metadata.name}`;
-      this.storageClient.getFile(
-        this.containerName,
-        `${entityRootDir}/index.html`,
-        (err: any, file: any) => {
-          if (!err && file) {
-            return Promise.resolve(true);
-          }
-          return Promise.resolve(false);
-        },
-      );
-      return Promise.resolve(true);
+
+      return new Promise(res => {
+        this.storageClient.getFile(
+          this.containerName,
+          `${entityRootDir}/index.html`,
+          (err: any, file: any) => {
+            console.log(file);
+            if (!err && file) {
+              res(true);
+            } else res(false);
+          },
+        );
+      });
     } catch (e) {
       return Promise.resolve(false);
     }
