7dd22520c4cc0e5c5f2ffc2a22347d5737d715d0
Jonah Back <jback@legalzoom.com>
2021-02-27 12:13:11 -0800

    Remove incorrect comment
    Signed-off-by: ebarrios <esteban.barrios@trivago.com>

--- plugins/kubernetes-backend/src/service/router.ts
+++ plugins/kubernetes-backend/src/service/router.ts
@@ -104,7 +104,6 @@ export async function createRouter(
 
   if (options.clusterSupplier) {
     clusterDetails = await options.clusterSupplier.getClusters();
-    // Append supplied clusters to cluster retrieved via getCombinedClusterDetails
   } else {
     clusterDetails = await getCombinedClusterDetails(
       clusterLocatorMethods,
