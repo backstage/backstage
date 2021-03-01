2eab0de64515fbf2f2925b43e04f0a17bd77fa19
Nir Gazit <nir.gzt@gmail.com>
2021-03-01 09:27:01 +0200

    Added comment explaining why emitting optional locations

--- plugins/catalog-backend/src/ingestion/processors/GithubDiscoveryProcessor.ts
+++ plugins/catalog-backend/src/ingestion/processors/GithubDiscoveryProcessor.ts
@@ -94,6 +94,9 @@ export class GithubDiscoveryProcessor implements CatalogProcessor {
             type: 'url',
             target: `${repository.url}${catalogPath}`,
           },
+          // Not all locations may actually exist, since the user defined them as a wildcard pattern.
+          // Thus, we omit them as optional and let the downstream processor find them while not outputting
+          // an error if it couldn't.
           true,
         ),
       );
