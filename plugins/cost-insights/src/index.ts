8df152f49abfae017d1e5f973d1114e5614db4f6
Ryan Vazquez <ryanv@spotify.com>
2021-03-03 15:10:17 -0500

    migrate mocks to test utils
    Signed-off-by: Ryan Vazquez <ryanv@spotify.com>
    Signed-off-by: ebarrios <esteban.barrios@trivago.com>

--- plugins/cost-insights/src/index.ts
+++ plugins/cost-insights/src/index.ts
@@ -23,7 +23,7 @@ export {
 } from './plugin';
 export { ExampleCostInsightsClient } from './example';
 export { BarChart, LegendItem, CostGrowth } from './components';
+export { MockConfigProvider, MockCurrencyProvider } from './testUtils';
 export * from './api';
 export * from './alerts';
 export * from './types';
-export * from './testUtils';
