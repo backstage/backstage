e15f7a6464657325d9134f7a436bcad394dbf670
ebarrios <esteban.barrios@trivago.com>
2021-03-05 17:02:50 +0100

    Added missing row to variables
    Signed-off-by: ebarrios <esteban.barrios@trivago.com>

--- plugins/kafka/src/components/ConsumerGroupOffsets/ConsumerGroupOffsets.tsx
+++ plugins/kafka/src/components/ConsumerGroupOffsets/ConsumerGroupOffsets.tsx
@@ -63,7 +63,7 @@ const generatedColumns: TableColumn[] = [
     render: (row: Partial<TopicPartitionInfo>) => {
       let lag = undefined;
       if (row.topicOffset && row.groupOffset) {
-        lag = +topicOffset - +groupOffset;
+        lag = +row.topicOffset - +row.groupOffset;
       }
       return <>{lag ?? ''}</>;
     },
