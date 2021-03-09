72f59d5528f435a597b2632a0cdd6bee541fa559
Patrik Oldsberg <poldsberg@gmail.com>
2021-03-09 19:58:54 +0100

    todo: add todo api types and empty client
    Signed-off-by: Patrik Oldsberg <poldsberg@gmail.com>

---
+++ plugins/todo/src/api/types.ts
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2021 Spotify AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { createApiRef } from '@backstage/core';
+import { Entity } from '@backstage/catalog-model';
+
+export type TodoItem = {
+  text: string;
+  author?: string;
+  viewUrl?: string;
+  editUrl?: string;
+};
+
+export type TodoListOptions = {
+  entity?: Entity;
+  cursor?: string;
+};
+
+export type TodoListResult = {
+  items: TodoItem[];
+  totalCount: number;
+  cursors: {
+    prev: string;
+    self: string;
+    next: string;
+  };
+};
+
+export interface TodoApi {
+  listTodos(options: TodoListOptions): Promise<TodoListResult>;
+}
+
+export const todoApiRef = createApiRef<TodoApi>({
+  id: 'plugin.todo.api',
+  description: 'Lists TODOs',
+});
