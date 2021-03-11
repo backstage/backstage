010aed7848277b1ff096dbcb20ade89125ba2c7b
Oliver Sand <oliver.sand@sda-se.com>
2021-03-11 18:18:30 +0100

    Add `AnnotateScmSlugEntityProcessor`
    Signed-off-by: Oliver Sand <oliver.sand@sda-se.com>

---
+++ .changeset/angry-boats-hunt.md
@@ -0,0 +1,15 @@
+---
+'@backstage/plugin-catalog-backend': patch
+---
+
+Add `AnnotateScmSlugEntityProcessor` that automatically adds the
+`github.com/project-slug` annotation for components coming from GitHub.
+
+The processor is optional and not automatically registered in the catalog
+builder. To add it to your instance, add it to your `CatalogBuilder` using
+`addProcessor()`:
+
+```typescript
+const builder = new CatalogBuilder(env);
+builder.addProcessor(new AnnotateScmSlugEntityProcessor());
+```
---
+++ plugins/catalog-backend/src/ingestion/processors/AnnotateScmSlugEntityProcessor.ts
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2021 Spotify AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { Entity, LocationSpec } from '@backstage/catalog-model';
+import parseGitUrl from 'git-url-parse';
+import { identity, merge, pickBy } from 'lodash';
+import { CatalogProcessor } from './types';
+
+const GITHUB_ACTIONS_ANNOTATION = 'github.com/project-slug';
+
+export class AnnotateScmSlugEntityProcessor implements CatalogProcessor {
+  async preProcessEntity(
+    entity: Entity,
+    location: LocationSpec,
+  ): Promise<Entity> {
+    if (entity.kind !== 'Component' || location.type !== 'url') {
+      return entity;
+    }
+
+    const gitUrl = parseGitUrl(location.target);
+    let githubProjectSlug =
+      entity.metadata.annotations?.[GITHUB_ACTIONS_ANNOTATION];
+
+    if (gitUrl.source === 'github.com' && !githubProjectSlug) {
+      githubProjectSlug = `${gitUrl.owner}/${gitUrl.name}`;
+    }
+
+    return merge(
+      {
+        metadata: {
+          annotations: pickBy(
+            {
+              [GITHUB_ACTIONS_ANNOTATION]: githubProjectSlug,
+            },
+            identity,
+          ),
+        },
+      },
+      entity,
+    );
+  }
+}
