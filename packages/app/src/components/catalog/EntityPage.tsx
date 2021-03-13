d40fd8127d80964e54925b1a39ae207caeb38b2b
Adam Harvey <adam.harvey@dxc.com>
2021-03-13 14:02:56 -0500

    Remove extraneous breakpoints
    Signed-off-by: Adam Harvey <adam.harvey@dxc.com>

--- packages/app/src/components/catalog/EntityPage.tsx
+++ packages/app/src/components/catalog/EntityPage.tsx
@@ -439,13 +439,13 @@ const GroupEntityPage = ({ entity }: { entity: Entity }) => (
 
 const SystemOverviewContent = ({ entity }: { entity: SystemEntity }) => (
   <Grid container spacing={3} alignItems="stretch">
-    <Grid item md={6} sm={6}>
+    <Grid item md={6}>
       <AboutCard entity={entity} variant="gridItem" />
     </Grid>
-    <Grid item md={6} sm={12}>
+    <Grid item md={6}>
       <EntityHasComponentsCard variant="gridItem" />
     </Grid>
-    <Grid item md={6} sm={12}>
+    <Grid item md={6}>
       <EntityHasApisCard variant="gridItem" />
     </Grid>
   </Grid>
