3fb45a60efe5091e0f3facec5b570b48f519b040
gamze.senturk <gamze.senturk.95@gmail.com>
2021-03-23 11:14:34 +0300

    Round function added to metrics
    Signed-off-by: Mert Can Bilgiç <mert@ieuacm.com>

--- plugins/chrome-ux-report/src/components/ChromeUXReportTable.tsx
+++ plugins/chrome-ux-report/src/components/ChromeUXReportTable.tsx
@@ -24,6 +24,10 @@ import { chromeuxReportApiRef } from "../api";
 import { useAsync } from "react-use";
 import Alert from "@material-ui/lab/Alert";
 
+const returnRoundOfMetric = (metric: any) => {
+  return Math.round(metric * 100);
+}
+
 export const ChromeUXReportTable = ({ origin }: { origin: string; }) => {
   const chromeUXReportApi = useApi(chromeuxReportApiRef);
   const columns: TableColumn[] = [
@@ -65,7 +69,11 @@ export const ChromeUXReportTable = ({ origin }: { origin: string; }) => {
     connection_type,
     first_contentful_paint,
     largest_contentful_paint,
-    dom_content_loaded
+    dom_content_loaded,
+    first_input,
+    onload,
+    layout_instability,
+    time_to_first_byte
   } = value;
 
   return <Table
@@ -85,15 +93,45 @@ export const ChromeUXReportTable = ({ origin }: { origin: string; }) => {
     }, {
       metricName: 'FIRST CONTENTFUL PAINT',
       metricValue: '-',
-      fast: first_contentful_paint.rates.fast,
-      average: largest_contentful_paint.rates.fast,
-      slow: dom_content_loaded.rates.fast
+      fast: `${returnRoundOfMetric(first_contentful_paint.rates.fast)}%`,
+      average:`${returnRoundOfMetric(first_contentful_paint.rates.average)}%`,
+      slow: `${returnRoundOfMetric(first_contentful_paint.rates.slow)}%`
     }, {
       metricName: 'LARGEST CONTENTFUL PAINT',
       metricValue: '-',
-      fast: first_contentful_paint.rates.fast,
-      average: largest_contentful_paint.rates.fast,
-      slow: dom_content_loaded.rates.fast
+      fast: `${returnRoundOfMetric(largest_contentful_paint.rates.fast)}%`,
+      average: `${returnRoundOfMetric(largest_contentful_paint.rates.average)}%`,
+      slow: `${returnRoundOfMetric(largest_contentful_paint.rates.slow)}%`
+    }, {
+      metricName: 'DOM CONTENT LOADED',
+      metricValue: '-',
+      fast: `${returnRoundOfMetric(dom_content_loaded.rates.fast)}%`,
+      average: `${returnRoundOfMetric(dom_content_loaded.rates.average)}%`,
+      slow: `${returnRoundOfMetric(dom_content_loaded.rates.slow)}%`
+    }, {
+      metricName: 'FIRST INPUT',
+      metricValue: '-',
+      fast: `${returnRoundOfMetric(first_input.rates.fast)}%`,
+      average: `${returnRoundOfMetric(first_input.rates.average)}%`,
+      slow: `${returnRoundOfMetric(first_input.rates.slow)}%`
+    }, {
+      metricName: 'ONLOAD',
+      metricValue: '-',
+      fast: `${returnRoundOfMetric(onload.rates.fast)}%`,
+      average: `${returnRoundOfMetric(onload.rates.average)}%`,
+      slow: `${returnRoundOfMetric(onload.rates.slow)}%`
+    }, {
+      metricName: 'LAYOUT INSTABILITY',
+      metricValue: '-',
+      fast: `${returnRoundOfMetric(layout_instability.rates.fast)}%`,
+      average: `${returnRoundOfMetric(layout_instability.rates.average)}%`,
+      slow: `${returnRoundOfMetric(layout_instability.rates.slow)}%`
+    }, {
+      metricName: 'TIME TO FIRST BYTE',
+      metricValue: '-',
+      fast: `${returnRoundOfMetric(time_to_first_byte.rates.fast)}%`,
+      average: `${returnRoundOfMetric(time_to_first_byte.rates.average)}%`,
+      slow: `${returnRoundOfMetric(time_to_first_byte.rates.slow)}%`
     }]}
     columns={columns}
   />;
