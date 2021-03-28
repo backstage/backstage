2be220f77134e99e5e78b148c0f1a9b8530c1fc2
Eric Peterson <ericpeterson@spotify.com>
2021-03-28 23:32:34 +0200

    WIP redirect plugin for comment.
    Signed-off-by: Eric Peterson <ericpeterson@spotify.com>

---
+++ plugins/redirect/README.md
@@ -0,0 +1,54 @@
+# Redirects
+
+This plugin allows you to define basic, client-side redirects for your
+Backstage instance via app config. Redirects merely require a `from` and a `to`
+value, and values may contain route parameters as well as `*`.
+
+## Installation
+
+1. Use `yarn` or `npm` to install this plugin `@backstage/plugin-redirects`
+2. Import the `<Redirects />` component from this plugin in your `App.tsx` file
+   like so: `import { Redirects } from '@backstage/plugin-redirect';`
+3. Add the `<Redirects />` component as a child of `<AppRouter>`, and a sibling
+   of `<Root>`.
+4. Add redirect configurations to your `app-config.yaml`, like so:
+
+```yaml
+redirects:
+  patterns:
+    - from: '/old/path'
+      to: '/new/path'
+```
+
+## Usage
+
+For simple, single-page redirects, just set `from` and `to` as the exact paths
+you wish to redirect from/to:
+
+```yaml
+redirects:
+  patterns:
+    - from: '/old-path'
+    - to: '/new-path'
+```
+
+If you wish to redirect all sub-paths from a given path, you can use the `*`
+character at the end of both your `from` and `to` keys, like so:
+
+```yaml
+redirects:
+  patterns:
+    - from: '/docs/default/Component/solr-searcher/*'
+    - to: '/docs/default/Component/elastic-searcher/*'
+```
+
+You may also use route parameters in your redirect definition if, for example,
+you wish to redirect a single view for all instances of a given set of
+parameters.
+
+```yaml
+redirects:
+  patterns:
+    - from: '/catalog/:namespace/:kind/:name/diy-ci'
+    - to: '/catalog/:namespace/:kind/:name/circle-ci'
+```
---
+++ plugins/redirect/src/components/Redirects/Redirects.tsx
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2021 Spotify AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from 'react';
+import { Navigate, Route, Routes, useParams } from 'react-router';
+import { configApiRef, useApi } from '@backstage/core';
+
+interface RedirectProps {
+  from: string;
+  to: string;
+}
+
+function NavigateRedirect({ to }: Pick<RedirectProps, 'to'>) {
+  const params = useParams();
+  let finalTo = to;
+
+  Object.entries(params).forEach(([name, value]) => {
+    finalTo = finalTo.replace(name === '*' ? name : `:${name}`, value);
+  });
+
+  return <Navigate replace to={finalTo} />;
+}
+
+export const Redirects = () => {
+  const redirects =
+    useApi(configApiRef).getOptionalConfigArray('redirects.patterns') || [];
+  return (
+    <Routes>
+      {redirects.map(redirect => {
+        return (
+          <Route
+            key={redirect.get('from')}
+            path={redirect.get('from')}
+            element={<NavigateRedirect to={redirect.get('to')} />}
+          />
+        );
+      })}
+    </Routes>
+  );
+};
