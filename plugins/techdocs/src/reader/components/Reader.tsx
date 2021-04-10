f935d8062449115b5eae2c143170e2fd43771e51
David Tuite <david@roadie.io>
2021-04-10 10:35:41 +0100

    Add missing closing bracket in CSS
    Signed-off-by: David Tuite <david@roadie.io>

--- plugins/techdocs/src/reader/components/Reader.tsx
+++ plugins/techdocs/src/reader/components/Reader.tsx
@@ -222,6 +222,7 @@ export const Reader = ({ entityId, onReady }: Props) => {
         :host {
           --md-tasklist-icon: url('data:image/svg+xml;charset=utf-8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 2A10 10 0 002 12a10 10 0 0010 10 10 10 0 0010-10A10 10 0 0012 2z"/></svg>');
           --md-tasklist-icon--checked: url('data:image/svg+xml;charset=utf-8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10 10-4.5 10-10S17.5 2 12 2m-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/></svg>');
+        }
         `,
       }),
     ]);
