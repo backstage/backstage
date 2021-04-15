66fbcce83c0d31dee3f2f154946c79219012b537
blam <ben@blam.sh>
2021-04-15 11:14:04 +0200

    chore: removing the deprecations
    Signed-off-by: blam <ben@blam.sh>

--- packages/cli/src/lib/bundler/config.ts
+++ packages/cli/src/lib/bundler/config.ts
@@ -258,7 +258,7 @@ export async function createBackendConfig(
         modulesDir: paths.rootNodeModules,
         additionalModuleDirs: moduleDirs,
         allowlist: ['webpack/hot/poll?100', ...localPackageNames],
-      }) as any,
+      }),
     ],
     target: 'node' as const,
     node: {
@@ -358,8 +358,11 @@ function nodeExternalsWithResolve(
     },
   });
 
-  return (context: string, request: string, callback: any) => {
-    currentContext = context;
+  return (
+    { context, request }: { context?: string; request?: string },
+    callback: any,
+  ) => {
+    currentContext = context!;
     return externals(context, request, callback);
   };
 }
