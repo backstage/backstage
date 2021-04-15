fcb8cfa7b99b0760f2ad63f480781e8098644a6e
blam <ben@blam.sh>
2021-04-15 09:45:37 +0200

    chore: fix types for nodeExternals
    Signed-off-by: blam <ben@blam.sh>

--- packages/cli/src/types.d.ts
+++ packages/cli/src/types.d.ts
@@ -31,3 +31,67 @@ declare module '@svgr/rollup' {
 declare module '@rollup/plugin-yaml';
 
 declare module 'terser-webpack-plugin';
+
+declare module 'webpack-node-externals' {
+  export default function webpackNodeExternals(
+    options?: webpackNodeExternals.Options,
+  ): any;
+
+  namespace webpackNodeExternals {
+    type AllowlistOption = string | RegExp | AllowlistFunctionType;
+    type ImportTypeCallback = (moduleName: string) => string;
+    /** a function that accepts the module name and returns whether it should be included */
+    type AllowlistFunctionType = (moduleName: string) => boolean;
+    interface ModulesFromFileType {
+      exclude?: string | string[];
+      include?: string | string[];
+    }
+
+    interface Options {
+      /**
+       * An array for the externals to allow, so they will be included in the bundle.
+       * Can accept exact strings ('module_name'), regex patterns (/^module_name/), or a
+       * function that accepts the module name and returns whether it should be included.
+       * Important - if you have set aliases in your webpack config with the exact
+       * same names as modules in node_modules, you need to allowlist them so Webpack will know
+       * they should be bundled.
+       * @default []
+       */
+      allowlist?: AllowlistOption[] | AllowlistOption;
+      /**
+       * @default ['.bin']
+       */
+      binaryDirs?: string[];
+      /**
+       * The method in which unbundled modules will be required in the code. Best to leave as
+       * 'commonjs' for node modules.
+       * @default 'commonjs'
+       */
+      importType?:
+        | 'var'
+        | 'this'
+        | 'commonjs'
+        | 'amd'
+        | 'umd'
+        | ImportTypeCallback;
+      /**
+       * The folder in which to search for the node modules.
+       * @default 'node_modules'
+       */
+      modulesDir?: string;
+      /**
+       * Additional folders to look for node modules.
+       */
+      additionalModuleDirs?: string[];
+      /**
+       * Read the modules from the package.json file instead of the node_modules folder.
+       * @default false
+       */
+      modulesFromFile?: boolean | ModulesFromFileType;
+      /**
+       * @default false
+       */
+      includeAbsolutePaths?: boolean;
+    }
+  }
+}
