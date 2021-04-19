8e09c5fa2d9d394355822cf6426d2b1e573a2f49
Rickard Dybeck <r.dybeck@gmail.com>
2021-04-19 12:46:33 -0400

    remove string interpolation for theme spacing
    Co-authored-by: Fredrik Adelöw <freben@gmail.com>
    Signed-off-by: Fredrik Adelöw <freben@gmail.com>

--- plugins/code-coverage/src/components/FileExplorer/CoverageRow.tsx
+++ plugins/code-coverage/src/components/FileExplorer/CoverageRow.tsx
@@ -32,7 +32,7 @@ const useStyles = makeStyles(theme => ({
     borderRight: `1px solid ${theme.palette.grey[500]}`,
     textAlign: 'center',
     color: 'white', // need to enforce this color since it needs to stand out against colored background
-    paddingLeft: `${theme.spacing(1)}`,
+    paddingLeft: theme.spacing(1),
     paddingRight: `${theme.spacing(1)}`,
   },
   countRoundedRectangle: {
