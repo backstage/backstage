d7ef50d9647d617dc86443b520fecba3fd446736
Heather Lee <heatherl@splunk.com>
2021-05-05 16:09:26 -0700

    Remove extra definition of EntityRow from catalog-model
    Signed-off-by: Heather Lee <heatherl@splunk.com>

--- packages/catalog-model/src/index.ts
+++ packages/catalog-model/src/index.ts
@@ -18,5 +18,5 @@ export * from './entity';
 export { EntityPolicies } from './EntityPolicies';
 export * from './kinds';
 export * from './location';
-export type { EntityName, EntityRef, JSONSchema, EntityRow } from './types';
+export type { EntityName, EntityRef, JSONSchema } from './types';
 export * from './validation';
