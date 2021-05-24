d42bb71aadbade4fd90d70f3d604e9d2a9ec8553
Emma Indal <emma.indahl@gmail.com>
2021-05-24 10:07:49 +0200

    fix notice header
    Signed-off-by: Emma Indal <emma.indahl@gmail.com>
    Signed-off-by: Eric Peterson <ericpeterson@spotify.com>

--- plugins/search/src/components/DefaultResultListItem/index.ts
+++ plugins/search/src/components/DefaultResultListItem/index.ts
@@ -11,8 +11,7 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * l
-imitations under the License.
+ * limitations under the License.
  */
 
 export { DefaultResultListItem } from './DefaultResultListItem';
