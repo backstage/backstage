43d0e2d722369c8cd659ebc86b998a1df3edb95b
Allan Juhanson <allan.juhanson@energia.ee>
2021-05-24 11:47:44 +0300

    created plugin kubectl-snippets

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -56,6 +56,7 @@ import { Root } from './components/Root';
 import { entityPage } from './components/catalog/EntityPage';
 import { providers } from './identityProviders';
 import * as plugins from './plugins';
+import { KubectlSnippetsPage } from '@backstage/plugin-kubectl-snippets';
 
 const app = createApp({
   apis,
@@ -133,6 +134,7 @@ const routes = (
       element={<CostInsightsLabelDataflowInstructionsPage />}
     />
     <Route path="/settings" element={<UserSettingsPage />} />
+    <Route path="/kubectl-snippets" element={<KubectlSnippetsPage />} />
   </FlatRoutes>
 );
 
