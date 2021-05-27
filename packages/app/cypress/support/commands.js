5a1c284b2c1fc04a24998bcf823a8d41c39cf121
Camila Belo <camilaibs@gmail.com>
2021-05-27 17:55:06 +0200

    test(app/components/search): complete Page coverage
    Signed-off-by: Camila Belo <camilaibs@gmail.com>
    Signed-off-by: Eric Peterson <ericpeterson@spotify.com>

--- packages/app/cypress/support/commands.js
+++ packages/app/cypress/support/commands.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Spotify AB
+ * Copyright 2021 Spotify AB
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,5 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import '@testing-library/cypress/add-commands';
-import './commands';
+Cypress.Commands.add('enterAsGuest', () => {
+  cy.visit('/');
+  cy.get('button').contains('Enter').click();
+});
