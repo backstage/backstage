0160678b18e53901c369a148ab9895c10b5bec40
Patrik Oldsberg <poldsberg@gmail.com>
2021-06-03 00:35:33 +0200

    core-api: bring in RouteRef type from core-plugin-api to make them type compatible
    Signed-off-by: Patrik Oldsberg <poldsberg@gmail.com>

--- packages/core-api/src/routing/types.ts
+++ packages/core-api/src/routing/types.ts
@@ -16,6 +16,7 @@
 
 import { IconComponent } from '../icons/types';
 import { getOrCreateGlobalSingleton } from '../lib/globalObject';
+import { RouteRef as NewRouteRef } from '@backstage/core-plugin-api';
 
 export type AnyParams = { [param in string]: string } | undefined;
 export type ParamKeys<Params extends AnyParams> = keyof Params extends never
@@ -34,7 +35,11 @@ export type RouteFunc<Params extends AnyParams> = (
   ...[params]: Params extends undefined ? readonly [] : readonly [Params]
 ) => string;
 
-export const routeRefType: unique symbol = getOrCreateGlobalSingleton<any>(
+type RouteRefType = Exclude<
+  keyof NewRouteRef,
+  'params' | 'path' | 'title' | 'icon'
+>;
+export const routeRefType: RouteRefType = getOrCreateGlobalSingleton<any>(
   'route-ref-type',
   () => Symbol('route-ref-type'),
 );
