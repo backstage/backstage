5c4e6aee2536fef8ec00cdeca5b0812ce481449c
Andrew Thauer <athauer@wealthsimple.com>
2021-06-16 19:46:25 -0400

    feat(explore): customizable explore page
    Signed-off-by: Andrew Thauer <athauer@wealthsimple.com>

--- plugins/explore/src/components/index.ts
+++ plugins/explore/src/components/index.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Spotify AB
+ * Copyright 2021 Spotify AB
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,4 @@
  * limitations under the License.
  */
 
-export { ExploreLayout } from './components';
-export * from './extensions';
-export { explorePlugin, explorePlugin as plugin } from './plugin';
-export * from './routes';
+export { ExploreLayout } from './ExploreLayout';
