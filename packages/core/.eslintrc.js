27bc65bf11d540e79a5992d1eaf4e35ed9dcac0a
Patrik Oldsberg <poldsberg@gmail.com>
2021-06-22 17:29:26 +0200

    packages: remove core and core-api
    Signed-off-by: Patrik Oldsberg <poldsberg@gmail.com>

--- packages/core/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: [require.resolve('@backstage/cli/config/eslint')],
-  rules: {
-    // TODO: add prop types to JS and remove
-    'react/prop-types': 0,
-    'jest/expect-expect': 0,
-  },
-};
