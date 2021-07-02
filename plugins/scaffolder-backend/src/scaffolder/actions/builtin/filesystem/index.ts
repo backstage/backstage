62c2f10f7d3e4e54fc2007052a782ca087bf6a72
Andrea Falzetti <afalzetti@gmail.com>
2021-07-02 17:16:39 +0100

    feat(scaffolder): add fs:delete custom step
    Signed-off-by: Andrea Falzetti <afalzetti@gmail.com>

--- plugins/scaffolder-backend/src/scaffolder/actions/builtin/filesystem/index.ts
+++ plugins/scaffolder-backend/src/scaffolder/actions/builtin/filesystem/index.ts
@@ -14,9 +14,5 @@
  * limitations under the License.
  */
 
-export * from './catalog';
-export { createBuiltinActions } from './createBuiltinActions';
-export * from './debug';
-export * from './fetch';
-export * from './filesystem';
-export * from './publish';
+export { createFilesystemDeleteAction } from './delete';
+export { createFilesystemRenameAction } from './rename';
