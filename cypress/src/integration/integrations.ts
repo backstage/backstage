98e5a4919379f9547bf508fae9c0c5ea0b08a2eb
Ben Lambert <ben@blam.sh>
2021-07-06 12:24:28 +0200

    chore: change timeout
    Signed-off-by: Ben Lambert <ben@blam.sh>

--- cypress/src/integration/integrations.ts
+++ cypress/src/integration/integrations.ts
@@ -27,7 +27,7 @@ describe('Integrations', () => {
         type: 'url',
       });
 
-      cy.wait(1000);
+      cy.wait(5000);
 
       cy.visit('/catalog');
       cy.get('[data-testid="user-picker-all"]').click();
@@ -54,7 +54,7 @@ describe('Integrations', () => {
         type: 'url',
       });
 
-      cy.wait(1000);
+      cy.wait(5000);
 
       cy.visit('/catalog');
       cy.get('[data-testid="user-picker-all"]').click();
@@ -71,7 +71,7 @@ describe('Integrations', () => {
         type: 'url',
       });
 
-      cy.wait(1000);
+      cy.wait(5000);
 
       cy.visit('/catalog');
       cy.get('[data-testid="user-picker-all"]').click();
