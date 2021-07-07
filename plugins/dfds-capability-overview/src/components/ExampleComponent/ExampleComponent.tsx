d22f4c62c07003726fdacbfe336005bb0b4f6645
kdichev <konstantin.dichev@gmail.com>
2021-07-07 16:04:25 +0200

    add logs

--- plugins/dfds-capability-overview/src/components/ExampleComponent/ExampleComponent.tsx
+++ plugins/dfds-capability-overview/src/components/ExampleComponent/ExampleComponent.tsx
@@ -205,7 +205,8 @@ const CapabilitiesListBase = () => {
     };
     fetchData();
   }, [authApi]);
-
+  // @ts-ignore
+  console.log(capData); // eslint-disable-line
   return (
     <Page themeId="tool">
       <Header title="Welcome to Capability Discoverability!">
