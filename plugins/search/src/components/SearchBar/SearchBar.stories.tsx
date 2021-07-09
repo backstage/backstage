c47bb87c39d06c8bb4fc4c17d71e78f681490a3e
Emma Indal <emma.indahl@gmail.com>
2021-07-09 12:21:19 +0200

    add stories to storybook for reusable search components
    Signed-off-by: Emma Indal <emma.indahl@gmail.com>

---
+++ plugins/search/src/components/SearchBar/SearchBar.stories.tsx
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2021 Spotify AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from 'react';
+import { Grid } from '@material-ui/core';
+import { SearchBar, SearchContext } from '../index';
+import { MemoryRouter } from 'react-router';
+
+export default {
+  title: 'Plugins/Search/SearchBar',
+  component: SearchBar,
+};
+
+const defaultValue = {
+  term: '',
+  setTerm: () => {},
+};
+
+export const Default = () => {
+  return (
+    <MemoryRouter>
+      {/* @ts-ignore (defaultValue requires more than what is used here) */}
+      <SearchContext.Provider value={defaultValue}>
+        <Grid container direction="row">
+          <Grid item xs={12}>
+            <SearchBar />
+          </Grid>
+        </Grid>
+      </SearchContext.Provider>
+    </MemoryRouter>
+  );
+};
