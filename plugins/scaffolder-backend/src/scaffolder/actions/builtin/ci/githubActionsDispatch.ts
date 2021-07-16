2ca8855b0d5257f903e63ed527f889f50cb69633
Andrea Falzetti <afalzetti@gmail.com>
2021-07-16 09:31:36 +0100

    fix: remove ctx.token
    Signed-off-by: Andrea Falzetti <afalzetti@gmail.com>

--- plugins/scaffolder-backend/src/scaffolder/actions/builtin/ci/githubActionsDispatch.ts
+++ plugins/scaffolder-backend/src/scaffolder/actions/builtin/ci/githubActionsDispatch.ts
@@ -105,7 +105,7 @@ export function createGithubActionsDispatchAction(options: {
         );
       }
       const client = new Octokit({
-        auth: ctx.token ?? token,
+        auth: token,
         baseUrl: integrationConfig.config.apiBaseUrl,
         previews: ['nebula-preview'],
       });
