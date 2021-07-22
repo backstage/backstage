b6e36c9a01a6d25a5530bcdd1deea1e8c444e55e
Juan Lulkin <jmaiz@spotify.com>
2021-07-22 10:27:04 +0200

    Remove unused fragment
    Signed-off-by: Juan Lulkin <jmaiz@spotify.com>

--- plugins/user-settings/src/components/FeatureFlags/FeatureFlagsItem.tsx
+++ plugins/user-settings/src/components/FeatureFlags/FeatureFlagsItem.tsx
@@ -31,17 +31,15 @@ type Props = {
 };
 
 export const FlagItem = ({ flag, enabled, toggleHandler }: Props) => (
-  <>
-    <ListItem divider button onClick={() => toggleHandler(flag.name)}>
-      <ListItemIcon>
-        <Tooltip placement="top" arrow title={enabled ? 'Disable' : 'Enable'}>
-          <Switch color="primary" checked={enabled} name={flag.name} />
-        </Tooltip>
-      </ListItemIcon>
-      <ListItemText
-        primary={flag.name}
-        secondary={`Registered in ${flag.pluginId} plugin`}
-      />
-    </ListItem>
-  </>
+  <ListItem divider button onClick={() => toggleHandler(flag.name)}>
+    <ListItemIcon>
+      <Tooltip placement="top" arrow title={enabled ? 'Disable' : 'Enable'}>
+        <Switch color="primary" checked={enabled} name={flag.name} />
+      </Tooltip>
+    </ListItemIcon>
+    <ListItemText
+      primary={flag.name}
+      secondary={`Registered in ${flag.pluginId} plugin`}
+    />
+  </ListItem>
 );
