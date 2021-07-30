c189c5da5d58d2fa40bdef8c4bd67dc5691cf9ee
Himanshu Mishra <himanshu@orkohunter.net>
2021-07-30 12:47:17 +0200

    chore: fix typo in EntityPage
    Signed-off-by: Himanshu Mishra <himanshu@orkohunter.net>

--- packages/create-app/templates/default-app/packages/app/src/components/catalog/EntityPage.tsx
+++ packages/create-app/templates/default-app/packages/app/src/components/catalog/EntityPage.tsx
@@ -160,7 +160,7 @@ const websiteEntityPage = (
 /**
  * NOTE: This page is designed to work on small screens such as mobile devices.
  * This is based on Material UI Grid. If breakpoints are used, each grid item must set the `xs` prop to a column size or to `true`,
- * since this does not default. If no breakpoints are used, the items will equitably share the asvailable space.
+ * since this does not default. If no breakpoints are used, the items will equitably share the available space.
  * https://material-ui.com/components/grid/#basic-grid.
  */
 
