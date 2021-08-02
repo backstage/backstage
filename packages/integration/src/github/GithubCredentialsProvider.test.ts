53ed456a682f3290d0a1c0c7e67ad28cb74ffc46
Aurelio Saraiva <aureliosaraiva@gmail.com>
2021-08-02 10:11:04 -0300

    fixed bug on when compared in organzation name
    Signed-off-by: Aurelio Saraiva <aureliosaraiva@gmail.com>

--- packages/integration/src/github/GithubCredentialsProvider.test.ts
+++ packages/integration/src/github/GithubCredentialsProvider.test.ts
@@ -254,4 +254,58 @@ describe('GithubCredentialsProvider tests', () => {
       }),
     ).resolves.toEqual({ headers: undefined, token: undefined, type: 'token' });
   });
+
+  // it('should return installation', async () => {
+  //   const githubProvider = GithubCredentialsProvider.create({
+  //     host: 'github.com',
+  //     apps: [
+  //       {
+  //         appId: 1,
+  //         privateKey: 'privateKey',
+  //         webhookSecret: '123',
+  //         clientId: 'CLIENT_ID',
+  //         clientSecret: 'CLIENT_SECRET',
+  //       },
+  //     ],
+  //   });
+  //
+  //   octokit.apps.listInstallations.mockResolvedValue(({
+  //     data: [],
+  //   } as unknown) as RestEndpointMethodTypes['apps']['listInstallations']['response']);
+  //
+  //   await expect(
+  //     githubProvider.getInstallationCredentials('Backstage'),
+  //   ).resolves.toEqual({ headers: undefined, token: undefined, type: 'token' });
+  // });
+
+  it('should to create and ignore case sensitive when creating a token', async () => {
+    octokit.apps.listInstallations.mockResolvedValue({
+      headers: {
+        etag: '123',
+      },
+      data: [
+        {
+          id: 1,
+          repository_selection: 'all',
+          account: {
+            login: 'BACKSTAGE',
+          },
+        },
+      ],
+    } as RestEndpointMethodTypes['apps']['listInstallations']['response']);
+
+    octokit.apps.createInstallationAccessToken.mockResolvedValueOnce({
+      data: {
+        expires_at: DateTime.local().plus({ hour: 1 }).toString(),
+        token: 'secret_token',
+      },
+    } as RestEndpointMethodTypes['apps']['createInstallationAccessToken']['response']);
+
+    const { token, headers } = await github.getCredentials({
+      url: 'https://github.com/backstage',
+    });
+
+    expect(headers).toEqual({ Authorization: 'Bearer secret_token' });
+    expect(token).toEqual('secret_token');
+  });
 });
