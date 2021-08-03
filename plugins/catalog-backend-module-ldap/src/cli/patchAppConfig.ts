6f708641ba922c30ab4bae166bf0f3d3717d32bd
Tejas Kumar <tejask@spotify.com>
2021-08-03 11:25:37 +0200

    Run Prettier
    Signed-off-by: Tejas Kumar <tejask@spotify.com>

--- plugins/catalog-backend-module-ldap/src/cli/patchAppConfig.ts
+++ plugins/catalog-backend-module-ldap/src/cli/patchAppConfig.ts
@@ -25,118 +25,116 @@ type Params = {
   groupFilter: string;
 };
 
-export const patchAppConfig =
-  ({
-    bindDn,
-    ldapHostUrl,
-    groupBaseDn,
-    groupFilter,
-    userBaseDn,
-    userFilter,
-  }: Params) =>
-  (
-    filePath: string,
-    readFile: (
-      path: PathLike | number,
-      options: { encoding: BufferEncoding; flag?: string } | BufferEncoding,
-    ) => string = readFileSync,
-    writeFile = writeFileSync,
-  ) => {
-    const appConfig = readFile(filePath, 'utf-8');
-    const doc = parseDocument(appConfig);
-    const config = parse(appConfig);
-    const locationsNode = doc.createNode({
-      type: 'ldap-org',
-      target: ldapHostUrl,
-    });
-    const ldapProcessor = {
-      providers: [
-        {
-          target: ldapHostUrl,
-          bind: { dn: bindDn, secret: '${LDAP_SECRET}' },
-          users: {
-            dn: userBaseDn || 'Base DN to search for users goes here',
-            options: {
-              paged: true,
-              filter: userFilter || '(objectclass=spotifyEmployee)',
-              scope: 'sub',
-            },
-          },
-          groups: {
-            dn: groupBaseDn || 'Base DN to search for groups goes here',
-            options: {
-              paged: true,
-              filter: groupFilter || '(objectclass=spotifyEmployee)',
-              scope: 'sub',
-            },
+export const patchAppConfig = ({
+  bindDn,
+  ldapHostUrl,
+  groupBaseDn,
+  groupFilter,
+  userBaseDn,
+  userFilter,
+}: Params) => (
+  filePath: string,
+  readFile: (
+    path: PathLike | number,
+    options: { encoding: BufferEncoding; flag?: string } | BufferEncoding,
+  ) => string = readFileSync,
+  writeFile = writeFileSync,
+) => {
+  const appConfig = readFile(filePath, 'utf-8');
+  const doc = parseDocument(appConfig);
+  const config = parse(appConfig);
+  const locationsNode = doc.createNode({
+    type: 'ldap-org',
+    target: ldapHostUrl,
+  });
+  const ldapProcessor = {
+    providers: [
+      {
+        target: ldapHostUrl,
+        bind: { dn: bindDn, secret: '${LDAP_SECRET}' },
+        users: {
+          dn: userBaseDn || 'Base DN to search for users goes here',
+          options: {
+            paged: true,
+            filter: userFilter || '(objectclass=spotifyEmployee)',
+            scope: 'sub',
           },
         },
-      ],
-    };
-    const processorsNode = doc.createNode(ldapProcessor);
-
-    visit(doc, {
-      Pair: (_, pair) => {
-        if (!pair.key) {
-          return visit.SKIP;
-        }
-        // @ts-expect-error because `value` can be anything.
-        if (config.catalog.locations && pair.key.value === 'locations') {
-          // @ts-expect-error because `value` can be anything.
-          pair.value.items.push(locationsNode);
-          return visit.SKIP;
-        }
-
-        return undefined;
+        groups: {
+          dn: groupBaseDn || 'Base DN to search for groups goes here',
+          options: {
+            paged: true,
+            filter: groupFilter || '(objectclass=spotifyEmployee)',
+            scope: 'sub',
+          },
+        },
       },
-    });
-
-    visit(doc, {
-      Pair: (_, pair) => {
-        if (!pair.key) {
-          return visit.SKIP;
-        }
-        // @ts-expect-error because `value` can be anything.
-        if (pair.key.value !== 'catalog') {
-          return visit.SKIP;
-        }
-
-        // @ts-expect-error because `value` can be anything.
-        const doWeAlreadyHaveProcessors = pair.value.items.find(
-          (i: Pair<{ value: string }, unknown>) => i.key.value === 'processors',
-        );
-        if (doWeAlreadyHaveProcessors) {
-          // @ts-expect-error because `value` can be anything.
-          visit(pair.value, {
-            Pair: (__, subpair) => {
-              if (!subpair.key) {
-                return visit.SKIP;
-              }
-
-              // @ts-expect-error because `value` can be anything.
-              if (subpair.key.value !== 'processors') {
-                return visit.SKIP;
-              }
-
-              // @ts-expect-error because `value` can be anything.
-              subpair.value.set('ldapOrg', processorsNode);
-              return visit.SKIP;
-            },
-          });
-          return visit.SKIP;
-        }
-        // @ts-expect-error because `value` can be anything.
-        pair.value.items.push(
-          doc.createPair(
-            'processors',
-            doc.createPair('ldapOrg', doc.createNode(ldapProcessor)),
-          ),
-        );
-        return visit.SKIP;
-      },
-    });
-
-    const output = doc.toString();
-    writeFile(filePath, output);
-    return output;
+    ],
   };
+  const processorsNode = doc.createNode(ldapProcessor);
+
+  visit(doc, {
+    Pair: (_, pair) => {
+      if (!pair.key) {
+        return visit.SKIP;
+      }
+      // @ts-expect-error because `value` can be anything.
+      if (config.catalog.locations && pair.key.value === 'locations') {
+        // @ts-expect-error because `value` can be anything.
+        pair.value.items.push(locationsNode);
+        return visit.SKIP;
+      }
+
+      return undefined;
+    },
+  });
+
+  visit(doc, {
+    Pair: (_, pair) => {
+      if (!pair.key) {
+        return visit.SKIP;
+      }
+      // @ts-expect-error because `value` can be anything.
+      if (pair.key.value !== 'catalog') {
+        return visit.SKIP;
+      }
+
+      // @ts-expect-error because `value` can be anything.
+      const doWeAlreadyHaveProcessors = pair.value.items.find(
+        (i: Pair<{ value: string }, unknown>) => i.key.value === 'processors',
+      );
+      if (doWeAlreadyHaveProcessors) {
+        // @ts-expect-error because `value` can be anything.
+        visit(pair.value, {
+          Pair: (__, subpair) => {
+            if (!subpair.key) {
+              return visit.SKIP;
+            }
+
+            // @ts-expect-error because `value` can be anything.
+            if (subpair.key.value !== 'processors') {
+              return visit.SKIP;
+            }
+
+            // @ts-expect-error because `value` can be anything.
+            subpair.value.set('ldapOrg', processorsNode);
+            return visit.SKIP;
+          },
+        });
+        return visit.SKIP;
+      }
+      // @ts-expect-error because `value` can be anything.
+      pair.value.items.push(
+        doc.createPair(
+          'processors',
+          doc.createPair('ldapOrg', doc.createNode(ldapProcessor)),
+        ),
+      );
+      return visit.SKIP;
+    },
+  });
+
+  const output = doc.toString();
+  writeFile(filePath, output);
+  return output;
+};
