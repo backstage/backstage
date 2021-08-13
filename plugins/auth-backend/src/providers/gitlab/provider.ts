f218207913fde5cbbcdd4f14d0ef2284f75b4911
Tom Opdebeeck <tom.opdebeeck@studiohyperdrive.be>
2021-08-13 11:53:05 +0200

    fix(auth-backend): cleanup profile transform * make more consistent with github flow
    Signed-off-by: Tom Opdebeeck <tom.opdebeeck@studiohyperdrive.be>

--- plugins/auth-backend/src/providers/gitlab/provider.ts
+++ plugins/auth-backend/src/providers/gitlab/provider.ts
@@ -90,13 +90,9 @@ export const gitlabDefaultSignInResolver: SignInResolver<OAuthResult> = async (
   info,
   ctx,
 ) => {
-  const { profile } = info;
+  const { result } = info;
 
-  if (!profile.email) {
-    throw new Error('Profile contained no email');
-  }
-
-  const id = extractGitLabUserId(profile);
+  const id = result.fullProfile.username || result.fullProfile.id;
 
   const token = await ctx.tokenIssuer.issueToken({
     claims: { sub: id, ent: [`user:default/${id}`] },
@@ -108,9 +104,18 @@ export const gitlabDefaultSignInResolver: SignInResolver<OAuthResult> = async (
 export const gitlabDefaultAuthHandler: AuthHandler<OAuthResult> = async ({
   fullProfile,
   params,
-}) => ({
-  profile: makeProfileInfo(fullProfile, params.id_token),
-});
+}) => {
+  fullProfile.photos = [
+    ...(fullProfile.photos ?? []),
+    ...((fullProfile as FullProfile).avatarUrl
+      ? [{ value: (fullProfile as FullProfile).avatarUrl as string }]
+      : []),
+  ];
+
+  return {
+    profile: makeProfileInfo(fullProfile, params.id_token),
+  };
+};
 
 export class GitlabAuthProvider implements OAuthHandlers {
   private readonly _strategy: GitlabStrategy;
@@ -225,30 +230,6 @@ export class GitlabAuthProvider implements OAuthHandlers {
     }
 
     return response;
-  }
-
-  private transformResult(result: OAuthResult): OAuthResult {
-    const { fullProfile, ...authResult } = result;
-
-    fullProfile.photos = [
-      ...(fullProfile.photos ?? []),
-      ...((fullProfile as FullProfile).avatarUrl
-        ? [{ value: (fullProfile as FullProfile).avatarUrl as string }]
-        : []),
-    ];
-
-    const profile = makeProfileInfo(fullProfile);
-
-    if (!profile.email) {
-      throw new Error('Profile contained no email');
-    }
-
-    fullProfile.id = extractGitLabUserId(profile);
-
-    return {
-      ...authResult,
-      fullProfile,
-    };
   }
 }
 
