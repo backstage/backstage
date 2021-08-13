c097e8893beb95f4837d6974a8da099b30c7e43b
Tom Opdebeeck <tom.opdebeeck@studiohyperdrive.be>
2021-08-13 11:35:30 +0200

    fix: revert changes in github provider
    Signed-off-by: Tom Opdebeeck <tom.opdebeeck@studiohyperdrive.be>

--- plugins/auth-backend/src/providers/github/provider.ts
+++ plugins/auth-backend/src/providers/github/provider.ts
@@ -79,7 +79,7 @@ export class GithubAuthProvider implements OAuthHandlers {
     const profile = makeProfileInfo(
       {
         ...fullProfile,
-        id: fullProfile.id,
+        id: fullProfile.username || fullProfile.id,
         displayName:
           fullProfile.displayName || fullProfile.username || fullProfile.id,
       },
@@ -95,7 +95,7 @@ export class GithubAuthProvider implements OAuthHandlers {
           expiresInSeconds: params.expires_in,
         },
         backstageIdentity: {
-          id: fullProfile.id,
+          id: fullProfile.username || fullProfile.id,
         },
       },
     };
