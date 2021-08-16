c458408e5c96a14796247cace9ba4bfc2d3832e1
Patrik Oldsberg <poldsberg@gmail.com>
2021-08-16 13:31:52 +0200

    backend-common: remove hardcoded size check and replace with filter param
    Signed-off-by: Patrik Oldsberg <poldsberg@gmail.com>

--- packages/backend-common/src/reading/types.ts
+++ packages/backend-common/src/reading/types.ts
@@ -58,7 +58,7 @@ export type ReadTreeOptions = {
    *
    * If no filter is provided all files are extracted.
    */
-  filter?(path: string): boolean;
+  filter?(path: string, info?: { size: number }): boolean;
 
   /**
    * An etag can be provided to check whether readTree's response has changed from a previous execution.
@@ -114,7 +114,7 @@ export type FromArchiveOptions = {
   // etag of the blob
   etag: string;
   // Filter passed on from the ReadTreeOptions
-  filter?: (path: string) => boolean;
+  filter?: (path: string, info?: { size: number }) => boolean;
 };
 
 export interface ReadTreeResponseFactory {
