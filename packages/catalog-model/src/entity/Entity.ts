13dc3735cc345783cac068027a49b23a048daf86
Fredrik Adelöw <freben@gmail.com>
2021-08-19 14:52:05 +0200

    Add the metadata.title field to all kinds
    Signed-off-by: Fredrik Adelöw <freben@gmail.com>

--- packages/catalog-model/src/entity/Entity.ts
+++ packages/catalog-model/src/entity/Entity.ts
@@ -103,7 +103,13 @@ export type EntityMeta = JsonObject & {
    * The name of the entity.
    *
    * Must be unique within the catalog at any given point in time, for any
-   * given namespace + kind pair.
+   * given namespace + kind pair. This value is part of the technical
+   * identifier of the entity, and as such it will appear in URLs, database
+   * tables, entity references, and similar. It is subject to restrictions
+   * regarding what characters are allowed.
+   *
+   * If you want to use a different, more human readable string with fewer
+   * restrictions on it in user interfaces, see the `title` field below.
    */
   name: string;
 
@@ -111,6 +117,23 @@ export type EntityMeta = JsonObject & {
    * The namespace that the entity belongs to.
    */
   namespace?: string;
+
+  /**
+   * A nice display name of the entity, to be presented in user interfaces
+   * instead of the `name` property above, when available.
+   *
+   * This field is sometimes useful when the `name` is cumbersome or ends up
+   * being perceived as overly technical. The title generally does not have
+   * as stringent format requirements on it, so it may contain special
+   * characters and be more explanatory. Do keep it very short though, and
+   * avoid situations where a title can be confused with the name of another
+   * entity, or where two entities share a title.
+   *
+   * Note that this is only for display purposes, and may be ignored by some
+   * parts of the code. Entity references still always make use of the `name`
+   * property, not the title.
+   */
+  title?: string;
 
   /**
    * A short (typically relatively few words, on one line) description of the
