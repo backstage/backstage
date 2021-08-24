cfcb486aa094617dbcab1754435d2bae3fb3b6c0
Dominik Henneke <dominik.henneke@sda-se.com>
2021-08-24 10:56:43 +0200

    Add system icons for the built-in entity types and use them in the entity list of the `catalog-import` plugin
    Signed-off-by: Dominik Henneke <dominik.henneke@sda-se.com>

--- packages/core-app-api/src/app/icons.tsx
+++ packages/core-app-api/src/app/icons.tsx
@@ -15,44 +15,59 @@
  */
 
 import { IconComponent } from '@backstage/core-plugin-api';
-import MuiMenuBookIcon from '@material-ui/icons/MenuBook';
+import MuiApartmentIcon from '@material-ui/icons/Apartment';
 import MuiBrokenImageIcon from '@material-ui/icons/BrokenImage';
+import MuiCategoryIcon from '@material-ui/icons/Category';
 import MuiChatIcon from '@material-ui/icons/Chat';
 import MuiDashboardIcon from '@material-ui/icons/Dashboard';
+import MuiDocsIcon from '@material-ui/icons/Description';
 import MuiEmailIcon from '@material-ui/icons/Email';
+import MuiExtensionIcon from '@material-ui/icons/Extension';
 import MuiGitHubIcon from '@material-ui/icons/GitHub';
 import MuiHelpIcon from '@material-ui/icons/Help';
+import MuiLocationOnIcon from '@material-ui/icons/LocationOn';
+import MuiMemoryIcon from '@material-ui/icons/Memory';
+import MuiMenuBookIcon from '@material-ui/icons/MenuBook';
 import MuiPeopleIcon from '@material-ui/icons/People';
 import MuiPersonIcon from '@material-ui/icons/Person';
 import MuiWarningIcon from '@material-ui/icons/Warning';
-import MuiDocsIcon from '@material-ui/icons/Description';
 
 type AppIconsKey =
+  | 'api'
   | 'brokenImage'
   | 'catalog'
   | 'chat'
+  | 'component'
   | 'dashboard'
   | 'docs'
+  | 'domain'
   | 'email'
   | 'github'
   | 'group'
   | 'help'
+  | 'location'
   | 'user'
+  | 'system'
   | 'warning';
 
 export type AppIcons = { [key in AppIconsKey]: IconComponent };
 
 export const defaultAppIcons: AppIcons = {
+  api: MuiExtensionIcon,
   brokenImage: MuiBrokenImageIcon,
   // To be confirmed: see https://github.com/backstage/backstage/issues/4970
   catalog: MuiMenuBookIcon,
   chat: MuiChatIcon,
+  component: MuiMemoryIcon,
   dashboard: MuiDashboardIcon,
   docs: MuiDocsIcon,
+  domain: MuiApartmentIcon,
   email: MuiEmailIcon,
   github: MuiGitHubIcon,
   group: MuiPeopleIcon,
   help: MuiHelpIcon,
+  location: MuiLocationOnIcon,
   user: MuiPersonIcon,
+  system: MuiCategoryIcon,
   warning: MuiWarningIcon,
 };
