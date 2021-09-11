87e63d8181fcfe986e29544f21016eef5f00e24c
Ryan Vazquez <ryanv@spotify.com>
2021-09-11 14:57:07 -0400

    fix: remove title and wrapper margins; add subtitle margins
    Signed-off-by: Ryan Vazquez <ryanv@spotify.com>

--- packages/core-components/src/layout/Header/Header.tsx
+++ packages/core-components/src/layout/Header/Header.tsx
@@ -41,7 +41,6 @@ const useStyles = makeStyles<BackstageTheme>(theme => ({
   leftItemsBox: {
     maxWidth: '100%',
     flexGrow: 1,
-    marginBottom: theme.spacing(1),
   },
   rightItemsBox: {
     width: 'auto',
@@ -50,11 +49,13 @@ const useStyles = makeStyles<BackstageTheme>(theme => ({
     color: theme.palette.bursts.fontColor,
     wordBreak: 'break-all',
     fontSize: 'calc(24px + 6 * ((100vw - 320px) / 680))',
-    marginBottom: theme.spacing(1),
+    marginBottom: 0,
   },
   subtitle: {
     color: 'rgba(255, 255, 255, 0.8)',
     lineHeight: '1.0em',
+    display: 'inline-block', // prevents margin collapse of adjacent siblings
+    marginTop: theme.spacing(1),
   },
   type: {
     textTransform: 'uppercase',
