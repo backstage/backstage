8424abe9897d2bd18e9ef08045ef053b373d1c36
Patrik Oldsberg <poldsberg@gmail.com>
2021-09-17 19:53:48 +0200

    docs/layout: experiment 6 - bit of implementation
    Signed-off-by: Patrik Oldsberg <poldsberg@gmail.com>

--- plugins/welcome/src/components/Experiment/Experiment6.tsx
+++ plugins/welcome/src/components/Experiment/Experiment6.tsx
@@ -14,13 +14,137 @@
  * limitations under the License.
  */
 
-import React from 'react';
+import React, {
+  createContext,
+  ReactNode,
+  useCallback,
+  useContext,
+  useRef,
+  useState,
+} from 'react';
+import { InfoCard } from '@backstage/core-components';
+import { Grid } from '@material-ui/core';
+
+function MyCard() {
+  return <div style={{ background: 'blue' }}>Hello I am a card</div>;
+}
+
+type LayoutSize = {
+  width: number;
+  height: number;
+};
+
+function useResizeObserver(onResize: (size: LayoutSize) => void) {
+  const [observer] = useState(
+    () =>
+      new ResizeObserver(entries => {
+        const [entry] = entries;
+
+        if (entry) {
+          const { width, height } = entry.contentRect;
+          onResize({ width, height });
+        }
+      }),
+  );
+
+  const elementRef = useRef<HTMLDivElement | null>(null);
+
+  const setRef = useCallback(
+    (element: HTMLDivElement | null) => {
+      if (elementRef.current) {
+        observer.unobserve(elementRef.current);
+      }
+
+      if (element) {
+        observer.observe(element);
+      }
+
+      elementRef.current = element;
+    },
+    [observer],
+  );
+
+  return [setRef];
+}
+
+function LayoutContract({ children }: { children: ReactNode }) {
+  const [size, setSize] = useState({ width: 0, height: 0 });
+  const [setResizeRef] = useResizeObserver(setSize);
+
+  return (
+    <div
+      style={{
+        background: 'red',
+        width: '100%',
+        height: '100%',
+        minWidth: '100%',
+        maxWidth: '100%',
+        minHeight: '100%',
+        maxHeight: '100%',
+        position: 'relative',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'stretch',
+        justifyContent: 'stretch',
+      }}
+      ref={setResizeRef}
+    >
+      <div style={{ position: 'absolute', right: 0, bottom: 0 }}>
+        I am {size.width} x {size.height}
+      </div>
+      {children}
+    </div>
+  );
+}
+
+type Component = (props: { children: ReactNode }) => JSX.Element | null;
+
+const CardLayoutContext = createContext<Component | undefined>(undefined);
+
+function InfoCardLayout({ children }: { children: ReactNode }) {
+  return (
+    <CardLayoutContext.Provider
+      value={props => <InfoCard variant="gridItem" children={props.children} />}
+    >
+      {children}
+    </CardLayoutContext.Provider>
+  );
+}
+
+function createCardExtension<T extends {}>(
+  Component: (props: T) => JSX.Element | null,
+): (props: T) => JSX.Element {
+  return props => {
+    const CardComponent = useContext(CardLayoutContext);
+    if (!CardComponent) {
+      throw new Error('No CardComponent');
+    }
+    return (
+      <CardComponent>
+        <LayoutContract>
+          <Component {...props} />
+        </LayoutContract>
+      </CardComponent>
+    );
+  };
+}
+
+const MyCardExtension = createCardExtension(MyCard);
 
 export function Experiment6() {
   return (
     <div>
       <h1>Experiment 6</h1>
-      <p>This is an experiment.</p>
+      <InfoCardLayout>
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <MyCardExtension />
+          </Grid>
+          <Grid item xs={6}>
+            <div style={{ background: 'green', height: 200 }} />
+          </Grid>
+        </Grid>
+      </InfoCardLayout>
     </div>
   );
 }
