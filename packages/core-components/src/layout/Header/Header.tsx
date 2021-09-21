3e3869a3c363d290f5696600f17e45b8f37f1b80
Daniel Ortiz Lira <dortiz@vmware.com>
2021-09-21 11:45:56 -0500

    Expose layout components to material-ui overrides system by providing a name
    Signed-off-by: Daniel Ortiz Lira <dortiz@vmware.com>

--- packages/core-components/src/layout/Header/Header.tsx
+++ packages/core-components/src/layout/Header/Header.tsx
@@ -22,64 +22,78 @@ import { Helmet } from 'react-helmet';
 import { Link } from '../../components/Link';
 import { Breadcrumbs } from '../Breadcrumbs';
 
-const useStyles = makeStyles<BackstageTheme>(theme => ({
-  header: {
-    gridArea: 'pageHeader',
-    padding: theme.spacing(3),
-    width: '100%',
-    boxShadow: '0 0 8px 3px rgba(20, 20, 20, 0.3)',
-    position: 'relative',
-    zIndex: 100,
-    display: 'flex',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    alignItems: 'center',
-    backgroundImage: theme.page.backgroundImage,
-    backgroundPosition: 'center',
-    backgroundSize: 'cover',
-  },
-  leftItemsBox: {
-    maxWidth: '100%',
-    flexGrow: 1,
-  },
-  rightItemsBox: {
-    width: 'auto',
-  },
-  title: {
-    color: theme.palette.bursts.fontColor,
-    wordBreak: 'break-all',
-    fontSize: 'calc(24px + 6 * ((100vw - 320px) / 680))',
-    marginBottom: 0,
-  },
-  subtitle: {
-    color: 'rgba(255, 255, 255, 0.8)',
-    lineHeight: '1.0em',
-    display: 'inline-block', // prevents margin collapse of adjacent siblings
-    marginTop: theme.spacing(1),
-  },
-  type: {
-    textTransform: 'uppercase',
-    fontSize: 11,
-    opacity: 0.8,
-    marginBottom: theme.spacing(1),
-    color: theme.palette.bursts.fontColor,
-  },
-  breadcrumb: {
-    fontSize: 'calc(15px + 1 * ((100vw - 320px) / 680))',
-    color: theme.palette.bursts.fontColor,
-  },
-  breadcrumbType: {
-    fontSize: 'inherit',
-    opacity: 0.7,
-    marginRight: -theme.spacing(0.3),
-    marginBottom: theme.spacing(0.3),
-  },
-  breadcrumbTitle: {
-    fontSize: 'inherit',
-    marginLeft: -theme.spacing(0.3),
-    marginBottom: theme.spacing(0.3),
-  },
-}));
+export type HeaderClassKey =
+  | 'header'
+  | 'leftItemsBox'
+  | 'rightItemsBox'
+  | 'title'
+  | 'subtitle'
+  | 'type'
+  | 'breadcrumb'
+  | 'breadcrumbType'
+  | 'breadcrumbTitle';
+
+const useStyles = makeStyles<BackstageTheme>(
+  theme => ({
+    header: {
+      gridArea: 'pageHeader',
+      padding: theme.spacing(3),
+      width: '100%',
+      boxShadow: '0 0 8px 3px rgba(20, 20, 20, 0.3)',
+      position: 'relative',
+      zIndex: 100,
+      display: 'flex',
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      alignItems: 'center',
+      backgroundImage: theme.page.backgroundImage,
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+    },
+    leftItemsBox: {
+      maxWidth: '100%',
+      flexGrow: 1,
+    },
+    rightItemsBox: {
+      width: 'auto',
+    },
+    title: {
+      color: theme.palette.bursts.fontColor,
+      wordBreak: 'break-all',
+      fontSize: 'calc(24px + 6 * ((100vw - 320px) / 680))',
+      marginBottom: 0,
+    },
+    subtitle: {
+      color: 'rgba(255, 255, 255, 0.8)',
+      lineHeight: '1.0em',
+      display: 'inline-block', // prevents margin collapse of adjacent siblings
+      marginTop: theme.spacing(1),
+    },
+    type: {
+      textTransform: 'uppercase',
+      fontSize: 11,
+      opacity: 0.8,
+      marginBottom: theme.spacing(1),
+      color: theme.palette.bursts.fontColor,
+    },
+    breadcrumb: {
+      fontSize: 'calc(15px + 1 * ((100vw - 320px) / 680))',
+      color: theme.palette.bursts.fontColor,
+    },
+    breadcrumbType: {
+      fontSize: 'inherit',
+      opacity: 0.7,
+      marginRight: -theme.spacing(0.3),
+      marginBottom: theme.spacing(0.3),
+    },
+    breadcrumbTitle: {
+      fontSize: 'inherit',
+      marginLeft: -theme.spacing(0.3),
+      marginBottom: theme.spacing(0.3),
+    },
+  }),
+  { name: 'BackstageHeader' },
+);
 
 type HeaderStyles = ReturnType<typeof useStyles>;
 
@@ -195,20 +209,22 @@ export function Header(props: PropsWithChildren<Props>) {
     <>
       <Helmet titleTemplate={titleTemplate} defaultTitle={defaultTitle} />
       <header style={style} className={classes.header}>
-        <Box className={classes.leftItemsBox}>
-          <TypeFragment
-            classes={classes}
-            type={type}
-            typeLink={typeLink}
-            pageTitle={pageTitle}
-          />
-          <TitleFragment
-            classes={classes}
-            pageTitle={pageTitle}
-            tooltip={tooltip}
-          />
-          <SubtitleFragment classes={classes} subtitle={subtitle} />
-        </Box>
+        <div style={{ border: '2px solid green' }}>
+          <Box className={classes.leftItemsBox}>
+            <TypeFragment
+              classes={classes}
+              type={type}
+              typeLink={typeLink}
+              pageTitle={pageTitle}
+            />
+            <TitleFragment
+              classes={classes}
+              pageTitle={pageTitle}
+              tooltip={tooltip}
+            />
+            <SubtitleFragment classes={classes} subtitle={subtitle} />
+          </Box>
+        </div>
         <Grid container className={classes.rightItemsBox} spacing={4}>
           {children}
         </Grid>
