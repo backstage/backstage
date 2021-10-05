30cd80217f630e13bcdc7c4a93a1c9d0ca2e1f46
Brandon S <brandon.sheppard@va.gov>
2021-10-05 15:29:07 -0400

    Adding ib image (#94)
    * pulling local IB image
    
    * removed extra text in dockerfile
    
    * remove git workflows, dependabot, etc.
    
    * troubleshooting - getting current user
    
    * troubleshooting - change to root user for adding app files
    
    * troubleshooting - change /app owner to node
    
    * troubleshooting - expose 80
    
    * updated exposed port
    
    * adding ib image to local and updating local tech doc config
    
    * updated local dockerfile
    
    * updated dockerfile

--- scripts/start_local.sh
+++ scripts/start_local.sh
@@ -1,6 +1,12 @@
+# build the application
 yarn install
 yarn tsc
 yarn build
+
+# build the container image
+# ** Do we want to map local code repo as volume in container? **
 docker build -t bih/bih-backend . -f packages/backend/Dockerfile-local --no-cache
+
+# build docker env and bring online
 docker-compose build
 docker-compose up
