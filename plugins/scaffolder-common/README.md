445d222a487d62629d3226addc3fcbcca90df055
Patrik Oldsberg <poldsberg@gmail.com>
2021-10-05 13:28:33 +0200

    added scaffolder-common package with beta3 entity definitions
    Co-authored-by: Fredrik Adelöw <freben@gmail.com>
    Co-authored-by: blam <ben@blam.sh>
    Co-authored-by: Johan Haals <johan.haals@gmail.com>
    Signed-off-by: Patrik Oldsberg <poldsberg@gmail.com>

---
+++ plugins/scaffolder-common/README.md
@@ -0,0 +1,3 @@
+# @backstage/plugin-scaffolder-common
+
+Common types and functionalities for the scaffolder, to be shared between scaffolder and scaffolder-backend.
---
+++ plugins/scaffolder-common/src/index.ts
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2020 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Common functionalities for the scaffolder, to be shared between scaffolder and scaffolder-backend plugin
+ *
+ * @packageDocumentation
+ */
+
+// The `{ default as ...}` re-export does not seem to work for json in tests
+import templateEntityV1beta3Schema from './Template.v1beta3.schema.json';
+
+export type { TemplateEntityV1beta3 } from './TemplateEntityV1beta3';
+export { templateEntityV1beta3Schema };
