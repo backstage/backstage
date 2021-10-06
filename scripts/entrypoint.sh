e22ffe5b58fbadb0d2b722d11f2e0e7711ad2eb8
Sheppard <Brandon.Sheppard@va.gov>
2021-10-06 13:43:13 -0400

    testing envconsul by passing some KVs at startup

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,6 +3,8 @@
 # app container entrypoint script for starting envconsul as
 # a sub-process then running the application
 
-envconsul -config "config.hcl" env
+envconsul -config "config.hcl" env \
+ADDRESS=1.2.3.4 \
+PORT=55
 
 node packages/backend --config app-config.yaml
