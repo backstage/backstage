5c079092ae8ece04e23f0dabf7120e5385a9a694
Sheppard <Brandon.Sheppard@va.gov>
2021-10-06 12:47:17 -0400

    testing envconsul cli form entrypoint

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+
+# app container entrypoint script for starting envconsul as
+# a sub-process then running the application
+
+envconsul -h
+
 node packages/backend --config app-config.yaml
