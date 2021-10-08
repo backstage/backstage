8ca030d8223d7d06f7f85917af3c7187332870f0
Quincy Jones <quincy.jones1@va.gov>
2021-10-08 18:29:57 -0400

    testing entrypoint script

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,6 +3,4 @@
 # app container entrypoint script for starting envconsul as
 # a sub-process then running the application
 
-envconsul -log-level debug -config "config.hcl" env
-
-node packages/backend --config app-config.yaml
+/bin/envconsul -consul-addr=${CONSUL_ADDR} -consul-token=${CONSUL_TOKEN} -prefix="config/bih/development" node packages/backend --config app-config.yaml
