8f3e411d26ef67d23f5ea764e1ac044f060738ab
Eric Peterson <ericpeterson@spotify.com>
2021-10-10 20:46:49 +0200

    Update create-app.
    Signed-off-by: Eric Peterson <ericpeterson@spotify.com>

--- packages/create-app/templates/default-app/packages/backend/src/plugins/search.ts
+++ packages/create-app/templates/default-app/packages/backend/src/plugins/search.ts
@@ -5,8 +5,8 @@ import {
   LunrSearchEngine,
 } from '@backstage/plugin-search-backend-node';
 import { PluginEnvironment } from '../types';
-import { DefaultCatalogCollator } from '@backstage/plugin-catalog-backend';
-import { DefaultTechDocsCollator } from '@backstage/plugin-techdocs-backend';
+import { DefaultCatalogCollatorFactory } from '@backstage/plugin-catalog-backend';
+import { DefaultTechDocsCollatorFactory } from '@backstage/plugin-techdocs-backend';
 
 export default async function createPlugin({
   logger,
@@ -22,7 +22,7 @@ export default async function createPlugin({
   // collator gathers entities from the software catalog.
   indexBuilder.addCollator({
     defaultRefreshIntervalSeconds: 600,
-    collator: DefaultCatalogCollator.fromConfig(config, {
+    factory: DefaultCatalogCollatorFactory.fromConfig(config, {
       discovery,
       tokenManager,
     }),
@@ -31,7 +31,7 @@ export default async function createPlugin({
   // collator gathers entities from techdocs.
   indexBuilder.addCollator({
     defaultRefreshIntervalSeconds: 600,
-    collator: DefaultTechDocsCollator.fromConfig(config, {
+    factory: DefaultTechDocsCollatorFactory.fromConfig(config, {
       discovery,
       logger,
       tokenManager,
