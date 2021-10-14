8ecfc2556b32c824b1e44c8f1bf1b6b7efb78666
Julio Zynger <julio.zynger@soundcloud.com>
2021-10-14 15:39:21 +0200

    Prettier

--- packages/cli/src/commands/index.ts
+++ packages/cli/src/commands/index.ts
@@ -193,7 +193,10 @@ export function registerCommands(program: CommanderStatic) {
   program
     .command('versions:bump')
     .description('Bump Backstage packages to the latest versions')
-    .option('--prefix <prefix>', 'npm packages prefix to bump, defaults to @backstage')
+    .option(
+      '--prefix <prefix>',
+      'npm packages prefix to bump, defaults to @backstage',
+    )
     .action(lazy(() => import('./versions/bump').then(m => m.default)));
 
   program
