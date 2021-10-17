b81299a332b0e1554d368bdd30d46d7a7b67819e
Patrik Oldsberg <poldsberg@gmail.com>
2021-10-17 15:25:57 +0200

    techdocs-common: avoid global type declarations
    Signed-off-by: Patrik Oldsberg <poldsberg@gmail.com>

--- packages/techdocs-common/src/testUtils/types.ts
+++ packages/techdocs-common/src/testUtils/types.ts
@@ -14,9 +14,11 @@
  * limitations under the License.
  */
 
-declare module NodeJS {
-  interface Global {
-    rootDir: string;
-    storageFilesMock: IStorageFilesMock;
-  }
+export interface IStorageFilesMock {
+  emptyFiles(): void;
+  fileExists(targetPath: string): boolean;
+  readFile(targetPath: string): Buffer;
+  writeFile(targetPath: string, sourcePath: string): void;
+  writeFile(targetPath: string, sourceBuffer: Buffer): void;
+  writeFile(targetPath: string, source: string | Buffer): void;
 }
