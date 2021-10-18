874eee08b9ba4e12efc2b2774f34ec528f9e57ff
Mike Lewis <mtlewis@users.noreply.github.com>
2021-10-18 14:27:11 +0100

    authz: add useEntityPermission hook in plugin-catalog-react
    Signed-off-by: Mike Lewis <mtlewis@users.noreply.github.com>

---
+++ plugins/catalog-react/src/hooks/useEntityPermission.ts
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2021 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { stringifyEntityRef } from '@backstage/catalog-model';
+import { Permission } from '@backstage/permission-common';
+import { usePermission } from '@backstage/plugin-permission';
+import { useEntity } from './useEntity';
+
+export const useEntityPermission = (permission: Permission) => {
+  // TODO(mtlewis/orkohunter): what happens if there's no entity?
+  const { entity } = useEntity();
+
+  return usePermission(permission, stringifyEntityRef(entity));
+};
