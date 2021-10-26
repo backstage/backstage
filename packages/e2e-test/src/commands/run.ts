fbedcebe7cf3ed8713e4e7e215b29d7402d38643
blam <ben@blam.sh>
2021-10-26 11:59:00 +0200

    chore: turn on some debug logging
    Signed-off-by: blam <ben@blam.sh>

--- packages/e2e-test/src/commands/run.ts
+++ packages/e2e-test/src/commands/run.ts
@@ -357,6 +357,7 @@ async function testAppServe(pluginName: string, appDir: string) {
     for (let attempts = 1; ; attempts++) {
       try {
         const browser = new Browser();
+        browser.debug();
 
         await waitForPageWithText(browser, '/', 'My Company Catalog');
         await waitForPageWithText(
