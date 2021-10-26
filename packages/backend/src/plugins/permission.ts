c630a4b1bc847c45ee60ac8e228258dce82ba86e
Mike Lewis <mtlewis@users.noreply.github.com>
2021-10-26 23:14:25 +0100

    authz: rename PermissionHandler to PermissionPolicy
    Signed-off-by: Mike Lewis <mtlewis@users.noreply.github.com>

--- packages/backend/src/plugins/permission.ts
+++ packages/backend/src/plugins/permission.ts
@@ -15,7 +15,7 @@
  */
 
 import { createRouter } from '@backstage/plugin-permission-backend';
-import { SimplePermissionHandler } from '@backstage/plugin-permission-handler-simple';
+import { SimplePermissionPolicy } from '@backstage/plugin-permission-policy-simple';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
 
@@ -26,6 +26,6 @@ export default async function createPlugin(
   return await createRouter({
     logger,
     config,
-    permissionHandler: new SimplePermissionHandler(),
+    policy: new SimplePermissionPolicy(),
   });
 }
---
+++ plugins/permission-policy-simple/README.md
@@ -0,0 +1,9 @@
+# permission-policy-simple
+
+Welcome to the permission-policy-simple backend plugin!
+
+_This plugin was created through the Backstage CLI_
+
+## Getting started
+
+Your plugin has been added to the example app in this repository, meaning you'll be able to access it by running `yarn start` in the root directory, and then navigating to [/permission-policy-simple](http://localhost:3000/permission-policy-simple).
