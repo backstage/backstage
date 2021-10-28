225502847865dede65168cd04910c8a6ebd1a2b3
Aaron Wise <Wisertown@users.noreply.github.com>
2021-10-28 15:05:30 -0400

    done with styling

--- packages/app/src/components/custom/DashboardTable.tsx
+++ packages/app/src/components/custom/DashboardTable.tsx
@@ -2,55 +2,93 @@ import React from 'react';
 /* import { Link as RouterLink } from 'react-router-dom';
 import { useAsync } from 'react-use'; */
 import { Table, TableColumn } from '@backstage/core-components';
-
+import { makeStyles } from '@material-ui/core/styles';
+import { Grid, Typography } from '@material-ui/core';
 /* import { getEntitySourceLocation } from '@backstage/catalog-model';
 import { Link, Tab } from '@material-ui/core';
 import ExternalLinkIcon from '@material-ui/icons/Launch'; */
+const useStyles = makeStyles({
+  gridParent: {
+    height: '100%',
+  },
+  heading: {
+    marginTop: '0px',
+  },
+  linkParent: {
+    width: '100%',
+    textAlign: 'center',
+  },
+  link: {
+    color: '#112e51',
+    textDecoration: 'underline',
+  },
+});
 
 export const DashboardTable = () => {
-    const columns: TableColumn[] = [
-        {
-            title: 'ProductInfo',
-            field: 'prodInfo',
-        },
-        {
-            title: 'CICD',
-            field: 'cicd',
-        },
-        {
-            title: 'Security',
-            field: 'security',
-        },
-        {
-            title: 'Other',
-            field: 'other',
-        }
-    ];
-    const testData = [
-        {
-            prodInfo: "ProdInfoTEST",
-            cicd: null,
-            security: null,
-            other: null,
-        },
-        {
-            prodInfo: 'test again',
-            cicd:"cicdTest"
-        },
-        {
-            security: "securitytest"
-        },
-        {
-            other: 'othertest'
-        }
+  const classes = useStyles();
+  const columns: TableColumn[] = [
+    {
+      title: 'ProductInfo',
+      field: 'prodInfo',
+    },
+    {
+      title: 'CICD',
+      field: 'cicd',
+    },
+    {
+      title: 'Security',
+      field: 'security',
+    },
+    {
+      title: 'Other',
+      field: 'other',
+    },
+  ];
+  const testData = [
+    {
+      prodInfo: (
+        <Grid>
+          <h3 className={classes.heading}>Important Links</h3>
+          <Grid className={classes.linkParent}>
+            <a href="#" className={classes.link}>
+              https://Link1.com
+            </a>
+          </Grid>
+        </Grid>
+      ),
+      cicd: null,
+      security: null,
+      other: (
+        <Grid>
+          <Typography>3 of 4 pods on</Typography>
+        </Grid>
+      ),
+    },
+    {
+      prodInfo: (
+        <Grid>
+          <h3 className={classes.heading}>Product Contact List</h3>
+          <Grid className={classes.linkParent}>
+            <Typography>James Madison: JamesM@president.com</Typography>
+          </Grid>
+        </Grid>
+      ),
+      cicd: 'cicdTest',
+    },
+    {
+      security: 'securitytest',
+    },
+    {
+      other: 'othertest',
+    },
+  ];
 
-    ];
-    
-    return(
-        <Table
-        title="Current Status"
-        columns={columns}
-        data={testData}
-       />
-    )
-}
+  return (
+    <Table
+      title="Current Status"
+      columns={columns}
+      data={testData}
+      options={{ paging: false }}
+    />
+  );
+};
--- plugins/dashboard/src/components/DashboardComponent/DashboardComponent.tsx
+++ plugins/dashboard/src/components/DashboardComponent/DashboardComponent.tsx
@@ -1,28 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Grid } from '@material-ui/core';
-import {
-  Header,
-  Page,
-  Content,
-} from '@backstage/core-components';
+import { Header, Page, Content } from '@backstage/core-components';
+import { makeStyles } from '@material-ui/core/styles';
 import { DashboardTable } from '../../../../../packages/app/src/components/custom';
 import { LeftNavComponent } from '../LeftNavComponent';
 
-export const DashboardComponent = () => (
-  <Page themeId="tool">
-    <Header title="DEV OPS Dashboard">
-    </Header>
-    <Content>
-      <Grid container > 
-    <Grid lg={2} direction="column">
-      <LeftNavComponent />
-    </Grid>
-      <Grid lg={10} direction="column">
-        <Grid item>
-            <DashboardTable />
+const useStyles = makeStyles({
+  gridPadding: {
+    padding: '10px',
+  },
+});
+
+export const DashboardComponent = () => {
+  const classes = useStyles();
+  const [table, setTable] = useState('home');
+
+  return (
+    <Page themeId="tool">
+      <Header title="DevOps Dashboard"></Header>
+      <Content>
+        <Grid container>
+          <Grid
+            container
+            lg={2}
+            item={true}
+            direction="column"
+            className={classes.gridPadding}
+          >
+            <LeftNavComponent tableProp={{ table, setTable }} />
+          </Grid>
+          <Grid
+            container
+            lg={10}
+            item={true}
+            direction="column"
+            className={classes.gridPadding}
+          >
+            <Grid item>
+              <DashboardTable />
+            </Grid>
+          </Grid>
         </Grid>
-      </Grid>
-      </Grid>
-    </Content>
-  </Page>
-);
+      </Content>
+    </Page>
+  );
+};
