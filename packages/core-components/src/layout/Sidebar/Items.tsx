4891a8f2e6f8e3c0993b6826676ea97bf0592895
Dede Hamzah <dehamzah@gmail.com>
2021-11-03 16:08:27 +0700

    Add Props Icon for Sidebar Item SidebarSearchField and Settings
    Signed-off-by: Dede Hamzah <dehamzah@gmail.com>

--- packages/core-components/src/layout/Sidebar/Items.tsx
+++ packages/core-components/src/layout/Sidebar/Items.tsx
@@ -293,11 +293,13 @@ export const SidebarItem = forwardRef<any, SidebarItemProps>((props, ref) => {
 type SidebarSearchFieldProps = {
   onSearch: (input: string) => void;
   to?: string;
+  icon?: IconComponent;
 };
 
 export function SidebarSearchField(props: SidebarSearchFieldProps) {
   const [input, setInput] = useState('');
   const classes = useStyles();
+  const Icon = props.icon ? props.icon : SearchIcon;
 
   const search = () => {
     props.onSearch(input);
@@ -329,7 +331,7 @@ export function SidebarSearchField(props: SidebarSearchFieldProps) {
 
   return (
     <div className={classes.searchRoot}>
-      <SidebarItem icon={SearchIcon} to={props.to} onClick={handleItemClick}>
+      <SidebarItem icon={Icon} to={props.to} onClick={handleItemClick}>
         <TextField
           placeholder="Search"
           value={input}
