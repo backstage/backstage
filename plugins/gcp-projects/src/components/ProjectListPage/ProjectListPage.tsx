1e33ce7407ce162bf706cc805aa3cd87173feded
Jeremy Guarini <jguarini@paloaltonetworks.com>
2021-11-11 12:41:30 -0800

    swapout material-table for backstage table
    Signed-off-by: Jeremy Guarini <jguarini@paloaltonetworks.com>

--- plugins/gcp-projects/src/components/ProjectListPage/ProjectListPage.tsx
+++ plugins/gcp-projects/src/components/ProjectListPage/ProjectListPage.tsx
@@ -16,16 +16,7 @@
 
 //  NEEDS WORK
 import { Button, LinearProgress, Tooltip, Typography } from '@material-ui/core';
-import MaterialTable from 'material-table';
-import ArrowDownward from '@material-ui/icons/ArrowDownward';
-import ChevronLeft from '@material-ui/icons/ChevronLeft';
-import ChevronRight from '@material-ui/icons/ChevronRight';
-import Clear from '@material-ui/icons/Clear';
-import FilterList from '@material-ui/icons/FilterList';
-import FirstPage from '@material-ui/icons/FirstPage';
-import LastPage from '@material-ui/icons/LastPage';
-import Search from '@material-ui/icons/Search';
-import React, { forwardRef } from 'react';
+import React from 'react';
 
 import { useAsync } from 'react-use';
 import { gcpApiRef, Project } from '../../api';
@@ -37,6 +28,7 @@ import {
   HeaderLabel,
   Link,
   Page,
+  Table,
   SupportButton,
   WarningPanel,
 } from '@backstage/core-components';
@@ -90,31 +82,7 @@ const PageContents = () => {
 
   return (
     <div style={{ height: '95%', width: '100%' }}>
-      <MaterialTable
-        icons={{
-          Filter: forwardRef((props, ref) => (
-            <FilterList {...props} ref={ref} />
-          )),
-          FirstPage: forwardRef((props, ref) => (
-            <FirstPage {...props} ref={ref} />
-          )),
-          LastPage: forwardRef((props, ref) => (
-            <LastPage {...props} ref={ref} />
-          )),
-          NextPage: forwardRef((props, ref) => (
-            <ChevronRight {...props} ref={ref} />
-          )),
-          PreviousPage: forwardRef((props, ref) => (
-            <ChevronLeft {...props} ref={ref} />
-          )),
-          ResetSearch: forwardRef((props, ref) => (
-            <Clear {...props} ref={ref} />
-          )),
-          Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-          SortArrow: forwardRef((props, ref) => (
-            <ArrowDownward {...props} ref={ref} />
-          )),
-        }}
+      <Table
         columns={[
           {
             field: 'name',
@@ -150,10 +118,8 @@ const PageContents = () => {
           })) || []
         }
         options={{
-          filtering: true,
           pageSize: 5,
           pageSizeOptions: [5, 10, 25, 50, 100],
-          showTitle: false,
         }}
       />
     </div>
