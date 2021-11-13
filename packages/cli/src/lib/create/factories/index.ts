6dcfe227a29006a43763bf2c1d5e345a4bfc0cb3
Patrik Oldsberg <poldsberg@gmail.com>
2021-11-13 16:32:20 +0100

    cli: add create factory for scaffolder modules
    Signed-off-by: Patrik Oldsberg <poldsberg@gmail.com>

--- packages/cli/src/lib/create/factories/index.ts
+++ packages/cli/src/lib/create/factories/index.ts
@@ -17,3 +17,4 @@
 export { frontendPlugin } from './frontendPlugin';
 export { backendPlugin } from './backendPlugin';
 export { pluginCommon } from './pluginCommon';
+export { scaffolderModule } from './scaffolderModule';
