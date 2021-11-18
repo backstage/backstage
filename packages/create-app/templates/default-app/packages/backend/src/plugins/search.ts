860254207c7e85dcbc62486f60f018c4dcb42fbe
Emma Indal <emmai@spotify.com>
2021-11-18 14:45:06 +0100

    prettier
    Signed-off-by: Emma Indal <emmai@spotify.com>

--- packages/create-app/templates/default-app/packages/backend/src/plugins/search.ts
+++ packages/create-app/templates/default-app/packages/backend/src/plugins/search.ts
@@ -24,7 +24,6 @@ export default async function createPlugin({
     collator: DefaultCatalogCollator.fromConfig(config, { discovery }),
   });
 
-
   // collator gathers entities from techdocs.
   indexBuilder.addCollator({
     defaultRefreshIntervalSeconds: 600,
