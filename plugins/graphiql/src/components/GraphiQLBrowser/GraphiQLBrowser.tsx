0f7665a10bc28e13d15fc77418338aaa1cb8cf54
Chad Jensen <Chad.Jensen@c2fo.com>
2021-11-19 22:13:16 -0600

    fixing other prettier issue
    Signed-off-by: Chad Jensen <Chad.Jensen@c2fo.com>

--- plugins/graphiql/src/components/GraphiQLBrowser/GraphiQLBrowser.tsx
+++ plugins/graphiql/src/components/GraphiQLBrowser/GraphiQLBrowser.tsx
@@ -73,7 +73,12 @@ export const GraphiQLBrowser = ({ endpoints }: GraphiQLBrowserProps) => {
         </Tabs>
         <Divider />
         <div className={classes.graphiQlWrapper}>
-          <GraphiQL headerEditorEnabled={true} key={tabIndex} fetcher={fetcher} storage={storage} />
+          <GraphiQL
+            headerEditorEnabled={true}
+            key={tabIndex}
+            fetcher={fetcher}
+            storage={storage}
+          />
         </div>
       </Suspense>
     </div>
