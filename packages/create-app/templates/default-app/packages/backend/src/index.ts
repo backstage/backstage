b3c67c7197292f471634389ccf92b51a51f1a7dc
Joon Park <joonp@spotify.com>
2021-12-08 16:29:11 +0000

    Remove noop from create app template
    Signed-off-by: Joon Park <joonp@spotify.com>

--- packages/create-app/templates/default-app/packages/backend/src/index.ts
+++ packages/create-app/templates/default-app/packages/backend/src/index.ts
@@ -38,7 +38,7 @@ function makeCreateEnv(config: Config) {
 
   const cacheManager = CacheManager.fromConfig(config);
   const databaseManager = DatabaseManager.fromConfig(config);
-  const tokenManager = ServerTokenManager.noop();
+  const tokenManager = ServerTokenManager.default({ config, logger: root });
 
   return (plugin: string): PluginEnvironment => {
     const logger = root.child({ type: 'plugin', plugin });
