5cd6880dbe3de998562a43e4cda28b2959334c40
Karan Shah <karan.shah@simplybusiness.co.uk>
2021-12-22 14:59:19 +0000

    Generate the initial plugin code
    Signed-off-by: Karan Shah <karan.shah@simplybusiness.co.uk>

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -88,6 +88,7 @@ import * as plugins from './plugins';
 
 import { techDocsPage } from './components/techdocs/TechDocsPage';
 import { ApacheAirflowPage } from '@backstage/plugin-apache-airflow';
+import { AirbrakePage } from '@backstage/plugin-airbrake';
 
 const app = createApp({
   apis,
@@ -220,6 +221,7 @@ const routes = (
     <Route path="/settings" element={<UserSettingsPage />} />
     <Route path="/azure-pull-requests" element={<AzurePullRequestsPage />} />
     <Route path="/apache-airflow" element={<ApacheAirflowPage />} />
+    <Route path="/airbrake" element={<AirbrakePage />} />
   </FlatRoutes>
 );
 
