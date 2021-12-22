1afa88ee59805fa3f595d26a902d6a684a74efed
blam <ben@blam.sh>
2021-12-22 04:50:54 +0100

    chore: think i've finished the refactor of this bloody component now
    Signed-off-by: blam <ben@blam.sh>

--- plugins/scaffolder/src/components/fields/RepoUrlPicker/index.ts
+++ plugins/scaffolder/src/components/fields/RepoUrlPicker/index.ts
@@ -13,5 +13,5 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export { RepoUrlPicker } from './NewRepoUrlPicker';
+export { RepoUrlPicker } from './RepoUrlPicker';
 export { repoPickerValidation } from './validation';
