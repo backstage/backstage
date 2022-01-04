2d3fd91e33567bc45b4f33657ac11366e833d614
Patrik Oldsberg <poldsberg@gmail.com>
2022-01-04 11:46:49 +0100

    test-utils: add MockConfigApi
    Signed-off-by: Patrik Oldsberg <poldsberg@gmail.com>

--- packages/test-utils/src/testUtils/apis/ConfigApi/index.ts
+++ packages/test-utils/src/testUtils/apis/ConfigApi/index.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 The Backstage Authors
+ * Copyright 2022 The Backstage Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,4 @@
  * limitations under the License.
  */
 
-export * from './AnalyticsApi';
-export * from './ConfigApi';
-export * from './ErrorApi';
-export * from './StorageApi';
+export { MockConfigApi } from './MockConfigApi';
