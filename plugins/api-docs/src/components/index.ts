6f4f3422d809987bcd1891908417d49d45c58688
David Weber <david.weber@w3tec.ch>
2022-01-14 23:19:50 +0100

    Export GraphQlDefinitionWidget
    This is important for reuse of the widget.
    
    Signed-off-by: David Weber <david.weber@baloise.ch>

--- plugins/api-docs/src/components/index.ts
+++ plugins/api-docs/src/components/index.ts
@@ -19,5 +19,6 @@ export * from './ApiDefinitionCard';
 export * from './ApisCards';
 export * from './AsyncApiDefinitionWidget';
 export * from './ComponentsCards';
+export * from './GraphQlDefinitionWidget';
 export * from './OpenApiDefinitionWidget';
 export * from './PlainApiDefinitionWidget';
