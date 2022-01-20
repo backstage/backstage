95e59ed0202e5dc3ab29fc0a48579b5da637258f
Joon Park <joonp@spotify.com>
2022-01-20 17:24:45 +0000

    Add caching to the useEntityPermission hook
    Signed-off-by: Joon Park <joonp@spotify.com>

--- plugins/catalog-react/src/hooks/useEntityPermission.ts
+++ plugins/catalog-react/src/hooks/useEntityPermission.ts
@@ -17,6 +17,7 @@
 import { stringifyEntityRef } from '@backstage/catalog-model';
 import { Permission } from '@backstage/plugin-permission-common';
 import { usePermission } from '@backstage/plugin-permission-react';
+import { useRef } from 'react';
 import { useEntity } from './useEntity';
 
 /**
@@ -24,6 +25,11 @@ import { useEntity } from './useEntity';
  * {@link @backstage/plugin-permission-react#usePermission} hook which uses the
  * current entity in context to make an authorization request for the given
  * permission.
+ *
+ * This hook caches the authorization decision based on the permission + the
+ * entity, and returns the cache match value as the default `allowed` value
+ * while loading. This helps avoid flicker in UI elements that would be
+ * conditionally rendered based on the `allowed` result of this hook.
  *
  * Note: this hook blocks the permission request until the entity has loaded in
  * context. If you have the entityRef and need concurrent requests, use the
@@ -36,6 +42,19 @@ export function useEntityPermission(permission: Permission): {
   error?: Error;
 } {
   const { entity, loading: loadingEntity, error: entityError } = useEntity();
+
+  let defaultValue: boolean;
+  const cache = useRef<Record<string, boolean>>({});
+  if (
+    entity &&
+    cache.current[`${permission.name}${stringifyEntityRef(entity)}`]
+  ) {
+    defaultValue =
+      cache.current[`${permission.name}${stringifyEntityRef(entity)}`];
+  } else {
+    defaultValue = false;
+  }
+
   const {
     allowed,
     loading: loadingPermission,
@@ -46,10 +65,11 @@ export function useEntityPermission(permission: Permission): {
   );
 
   if (loadingEntity || loadingPermission) {
-    return { loading: true, allowed: false };
+    return { loading: true, allowed: defaultValue };
   }
   if (entityError) {
     return { loading: false, allowed: false, error: entityError };
   }
+  cache.current[`${permission.name}${stringifyEntityRef(entity)}`] = allowed;
   return { loading: false, allowed, error: permissionError };
 }
