6d9f426eabc5d32d7d77c5101e10b0154bbe538c
blam <ben@blam.sh>
2022-01-25 16:23:43 +0100

    feat: added a SecretsContext for storing the secrets in the frontend and being able to add secrets to the context using a hooks
    Signed-off-by: blam <ben@blam.sh>

---
+++ plugins/scaffolder/src/components/secrets/SecretsContext.tsx
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2022 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import React, {
+  useState,
+  useCallback,
+  useContext,
+  createContext,
+  PropsWithChildren,
+} from 'react';
+import { JsonObject } from '@backstage/types';
+
+type SecretsContextContents = {
+  secrets: JsonObject;
+  setSecrets: React.Dispatch<React.SetStateAction<JsonObject>>;
+};
+
+/**
+ * The actual context object.
+ */
+export const SecretsContext = createContext<SecretsContextContents | undefined>(
+  undefined,
+);
+
+/**
+ * The Context Provider that holds the state for the secrets.
+ *
+ * @public
+ */
+export const SecretsContextProvider = ({ children }: PropsWithChildren<{}>) => {
+  const [secrets, setSecrets] = useState<JsonObject>({});
+
+  return (
+    <SecretsContext.Provider value={{ secrets, setSecrets }}>
+      {children}
+    </SecretsContext.Provider>
+  );
+};
+
+/**
+ * Hook to access the secrets context.
+ * @public
+ */
+export const useSecretsContext = () => {
+  const value = useContext(SecretsContext);
+  if (!value) {
+    throw new Error(
+      'useSecretsContext must be used within a SecretsContextProvider',
+    );
+  }
+
+  const { secrets, setSecrets } = value;
+
+  const setSecret = useCallback(
+    (input: JsonObject) => {
+      setSecrets({ ...secrets, ...input });
+    },
+    [secrets, setSecrets],
+  );
+
+  return { setSecret };
+};
