90a8061a681a03496711e5ec1ce4ff368207572a
Eric Peterson <i.am@eric.pe>
2022-01-30 18:58:03 +0100

    Deprecate useless plugin export.
    Signed-off-by: Eric Peterson <i.am@eric.pe>
    Signed-off-by: Gabriel Dantas <gabriel.dantas98@outlook.com.br>

--- plugins/analytics-module-ga/src/plugin.ts
+++ plugins/analytics-module-ga/src/plugin.ts
@@ -16,6 +16,9 @@
 import { createPlugin } from '@backstage/core-plugin-api';
 
 /**
+ * @deprecated Importing and including this plugin in an app has no effect.
+ * This will be removed in a future release.
+ *
  * @public
  */
 export const analyticsModuleGA = createPlugin({
