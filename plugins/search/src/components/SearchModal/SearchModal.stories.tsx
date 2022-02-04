eb1386e284ee534562e19c65ba716e2c3dc0fc46
Vincenzo Scamporlino <me@vinzscam.dev>
2022-02-04 20:07:01 +0100

    Split storybook SearchContextProvider
    Signed-off-by: Vincenzo Scamporlino <me@vinzscam.dev>

--- plugins/search/src/components/SearchModal/SearchModal.stories.tsx
+++ plugins/search/src/components/SearchModal/SearchModal.stories.tsx
@@ -16,10 +16,9 @@
 
 import { wrapInTestApp } from '@backstage/test-utils';
 import { Button } from '@material-ui/core';
-import React, { ComponentType } from 'react';
+import React, { ComponentType, useState } from 'react';
 import { rootRouteRef } from '../../plugin';
-import { useSearch } from '../SearchContext';
-import { SearchContextProvider } from '../SearchContext/SearchContextForStorybook.stories';
+import { SearchApiProvider } from '../SearchContext/SearchContextForStorybook.stories';
 import { SearchModal } from './SearchModal';
 
 const mockResults = {
@@ -57,23 +56,32 @@ export default {
   decorators: [
     (Story: ComponentType<{}>) =>
       wrapInTestApp(
-        <SearchContextProvider mockedResults={mockResults}>
+        <SearchApiProvider mockedResults={mockResults}>
           <Story />
-        </SearchContextProvider>,
+        </SearchApiProvider>,
         { mountedRoutes: { '/search': rootRouteRef } },
       ),
   ],
 };
 
 export const Default = () => {
-  const { open, toggleModal } = useSearch();
+  const [state, setState] = useState({ hidden: true, opened: false });
+  const handleToggleModal = () =>
+    setState(previousState => ({
+      opened: true,
+      hidden: !previousState.hidden,
+    }));
 
   return (
     <>
-      <Button variant="contained" color="primary" onClick={toggleModal}>
+      <Button variant="contained" color="primary" onClick={handleToggleModal}>
         Toggle Search Modal
       </Button>
-      <SearchModal open={open} toggleModal={toggleModal} />
+      <SearchModal
+        open={state.opened}
+        hidden={state.hidden}
+        toggleModal={handleToggleModal}
+      />
     </>
   );
 };
