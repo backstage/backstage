d0c71e2aa4f9482b8a210588353391fa2057ab9b
Patrik Oldsberg <poldsberg@gmail.com>
2022-02-07 12:36:36 +0100

    cli: use node API from ESLint to invoke it rather than a subprocess
    Signed-off-by: Patrik Oldsberg <poldsberg@gmail.com>

--- packages/cli/src/commands/lint.ts
+++ packages/cli/src/commands/lint.ts
@@ -15,19 +15,29 @@
  */
 
 import { Command } from 'commander';
-import { run } from '../lib/run';
 import { paths } from '../lib/paths';
+import { ESLint } from 'eslint';
 
-export default async (cmd: Command, cmdArgs: string[]) => {
-  const args = [
-    '--ext=js,jsx,ts,tsx,mjs,cjs',
-    '--max-warnings=0',
-    `--format=${cmd.format}`,
-    ...(cmdArgs ?? [paths.targetDir]),
-  ];
+export default async (cmd: Command) => {
+  const eslint = new ESLint({
+    cwd: paths.targetDir,
+    fix: cmd.fix,
+    extensions: ['js', 'jsx', 'ts', 'tsx', 'mjs', 'cjs'],
+  });
+
+  const results = await eslint.lintFiles(['.']);
+
   if (cmd.fix) {
-    args.push('--fix');
+    await ESLint.outputFixes(results);
   }
 
-  await run('eslint', args);
+  const formatter = await eslint.loadFormatter(cmd.format);
+  const resultText = formatter.format(results);
+
+  // If there is any feedback at all, we treat it as a lint failure. This should be
+  // consistent with our old behavior of passing `--max-warnings=0` when invoking eslint.
+  if (resultText) {
+    console.log(resultText);
+    process.exit(1);
+  }
 };
