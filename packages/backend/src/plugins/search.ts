d0993bc6f1956b285ebeac910afa5c315809228a
Eric Peterson <ericpeterson@spotify.com>
2022-02-26 20:48:20 +0100

    Wire up app/create-app to use stream-based search implementations
    Signed-off-by: Eric Peterson <ericpeterson@spotify.com>

--- packages/backend/src/plugins/search.ts
+++ packages/backend/src/plugins/search.ts
@@ -18,7 +18,7 @@ import {
   useHotCleanup,
 } from '@backstage/backend-common';
 import { Config } from '@backstage/config';
-import { DefaultCatalogCollator } from '@backstage/plugin-catalog-backend';
+import { DefaultCatalogCollatorFactory } from '@backstage/plugin-catalog-backend';
 import { createRouter } from '@backstage/plugin-search-backend';
 import { ElasticSearchSearchEngine } from '@backstage/plugin-search-backend-module-elasticsearch';
 import { PgSearchEngine } from '@backstage/plugin-search-backend-module-pg';
@@ -27,7 +27,7 @@ import {
   LunrSearchEngine,
   SearchEngine,
 } from '@backstage/plugin-search-backend-node';
-import { DefaultTechDocsCollator } from '@backstage/plugin-techdocs-backend';
+import { DefaultTechDocsCollatorFactory } from '@backstage/plugin-techdocs-backend';
 import { Logger } from 'winston';
 import { PluginEnvironment } from '../types';
 
@@ -70,7 +70,7 @@ export default async function createPlugin({
   // particular collator gathers entities from the software catalog.
   indexBuilder.addCollator({
     defaultRefreshIntervalSeconds: 600,
-    collator: DefaultCatalogCollator.fromConfig(config, {
+    factory: DefaultCatalogCollatorFactory.fromConfig(config, {
       discovery,
       tokenManager,
     }),
@@ -78,7 +78,7 @@ export default async function createPlugin({
 
   indexBuilder.addCollator({
     defaultRefreshIntervalSeconds: 600,
-    collator: DefaultTechDocsCollator.fromConfig(config, {
+    factory: DefaultTechDocsCollatorFactory.fromConfig(config, {
       discovery,
       logger,
       tokenManager,
