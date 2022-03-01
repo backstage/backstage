862e41623974165913fdd4244bd04fd7ed986c02
Johan Haals <johan.haals@gmail.com>
2022-03-01 13:34:38 +0100

    catalog-backend: Remove entityRef from CatalogProcessorRelationResult
    Signed-off-by: Johan Haals <johan.haals@gmail.com>

--- plugins/catalog-backend/src/ingestion/processors/types.ts
+++ plugins/catalog-backend/src/ingestion/processors/types.ts
@@ -193,7 +193,6 @@ export type CatalogProcessorEntityResult = {
 export type CatalogProcessorRelationResult = {
   type: 'relation';
   relation: EntityRelationSpec;
-  entityRef?: string;
 };
 
 /** @public */
