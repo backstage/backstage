048de3df6c8d980098c25bd57391bd4e4e172d76
Gustaf Räntilä <g.rantila@gmail.com>
2022-03-15 17:57:33 +0100

    feat(core): Added Plugin React context
    Added PluginContextProvider, with hook, to be able to traverse the plugin stack from a React component.
    
    This can eventually be used for the ErrorBoundary and AnalyticsContext.
    
    Signed-off-by: Gustaf Räntilä <g.rantila@gmail.com>

--- packages/core-plugin-api/src/plugin-context/index.ts
+++ packages/core-plugin-api/src/plugin-context/index.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 The Backstage Authors
+ * Copyright 2022 The Backstage Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,17 +14,5 @@
  * limitations under the License.
  */
 
-/**
- * Core API used by Backstage plugins
- *
- * @packageDocumentation
- */
-
-export * from './analytics';
-export * from './apis';
-export * from './app';
-export * from './extensions';
-export * from './icons';
-export * from './plugin';
-export * from './plugin-context';
-export * from './routing';
+export * from './context';
+export * from './types';
