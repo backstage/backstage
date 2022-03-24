f534f52696ba398bcb23a8cf7f323f0b973d5b9e
blam <ben@blam.sh>
2022-03-24 11:55:49 +0100

    chore: added step extraction with tests
    Signed-off-by: blam <ben@blam.sh>

--- plugins/scaffolder/src/next/TemplateWizardPage/Stepper/schema.ts
+++ plugins/scaffolder/src/next/TemplateWizardPage/Stepper/schema.ts
@@ -13,27 +13,99 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import { JsonObject } from '@backstage/types';
 import { UiSchema } from '@rjsf/core';
 import { JSONSchema7 } from 'json-schema';
 
 import { TemplateParameterSchema } from '../../../types';
 
-export const extractSchemaFromManifest = (
-  manifest: TemplateParameterSchema,
-): { uiSchema: UiSchema; schema: JSONSchema7 } => {
-  const schema = manifest.steps[0].schema;
-  const uiSchema = JSON.parse(JSON.stringify(schema), (key, value) => {
-    if (typeof value === 'object') {
-      return Object.fromEntries(
-        Object.entries(value).filter(
-          ([k, v]) => k.startsWith('ui:') || typeof v === 'object',
-        ),
-      );
+function isObject(value: unknown): value is JsonObject {
+  return typeof value === 'object' && value !== null && !Array.isArray(value);
+}
+
+function extractUiSchema(schema: JsonObject, uiSchema: JsonObject) {
+  if (!isObject(schema)) {
+    return;
+  }
+
+  const { properties, items, anyOf, oneOf, allOf, dependencies } = schema;
+
+  for (const propName in schema) {
+    if (!schema.hasOwnProperty(propName)) {
+      continue;
     }
 
-    return value;
-  });
+    if (propName.startsWith('ui:')) {
+      uiSchema[propName] = schema[propName];
+      delete schema[propName];
+    }
+  }
 
-  console.log(uiSchema);
-  return { uiSchema, schema: {} };
+  if (isObject(properties)) {
+    for (const propName in properties) {
+      if (!properties.hasOwnProperty(propName)) {
+        continue;
+      }
+
+      const schemaNode = properties[propName];
+      if (!isObject(schemaNode)) {
+        continue;
+      }
+      const innerUiSchema = {};
+      uiSchema[propName] = innerUiSchema;
+      extractUiSchema(schemaNode, innerUiSchema);
+    }
+  }
+
+  if (isObject(items)) {
+    const innerUiSchema = {};
+    uiSchema.items = innerUiSchema;
+    extractUiSchema(items, innerUiSchema);
+  }
+
+  if (Array.isArray(anyOf)) {
+    for (const schemaNode of anyOf) {
+      if (!isObject(schemaNode)) {
+        continue;
+      }
+      extractUiSchema(schemaNode, uiSchema);
+    }
+  }
+
+  if (Array.isArray(oneOf)) {
+    for (const schemaNode of oneOf) {
+      if (!isObject(schemaNode)) {
+        continue;
+      }
+      extractUiSchema(schemaNode, uiSchema);
+    }
+  }
+
+  if (Array.isArray(allOf)) {
+    for (const schemaNode of allOf) {
+      if (!isObject(schemaNode)) {
+        continue;
+      }
+      extractUiSchema(schemaNode, uiSchema);
+    }
+  }
+
+  if (isObject(dependencies)) {
+    for (const depName of Object.keys(dependencies)) {
+      const schemaNode = dependencies[depName];
+      if (!isObject(schemaNode)) {
+        continue;
+      }
+      extractUiSchema(schemaNode, uiSchema);
+    }
+  }
+}
+
+export const extractSchemaFromStep = (
+  inputStep: JSONSchema7,
+): { uiSchema: UiSchema; schema: JSONSchema7 } => {
+  const uiSchema: UiSchema = {};
+  const returnSchema: JsonObject = JSON.parse(JSON.stringify(inputStep));
+  extractUiSchema(returnSchema, uiSchema);
+  return { uiSchema, schema: returnSchema };
 };
