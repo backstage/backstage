948d6425de43afae0ef7dfc840795540fc0fa063
Camila Belo <camilaibs@gmail.com>
2022-03-29 20:03:48 +0200

    feat(techdocs): create addons package
    Co-authored-by: Emma Indal <emma.indahl@gmail.com>
    Signed-off-by: Camila Belo <camilaibs@gmail.com>

---
+++ packages/techdocs-addons/src/index.ts
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2022 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Package encapsulating the TechDocs Addon framework.
+ *
+ * @packageDocumentation
+ */
+
+export {
+  useTechDocsAddons,
+  createTechDocsAddon,
+  TechDocsAddons,
+  TECHDOCS_ADDONS_WRAPPER_KEY,
+} from './addons';
+export { TechDocsAddonLocations } from './types';
+export type { TechDocsAddonAsyncMetadata, TechDocsAddonOptions } from './types';
