344018c0aa1243015d99338749a0d15f4db2277f
Emma Indal <emma.indahl@gmail.com>
2022-04-06 17:54:58 +0200

    add hooks used for give feedback addon
    Co-authored-by: Camila Belo <camilaibs@gmail.com>
    Co-authored-by: Anders Näsman <andersn@spotify.com>
    Signed-off-by: Emma Indal <emma.indahl@gmail.com>

--- plugins/techdocs/src/reader/components/TechDocsReaderPage/hooks.test.ts
+++ plugins/techdocs/src/reader/components/TechDocsReaderPage/hooks.test.ts
@@ -14,13 +14,29 @@
  * limitations under the License.
  */
 
-import { useShadowRoot, useShadowRootElements } from './hooks';
+import {
+  useShadowRoot,
+  useShadowRootElements,
+  useShadowRootSelection,
+} from './hooks';
 import { renderHook } from '@testing-library/react-hooks';
+import { fireEvent, waitFor } from '@testing-library/react';
+
+const fireSelectionChangeEvent = (window: Window) => {
+  const selectionChangeEvent = window.document.createEvent('Event');
+  selectionChangeEvent.initEvent('selectionchange', true, true);
+  window.document.addEventListener('selectionchange', () => {}, false);
+  fireEvent(window.document, selectionChangeEvent);
+};
+
+const getSelection = jest.fn();
 
 const mockShadowRoot = () => {
   const div = document.createElement('div');
   const shadowRoot = div.attachShadow({ mode: 'open' });
   shadowRoot.innerHTML = '<h1>Shadow DOM Mock</h1>';
+  (shadowRoot as ShadowRoot & Pick<Document, 'getSelection'>).getSelection =
+    getSelection;
   return shadowRoot;
 };
 
@@ -31,6 +47,28 @@ jest.mock('./context', () => {
     useTechDocsReaderPage: () => ({ shadowRoot }),
   };
 });
+
+const selection = {
+  type: 'Range',
+  rangeCount: 1,
+  isCollapsed: true,
+  getRangeAt: () => ({
+    startContainer: 'this is a sentence',
+    endContainer: 'this is a sentence',
+    startOffset: 1,
+    endOffset: 3,
+    getBoundingClientRect: () => ({
+      right: 100,
+      top: 100,
+      width: 100,
+      height: 100,
+    }),
+  }),
+  toString: () => 'his ',
+  containsNode: () => true,
+} as unknown as Selection;
+
+getSelection.mockReturnValue(selection);
 
 describe('hooks', () => {
   describe('useShadowRoot', () => {
@@ -46,6 +84,20 @@ describe('hooks', () => {
       const { result } = renderHook(() => useShadowRootElements(['h1']));
 
       expect(result.current).toHaveLength(1);
+    });
+  });
+
+  describe('useShadowRootSelection', () => {
+    it('should return shadow root selection', async () => {
+      const { result } = renderHook(() => useShadowRootSelection(0));
+
+      expect(result.current).toBeNull();
+
+      fireSelectionChangeEvent(window);
+
+      await waitFor(() => {
+        expect(result.current?.toString()).toEqual('his ');
+      });
     });
   });
 });
