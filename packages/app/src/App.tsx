b7b9963d2985bdfe832ef0ca31cdc9da54f3972d
Emma Indal <emma.indahl@gmail.com>
2022-04-08 11:00:01 +0200

    no support for cusomt reader page in app when using addons
    Co-authored-by: Camila Belo <camilaibs@gmail.com>
    Co-authored-by: Anders Näsman <andersn@spotify.com>
    Co-authored-by: Otto Sichert <git@ottosichert.de>
    Co-authored-by: Emma Indal <emma.indahl@gmail.com>
    Signed-off-by: Emma Indal <emma.indahl@gmail.com>

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -90,7 +90,6 @@ import { searchPage } from './components/search/SearchPage';
 import { providers } from './identityProviders';
 import * as plugins from './plugins';
 
-import { techDocsPage } from './components/techdocs/TechDocsPage';
 import { ApacheAirflowPage } from '@backstage/plugin-apache-airflow';
 import { PermissionedRoute } from '@backstage/plugin-permission-react';
 import { catalogEntityCreatePermission } from '@backstage/plugin-catalog-common';
@@ -180,7 +179,6 @@ const routes = (
       path="/docs/:namespace/:kind/:name/*"
       element={<TechDocsReaderPage />}
     >
-      {techDocsPage}
       <TechDocsAddons>
         <ReportIssue />
       </TechDocsAddons>
