7cd399ede0507033123387202835fa95433ec93c
Fredrik Adelöw <freben@gmail.com>
2022-04-27 11:01:31 +0200

    most of these casts are apparently no longer necessary
    Signed-off-by: Fredrik Adelöw <freben@gmail.com>

--- plugins/auth-backend/src/providers/auth0/provider.ts
+++ plugins/auth-backend/src/providers/auth0/provider.ts
@@ -71,6 +71,8 @@ export class Auth0AuthProvider implements OAuthHandlers {
         clientSecret: options.clientSecret,
         callbackURL: options.callbackUrl,
         domain: options.domain,
+        // We need passReqToCallback set to false to get params, but there's
+        // no matching type signature for that, so instead behold this beauty
         passReqToCallback: false as true,
       },
       (
