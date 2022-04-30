bde245f0bfbc953003a8b8f795f5e7c6bd1251bf
David Weber <david.weber@w3tec.ch>
2022-04-30 20:05:58 +0200

    feat: add dashboard URL feature and fix minor styling issues
    Signed-off-by: David Weber <david.weber@w3tec.ch>

--- plugins/kafka/config.d.ts
+++ plugins/kafka/config.d.ts
@@ -13,6 +13,19 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export const KAFKA_CONSUMER_GROUP_ANNOTATION =
-  'kafka.apache.org/consumer-groups';
-export const KAFKA_DASHBOARD_URL = 'kafka.apache.org/dashboard-urls';
+export interface Config {
+  kafka?: {
+    clusters: Array<{
+      /**
+       * Cluster name
+       * @visibility frontend
+       */
+      name: string;
+      /**
+       * Cluster dashboard url
+       * @visibility frontend
+       */
+      dashboardUrl?: string;
+    }>;
+  };
+}
