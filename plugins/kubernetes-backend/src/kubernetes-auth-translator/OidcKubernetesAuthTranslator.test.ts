d2c5a3232398d26a10ad2d69146202f2b5fa192d
Daniel Bravo <dbravo@vmware.com>
2022-04-30 09:30:38 -0500

    Add oidc authProvider to kubernetes-backend plugin
    Signed-off-by: Daniel Bravo <dbravo@vmware.com>

---
+++ plugins/kubernetes-backend/src/kubernetes-auth-translator/OidcKubernetesAuthTranslator.test.ts
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2020 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { OidcKubernetesAuthTranslator } from './OidcKubernetesAuthTranslator';
+import { ClusterDetails } from '../types/types';
+import { Entity } from '@backstage/catalog-model';
+
+describe('OidcKubernetesAuthTranslator tests', () => {
+  const at = new OidcKubernetesAuthTranslator();
+  const entity: Entity = {
+    apiVersion: 'v1',
+    kind: 'service',
+    metadata: { name: 'test' },
+  };
+  const baseClusterDetails: ClusterDetails = {
+    name: 'test',
+    authProvider: 'oidc',
+    url: '',
+  };
+
+  it('returns cluster details with auth token', async () => {
+    const details = await at.decorateClusterDetailsWithAuth(
+      {
+        oidcTokenProvider: 'okta',
+        ...baseClusterDetails,
+      },
+      {
+        auth: { okta: 'fakeToken' },
+        entity,
+      },
+    );
+
+    expect(details.serviceAccountToken).toBe('fakeToken');
+  });
+
+  it('returns error when oidcTokenProvider is not configured', async () => {
+    await expect(
+      at.decorateClusterDetailsWithAuth(baseClusterDetails, { entity }),
+    ).rejects.toThrow(
+      'oidc authProvider requires a configured oidcTokenProvider',
+    );
+  });
+
+  it('returns error when token is not included in request body', async () => {
+    await expect(
+      at.decorateClusterDetailsWithAuth(
+        { oidcTokenProvider: 'okta', ...baseClusterDetails },
+        { entity },
+      ),
+    ).rejects.toThrow('Auth token not found under auth.okta in request body');
+  });
+});
--- plugins/kubernetes-common/src/types.ts
+++ plugins/kubernetes-common/src/types.ts
@@ -30,7 +30,7 @@ import { Entity } from '@backstage/catalog-model';
 
 export interface KubernetesRequestBody {
   auth?: {
-    google?: string;
+    [key: string]: string;
   };
   entity: Entity;
 }
