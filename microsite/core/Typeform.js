69720cec67a6f313a12e0325679cff2286ac567b
Joon Park <joonp@spotify.com>
2022-05-18 17:09:04 +0100

    Add typeform survey banner (#11549)
    Signed-off-by: Joon Park <joonp@spotify.com>

---
+++ microsite/core/Typeform.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export function Typeform() {
+  return (
+    <>
+      <div
+        data-tf-sidetab="zcOaKikB"
+        data-tf-width="320"
+        data-tf-height="400"
+        data-tf-custom-icon="https://images.typeform.com/images/769rUZbqPrvq"
+        data-tf-button-color="#5CD6C8"
+        data-tf-button-text="New Adopters"
+        data-tf-iframe-props="title=Public Adopter: Hand Raiser Form"
+        data-tf-medium="snippet"
+        data-tf-hidden="utm_source=.iobanner"
+        style={{ all: 'unset' }}
+      />
+      <script src="//embed.typeform.com/next/embed.js" />
+    </>
+  );
+}
