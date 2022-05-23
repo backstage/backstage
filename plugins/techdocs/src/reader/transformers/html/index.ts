0a5e6ffcf603286d5d5122f1ec3f7fb7a6a7edb5
Camila Belo <camilaibs@gmail.com>
2022-05-23 08:34:32 +0200

    refactor(techdocs): export html sanitizer transformer
    Signed-off-by: Camila Belo <camilaibs@gmail.com>

--- plugins/techdocs/src/reader/transformers/html/index.ts
+++ plugins/techdocs/src/reader/transformers/html/index.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 The Backstage Authors
+ * Copyright 2022 The Backstage Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,17 +14,4 @@
  * limitations under the License.
  */
 
-export * from './html';
-export * from './addBaseUrl';
-export * from './addGitFeedbackLink';
-export * from './addSidebarToggle';
-export * from './rewriteDocLinks';
-export * from './addLinkClickListener';
-export * from './copyToClipboard';
-export * from './removeMkdocsHeader';
-export * from './simplifyMkdocsFooter';
-export * from './onCssReady';
-export * from './sanitizeDOM';
-export * from './injectCss';
-export * from './scrollIntoAnchor';
-export * from './transformer';
+export { useHtmlTransformer } from './transformer';
