b1c5b42849a400dbac414df2d8f20c813d2111a1
Alex Rybchenko <arybchenko@box.com>
2022-06-15 13:58:33 +0200

    added type predicates for entities
    Signed-off-by: Alex Rybchenko <arybchenko@box.com>

--- plugins/catalog/src/components/EntitySwitch/index.ts
+++ plugins/catalog/src/components/EntitySwitch/index.ts
@@ -16,4 +16,4 @@
 
 export { EntitySwitch } from './EntitySwitch';
 export type { EntitySwitchProps, EntitySwitchCaseProps } from './EntitySwitch';
-export { isKind, isNamespace, isComponentType } from './conditions';
+export * from './conditions';
