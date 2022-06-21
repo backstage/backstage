a145672f0f273ffbce6db6d1afbc2cf14ca6627b
Patrick Jungermann <Patrick.Jungermann@gmail.com>
2022-06-22 00:18:59 +0200

    feat: align msgraph provider config
    Align msgraph entity provider config with the config of other entity providers.
    
    Closes: #12065
    Signed-off-by: Patrick Jungermann <Patrick.Jungermann@gmail.com>

--- plugins/catalog-backend-module-msgraph/config.d.ts
+++ plugins/catalog-backend-module-msgraph/config.d.ts
@@ -25,6 +25,7 @@ export interface Config {
     processors?: {
       /**
        * MicrosoftGraphOrgReaderProcessor configuration
+       * @deprecated Use `catalog.providers.microsoftGraphOrg` instead.
        */
       microsoftGraphOrg?: {
         /**
@@ -101,6 +102,174 @@ export interface Config {
           userGroupMemberSearch?: string;
         }>;
       };
+    };
+    /**
+     * List of provider-specific options and attributes
+     */
+    providers?: {
+      /**
+       * MicrosoftGraphOrgEntityProvider configuration.
+       */
+      microsoftGraphOrg?:
+        | {
+            /**
+             * The prefix of the target that this matches on, e.g.
+             * "https://graph.microsoft.com/v1.0", with no trailing slash.
+             */
+            target: string;
+            /**
+             * The auth authority used.
+             *
+             * Default value "https://login.microsoftonline.com"
+             */
+            authority?: string;
+            /**
+             * The tenant whose org data we are interested in.
+             */
+            tenantId: string;
+            /**
+             * The OAuth client ID to use for authenticating requests.
+             */
+            clientId: string;
+            /**
+             * The OAuth client secret to use for authenticating requests.
+             *
+             * @visibility secret
+             */
+            clientSecret: string;
+
+            user?: {
+              /**
+               * The "expand" argument to apply to users.
+               *
+               * E.g. "manager".
+               */
+              expand?: string;
+              /**
+               * The filter to apply to extract users.
+               *
+               * E.g. "accountEnabled eq true and userType eq 'member'"
+               */
+              filter?: string;
+            };
+
+            group?: {
+              /**
+               * The "expand" argument to apply to groups.
+               *
+               * E.g. "member".
+               */
+              expand?: string;
+              /**
+               * The filter to apply to extract groups.
+               *
+               * E.g. "securityEnabled eq false and mailEnabled eq true"
+               */
+              filter?: string;
+              /**
+               * The search criteria to apply to extract users by groups memberships.
+               *
+               * E.g. "\"displayName:-team\"" would only match groups which contain '-team'
+               */
+              search?: string;
+              /**
+               * The fields to be fetched on query.
+               *
+               * E.g. ["id", "displayName", "description"]
+               */
+              select?: string[];
+            };
+
+            userGroupMember?: {
+              /**
+               * The filter to apply to extract users by groups memberships.
+               *
+               * E.g. "displayName eq 'Backstage Users'"
+               */
+              filter?: string;
+              /**
+               * The search criteria to apply to extract groups.
+               *
+               * E.g. "\"displayName:-team\"" would only match groups which contain '-team'
+               */
+              search?: string;
+            };
+          }
+        | Record<
+            string,
+            {
+              /**
+               * The prefix of the target that this matches on, e.g.
+               * "https://graph.microsoft.com/v1.0", with no trailing slash.
+               */
+              target: string;
+              /**
+               * The auth authority used.
+               *
+               * Default value "https://login.microsoftonline.com"
+               */
+              authority?: string;
+              /**
+               * The tenant whose org data we are interested in.
+               */
+              tenantId: string;
+              /**
+               * The OAuth client ID to use for authenticating requests.
+               */
+              clientId: string;
+              /**
+               * The OAuth client secret to use for authenticating requests.
+               *
+               * @visibility secret
+               */
+              clientSecret: string;
+
+              user?: {
+                /**
+                 * The filter to apply to extract users.
+                 *
+                 * E.g. "accountEnabled eq true and userType eq 'member'"
+                 */
+                filter?: string;
+              };
+
+              group?: {
+                /**
+                 * The filter to apply to extract groups.
+                 *
+                 * E.g. "securityEnabled eq false and mailEnabled eq true"
+                 */
+                filter?: string;
+                /**
+                 * The search criteria to apply to extract users by groups memberships.
+                 *
+                 * E.g. "\"displayName:-team\"" would only match groups which contain '-team'
+                 */
+                search?: string;
+                /**
+                 * The fields to be fetched on query.
+                 *
+                 * E.g. ["id", "displayName", "description"]
+                 */
+                select?: string[];
+              };
+
+              userGroupMember?: {
+                /**
+                 * The filter to apply to extract users by groups memberships.
+                 *
+                 * E.g. "displayName eq 'Backstage Users'"
+                 */
+                filter?: string;
+                /**
+                 * The search criteria to apply to extract groups.
+                 *
+                 * E.g. "\"displayName:-team\"" would only match groups which contain '-team'
+                 */
+                search?: string;
+              };
+            }
+          >;
     };
   };
 }
