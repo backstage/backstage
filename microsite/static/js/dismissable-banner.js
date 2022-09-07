38d4f9a8cfe0b4bf0e5937cd486fad642252178c
blam <ben@blam.sh>
2022-09-07 15:58:07 +0200

    chore: fixing issue with the microsite banner positioning
    Signed-off-by: blam <ben@blam.sh>

--- microsite/static/js/dismissable-banner.js
+++ microsite/static/js/dismissable-banner.js
@@ -10,7 +10,7 @@ window.addEventListener('DOMContentLoaded', () => {
     const dismissButton = banner.querySelector('[data-banner-dismiss]');
     if (dismissButton) {
       dismissButton.addEventListener('click', () => {
-        banner.classList.add('Banner--hidden');
+        banner.classList.add('Banner--hiding');
         localStorage.setItem(storageKey, 'true');
       });
     }
