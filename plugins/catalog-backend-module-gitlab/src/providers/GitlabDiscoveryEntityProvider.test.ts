993a7817e07fe2c2ddb6a8184ff34f263ee75d79
Ruslan.Nasyrov <goququ@ya.ru>
2022-09-14 15:16:32 +0500

    fix
    Signed-off-by: Ruslan.Nasyrov <goququ@ya.ru>

--- plugins/catalog-backend-module-gitlab/src/providers/GitlabDiscoveryEntityProvider.test.ts
+++ plugins/catalog-backend-module-gitlab/src/providers/GitlabDiscoveryEntityProvider.test.ts
@@ -155,6 +155,7 @@ describe('GitlabDiscoveryEntityProvider', () => {
     const schedule = new PersistingTaskRunner();
     const entityProviderConnection: EntityProviderConnection = {
       applyMutation: jest.fn(),
+      refresh: jest.fn(),
     };
     const provider = GitlabDiscoveryEntityProvider.fromConfig(config, {
       logger,
