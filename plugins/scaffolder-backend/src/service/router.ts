4acad115c1644ae0169a129c4d2fe1e763fe83cd
Patrik Oldsberg <poldsberg@gmail.com>
2022-09-26 19:33:43 +0200

    Update plugins/scaffolder-backend/src/service/router.ts
    Signed-off-by: Patrik Oldsberg <poldsberg@gmail.com>

--- plugins/scaffolder-backend/src/service/router.ts
+++ plugins/scaffolder-backend/src/service/router.ts
@@ -148,7 +148,7 @@ export async function createRouter(
   options: RouterOptions,
 ): Promise<express.Router> {
   const router = Router();
-  // Be generous in upload size to support a wide rande of templates in dry-run mode.
+  // Be generous in upload size to support a wide range of templates in dry-run mode.
   router.use(express.json({ limit: '10MB' }));
 
   const {
