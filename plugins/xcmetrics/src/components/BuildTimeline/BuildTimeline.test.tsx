a230bf2a9626f840325015e404e04b231a339e58
Leonardo Maier <leonarmaier@gmail.com>
2022-10-01 10:52:29 -0300

    fix formatting issues
    Signed-off-by: Leonardo Maier <leonarmaier@gmail.com>

--- plugins/xcmetrics/src/components/BuildTimeline/BuildTimeline.test.tsx
+++ plugins/xcmetrics/src/components/BuildTimeline/BuildTimeline.test.tsx
@@ -24,12 +24,12 @@ jest.mock('recharts', () => {
   const OriginalModule = jest.requireActual('recharts');
 
   return {
-      ...OriginalModule,
-      ResponsiveContainer: ({ children }: any) => (
-          <OriginalModule.ResponsiveContainer width={100} aspect={1}>
-              {children}
-          </OriginalModule.ResponsiveContainer>
-      ),
+    ...OriginalModule,
+    ResponsiveContainer: ({ children }: any) => (
+      <OriginalModule.ResponsiveContainer width={100} aspect={1}>
+        {children}
+      </OriginalModule.ResponsiveContainer>
+    ),
   };
 });
 
