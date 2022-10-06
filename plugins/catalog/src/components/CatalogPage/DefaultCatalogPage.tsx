0c0ac14bc43dabe459a22e132a3ae28c3825a1e3
Mathias Bronner <mathiasb@spotify.com>
2022-10-06 13:43:58 +0200

    clean up dev instance of component
    Signed-off-by: Mathias Bronner <mathiasb@spotify.com>

--- plugins/catalog/src/components/CatalogPage/DefaultCatalogPage.tsx
+++ plugins/catalog/src/components/CatalogPage/DefaultCatalogPage.tsx
@@ -85,11 +85,6 @@ export function DefaultCatalogPage(props: DefaultCatalogPageProps) {
           </ContentHeader>
           <CatalogFilterLayout>
             <CatalogFilterLayout.Filters>
-              <GroupListPicker
-                label="Search for your future"
-                groupTypes={['team', 'org', 'department']}
-                defaultGroup="This"
-              />
               <EntityKindPicker initialFilter={initialKind} />
               <EntityTypePicker />
               <UserListPicker initialFilter={initiallySelectedFilter} />
