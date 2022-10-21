30e43717c7247caa55d998ce0981d9b5c335878f
Fredrik Adelöw <freben@gmail.com>
2022-10-21 17:12:36 +0200

    move HumanDuration to @backstage/types
    Signed-off-by: Fredrik Adelöw <freben@gmail.com>

--- packages/backend-tasks/src/deprecated.ts
+++ packages/backend-tasks/src/deprecated.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 The Backstage Authors
+ * Copyright 2022 The Backstage Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,12 +14,12 @@
  * limitations under the License.
  */
 
-/**
- * Common TypeScript types used within Backstage
- *
- * @packageDocumentation
- */
+import { HumanDuration as TypesHumanDuration } from '@backstage/types';
 
-export type { JsonArray, JsonObject, JsonPrimitive, JsonValue } from './json';
-export type { Observable, Observer, Subscription } from './observable';
-export type { HumanDuration } from './time';
+/**
+ * Human friendly durations object.
+ *
+ * @public
+ * @deprecated Import from `@backstage/types` instead
+ */
+export type HumanDuration = TypesHumanDuration;
