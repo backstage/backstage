ef9bb62876378f539e382f94ac0cc9a2ec6d45c6
Fredrik Adelöw <freben@gmail.com>
2022-10-21 17:12:36 +0200

    move HumanDuration to @backstage/types
    Signed-off-by: Fredrik Adelöw <freben@gmail.com>

--- packages/types/src/time.test.ts
+++ packages/types/src/time.test.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 The Backstage Authors
+ * Copyright 2022 The Backstage Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,12 +14,23 @@
  * limitations under the License.
  */
 
-/**
- * Common TypeScript types used within Backstage
- *
- * @packageDocumentation
- */
+import { HumanDuration } from './time';
+import { Duration } from 'luxon';
 
-export type { JsonArray, JsonObject, JsonPrimitive, JsonValue } from './json';
-export type { Observable, Observer, Subscription } from './observable';
-export type { HumanDuration } from './time';
+describe('time', () => {
+  describe('HumanDuration', () => {
+    const durations: HumanDuration[] = [
+      { years: 1 },
+      { months: 1 },
+      { weeks: 1 },
+      { days: 1 },
+      { hours: 1 },
+      { minutes: 1 },
+      { seconds: 1 },
+      { milliseconds: 1 },
+    ];
+    it.each(durations)('successfully parsed by luxon, %p', d => {
+      expect(Duration.fromObject(d).toObject()).toEqual(d);
+    });
+  });
+});
--- plugins/tech-insights-backend/src/service/fact/createFactRetriever.ts
+++ plugins/tech-insights-backend/src/service/fact/createFactRetriever.ts
@@ -13,7 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { HumanDuration } from '@backstage/backend-tasks';
+
+import { HumanDuration } from '@backstage/types';
 import {
   FactLifecycle,
   FactRetriever,
