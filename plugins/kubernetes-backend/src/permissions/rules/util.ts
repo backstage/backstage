f03cb385253ef77f186c68b820376990d60a4f6a
Ruben Vallejo <rvallejo@gmail.com>
2022-10-26 15:47:37 -0400

    rework kubernetespermissionrules to integrate with new permissionRuleParams type
    Signed-off-by: Ruben Vallejo <rvallejo@gmail.com>

--- plugins/kubernetes-backend/src/permissions/rules/util.ts
+++ plugins/kubernetes-backend/src/permissions/rules/util.ts
@@ -1,9 +1,11 @@
 import { KubernetesObjectTypes } from '@backstage/plugin-kubernetes-backend';
-import { RESOURCE_TYPE_KUBERNETES_RESOURCE } from '@backstage/plugin-kubernetes-common';
+import { KubernetesObjectSearchFilter, RESOURCE_TYPE_KUBERNETES_RESOURCE } from '@backstage/plugin-kubernetes-common';
 import {
   makeCreatePermissionRule,
   PermissionRule,
 } from '@backstage/plugin-permission-node';
+import { PermissionRuleParams } from '@backstage/plugin-permission-common';
+
 /**
  * Convenience type for {@link @backstage/plugin-permission-node#PermissionRule}
  * instances with the correct resource type and resource to work with
@@ -13,8 +15,10 @@ import {
  */
 
 //TODO: (rubenv-dev) switch KubernetesObjectTypes type requirement to something wider like ObjectToFetch to be used on the custom resources endpoint. 
-export type KubernetesPermissionRule<TParams extends unknown[] = unknown[]> =
-  PermissionRule<KubernetesObjectTypes, unknown,'kubernetes-resource', TParams>;
+
+export type KubernetesPermissionRule<
+  TParams extends PermissionRuleParams = PermissionRuleParams,
+> = PermissionRule<KubernetesObjectTypes, KubernetesObjectSearchFilter, 'kubernetes-resource', TParams>;
 
 /**
  * Helper function for creating correctly-typed
@@ -25,6 +29,6 @@ export type KubernetesPermissionRule<TParams extends unknown[] = unknown[]> =
  */
 export const createKubernetesPermissionRule = makeCreatePermissionRule<
   KubernetesObjectTypes,
-  unknown,
+  KubernetesObjectSearchFilter,
   typeof RESOURCE_TYPE_KUBERNETES_RESOURCE
 >();
