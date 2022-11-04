db52cb945ec1446ef03e5adf50784b84dc2fabcb
Aramis Sennyey <sennyeya@amazon.com>
2022-11-04 15:51:54 -0400

    Prettier fixes.
    Signed-off-by: Aramis Sennyey <sennyeya@amazon.com>

--- packages/cli/src/__fixtures__/test-project/packages/app/src/index.tsx
+++ packages/cli/src/__fixtures__/test-project/packages/app/src/index.tsx
@@ -13,5 +13,5 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export {}
-console.log('Hello World.')
+export {};
+console.log('Hello World.');
