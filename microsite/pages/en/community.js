5e14b067ecfe99553edc48df950cca244b54248e
thisisobate <obasiuche62@gmail.com>
2022-11-08 23:14:09 +0100

    chore: set background colors to blend well
    Signed-off-by: thisisobate <obasiuche62@gmail.com>

--- microsite/pages/en/community.js
+++ microsite/pages/en/community.js
@@ -149,7 +149,7 @@ const Background = props => {
         </Block.Container>
       </Block>
 
-      <Block className="stripe-top bg-teal-bottom">
+      <Block className="stripe-bottom bg-black-grey">
         <Block.Container wrapped style={{ justifyContent: 'flex-start' }}>
           <Block.TextBox wide>
             <Block.Subtitle>Community initiatives</Block.Subtitle>
@@ -188,26 +188,21 @@ const Background = props => {
           <Block.TextBox wide>
             <Block.Subtitle>Trainings and Certifications</Block.Subtitle>
           </Block.TextBox>
-          <Block.Container>
-            <Block.TextBox style={{ flexShrink: '1', alignSelf: 'stretch' }}>
-              <Block.Container column>
-                <BulletLine />
-                <Block.SmallTitle small>
-                  Introduction to Backstage: Developer Portals Made Easy
-                  (LFS142x)
-                </Block.SmallTitle>
-                <Block.Paragraph>
-                  This is a course produced and curated by the Linux Foundation.
-                  This course introduces you to Backstage and how to get started
-                  with the project.
-                </Block.Paragraph>
-              </Block.Container>
+          <Block.TextBox style={{ flexShrink: '1', alignSelf: 'stretch' }}>
+            <BulletLine />
+            <Block.SmallTitle small>
+              Introduction to Backstage: Developer Portals Made Easy (LFS142x)
+            </Block.SmallTitle>
+            <Block.Paragraph>
+              This is a course produced and curated by the Linux Foundation.
+              This course introduces you to Backstage and how to get started
+              with the project.
+            </Block.Paragraph>
 
-              <Block.LinkButton href="https://training.linuxfoundation.org/training/introduction-to-backstage-developer-portals-made-easy-lfs142x/">
-                Learn more
-              </Block.LinkButton>
-            </Block.TextBox>
-          </Block.Container>
+            <Block.LinkButton href="https://training.linuxfoundation.org/training/introduction-to-backstage-developer-portals-made-easy-lfs142x/">
+              Learn more
+            </Block.LinkButton>
+          </Block.TextBox>
         </Block.Container>
       </Block>
 
