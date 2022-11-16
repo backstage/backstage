a0ac28dc0641123a7074af624f03097ca37d78bf
Fredrik Adelöw <freben@gmail.com>
2022-11-16 11:21:22 +0100

    Flesh out the dupe checking script
    Signed-off-by: Fredrik Adelöw <freben@gmail.com>

--- scripts/verify-lockfile-duplicates.js
+++ scripts/verify-lockfile-duplicates.js
@@ -15,44 +15,116 @@
  * limitations under the License.
  */
 
+/* eslint-disable import/no-extraneous-dependencies */
+
 const { execFile: execFileCb } = require('child_process');
+const { resolve: resolvePath, dirname: dirnamePath } = require('path');
+const globby = require('globby');
 const { promisify } = require('util');
 
 const execFile = promisify(execFileCb);
 
-async function main() {
-  try {
-    const { stdout } = await execFile('yarn', ['dedupe', '--check'], {
-      shell: true,
+async function findLockFiles() {
+  const projectRoot = resolvePath(__dirname, '..');
+
+  let files = process.argv.slice(2).filter(arg => !arg.startsWith('--'));
+
+  for (const argumentFile of files) {
+    if (!argumentFile.match(/(?:^|[\/\\])yarn.lock$/)) {
+      throw new Error(`Not a yarn.lock file path argument: "${argumentFile}"`);
+    }
+  }
+
+  if (!files.length) {
+    // Locate all lock files that are in the root or in an immediate subdirectory
+    files = await globby(['*/yarn.lock', 'yarn.lock'], {
+      cwd: projectRoot,
     });
-    console.log(stdout);
-  } catch (error) {
-    const stdout = error.stdout?.trim();
-    const stderr = error.stderr?.trim();
+  }
+
+  return files.map(file => ({
+    fileRelativeToProjectRoot: file,
+    directoryRelativeToProjectRoot: dirnamePath(file),
+    directoryAbsolute: resolvePath(projectRoot, dirnamePath(file)),
+  }));
+}
+
+async function main() {
+  const lockFiles = await findLockFiles();
+
+  let fix = false;
+  for (const arg of process.argv) {
+    if (arg.startsWith('--')) {
+      if (arg === '--fix') {
+        fix = true;
+      } else {
+        throw new Error(`Unknown argument ${arg}`);
+      }
+    }
+  }
+
+  for (const lockFile of lockFiles) {
+    console.log('Checking lock file', lockFile.fileRelativeToProjectRoot);
+
+    let stdout;
+    let stderr;
+    let failed;
+
+    try {
+      const result = await execFile(
+        'yarn',
+        ['dedupe', ...(fix ? [] : ['--check'])],
+        {
+          shell: true,
+          cwd: lockFile.directoryAbsolute,
+        },
+      );
+      stdout = result.stdout?.trim();
+      stderr = result.stderr?.trim();
+      failed = false;
+    } catch (error) {
+      stdout = error.stdout?.trim();
+      stderr = error.stderr?.trim();
+      failed = true;
+    }
 
     if (stdout) {
       console.log(stdout);
     }
+
     if (stderr) {
       console.error(stderr);
     }
 
-    console.error('');
-    console.error(
-      '*************************************************************************************',
-    );
-    console.error(
-      '* You have duplicate versions of some packages in the yarn.lock file.               *',
-    );
-    console.error(
-      '* To solve this, run `yarn dedupe` and commit all yarn.lock changes.                *',
-    );
-    console.error(
-      '*************************************************************************************',
-    );
-    console.error('');
+    if (failed) {
+      if (!fix) {
+        const command = `yarn dedupe --cwd ${lockFile.directoryRelativeToProjectRoot}`;
+        const padding = ' '.repeat(Math.max(0, 85 - 6 - command.length));
+        console.error('');
+        console.error(
+          '*************************************************************************************',
+        );
+        console.error(
+          '* You have duplicate versions of some packages in a yarn.lock file.                 *',
+        );
+        console.error(
+          '* To solve this, run the following command from the project root and commit all     *',
+        );
+        console.log(
+          '* yarn.lock changes.                                                                *',
+        );
+        console.log(
+          '*                                                                                   *',
+        );
+        console.log(`*   ${command}${padding} *`);
+        console.error(
+          '*************************************************************************************',
+        );
+        console.error('');
+      }
 
-    process.exit(1);
+      process.exit(1);
+    }
   }
 }
 
