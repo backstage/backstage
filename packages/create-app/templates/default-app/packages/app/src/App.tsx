ea5a175ec52969b1c321e6865bee84a2933ad7fe
Matcha @sennder <matcha.desoutter@sennder.com>
2022-11-22 16:00:26 +0100

    style: apply prettier to create-app's template to include TechDocsRedirect
    Signed-off-by: Matcha @sennder <matcha.desoutter@sennder.com>

--- packages/create-app/templates/default-app/packages/app/src/App.tsx
+++ packages/create-app/templates/default-app/packages/app/src/App.tsx
@@ -20,7 +20,10 @@ import {
   TechDocsReaderPage,
 } from '@backstage/plugin-techdocs';
 import { TechDocsAddons } from '@backstage/plugin-techdocs-react';
-import { ReportIssue, TechDocsRedirect } from '@backstage/plugin-techdocs-module-addons-contrib';
+import {
+  ReportIssue,
+  TechDocsRedirect,
+} from '@backstage/plugin-techdocs-module-addons-contrib';
 import { UserSettingsPage } from '@backstage/plugin-user-settings';
 import { apis } from './apis';
 import { entityPage } from './components/catalog/EntityPage';
