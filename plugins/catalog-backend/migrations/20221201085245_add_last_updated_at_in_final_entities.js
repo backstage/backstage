facbae6feff8b6715b75722e499f4fe876a8d6f7
iris <hellocomplex007@gmail.com>
2022-12-01 17:00:32 +0800

    Add last_updated_at in final_entities table
    Signed-off-by: iris <hellocomplex007@gmail.com>

---
+++ plugins/catalog-backend/migrations/20221201085245_add_last_updated_at_in_final_entities.js
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2022 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+exports.up = async function up(knex) {
+  await knex.schema.table('final_entities', table => {
+    table.timestamp('last_updated_at').nullable();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function down(knex) {
+  await knex.schema.table('final_entities', table => {
+    table.dropColumn('last_updated_at');
+  });
+};
