533a1a2db2929060a6150e5519a3a7cb1dd940e7
MALIN WIDÈN <malwi130@student.liu.se>
2022-12-07 14:18:41 +0100

    update to cypress 10
    Signed-off-by: MALIN WIDÈN <malwi130@student.liu.se>

--- packages/techdocs-cli/cypress/plugins/index.js
+++ packages/techdocs-cli/cypress/plugins/index.js
@@ -27,17 +27,4 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-const {
-  initPlugin,
-} = require('@frsource/cypress-plugin-visual-regression-diff/plugin');
-
-/**
- * @type {Cypress.PluginConfig}
- */
-// eslint-disable-next-line no-unused-vars
-module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-  initPlugin(on, config);
-  return config;
-};
+require('@frsource/cypress-plugin-visual-regression-diff/plugin');
