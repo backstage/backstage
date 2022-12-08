1e1bc430eba97ca379ea003ba518a60468702b06
Brian Fletcher <brian@roadie.io>
2022-12-08 13:38:03 +0000

    remove testing content
    Signed-off-by: Brian Fletcher <brian@roadie.io>

--- plugins/catalog-react/src/components/EntityPeekAheadPopover/EntityPeekAheadPopover.tsx
+++ plugins/catalog-react/src/components/EntityPeekAheadPopover/EntityPeekAheadPopover.tsx
@@ -127,15 +127,7 @@ export const EntityPeekAheadPopover = ({
               <>
                 <Typography color="textSecondary">{entity.kind}</Typography>
                 <Typography className={classes.descriptionTypography} paragraph>
-                  {/* {entity.metadata.description} */}
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
+                  {entity.metadata.description}
                 </Typography>
                 <Typography>{entity.spec?.type}</Typography>
                 <Box marginTop="0.5em">
