88abf5378d304892abf38cf030749c146b703fe8
Paul Cowan <paul.cowan@cutting.scot>
2022-12-12 16:30:53 +0000

    remove SecurityTab
    Signed-off-by: Paul Cowan <paul.cowan@cutting.scot>

--- plugins/scaffolder/src/next/EmbeddedScaffolderWorkflow/EmbeddedScaffolderWorkflow.test.tsx
+++ plugins/scaffolder/src/next/EmbeddedScaffolderWorkflow/EmbeddedScaffolderWorkflow.test.tsx
@@ -13,5 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export { SecurityTab } from './SecurityTab';
-export { showSecurityTab } from './showSecurityTab';
+describe('<EmbeddedScaffolderWorkflow />', () => {
+  it('should embed workflow inside another component', () => {});
+});
