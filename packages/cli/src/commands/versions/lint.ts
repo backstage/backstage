536d3e764af41a4600982d0a96200fd0549af26d
Patrik Oldsberg <poldsberg@gmail.com>
2022-12-13 23:45:08 +0100

    cli: fix missing arg for lockfile.save()
    Signed-off-by: Patrik Oldsberg <poldsberg@gmail.com>

--- packages/cli/src/commands/versions/lint.ts
+++ packages/cli/src/commands/versions/lint.ts
@@ -53,7 +53,8 @@ export default async (cmd: OptionValues) => {
 
   let success = true;
 
-  const lockfile = await Lockfile.load(paths.resolveTargetRoot('yarn.lock'));
+  const lockfilePath = paths.resolveTargetRoot('yarn.lock');
+  const lockfile = await Lockfile.load(lockfilePath);
   const result = lockfile.analyze({
     filter: includedFilter,
     localPackages: PackageGraph.fromPackages(
@@ -69,7 +70,7 @@ export default async (cmd: OptionValues) => {
 
   if (fix) {
     lockfile.replaceVersions(result.newVersions);
-    await lockfile.save();
+    await lockfile.save(lockfilePath);
   } else {
     const [newVersionsForbidden, newVersionsAllowed] = partition(
       result.newVersions,
