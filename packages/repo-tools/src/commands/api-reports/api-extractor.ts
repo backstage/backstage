c210d70218344e51d52ac7bafab1305a840ceb05
djamaile <rdjamaile@gmail.com>
2022-12-15 14:46:21 +0100

    chore: add try and catch around require resolve
    Signed-off-by: djamaile <rdjamaile@gmail.com>

--- packages/repo-tools/src/commands/api-reports/api-extractor.ts
+++ packages/repo-tools/src/commands/api-reports/api-extractor.ts
@@ -14,6 +14,8 @@
  * limitations under the License.
  */
 
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-restricted-imports */
 import {
   resolve as resolvePath,
   relative as relativePath,
