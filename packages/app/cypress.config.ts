99abb6e598795bd98501fe1d5a2dfb0597e4ed90
pbertrand <pbertrand@deezer.com>
2022-12-21 14:34:12 +0100

    Cypress config for app should be like this i think
    Signed-off-by: pbertrand <pbertrand@deezer.com>

--- packages/app/cypress.config.ts
+++ packages/app/cypress.config.ts
@@ -19,6 +19,7 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
     fixturesFolder: false,
-    supportFile: 'cypress/**/*.js',
+    specPattern: './cypress/integration/**/*.js',
+    supportFile: './cypress/support/index.js',
   },
 });
