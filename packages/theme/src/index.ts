543477fb3e16527e620c1c217ea9c8e0f8036e5a
Patrik Oldsberg <poldsberg@gmail.com>
2022-12-30 14:47:36 +0100

    theme: namespace MUI v4 methods and types
    Signed-off-by: Patrik Oldsberg <poldsberg@gmail.com>

--- packages/theme/src/index.ts
+++ packages/theme/src/index.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 The Backstage Authors
+ * Copyright 2022 The Backstage Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,7 +20,37 @@
  * @packageDocumentation
  */
 
-export * from './themes';
-export * from './baseTheme';
-export * from './types';
-export * from './pageTheme';
+export * from './v4';
+export type {
+  BackstagePaletteAdditions,
+  PageTheme,
+  PageThemeSelector,
+} from './types';
+
+import {
+  createV4Theme,
+  createV4ThemeOptions,
+  createV4ThemeOverrides,
+} from './v4';
+import type { SimpleV4ThemeOptions } from './v4';
+
+/**
+ * @public
+ * @deprecated Use {@link createV4Theme} instead.
+ */
+export const createTheme = createV4Theme;
+/**
+ * @public
+ * @deprecated Use {@link createV4ThemeOptions} instead.
+ */
+export const createThemeOptions = createV4ThemeOptions;
+/**
+ * @public
+ * @deprecated Use {@link createV4ThemeOverrides} instead.
+ */
+export const createThemeOverrides = createV4ThemeOverrides;
+/**
+ * @public
+ * @deprecated Use {@link SimpleV4ThemeOptions} instead.
+ */
+export type SimpleThemeOptions = SimpleV4ThemeOptions;
