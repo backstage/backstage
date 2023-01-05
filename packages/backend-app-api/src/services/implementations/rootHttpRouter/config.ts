1fed5327c37827106eab88ee76855113b8d329e7
Patrik Oldsberg <poldsberg@gmail.com>
2023-01-05 01:08:34 +0100

    backend-app-api: split out readCorsOptions
    Signed-off-by: Patrik Oldsberg <poldsberg@gmail.com>

--- packages/backend-app-api/src/services/implementations/rootHttpRouter/config.ts
+++ packages/backend-app-api/src/services/implementations/rootHttpRouter/config.ts
@@ -15,8 +15,6 @@
  */
 
 import { Config } from '@backstage/config';
-import { CorsOptions } from 'cors';
-import { Minimatch } from 'minimatch';
 
 export type BaseOptions = {
   listenPort?: string | number;
@@ -44,47 +42,6 @@ export type CertificateAttributes = {
  * A map from CSP directive names to their values.
  */
 export type CspOptions = Record<string, string[]>;
-
-type StaticOrigin = boolean | string | RegExp | (boolean | string | RegExp)[];
-
-type CustomOrigin = (
-  requestOrigin: string | undefined,
-  callback: (err: Error | null, origin?: StaticOrigin) => void,
-) => void;
-
-/**
- * Attempts to read a CORS options object from the root of a config object.
- *
- * @param config - The root of a backend config object
- * @returns A CORS options object, or undefined if not specified
- *
- * @example
- * ```json
- * {
- *   cors: {
- *    origin: "http://localhost:3000",
- *    credentials: true
- *   }
- * }
- * ```
- */
-export function readCorsOptions(config: Config): CorsOptions | undefined {
-  const cc = config.getOptionalConfig('cors');
-  if (!cc) {
-    return undefined;
-  }
-
-  return removeUnknown({
-    origin: createCorsOriginMatcher(getOptionalStringOrStrings(cc, 'origin')),
-    methods: getOptionalStringOrStrings(cc, 'methods'),
-    allowedHeaders: getOptionalStringOrStrings(cc, 'allowedHeaders'),
-    exposedHeaders: getOptionalStringOrStrings(cc, 'exposedHeaders'),
-    credentials: cc.getOptionalBoolean('credentials'),
-    maxAge: cc.getOptionalNumber('maxAge'),
-    preflightContinue: cc.getOptionalBoolean('preflightContinue'),
-    optionsSuccessStatus: cc.getOptionalNumber('optionsSuccessStatus'),
-  });
-}
 
 /**
  * Attempts to read a CSP options object from the root of a config object.
@@ -120,66 +77,4 @@ export function readCspOptions(
   }
 
   return result;
-}
-
-function getOptionalStringOrStrings(
-  config: Config,
-  key: string,
-): string | string[] | undefined {
-  const value = config.getOptional(key);
-  if (value === undefined || isStringOrStrings(value)) {
-    return value;
-  }
-  throw new Error(`Expected string or array of strings, got ${typeof value}`);
-}
-
-function createCorsOriginMatcher(
-  originValue: string | string[] | undefined,
-): CustomOrigin | undefined {
-  if (originValue === undefined) {
-    return originValue;
-  }
-
-  if (!isStringOrStrings(originValue)) {
-    throw new Error(
-      `Expected string or array of strings, got ${typeof originValue}`,
-    );
-  }
-
-  const allowedOrigin =
-    typeof originValue === 'string' ? [originValue] : originValue;
-
-  const allowedOriginPatterns =
-    allowedOrigin?.map(
-      pattern => new Minimatch(pattern, { nocase: true, noglobstar: true }),
-    ) ?? [];
-
-  return (origin, callback) => {
-    return callback(
-      null,
-      allowedOriginPatterns.some(pattern => pattern.match(origin ?? '')),
-    );
-  };
-}
-
-function isStringOrStrings(value: any): value is string | string[] {
-  return typeof value === 'string' || isStringArray(value);
-}
-
-function isStringArray(value: any): value is string[] {
-  if (!Array.isArray(value)) {
-    return false;
-  }
-  for (const v of value) {
-    if (typeof v !== 'string') {
-      return false;
-    }
-  }
-  return true;
-}
-
-function removeUnknown<T extends object>(obj: T): T {
-  return Object.fromEntries(
-    Object.entries(obj).filter(([, v]) => v !== undefined),
-  ) as T;
 }
