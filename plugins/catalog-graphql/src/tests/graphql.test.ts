f3416a66c558954f77135c8bbdd63449f243d90c
Dmitriy Lazarev <w@kich.dev>
2023-01-05 09:06:04 +0100

    Fix tests
    Signed-off-by: Dmitriy Lazarev <w@kich.dev>

--- plugins/catalog-graphql/src/tests/graphql.test.ts
+++ plugins/catalog-graphql/src/tests/graphql.test.ts
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 
+/* eslint-disable func-names */
 /* eslint-disable jest/no-standalone-expect */
 
 import { describe, beforeAll, it } from '@effection/jest';
