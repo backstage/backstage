c52fcca32c9595032874e32ecd547db24e666274
Patrik Oldsberg <poldsberg@gmail.com>
2023-01-14 14:16:34 +0100

    cli: add exports support for package builds
    Signed-off-by: Patrik Oldsberg <poldsberg@gmail.com>

--- packages/cli/src/lib/builder/types.ts
+++ packages/cli/src/lib/builder/types.ts
@@ -14,6 +14,8 @@
  * limitations under the License.
  */
 
+import { ExtendedPackageJSON } from '../monorepo';
+
 export enum Output {
   esm,
   cjs,
@@ -23,6 +25,7 @@ export enum Output {
 export type BuildOptions = {
   logPrefix?: string;
   targetDir?: string;
+  packageJson?: ExtendedPackageJSON;
   outputs: Set<Output>;
   minify?: boolean;
   useApiExtractor?: boolean;
