9fa7a7b98184b9d7fd07659607df571156b5e658
Patrik Oldsberg <poldsberg@gmail.com>
2023-01-16 10:41:48 +0100

    app-backend: migrate to use exports
    Signed-off-by: Patrik Oldsberg <poldsberg@gmail.com>

--- plugins/app-backend/src/alpha.ts
+++ plugins/app-backend/src/alpha.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 The Backstage Authors
+ * Copyright 2023 The Backstage Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,10 +14,5 @@
  * limitations under the License.
  */
 
-/**
- * A Backstage backend plugin that serves the Backstage frontend app
- *
- * @packageDocumentation
- */
-
-export * from './service/router';
+export { appPlugin } from './service/appPlugin';
+export type { AppPluginOptions } from './service/appPlugin';
