2d6e535e2f5e8c1d65c06e627db8376abc4f5d49
Patrik Oldsberg <poldsberg@gmail.com>
2023-01-16 01:04:13 +0100

    events-node: migrate to use exports
    Signed-off-by: Patrik Oldsberg <poldsberg@gmail.com>

--- plugins/events-node/src/alpha.ts
+++ plugins/events-node/src/alpha.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 The Backstage Authors
+ * Copyright 2023 The Backstage Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,10 +14,5 @@
  * limitations under the License.
  */
 
-/**
- * The events-node module for `@backstage/plugin-events-backend`.
- *
- * @packageDocumentation
- */
-
-export * from './api';
+export type { EventsExtensionPoint } from './extensions';
+export { eventsExtensionPoint } from './extensions';
