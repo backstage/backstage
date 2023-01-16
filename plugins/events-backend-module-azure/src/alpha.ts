c4a9541f3ab91bb377a73289b2ad94ede597f2d1
Patrik Oldsberg <poldsberg@gmail.com>
2023-01-16 09:48:22 +0100

    events-backend-module-azure: migrate to use exports
    Signed-off-by: Patrik Oldsberg <poldsberg@gmail.com>

--- plugins/events-backend-module-azure/src/alpha.ts
+++ plugins/events-backend-module-azure/src/alpha.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 The Backstage Authors
+ * Copyright 2023 The Backstage Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,11 +14,4 @@
  * limitations under the License.
  */
 
-/**
- * The module "azure" for the Backstage backend plugin "events-backend"
- * adding an event router for Azure DevOps.
- *
- * @packageDocumentation
- */
-
-export { AzureDevOpsEventRouter } from './router/AzureDevOpsEventRouter';
+export { azureDevOpsEventRouterEventsModule } from './service/AzureDevOpsEventRouterEventsModule';
--- plugins/events-backend-module-azure/src/service/AzureDevOpsEventRouterEventsModule.test.ts
+++ plugins/events-backend-module-azure/src/service/AzureDevOpsEventRouterEventsModule.test.ts
@@ -15,7 +15,7 @@
  */
 
 import { startTestBackend } from '@backstage/backend-test-utils';
-import { eventsExtensionPoint } from '@backstage/plugin-events-node';
+import { eventsExtensionPoint } from '@backstage/plugin-events-node/alpha';
 import { azureDevOpsEventRouterEventsModule } from './AzureDevOpsEventRouterEventsModule';
 import { AzureDevOpsEventRouter } from '../router/AzureDevOpsEventRouter';
 
