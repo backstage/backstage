dac6ddfae808ab1565ae337b129b607f32d458f2
Patrik Oldsberg <poldsberg@gmail.com>
2023-01-16 09:57:40 +0100

    catalog-node: migrate to use exports
    Signed-off-by: Patrik Oldsberg <poldsberg@gmail.com>

--- plugins/catalog-node/src/alpha.ts
+++ plugins/catalog-node/src/alpha.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 The Backstage Authors
+ * Copyright 2023 The Backstage Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,11 +14,6 @@
  * limitations under the License.
  */
 
-/**
- * The catalog-backend-node module for `@backstage/plugin-catalog-backend`.
- *
- * @packageDocumentation
- */
-
-export * from './api';
-export * from './processing';
+export { catalogServiceRef } from './catalogService';
+export type { CatalogProcessingExtensionPoint } from './extensions';
+export { catalogProcessingExtensionPoint } from './extensions';
