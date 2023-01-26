8f23856aa9d7fdd5636f7b2ca564baae1cd3846c
Jenah Blitz <jenah.blitz@simplybusiness.com>
2023-01-26 14:12:53 -0500

    Export isAirbrakeAvailable function for use in EntityPage
    Signed-off-by: Jenah Blitz <jenah.blitz@simplybusiness.com>

--- plugins/airbrake/src/components/isAirbrakeAvailable.ts
+++ plugins/airbrake/src/components/isAirbrakeAvailable.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 The Backstage Authors
+ * Copyright 2020 The Backstage Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,12 +14,8 @@
  * limitations under the License.
  */
 
-/**
- * The Airbrake plugin provides connectivity between Backstage and Airbrake (https://airbrake.io/).
- *
- * @packageDocumentation
- */
+import { Entity } from '@backstage/catalog-model';
+import { AIRBRAKE_PROJECT_ID_ANNOTATION } from './useProjectId';
 
-export { airbrakePlugin } from './plugin';
-export { EntityAirbrakeContent } from './extensions';
-export { isAirbrakeAvailable } from './components/isAirbrakeAvailable';
+export const isAirbrakeAvailable = (entity: Entity) =>
+  Boolean(entity.metadata.annotations?.[AIRBRAKE_PROJECT_ID_ANNOTATION]);
