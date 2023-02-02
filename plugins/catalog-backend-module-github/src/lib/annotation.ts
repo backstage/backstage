4a123fe82a823c33d81c426a59c2e073ceec309d
Jack Grigg <jrgrigg@gmail.com>
2023-02-02 11:06:06 +1100

    Prefer the github login annotation over the name field when applying location annotations
    Signed-off-by: Jack Grigg <jrgrigg@gmail.com>

---
+++ plugins/catalog-backend-module-github/src/lib/annotation.ts
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2020 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * The value of this annotation is the so-called login that identifies a user on
+[GitHub](https://github.com)
+ *
+ * @public
+ */
+export const ANNOTATION_GITHUB_USER_LOGIN = 'github.com/user-login';
