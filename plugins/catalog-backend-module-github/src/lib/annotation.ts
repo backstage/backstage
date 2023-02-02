20573bfe6a39c19ebdf4e9bd01e7c88eefa2295a
Jack Grigg <jrgrigg@gmail.com>
2023-02-02 15:52:59 +1100

    Fix comment
    Signed-off-by: Jack Grigg <jrgrigg@gmail.com>

--- plugins/catalog-backend-module-github/src/lib/annotation.ts
+++ plugins/catalog-backend-module-github/src/lib/annotation.ts
@@ -16,7 +16,10 @@
 
 /**
  * The value of this annotation is the so-called login that identifies a user on
-[GitHub](https://github.com)
+[GitHub](https://github.com) (either the public one, or a private GitHub
+Enterprise installation) that is related to this entity. It is on the format
+`<username>`, and is the same as can be seen in the URL location bar of the
+browser when viewing that user.
  *
  * @public
  */
