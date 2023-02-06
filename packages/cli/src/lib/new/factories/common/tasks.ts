f77555a0907eddffc1cd4fd7d302b44412ae7956
Hunter Bacot <hsbacot@gmail.com>
2023-02-06 09:46:54 -0800

    First crack or cloning templates from external git

--- packages/cli/src/lib/new/factories/common/tasks.ts
+++ packages/cli/src/lib/new/factories/common/tasks.ts
@@ -23,6 +23,13 @@ import { Lockfile } from '../../../versioning';
 import { createPackageVersionProvider } from '../../../version';
 import { CreateContext } from '../../types';
 
+// Inspired by https://github.com/xxorax/node-shell-escape/blob/master/shell-escape.js
+function escapeShellArg(s) {
+  let res = `'${s.replace(/'/g, "'\\''")}'`;
+  res = res.replace(/^(?:'')+/g, '').replace(/\\'''/g, "\\'");
+  return res;
+}
+
 export async function executePluginPackageTemplate(
   ctx: CreateContext,
   options: {
@@ -80,6 +87,80 @@ export async function executePluginPackageTemplate(
       );
     });
   });
+
+  ctx.markAsModified();
+}
+
+export async function executePluginTemplateFromGitURL(
+  ctx: CreateContext,
+  options: {
+    templateGitURL: string;
+    targetDir: string;
+    values: Record<string, unknown>;
+  },
+) {
+  const { targetDir, templateGitURL } = options;
+
+  let lockfile: Lockfile | undefined;
+  try {
+    lockfile = await Lockfile.load(paths.resolveTargetRoot('yarn.lock'));
+  } catch {
+    /* ignored */
+  }
+
+  Task.section('Checking Prerequisites');
+  const shortPluginDir = relativePath(paths.targetRoot, targetDir);
+  await Task.forItem('availability', shortPluginDir, async () => {
+    if (await fs.pathExists(targetDir)) {
+      throw new Error(
+        `A package with the same plugin ID already exists at ${chalk.cyan(
+          shortPluginDir,
+        )}. Please try again with a different ID.`,
+      );
+    }
+  });
+
+  const tempDir = await Task.forItem('creating', 'temp dir', async () => {
+    return await ctx.createTemporaryDirectory('backstage-create');
+  });
+
+  // construct clone command of passed url
+  Task.section(`Cloning Template - ${templateGitURL}`);
+  const gitCloneCommand = `git clone ${escapeShellArg(
+    templateGitURL,
+  )} ${tempDir}`;
+
+  //
+  await Task.forCommand(gitCloneCommand);
+
+  await Task.forCommand('rm -rf .git', { cwd: tempDir });
+
+  Task.section('Executing Template');
+  await templatingTask(
+    paths.resolveOwn(tempDir),
+    tempDir,
+    options.values,
+    createPackageVersionProvider(lockfile),
+    ctx.isMonoRepo,
+  );
+
+  // Format package.json if it exists
+  const pkgJsonPath = resolvePath(tempDir, 'package.json');
+  if (await fs.pathExists(pkgJsonPath)) {
+    const pkgJson = await fs.readJson(pkgJsonPath);
+    await fs.writeJson(pkgJsonPath, pkgJson, { spaces: 2 });
+  }
+
+  Task.section('Installing');
+  await Task.forItem('moving', shortPluginDir, async () => {
+    await fs.move(tempDir, targetDir).catch(error => {
+      throw new Error(
+        `Failed to move package from ${tempDir} to ${targetDir}, ${error.message}`,
+      );
+    });
+  });
+
+  Task.forCommand('yarn install', { cwd: paths.ownRoot });
 
   ctx.markAsModified();
 }
