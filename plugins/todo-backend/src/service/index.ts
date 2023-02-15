2040d38d39ef359f0eb208700882cff34890ffb0
Oleg S <97077423+RobotSail@users.noreply.github.com>
2023-02-15 14:31:50 -0500

    expose the todos-backend plugin using the new plugin system
    Signed-off-by: Oleg S <97077423+RobotSail@users.noreply.github.com>

--- plugins/todo-backend/src/service/index.ts
+++ plugins/todo-backend/src/service/index.ts
@@ -19,3 +19,5 @@ export type { RouterOptions } from './router';
 export type { TodoService, ListTodosRequest, ListTodosResponse } from './types';
 export { TodoReaderService } from './TodoReaderService';
 export type { TodoReaderServiceOptions } from './TodoReaderService';
+export { todosPlugin } from './plugin';
+export type { TodosPluginDependencies } from './plugin';
