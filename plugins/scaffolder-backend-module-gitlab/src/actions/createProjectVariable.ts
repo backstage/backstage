36e9f2faadf396e296774b427509ab388834dcab
Dominik <dominik@pfaffenbauer.at>
2023-02-16 08:18:56 +0100

    Update plugins/scaffolder-backend-module-gitlab/src/actions/createProjectVariable.ts
    Co-authored-by: Andreas Berger <Andy2003@users.noreply.github.com>
    Signed-off-by: Dominik <dominik@pfaffenbauer.at>

--- plugins/scaffolder-backend-module-gitlab/src/actions/createProjectVariable.ts
+++ plugins/scaffolder-backend-module-gitlab/src/actions/createProjectVariable.ts
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import { createTemplateAction } from '@backstage/plugin-scaffolder-backend';
+import { createTemplateAction } from '@backstage/plugin-scaffolder-node';
 import { ScmIntegrationRegistry } from '@backstage/integration';
 import { getToken } from '../util';
 import { Gitlab } from '@gitbeaker/node';
