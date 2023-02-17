229db22d34a89044f775680db3d2c5cc1e6259cc
Jonathan Mezach <jonathan.mezach@rr-wfm.com>
2023-02-17 15:42:31 +0100

    Expose annotation constant in API
    Signed-off-by: Jonathan Mezach <jonathan.mezach@rr-wfm.com>
    Signed-off-by: Bogdan Nechyporenko <bnechyporenko@bol.com>

--- plugins/octopus-deploy/src/constants.ts
+++ plugins/octopus-deploy/src/constants.ts
@@ -13,4 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+/** @public */
 export const OCTOPUS_DEPLOY_PROJECT_ID_ANNOTATION = 'octopus.com/project-id';
