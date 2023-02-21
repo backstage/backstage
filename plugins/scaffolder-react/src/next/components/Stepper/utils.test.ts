0c219d9c18763cf126d482a4dbb189fde8a855a0
Paul Cowan <paul.cowan@cutting.scot>
2023-02-21 13:08:30 +0000

    remove unused import
    Signed-off-by: Paul Cowan <paul.cowan@cutting.scot>

--- plugins/scaffolder-react/src/next/components/Stepper/utils.test.ts
+++ plugins/scaffolder-react/src/next/components/Stepper/utils.test.ts
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { type FormValidation } from './createAsyncValidators';
 import { hasErrors } from './utils';
 
 describe('hasErrors', () => {
