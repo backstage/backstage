b8cd7a97a94aab4e80d87cc0b1cb2a33345b1736
djamaile <rdjamaile@gmail.com>
2023-03-07 10:29:53 +0100

    chore: delete uffizi workflows
    Signed-off-by: djamaile <rdjamaile@gmail.com>

--- packages/cli/src/commands/deploy/programs/aws.ts
+++ packages/cli/src/commands/deploy/programs/aws.ts
@@ -14,20 +14,76 @@
  * limitations under the License.
  */
 
+import chalk from 'chalk';
 import * as awsx from '@pulumi/awsx';
+import * as aws from '@pulumi/aws';
 import { OptionValues } from 'commander';
 import { resolve } from 'path';
 
 export const AWSProgram = (opts: OptionValues) => {
   return async () => {
-    const repository = new awsx.ecr.Repository(`${opts.stack}-repository`, {
+    const repository = new awsx.ecr.Repository(`${opts.stack}`, {
       name: opts.stack,
     });
-    // @ts-ignore
-    const image = new awsx.ecr.Image('${opts.stack}-image', {
+
+    const image = new awsx.ecr.Image(`${opts.stack}-image`, {
       repositoryUrl: repository.url,
       path: resolve('.'),
       dockerfile: opts.dockerfile,
+    });
+
+    // create lightsail instance
+    const CONTAINER_NAME = `${opts.stack}-container`;
+    const containerService = new aws.lightsail.ContainerService(
+      CONTAINER_NAME,
+      {
+        name: `${opts.stack}-container-service`,
+        isDisabled: false,
+        power: 'nano',
+        scale: 1,
+        tags: {
+          backstage: opts.stack,
+        },
+      },
+    );
+
+    /* eslint-disable no-new */
+    new aws.lightsail.ContainerServiceDeploymentVersion(
+      `${opts.stack}-deployment`,
+      {
+        containers: [
+          {
+            containerName: CONTAINER_NAME,
+            image: image.imageUri,
+            commands: [],
+            ports: {
+              '7007': 'HTTP',
+            },
+            environment: {
+              BACKSTAGE_HOST: containerService.url,
+            },
+          },
+        ],
+        publicEndpoint: {
+          containerName: CONTAINER_NAME,
+          containerPort: 7007,
+          healthCheck: {
+            healthyThreshold: 2,
+            unhealthyThreshold: 2,
+            timeoutSeconds: 2,
+            intervalSeconds: 5,
+            path: '/',
+            successCodes: '200-499',
+          },
+        },
+        serviceName: containerService.name,
+      },
+    );
+
+    containerService.url.apply(url => {
+      process.stderr.write(
+        chalk.yellowBright(`\n\nInstance will be live at: ${url}\n\n`),
+      );
     });
   };
 };
