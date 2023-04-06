d35a9d79f3bafb9f3abce4800f0a58d9a35f98a8
Emma Indal <emma.indahl@gmail.com>
2023-04-06 16:01:03 +0200

    add aria label to close button in custom field explorer
    Signed-off-by: Emma Indal <emma.indahl@gmail.com>

--- plugins/scaffolder/src/components/TemplateEditorPage/CustomFieldExplorer.tsx
+++ plugins/scaffolder/src/components/TemplateEditorPage/CustomFieldExplorer.tsx
@@ -144,7 +144,7 @@ export const CustomFieldExplorer = ({
           </Select>
         </FormControl>
 
-        <IconButton size="medium" onClick={onClose}>
+        <IconButton size="medium" onClick={onClose} aria-label="Close">
           <CloseIcon />
         </IconButton>
       </div>
