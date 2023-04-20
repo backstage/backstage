e10cffa02e05b0bf262575e51950d0c2881dbf30
Joost Hofman <joost.hofman@ah.nl>
2023-04-20 16:25:28 +0200

    Move RadarTimeline to it's own component
    Signed-off-by: Joost Hofman <joost.hofman@ah.nl>

--- plugins/tech-radar/src/components/RadarDescription/RadarDescription.tsx
+++ plugins/tech-radar/src/components/RadarDescription/RadarDescription.tsx
@@ -22,18 +22,7 @@ import LinkIcon from '@material-ui/icons/Link';
 import { Link, MarkdownContent } from '@backstage/core-components';
 import { isValidUrl } from '../../utils/components';
 import type { EntrySnapshot } from '../../utils/types';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
-import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
-import AdjustIcon from '@material-ui/icons/Adjust';
-import { MovedState } from '../../api';
+import { RadarTimeline } from '../RadarTimeline';
 
 export type Props = {
   open: boolean;
@@ -62,62 +51,7 @@ const RadarDescription = (props: Props): JSX.Element => {
       </DialogTitle>
       <DialogContent dividers>
         <MarkdownContent content={description} />
-        <Typography variant="h6" gutterBottom>
-          History
-        </Typography>
-        <TableContainer component={Paper}>
-          <Table aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell align="left">Moved in direction</TableCell>
-                <TableCell align="left">Moved to ring</TableCell>
-                <TableCell align="left">Moved on date</TableCell>
-                <TableCell align="left">Description</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {timeline?.length === 0 && (
-                <TableRow key="no-timeline">
-                  <TableCell component="th" scope="row">
-                    No Timeline
-                  </TableCell>
-                </TableRow>
-              )}
-              {timeline?.map(timeEntry => (
-                <TableRow key={timeEntry.description}>
-                  <TableCell component="th" scope="row">
-                    {timeEntry.moved === MovedState.Up ? (
-                      <ArrowUpwardIcon />
-                    ) : (
-                      ''
-                    )}
-                    {timeEntry.moved === MovedState.Down ? (
-                      <ArrowDownwardIcon />
-                    ) : (
-                      ''
-                    )}
-                    {timeEntry.moved === MovedState.NoChange ? (
-                      <AdjustIcon />
-                    ) : (
-                      ''
-                    )}
-                  </TableCell>
-                  <TableCell align="left">
-                    {timeEntry.ring.name ? timeEntry.ring.name : ''}
-                  </TableCell>
-                  <TableCell align="left">
-                    {timeEntry.date.toLocaleDateString()
-                      ? timeEntry.date.toLocaleDateString()
-                      : ''}
-                  </TableCell>
-                  <TableCell align="left">
-                    {timeEntry.description ? timeEntry.description : ''}
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+        <RadarTimeline timeline={timeline} />
       </DialogContent>
       {showDialogActions(url, links) && (
         <DialogActions>
