40460441f516f1a915974bc00f86ed4fa8de14a1
josh <josh.timmons@hashicorp.com>
2023-06-09 23:26:44 -0400

    Use node-fetch in backend plugin
    Signed-off-by: josh <josh.timmons@hashicorp.com>
    Signed-off-by: Shaun Brown <shaunbrown@capitecbank.co.za>

--- plugins/nomad-backend/src/service/router.ts
+++ plugins/nomad-backend/src/service/router.ts
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+import fetch from 'node-fetch';
 import { errorHandler, requestLoggingHandler } from '@backstage/backend-common';
 import { Config } from '@backstage/config';
 import { InputError } from '@backstage/errors';
