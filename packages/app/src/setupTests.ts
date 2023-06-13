b72df3378ec8d4469444cab90c2cef545b3118eb
Patrik Oldsberg <poldsberg@gmail.com>
2023-06-13 15:39:37 +0200

    Apply suggestions from code review
    Co-authored-by: Fredrik Adelöw <freben@gmail.com>
    Signed-off-by: Patrik Oldsberg <poldsberg@gmail.com>

--- packages/app/src/setupTests.ts
+++ packages/app/src/setupTests.ts
@@ -19,4 +19,4 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
-import 'cross-fetch';
+import 'cross-fetch/polyfill';
