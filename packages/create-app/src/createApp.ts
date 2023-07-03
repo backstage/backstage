76b83a4422bb45fc755a62aa259ba5016f269b8a
Ant Weiss <anton@stagecentral.io>
2023-07-03 12:02:51 +0300

    Fix #18524 - add instruction to install deps when run with --skip-install
    Signed-off-by: Ant Weiss <anton@stagecentral.io>

--- packages/create-app/src/createApp.ts
+++ packages/create-app/src/createApp.ts
@@ -129,6 +129,13 @@ export default async (opts: OptionValues): Promise<void> => {
     );
     Task.log();
     Task.section('All set! Now you might want to');
+    if (!opts.skipInstall) {
+      Task.log(
+        `  Install the dependencies: ${chalk.cyan(
+          `cd ${answers.name} && yarn install`,
+        )}`,
+      );
+    }
     Task.log(`  Run the app: ${chalk.cyan(`cd ${answers.name} && yarn dev`)}`);
     Task.log(
       '  Set up the software catalog: https://backstage.io/docs/features/software-catalog/configuration',
