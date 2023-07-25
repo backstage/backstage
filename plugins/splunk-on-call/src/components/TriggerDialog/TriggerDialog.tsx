a204634b332e964910ee5e4db91b09c67c417445
Fredrik Adelöw <freben@gmail.com>
2023-07-25 09:58:49 +0200

    back out one final raw css statement change
    Signed-off-by: Fredrik Adelöw <freben@gmail.com>

--- plugins/splunk-on-call/src/components/TriggerDialog/TriggerDialog.tsx
+++ plugins/splunk-on-call/src/components/TriggerDialog/TriggerDialog.tsx
@@ -59,7 +59,7 @@ const useStyles = makeStyles((theme: Theme) =>
       display: 'flex',
       flexDirection: 'row',
       alignItems: 'center',
-      minWidth: `calc(100% - ${theme.spacing(2)})`,
+      minWidth: `calc(100% - ${theme.spacing(2)}px)`,
     },
     formHeader: {
       width: '50%',
