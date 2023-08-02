861c5708c26784f4511511f582a1ddc486fb1352
Patrik Oldsberg <poldsberg@gmail.com>
2023-08-02 14:32:32 +0200

    auth-node: add common sign-in resolvers
    Signed-off-by: Patrik Oldsberg <poldsberg@gmail.com>

---
+++ plugins/auth-node/src/sign-in/resolvers/byEmail.ts
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2023 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { createSignInResolverFactory } from '../createSignInResolverFactory';
+
+/**
+ * A common sign-in resolver that looks up the user using their email address
+ * as email of the entity.
+ */
+export const byEmail = createSignInResolverFactory({
+  create() {
+    return async (info, ctx) => {
+      const { profile } = info;
+
+      if (!profile.email) {
+        throw new Error('Login failed, user profile does not contain an email');
+      }
+
+      return ctx.signInWithCatalogUser({
+        filter: {
+          'spec.profile.email': profile.email,
+        },
+      });
+    };
+  },
+});
