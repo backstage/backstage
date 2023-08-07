35ccc2365a0756fc7ef100c5d003b3943135590d
Philipp Hugenroth <philipph@spotify.com>
2023-08-07 15:08:48 +0200

    Fix backend-plugin-api alpha api report
    Signed-off-by: Philipp Hugenroth <philipph@spotify.com>

--- packages/backend-app-api/src/alpha.ts
+++ packages/backend-app-api/src/alpha.ts
@@ -14,4 +14,4 @@
  * limitations under the License.
  */
 
-export { featureDiscoveryServiceFactory } from './featureDiscoveryServiceFactory';
+export { featureDiscoveryServiceFactory } from './alpha/featureDiscoveryServiceFactory';
