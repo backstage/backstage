2bc96ce69eeafc4d81eb5683be2e3f5e5bebbd4b
Adam Kunicki <kunickiaj@gmail.com>
2023-08-15 14:57:30 -0700

    Fixes the missing borders on customizable homepage cards
    Signed-off-by: Adam Kunicki <kunickiaj@gmail.com>

--- plugins/home/src/components/CustomHomepage/CustomHomepageGrid.tsx
+++ plugins/home/src/components/CustomHomepage/CustomHomepageGrid.tsx
@@ -68,7 +68,6 @@ const useStyles = makeStyles((theme: Theme) =>
       marginLeft: theme.spacing(2),
     },
     widgetWrapper: {
-      overflow: 'hidden',
       '& > div[class*="MuiCard-root"]': {
         width: '100%',
         height: '100%',
