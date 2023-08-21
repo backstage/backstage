f46953c850ea7a5b3e060409a2d8958c51a68131
Patrik Oldsberg <poldsberg@gmail.com>
2023-08-21 17:49:11 +0200

    app-next: fix test
    Signed-off-by: Patrik Oldsberg <poldsberg@gmail.com>

--- packages/app-next/src/App.test.tsx
+++ packages/app-next/src/App.test.tsx
@@ -16,7 +16,6 @@
 
 import React from 'react';
 import { renderWithEffects } from '@backstage/test-utils';
-import App from './App';
 
 describe('App', () => {
   it('should render', async () => {
@@ -42,6 +41,7 @@ describe('App', () => {
       ] as any,
     };
 
+    const { default: App } = await import('./App');
     const rendered = await renderWithEffects(<App />);
     expect(rendered.baseElement).toBeInTheDocument();
   });
