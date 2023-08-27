811d8f26f8451444b8b251e30afe656b81d4264c
David Lilienfeld <davidlilienfeld4@gmail.com>
2023-08-27 18:36:03 -0400

    Add Bitbucket Server Event Integration
    Signed-off-by: David Lilienfeld <davidlilienfeld4@gmail.com>

--- plugins/events-backend-module-bitbucket-server/src/alpha.ts
+++ plugins/events-backend-module-bitbucket-server/src/alpha.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 The Backstage Authors
+ * Copyright 2023 The Backstage Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,9 +14,5 @@
  * limitations under the License.
  */
 
-export { BitbucketServerClient, paginated } from './BitbucketServerClient';
-export type {
-  BitbucketServerPagedResponse,
-  BitbucketServerListOptions,
-} from './BitbucketServerClient';
-export * from './types';
+export { eventsModuleBitbucketServerEventRouter } from './service/eventsModuleBitbucketServerEventRouter';
+export { eventsModuleBitbucketServerEventRouter as default } from './service/eventsModuleBitbucketServerEventRouter';
