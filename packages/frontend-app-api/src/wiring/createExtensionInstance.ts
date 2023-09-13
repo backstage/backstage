e753c3f27b1b8d5bc5fb0b71f2c37d1bb8611321
Johan Haals <johan.haals@gmail.com>
2023-09-13 14:25:02 +0200

    Mark ExtensionInstance as internal
    Signed-off-by: Johan Haals <johan.haals@gmail.com>

--- packages/frontend-app-api/src/wiring/createExtensionInstance.ts
+++ packages/frontend-app-api/src/wiring/createExtensionInstance.ts
@@ -21,7 +21,7 @@ import {
 } from '@backstage/frontend-plugin-api';
 import mapValues from 'lodash/mapValues';
 
-/** @public */
+/** @internal */
 export interface ExtensionInstance {
   readonly $$type: '@backstage/ExtensionInstance';
 
