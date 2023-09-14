49c352603bbc9e0d9a112f618e879dae5410d92d
Patrik Oldsberg <poldsberg@gmail.com>
2023-09-14 17:13:20 +0200

    core-plugin-api: add AppLanguageApi implementation
    Signed-off-by: Patrik Oldsberg <poldsberg@gmail.com>

---
+++ packages/core-app-api/src/apis/implementations/AppLanguageApi/AppLanguageSelector.ts
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2023 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Internal import to avoid code duplication, this will lead to duplication in build output
+// eslint-disable-next-line @backstage/no-relative-monorepo-imports
+import { AppLanguageApi } from '@backstage/core-plugin-api/alpha';
+import { Observable } from '@backstage/types';
+import { BehaviorSubject } from '../../../lib';
+
+const STORAGE_KEY = 'language';
+const DEFAULT_LANGUAGE = 'en';
+
+/** @alpha */
+export interface AppLanguageSelectorOptions {
+  defaultLanguage?: string;
+  availableLanguages?: string[];
+}
+
+/**
+ * Exposes the available languages in the app and allows for switching of the active language.
+ *
+ * @alpha
+ */
+export class AppLanguageSelector implements AppLanguageApi {
+  static createWithStorage(options: AppLanguageSelectorOptions) {
+    const languages = options.availableLanguages ?? [DEFAULT_LANGUAGE];
+    if (languages.length !== new Set(languages).size) {
+      throw new Error(
+        `Supported languages may not contain duplicates, got '${languages.join(
+          "', '",
+        )}'`,
+      );
+    }
+    if (!languages.includes(DEFAULT_LANGUAGE)) {
+      throw new Error(`Supported languages must include '${DEFAULT_LANGUAGE}'`);
+    }
+
+    let initialLanguage = languages[0];
+
+    if (!window.localStorage) {
+      return new AppLanguageSelector(languages, initialLanguage);
+    }
+
+    const storedLanguage =
+      window.localStorage.getItem(STORAGE_KEY) ?? undefined;
+    if (storedLanguage && languages.includes(storedLanguage)) {
+      initialLanguage = storedLanguage;
+    }
+
+    const selector = new AppLanguageSelector(languages, initialLanguage);
+
+    selector.language$().subscribe(({ language }) => {
+      if (language !== window.localStorage.getItem(STORAGE_KEY)) {
+        window.localStorage.setItem(STORAGE_KEY, language);
+      }
+    });
+
+    window.addEventListener('storage', event => {
+      if (event.key === STORAGE_KEY) {
+        const language = localStorage.getItem(STORAGE_KEY) ?? undefined;
+        if (language) {
+          selector.setLanguage(language);
+        }
+      }
+    });
+
+    return selector;
+  }
+
+  #languages: string[];
+  #language: string;
+  #subject: BehaviorSubject<{ language: string }>;
+
+  private constructor(languages: string[], initialLanguage: string) {
+    this.#languages = languages;
+    this.#language = initialLanguage;
+    this.#subject = new BehaviorSubject<{ language: string }>({
+      language: initialLanguage,
+    });
+  }
+
+  getAvailableLanguages(): { languages: string[] } {
+    return { languages: this.#languages.slice() };
+  }
+
+  setLanguage(language?: string | undefined): void {
+    const lng = language ?? DEFAULT_LANGUAGE;
+    if (lng === this.#language) {
+      return;
+    }
+    if (lng && !this.#languages.includes(lng)) {
+      throw new Error(
+        `Failed to change language to '${lng}', available languages are '${this.#languages.join(
+          "', '",
+        )}'`,
+      );
+    }
+    this.#language = lng;
+    this.#subject.next({ language: lng });
+  }
+
+  getLanguage(): { language: string } {
+    return { language: this.#language };
+  }
+
+  language$(): Observable<{ language: string }> {
+    return this.#subject;
+  }
+}
