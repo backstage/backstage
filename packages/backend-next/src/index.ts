a5d592d0ad037eb9d764d6329e423198950a95e8
Andy Muldoon <andy.muldoon@ericsson.com>
2023-09-14 12:34:47 +0100

    Migrate sonarqube-backend plugin to the new backend system #18320
    Signed-off-by: Andy Muldoon <andy.muldoon@ericsson.com>

--- packages/backend-next/src/index.ts
+++ packages/backend-next/src/index.ts
@@ -44,5 +44,6 @@ backend.add(import('@backstage/plugin-search-backend-module-techdocs/alpha'));
 backend.add(import('@backstage/plugin-search-backend/alpha'));
 backend.add(import('@backstage/plugin-techdocs-backend/alpha'));
 backend.add(import('@backstage/plugin-todo-backend'));
+backend.add(import('@backstage/plugin-sonarqube-backend'));
 
 backend.start();
