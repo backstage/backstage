28f1ab2e1a885aaa541121ad57b0b279f78f95f7
Patrik Oldsberg <poldsberg@gmail.com>
2023-09-23 17:37:56 +0200

    catalog: replace configure with i18n
    Signed-off-by: Patrik Oldsberg <poldsberg@gmail.com>

--- plugins/catalog/src/translation.ts
+++ plugins/catalog/src/translation.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 The Backstage Authors
+ * Copyright 2023 The Backstage Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,16 +14,13 @@
  * limitations under the License.
  */
 
-import { usePluginOptions } from '@backstage/core-plugin-api/alpha';
+import { createTranslationRef } from '@backstage/core-plugin-api/alpha';
 
-export type CatalogPluginOptions = {
-  createButtonTitle: string;
-};
-
-/** @ignore */
-export type CatalogInputPluginOptions = {
-  createButtonTitle: string;
-};
-
-export const useCatalogPluginOptions = () =>
-  usePluginOptions<CatalogPluginOptions>();
+/** @alpha */
+export const catalogTranslationRef = createTranslationRef({
+  id: 'catalog',
+  messages: {
+    catalog_page_title: `{{orgName}} Catalog`,
+    catalog_page_create_button_title: 'Create',
+  },
+});
