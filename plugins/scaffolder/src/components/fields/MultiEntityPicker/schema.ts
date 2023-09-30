f02babaf8e6d2ec08f8953aca54d29f99ca45f04
Marc Rooding <marc@mrooding.me>
2023-09-30 16:49:20 +0200

    test: unit tests for the MultiEntityPicker
    Signed-off-by: Marc Rooding <marc@mrooding.me>

--- plugins/scaffolder/src/components/fields/MultiEntityPicker/schema.ts
+++ plugins/scaffolder/src/components/fields/MultiEntityPicker/schema.ts
@@ -13,8 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { makeFieldSchemaFromZod } from '@backstage/plugin-scaffolder';
 import { z } from 'zod';
+import { makeFieldSchemaFromZod } from '../utils';
 
 /**
  * @public
