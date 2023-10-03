9a3a42b7451fc23d8a244d6ee7ec9324dd7dd95b
Bogdan Nechyporenko <bnechyporenko@bol.com>
2023-10-03 14:41:10 +0200

    Fix for a Warning Panel
    Signed-off-by: Bogdan Nechyporenko <bnechyporenko@bol.com>

--- packages/core-components/src/components/WarningPanel/WarningPanel.tsx
+++ packages/core-components/src/components/WarningPanel/WarningPanel.tsx
@@ -71,7 +71,7 @@ export type WarningPanelClassKey =
 const useStyles = makeStyles<BackstageTheme>(
   theme => ({
     content: {
-      overflow: 'hidden',
+      lineBreak: 'anywhere',
     },
     panel: {
       backgroundColor: ({ severity }: WarningProps) =>
