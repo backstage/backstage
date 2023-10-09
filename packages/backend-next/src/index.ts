a8a4911713c4b2f4ac4da0031caef7b1def9594f
Camila Belo <camilaibs@gmail.com>
2023-10-09 15:49:21 +0200

    refactor: do not install stack overflow plugin on example apps
    Signed-off-by: Camila Belo <camilaibs@gmail.com>

--- packages/backend-next/src/index.ts
+++ packages/backend-next/src/index.ts
@@ -46,7 +46,6 @@ backend.add(import('@backstage/plugin-search-backend-module-explore/alpha'));
 backend.add(import('@backstage/plugin-search-backend-module-techdocs/alpha'));
 backend.add(import('@backstage/plugin-search-backend/alpha'));
 backend.add(import('@backstage/plugin-techdocs-backend/alpha'));
-backend.add(import('@backstage/plugin-stack-overflow-backend/alpha'));
 backend.add(import('@backstage/plugin-todo-backend'));
 backend.add(import('@backstage/plugin-sonarqube-backend'));
 
