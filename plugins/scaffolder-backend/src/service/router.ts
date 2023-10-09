b5c73ddc8c026b1a338ac2401aa57dbdda16291f
Bogdan Nechyporenko <bnechyporenko@bol.com>
2023-10-09 17:41:26 +0200

    Make it possible to configure certain tings per template
    Signed-off-by: Bogdan Nechyporenko <bnechyporenko@bol.com>
    Signed-off-by: PurseChicken <chris@tschantz.name>

--- plugins/scaffolder-backend/src/service/router.ts
+++ plugins/scaffolder-backend/src/service/router.ts
@@ -365,6 +365,7 @@ export async function createRouter(
         const parameters = [template.spec.parameters ?? []].flat();
         res.json({
           title: template.metadata.title ?? template.metadata.name,
+          configurations: template.spec.configurations ?? {},
           description: template.metadata.description,
           'ui:options': template.metadata['ui:options'],
           steps: parameters.map(schema => ({
