89e7248b6836e99fde66ab9a26091a6a36020c3a
AmbrishRamachandiran <ambrish.r@infosys.com>
2023-10-18 09:44:58 +0530

    Replaced the string compare logic with local compare
    Signed-off-by: AmbrishRamachandiran <ambrish.r@infosys.com>
    Signed-off-by: Josh Uvi <joshuauvbiekpahor@gmail.com>

--- plugins/bazaar/src/util/sortMethods.ts
+++ plugins/bazaar/src/util/sortMethods.ts
@@ -27,21 +27,11 @@ export const sortByDate = (a: BazaarProject, b: BazaarProject): number => {
 };
 
 export const sortByTitle = (a: BazaarProject, b: BazaarProject) => {
-  if (a.title < b.title) {
-    return -1;
-  } else if (a.title > b.title) {
-    return 1;
-  }
-  return 0;
+  return a.title.localeCompare(b.title);
 };
 
 export const sortByDecendingTitle = (a: BazaarProject, b: BazaarProject) => {
-  if (a.title < b.title) {
-    return 1;
-  } else if (a.title > b.title) {
-    return -1;
-  }
-  return 0;
+  return b.title.localeCompare(a.title);
 };
 
 export const sortByMembers = (a: BazaarProject, b: BazaarProject) => {
