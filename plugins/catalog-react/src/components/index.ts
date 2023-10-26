6c357184e27d86796fac6005ec6a597f994aa19d
blam <ben@blam.sh>
2023-10-26 18:35:55 +0200

    feat: move the MissingAnnotationEmptyState to catalog-react
    Signed-off-by: blam <ben@blam.sh>

--- plugins/catalog-react/src/components/index.ts
+++ plugins/catalog-react/src/components/index.ts
@@ -32,3 +32,4 @@ export * from './UserListPicker';
 export * from './EntityProcessingStatusPicker';
 export * from './EntityNamespacePicker';
 export * from './EntityAutocompletePicker';
+export * from './MissingAnnotationEmptyState';
