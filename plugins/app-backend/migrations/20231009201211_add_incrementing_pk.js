aee1dfa4b90d98be9ccc4553301d1b1e18962204
Pete Levine <lpete@vmware.com>
2023-10-27 12:39:06 -0400

    fix: only create temp table for sqlite
    Co-authored-by: Alex Rocha <alexr1@vmware.com>
    Signed-off-by: Pete Levine <lpete@vmware.com>

--- plugins/app-backend/migrations/20231009201211_add_incrementing_pk.js
+++ plugins/app-backend/migrations/20231009201211_add_incrementing_pk.js
@@ -21,54 +21,82 @@
  */
 exports.up = async function up(knex) {
   const isMySQL = knex.client.config.client.includes('mysql');
+  const isSqlite = knex.client.config.client.includes('sqlite3');
   // set up autoincrementing ID column on static_assets_cache pk
   // set path with a unique constraint in-place-of primary key
-  // Setup temporary tables
-  await knex.schema.renameTable(
-    'static_assets_cache',
-    'tmp_static_assets_cache',
-  );
-  await knex.schema.table('tmp_static_assets_cache', table => {
-    table.dropIndex('path', 'static_assets_cache_path_unique');
-    table.dropIndex(
-      'last_modified_at',
-      'static_asset_cache_last_modified_at_idx',
+  // Setup temporary tables for sqlite only due to modifying the schema
+  if (isSqlite) {
+    await knex.schema.renameTable(
+      'static_assets_cache',
+      'tmp_static_assets_cache',
     );
-  });
-  await knex.schema.createTable('static_assets_cache', table => {
-    table.comment(
-      'A cache of static assets that where previously deployed and may still be lazy-loaded by clients',
-    );
-    table
-      .increments('id', { primaryKey: true })
-      .comment('Primary key to distinguish unique lines from each other');
-    if (!isMySQL) {
-      // mysql doesn't allow unique constraints above 255 characters
-      table.text('path').unique().notNullable().comment('The path of the file');
-    } else {
-      table.text('path').notNullable().comment('The path of the file');
-    }
-    table
-      .dateTime('last_modified_at')
-      .defaultTo(knex.fn.now())
-      .notNullable()
-      .comment(
-        'Timestamp of when the asset was most recently seen in a deployment',
+    await knex.schema.table('tmp_static_assets_cache', table => {
+      table.dropIndex('path', 'static_assets_cache_path_unique');
+      table.dropIndex(
+        'last_modified_at',
+        'static_asset_cache_last_modified_at_idx',
       );
-    table.binary('content').notNullable().comment('The asset content');
-    table.index('last_modified_at', 'static_asset_cache_last_modified_at_idx');
-  });
-  // specifically for mysql specify a unique index up to 254 characters(mysql limit)
-  if (isMySQL) {
+    });
+    await knex.schema.createTable('static_assets_cache', table => {
+      table.comment(
+        'A cache of static assets that where previously deployed and may still be lazy-loaded by clients',
+      );
+      table
+        .increments('id', { primaryKey: true })
+        .comment('Primary key to distinguish unique lines from each other');
+      table.text('path').unique().notNullable().comment('The path of the file');
+      table
+        .dateTime('last_modified_at')
+        .defaultTo(knex.fn.now())
+        .notNullable()
+        .comment(
+          'Timestamp of when the asset was most recently seen in a deployment',
+        );
+      table.binary('content').notNullable().comment('The asset content');
+      table.index(
+        'last_modified_at',
+        'static_asset_cache_last_modified_at_idx',
+      );
+    });
     await knex.schema.raw(
-      'create unique index static_assets_cache_path_idx on static_assets_cache(path(254));',
+      `INSERT INTO static_assets_cache SELECT * FROM tmp_static_assets_cache`,
     );
+    // Clean up
+    await knex.schema.dropTable('tmp_static_assets_cache');
+  } else {
+    await knex.schema.alterTable('static_assets_cache', async table => {
+      await knex.schema
+        .hasColumn('static_assets_cache', 'id')
+        .then(async hasCol => {
+          if (hasCol) {
+            return;
+          }
+          table
+            .increments('id', { primaryKey: true })
+            .comment('Primary key to distinguish unique lines from each other');
+        });
+      await knex.schema
+        .hasColumn('static_assets_cache', 'path')
+        .then(async hasCol => {
+          if (hasCol) {
+            return;
+          }
+          if (!isMySQL) {
+            // mysql doesn't allow unique constraints above 255 characters
+            table
+              .text('path')
+              .unique()
+              .notNullable()
+              .comment('The path of the file');
+          } else {
+            table.text('path').notNullable().comment('The path of the file');
+            await knex.schema.raw(
+              'create unique index static_assets_cache_path_idx on static_assets_cache(path(254));',
+            );
+          }
+        });
+    });
   }
-  await knex.schema.raw(
-    `INSERT INTO static_assets_cache SELECT * FROM tmp_static_assets_cache`,
-  );
-  // Clean up
-  await knex.schema.dropTable('tmp_static_assets_cache');
 };
 
 /**
