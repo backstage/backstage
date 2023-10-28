35df801e094064a153b746662335f3ed00fe86b8
Patrik Oldsberg <poldsberg@gmail.com>
2023-10-28 14:10:26 +0200

    app-next: initial app visualizer plugin
    Signed-off-by: Patrik Oldsberg <poldsberg@gmail.com>

--- packages/app-next/src/App.tsx
+++ packages/app-next/src/App.tsx
@@ -32,6 +32,7 @@ import {
   createExtensionOverrides,
 } from '@backstage/frontend-plugin-api';
 import techdocsPlugin from '@backstage/plugin-techdocs/alpha';
+import { plugins } from './plugins';
 import { homePage } from './HomePage';
 import { convertLegacyApp } from '@backstage/core-compat-api';
 import { FlatRoutes } from '@backstage/core-app-api';
@@ -123,6 +124,7 @@ const app = createApp({
     techdocsPlugin,
     userSettingsPlugin,
     homePlugin,
+    ...plugins,
     ...collectedLegacyPlugins,
     createExtensionOverrides({
       extensions: [
