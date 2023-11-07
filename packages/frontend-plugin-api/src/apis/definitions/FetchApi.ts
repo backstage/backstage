aff45c634c6ba133553fa9442a96ec4feeb6846b
Vincenzo Scamporlino <vincenzos@spotify.com>
2023-11-07 17:40:46 +0100

    frontend-plugin-api: more relative imports
    Signed-off-by: Vincenzo Scamporlino <vincenzos@spotify.com>

--- packages/frontend-plugin-api/src/apis/definitions/FetchApi.ts
+++ packages/frontend-plugin-api/src/apis/definitions/FetchApi.ts
@@ -14,38 +14,5 @@
  * limitations under the License.
  */
 
-import { ApiRef, createApiRef } from '../system';
-
-/**
- * A wrapper for the fetch API, that has additional behaviors such as the
- * ability to automatically inject auth information where necessary.
- *
- * @public
- */
-export type FetchApi = {
-  /**
-   * The `fetch` implementation.
-   */
-  fetch: typeof fetch;
-};
-
-/**
- * The {@link ApiRef} of {@link FetchApi}.
- *
- * @remarks
- *
- * This is a wrapper for the fetch API, that has additional behaviors such as
- * the ability to automatically inject auth information where necessary.
- *
- * Note that the default behavior of this API (unless overridden by your org),
- * is to require that the user is already signed in so that it has auth
- * information to inject. Therefore, using the default implementation of this
- * utility API e.g. on the `SignInPage` or similar, would cause issues. In
- * special circumstances like those, you can use the regular system `fetch`
- * instead.
- *
- * @public
- */
-export const fetchApiRef: ApiRef<FetchApi> = createApiRef({
-  id: 'core.fetch',
-});
+// eslint-disable-next-line @backstage/no-relative-monorepo-imports
+export { type FetchApi, fetchApiRef } from '../../../../core-plugin-api';
