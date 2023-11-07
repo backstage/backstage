ae0bffe73fad49244618a4674476e6257515b988
Vincenzo Scamporlino <vincenzos@spotify.com>
2023-11-08 00:05:36 +0100

    frontend-plugin-api: add missing implementations
    Signed-off-by: Vincenzo Scamporlino <vincenzos@spotify.com>

--- packages/frontend-plugin-api/src/apis/system/useApi.tsx
+++ packages/frontend-plugin-api/src/apis/system/useApi.tsx
@@ -14,8 +14,5 @@
  * limitations under the License.
  */
 
-export { useApi, useApiHolder, withApis } from './useApi';
-export { createApiRef } from './ApiRef';
-export type { ApiRefConfig } from './ApiRef';
-export * from './types';
-export * from './helpers';
+// eslint-disable-next-line @backstage/no-relative-monorepo-imports
+export { useApiHolder, useApi, withApis } from '../../../../core-plugin-api';
