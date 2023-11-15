d69b0fcd2b29206d9ac592b421255703351bd245
Patrik Oldsberg <poldsberg@gmail.com>
2023-11-15 13:11:42 +0100

    frontend-test-utils: refactor to use createSpecializedApp + API report
    Signed-off-by: Patrik Oldsberg <poldsberg@gmail.com>

--- packages/frontend-test-utils/src/app/index.ts
+++ packages/frontend-test-utils/src/app/index.ts
@@ -14,4 +14,7 @@
  * limitations under the License.
  */
 
-export { createExtensionTester } from './createExtensionTester';
+export {
+  createExtensionTester,
+  type ExtensionTester,
+} from './createExtensionTester';
