fd7b543c793bc038f2065fa572fe6c1d2c8c769c
Min Kim <minkimcello@gmail.com>
2023-12-28 10:51:19 -0500

    Organize backend imports and add comments
    Signed-off-by: Min Kim <minkimcello@gmail.com>

--- packages/create-app/templates/default-app/packages/backend/src/index.ts
+++ packages/create-app/templates/default-app/packages/backend/src/index.ts
@@ -11,21 +11,25 @@ import { createBackend } from '@backstage/backend-defaults';
 const backend = createBackend();
 
 backend.add(import('@backstage/plugin-app-backend/alpha'));
-backend.add(import('@backstage/plugin-auth-backend'));
-backend.add(import('@backstage/plugin-auth-backend-module-github-provider'));
-backend.add(import('@backstage/plugin-catalog-backend/alpha'));
-backend.add(
-  import('@backstage/plugin-catalog-backend-module-scaffolder-entity-model'),
-);
-backend.add(import('@backstage/plugin-permission-backend/alpha'));
-backend.add(
-  import('@backstage/plugin-permission-backend-module-allow-all-policy'),
-);
 backend.add(import('@backstage/plugin-proxy-backend/alpha'));
 backend.add(import('@backstage/plugin-scaffolder-backend/alpha'));
+backend.add(import('@backstage/plugin-techdocs-backend/alpha'));
+
+// auth plugin
+backend.add(import('@backstage/plugin-auth-backend'));
+  // See https://backstage.io/docs/backend-system/building-backends/migrating#the-auth-plugin
+
+// catalog plugin
+backend.add(import('@backstage/plugin-catalog-backend/alpha'));
+backend.add(import('@backstage/plugin-catalog-backend-module-scaffolder-entity-model'));
+
+// permission plugin
+backend.add(import('@backstage/plugin-permission-backend/alpha'));
+backend.add(import('@backstage/plugin-permission-backend-module-allow-all-policy'));
+
+// search plugin
 backend.add(import('@backstage/plugin-search-backend/alpha'));
 backend.add(import('@backstage/plugin-search-backend-module-catalog/alpha'));
 backend.add(import('@backstage/plugin-search-backend-module-techdocs/alpha'));
-backend.add(import('@backstage/plugin-techdocs-backend/alpha'));
 
 backend.start();
