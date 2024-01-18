8f483eabcecec0355048bc0440e282985e521a5a
blam <ben@blam.sh>
2024-01-18 16:22:30 +0100

    feat: bitbucket backend module support
    Signed-off-by: blam <ben@blam.sh>

--- plugins/scaffolder-backend-module-bitbucket/src/alpha.ts
+++ plugins/scaffolder-backend-module-bitbucket/src/alpha.ts
@@ -13,4 +13,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export { azureModule as default } from './module';
+export { bitbucketModule as default } from './module';
