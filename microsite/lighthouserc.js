d53ff7c1b6f67ca389eb37ff68dfb6cf87ca379d
Jithen Shriyan <shriyanjithen@gmail.com>
2024-01-21 10:33:43 -0500

    [style] format
    Signed-off-by: Jithen Shriyan <shriyanjithen@gmail.com>

--- microsite/lighthouserc.js
+++ microsite/lighthouserc.js
@@ -14,53 +14,55 @@
  * limitations under the License.
  */
 
-var sidebars = require('./sidebars.json')
+var sidebars = require('./sidebars.json');
 
 module.exports = {
-    ci: {
-        collect: {
-            url: [
-                /** Home */
-                'http://localhost:3000',
-                /** Docs - Getting Started */
-                'http://localhost:3000/docs/getting-started',
-                /** Docs - Software Catalog */
-                'http://localhost:3000/docs/features/software-catalog',
-                /** Docs - Create a Plugin */
-                'http://localhost:3000/docs/plugins/create-a-plugin',
-                /** Docs - Designing for Backstage */
-                'http://localhost:3000/docs/dls/design',
-                /** Blog */
-                'http://localhost:3000/blog',
-                /** Plugins */
-                'http://localhost:3000/plugins',
-                /** Demos */
-                'http://localhost:3000/demos',
-                /** Community */
-                'http://localhost:3000/community',
-                /** Releases */
-                ...(sidebars.releases['Release Notes'].map((path) => `http://localhost:3000/docs/${path}`)),
-            ],
-            settings: {
-                onlyCategories: ['accessibility'],
-                output: ['html', 'json'],
-                outputPath: './.lighthouseci/reports',
-                preset: 'desktop',
-            },
-            // refers to root package scripts
-            startServerCommand: 'yarn run start:microsite',
-            startServerReadyPattern: 'compiled successfully',
-            startServerReadyTimeout: 600000,
-            numberOfRuns: 1,
-        },
-        assert: {
-            assertions: {
-                'categories:performance': 'off',
-                'categories:pwa': 'off',
-                'categories:best-practices': 'off',
-                'categories:seo': 'off',
-                'categories:accessibility': ['error', { minScore: 0.95 }],
-            },
-        },
+  ci: {
+    collect: {
+      url: [
+        /** Home */
+        'http://localhost:3000',
+        /** Docs - Getting Started */
+        'http://localhost:3000/docs/getting-started',
+        /** Docs - Software Catalog */
+        'http://localhost:3000/docs/features/software-catalog',
+        /** Docs - Create a Plugin */
+        'http://localhost:3000/docs/plugins/create-a-plugin',
+        /** Docs - Designing for Backstage */
+        'http://localhost:3000/docs/dls/design',
+        /** Blog */
+        'http://localhost:3000/blog',
+        /** Plugins */
+        'http://localhost:3000/plugins',
+        /** Demos */
+        'http://localhost:3000/demos',
+        /** Community */
+        'http://localhost:3000/community',
+        /** Releases */
+        ...sidebars.releases['Release Notes'].map(
+          path => `http://localhost:3000/docs/${path}`,
+        ),
+      ],
+      settings: {
+        onlyCategories: ['accessibility'],
+        output: ['html', 'json'],
+        outputPath: './.lighthouseci/reports',
+        preset: 'desktop',
+      },
+      // refers to root package scripts
+      startServerCommand: 'yarn run start:microsite',
+      startServerReadyPattern: 'compiled successfully',
+      startServerReadyTimeout: 600000,
+      numberOfRuns: 1,
     },
+    assert: {
+      assertions: {
+        'categories:performance': 'off',
+        'categories:pwa': 'off',
+        'categories:best-practices': 'off',
+        'categories:seo': 'off',
+        'categories:accessibility': ['error', { minScore: 0.95 }],
+      },
+    },
+  },
 };
