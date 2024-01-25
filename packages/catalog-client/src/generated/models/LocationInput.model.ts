c04c42b61a789221baed98f04933e19df49f549a
Aramis <sennyeyaramis@gmail.com>
2024-01-25 18:00:28 -0500

    feat(openapi-tooling): Fix oneOf client duplications.
    Signed-off-by: Aramis <sennyeyaramis@gmail.com>

--- packages/catalog-client/src/generated/models/LocationInput.model.ts
+++ packages/catalog-client/src/generated/models/LocationInput.model.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 The Backstage Authors
+ * Copyright 2024 The Backstage Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,6 +12,10 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ */
+
+/**
+ * NOTE: This class is auto generated, do not edit the class manually.
  */
 
 export interface LocationInput {
