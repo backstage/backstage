91f5447e98cc95d81a79d75912308d0a88cd6add
armandocomellas1 <cgarmando@google.com>
2024-02-08 14:15:36 -0600

    Modify the route backstage/backstage was duplicated
    Signed-off-by: armandocomellas1 <cgarmando@google.com>

--- packages/backend-common/src/reading/GoogleGcsUrlReader.test.ts
+++ packages/backend-common/src/reading/GoogleGcsUrlReader.test.ts
@@ -80,7 +80,7 @@ describe('GcsUrlReader', () => {
   });
   it('check if userAgent has been called with this key value', async () => {
     const getStorage: any = {
-      userAgent: `backstage/backstage/backend-common.GoogleGcsUrlReader/${packageinfo.version}`,
+      userAgent: `backstage/backend-common.GoogleGcsUrlReader/${packageinfo.version}`,
     };
     jest.mock('@google-cloud/storage', () => {
       return {
@@ -89,7 +89,7 @@ describe('GcsUrlReader', () => {
     });
     const getUserAgent = getStorage.userAgent.toString();
     expect(getUserAgent).toBe(
-      `backstage/backstage/backend-common.GoogleGcsUrlReader/${packageinfo.version}`,
+      `backstage/backend-common.GoogleGcsUrlReader/${packageinfo.version}`,
     );
   });
 
