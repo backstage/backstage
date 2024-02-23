12ba09bed4f8aa6df48b9c38c220eca755ff9289
tperehinets <tetiana.perehinets@gmail.com>
2024-02-22 19:48:01 -0500

    Added changeset
    Signed-off-by: tperehinets <tetiana.perehinets@gmail.com>

---
+++ .changeset/large-brooms-shop.md
@@ -0,0 +1,5 @@
+---
+'@backstage/plugin-tech-insights': patch
+---
+
+Added TechInsights plugin enhancement with the view that includes related check information for each Check.
--- packages/backend/src/plugins/techInsights.ts
+++ packages/backend/src/plugins/techInsights.ts
@@ -28,55 +28,6 @@ import {
   JsonRulesEngineFactCheckerFactory,
   JSON_RULE_ENGINE_CHECK_TYPE,
 } from '@backstage/plugin-tech-insights-backend-module-jsonfc';
-import { FactRetriever, FactRetrieverContext } from '@backstage/plugin-tech-insights-node';
-import { CatalogClient } from '@backstage/catalog-client';
-
-async function fetchProductsDomainFacts(ctx: FactRetrieverContext) {
-  // Handler function that retrieves the fact
-  const { discovery } = ctx;
-  const catalogClient = new CatalogClient({
-    discoveryApi: discovery,
-  });
-
-  const entities = await catalogClient.getEntities(
-    {
-      filter: [{ kind: 'component' }],
-    }
-  );
-
-
-  // Respond with an array of entity/fact values
-  return entities.items.map(it => {
-    const systemCount = 10
-    const systemNoDomainCount = 8
-    return {
-      // Entity information that this fact relates to
-      entity: {
-        namespace: it.metadata.namespace,
-        kind: it.kind,
-        name: it.metadata.name,
-      },
-
-      facts: {
-        systemCount: systemCount,
-        systemNoDomainCount: systemNoDomainCount,
-        fraction: systemNoDomainCount/systemCount
-      },
-    };
-  });
-}
-
-const domainFactRetriever: FactRetriever = {
-  id: 'product-domain-check',
-  version: '0.1.1',
-  entityFilter: [{ kind: 'service' }], //we're only going to define these metrics against the group entity
-  schema: {
-      systemNoDomainCount: { type: 'integer', description: 'Number of Products Not Mapped to Domains' },
-      systemCount: { type: 'integer', description: 'Number of systems owned by group' },
-      fraction: {type:'float', description:'ration of satisfactory/unsatisfactory'}
-    },
-  handler: fetchProductsDomainFacts
-};
 
 export default async function createPlugin(
   env: PluginEnvironment,
@@ -95,10 +46,6 @@ export default async function createPlugin(
       }),
       createFactRetrieverRegistration({
         cadence: '1 1 1 * *',
-        factRetriever: domainFactRetriever
-      }),
-      createFactRetrieverRegistration({
-        cadence: '1 1 1 * *',
         factRetriever: entityMetadataFactRetriever,
       }),
       createFactRetrieverRegistration({
@@ -109,18 +56,6 @@ export default async function createPlugin(
     factCheckerFactory: new JsonRulesEngineFactCheckerFactory({
       logger: env.logger,
       checks: [
-          {
-            id: 'domainCheck', 
-            type: JSON_RULE_ENGINE_CHECK_TYPE,
-            name: 'Domain Check for Products',
-            description: 'Verifies that the number of domainless Product is zero',
-            factIds: ['product-domain-check'],
-            rule: {
-                conditions: {
-                    all: [ { fact: 'systemNoDomainCount', operator: 'equal', value: 0 }  ],
-                },
-            },
-        },
         {
           id: 'titleCheck',
           type: JSON_RULE_ENGINE_CHECK_TYPE,
@@ -162,8 +97,6 @@ export default async function createPlugin(
       ],
     }),
   });
-
-  
 
   return await createRouter({
     ...techInsightsContext,
