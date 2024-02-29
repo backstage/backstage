3f541b116384b655b219c9ce724ff75610670049
Chase Bennett <chase_bennett@carmax.com>
2024-02-29 11:48:53 -0800

    Formatting
    Signed-off-by: Chase Bennett <chase_bennett@carmax.com>

--- packages/core-components/src/layout/Sidebar/SidebarSubmenuItem.tsx
+++ packages/core-components/src/layout/Sidebar/SidebarSubmenuItem.tsx
@@ -131,7 +131,7 @@ export type SidebarSubmenuItemProps = {
   icon?: IconComponent;
   dropdownItems?: SidebarSubmenuItemDropdownItem[];
   exact?: boolean;
-  initialShowDropDown?: boolean;
+  initialShowDropdown?: boolean;
 };
 
 /**
@@ -151,7 +151,7 @@ export const SidebarSubmenuItem = (props: SidebarSubmenuItemProps) => {
   let isActive = isLocationMatch(currentLocation, toLocation, exact);
 
   const [showDropDown, setShowDropDown] = useState(
-    props.initialShowDropDown ?? false,
+    props.initialShowDropdown ?? false,
   );
   const handleClickDropdown = () => {
     setShowDropDown(!showDropDown);
