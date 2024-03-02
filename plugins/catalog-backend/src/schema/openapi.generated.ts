fac18e3c26da1dd81ce5e786cd20003b945cb6cf
aramissennyeydd <aramis.sennyey@doordash.com>
2024-03-02 14:05:30 -0500

    example breakage
    Signed-off-by: aramissennyeydd <aramis.sennyey@doordash.com>

--- plugins/catalog-backend/src/schema/openapi.generated.ts
+++ plugins/catalog-backend/src/schema/openapi.generated.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 The Backstage Authors
+ * Copyright 2024 The Backstage Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -1072,7 +1072,7 @@ export const spec = {
             'application/json': {
               schema: {
                 type: 'object',
-                required: ['entityRefs'],
+                required: ['entityRefs', 'fields'],
                 properties: {
                   entityRefs: {
                     type: 'array',
