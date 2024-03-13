4323feb76e481c49a317e05b52e9c4b909c473b1
Aditya Kumar <aditya.kumar60@infosys.com>
2024-03-13 11:09:11 +0530

    Added Material UI 4 import rule to plugins/bazaar
    Signed-off-by: Aditya Kumar <aditya.kumar60@infosys.com>

--- plugins/bazaar/src/components/CardContentFields/CardContentFields.tsx
+++ plugins/bazaar/src/components/CardContentFields/CardContentFields.tsx
@@ -19,7 +19,7 @@ import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-import GridSize from '@material-ui/core/GridSize';
+import { GridSize } from '@material-ui/core/Grid';
 import { parseEntityRef } from '@backstage/catalog-model';
 import { Avatar, Link } from '@backstage/core-components';
 import { useRouteRef } from '@backstage/core-plugin-api';
