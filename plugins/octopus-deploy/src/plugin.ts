b55a10893773dab985238522b32f6f5d892ad6ea
Niek Rossen <niek.rossen@infosupport.com>
2024-03-18 13:16:58 +0100

    Refactor ProjectGroupDropdown component and schema
    Signed-off-by: Niek Rossen <niek.rossen@infosupport.com>

--- plugins/octopus-deploy/src/plugin.ts
+++ plugins/octopus-deploy/src/plugin.ts
@@ -66,6 +66,7 @@ export const EntityOctopusDeployContent = octopusDeployPlugin.provide(
   }),
 );
 
+/** @public */
 export const OctopusDeployDropdownFieldExtension = octopusDeployPlugin.provide(
   createScaffolderFieldExtension({
     name: 'OctopusDeployProjectGroupDropdown',
