6aec7259724643e330ea86b9ef25c53a6ebe00f5
Aditya Kumar <aditya.kumar60@infosys.com>
2024-03-18 12:49:51 +0530

    Add ESLint Rule to TechInsights Plugin
    Signed-off-by: Aditya Kumar <aditya.kumar60@infosys.com>

--- plugins/tech-insights/src/components/ScorecardsList/ScorecardsList.tsx
+++ plugins/tech-insights/src/components/ScorecardsList/ScorecardsList.tsx
@@ -16,10 +16,13 @@
 
 import React from 'react';
 import { useApi } from '@backstage/core-plugin-api';
-import { makeStyles, List, ListItem, ListItemText } from '@material-ui/core';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import { makeStyles } from '@material-ui/core/styles';
 import { techInsightsApiRef } from '../../api';
 import { CheckResult } from '@backstage/plugin-tech-insights-common';
-import { Alert } from '@material-ui/lab';
+import Alert from '@material-ui/lab/Alert';
 import { MarkdownContent } from '@backstage/core-components';
 
 const useStyles = makeStyles(theme => ({
