40d44e3efb136fe2f263a045f5cd466ca19f679a
Min Kim <minkimcello@gmail.com>
2024-03-18 10:31:21 -0400

    Add default sign in page with a guest provider
    Signed-off-by: Min Kim <minkimcello@gmail.com>

--- packages/create-app/templates/default-app/packages/app/src/App.tsx
+++ packages/create-app/templates/default-app/packages/app/src/App.tsx
@@ -27,7 +27,11 @@ import { entityPage } from './components/catalog/EntityPage';
 import { searchPage } from './components/search/SearchPage';
 import { Root } from './components/Root';
 
-import { AlertDisplay, OAuthRequestDialog } from '@backstage/core-components';
+import {
+  AlertDisplay,
+  OAuthRequestDialog,
+  SignInPage,
+} from '@backstage/core-components';
 import { createApp } from '@backstage/app-defaults';
 import { AppRouter, FlatRoutes } from '@backstage/core-app-api';
 import { CatalogGraphPage } from '@backstage/plugin-catalog-graph';
@@ -53,6 +57,17 @@ const app = createApp({
       catalogIndex: catalogPlugin.routes.catalogIndex,
     });
   },
+  components: {
+    SignInPage: props => (
+      <SignInPage
+        {...props}
+        auto
+        providers={[
+          'guest'
+        ]}
+      />
+    )
+  }
 });
 
 const routes = (
