cdcd70765d940b2bc244e3ebf2ca3d0060132ee0
AmbrishRamachandiran <ambrish.r@infosys.com>
2024-03-21 18:13:35 +0530

    Add ESLint Rule to config-schema Plugin
    Signed-off-by: AmbrishRamachandiran <ambrish.r@infosys.com>

--- plugins/config-schema/src/components/SchemaView/MetadataView.tsx
+++ plugins/config-schema/src/components/SchemaView/MetadataView.tsx
@@ -15,14 +15,12 @@
  */
 
 import { JsonValue } from '@backstage/types';
-import {
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableRow,
-  Typography,
-} from '@material-ui/core';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
+import Typography from '@material-ui/core/Typography';
 import { Schema } from 'jsonschema';
 import React from 'react';
 
