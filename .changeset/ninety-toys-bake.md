5f9877b6468fb673744b1227a7013ca1452d7199
Heikki Hellgren <heikki.hellgren@op.fi>
2024-04-02 10:32:05 +0300

    fix: allow unauthenticated signals connection to fix 401
    the signals handles authentication itself in the middleware thus we
    should allow unauthenticated requests to pass through
    
    Signed-off-by: Heikki Hellgren <heikki.hellgren@op.fi>

---
+++ .changeset/ninety-toys-bake.md
@@ -0,0 +1,5 @@
+---
+'@backstage/plugin-signals-backend': patch
+---
+
+Fix unauthorized signals connection by allowing unauthenticated requests
--- plugins/signals-backend/src/plugin.ts
+++ plugins/signals-backend/src/plugin.ts
@@ -58,7 +58,7 @@ export const signalsPlugin = createBackendPlugin({
           }),
         );
         httpRouter.addAuthPolicy({
-          path: '/health',
+          path: '*',
           allow: 'unauthenticated',
         });
       },
