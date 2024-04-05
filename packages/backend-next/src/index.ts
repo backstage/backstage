09ee58a0bde70e6eaf2b0e6a98a8a41348826083
blam <ben@blam.sh>
2024-04-05 13:26:37 +0200

    chore: add reproduction example
    Signed-off-by: blam <ben@blam.sh>

--- packages/backend-next/src/index.ts
+++ packages/backend-next/src/index.ts
@@ -15,6 +15,15 @@
  */
 
 import { createBackend } from '@backstage/backend-defaults';
+import {
+  coreServices,
+  createBackendModule,
+} from '@backstage/backend-plugin-api';
+import {
+  EntityProvider,
+  EntityProviderConnection,
+} from '@backstage/plugin-catalog-node';
+import { catalogProcessingExtensionPoint } from '@backstage/plugin-catalog-node';
 
 const backend = createBackend();
 
@@ -22,41 +31,113 @@ backend.add(import('@backstage/plugin-auth-backend'));
 backend.add(import('./authModuleGithubProvider'));
 backend.add(import('@backstage/plugin-auth-backend-module-guest-provider'));
 
-backend.add(import('@backstage/plugin-adr-backend'));
-backend.add(import('@backstage/plugin-app-backend/alpha'));
-backend.add(import('@backstage/plugin-azure-devops-backend'));
-backend.add(import('@backstage/plugin-badges-backend'));
-backend.add(import('@backstage/plugin-catalog-backend-module-unprocessed'));
-backend.add(
-  import('@backstage/plugin-catalog-backend-module-scaffolder-entity-model'),
-);
+// backend.add(import('@backstage/plugin-adr-backend'));
+// backend.add(import('@backstage/plugin-app-backend/alpha'));
+// backend.add(import('@backstage/plugin-azure-devops-backend'));
+// backend.add(import('@backstage/plugin-badges-backend'));
+// backend.add(import('@backstage/plugin-catalog-backend-module-unprocessed'));
+// backend.add(
+//   import('@backstage/plugin-catalog-backend-module-scaffolder-entity-model'),
+// );
 backend.add(import('@backstage/plugin-catalog-backend/alpha'));
-backend.add(import('@backstage/plugin-devtools-backend'));
-backend.add(import('@backstage/plugin-entity-feedback-backend'));
-backend.add(import('@backstage/plugin-jenkins-backend'));
-backend.add(import('@backstage/plugin-kubernetes-backend/alpha'));
-backend.add(import('@backstage/plugin-lighthouse-backend'));
-backend.add(import('@backstage/plugin-linguist-backend'));
-backend.add(import('@backstage/plugin-playlist-backend'));
-backend.add(import('@backstage/plugin-nomad-backend'));
+backend.add(
+  createBackendModule({
+    moduleId: 'provider',
+    pluginId: 'catalog',
+    register({ registerInit }) {
+      registerInit({
+        deps: {
+          processing: catalogProcessingExtensionPoint,
+          lifecycle: coreServices.rootLifecycle,
+        },
+        async init({ processing, lifecycle }) {
+          // once this is running you can register a github url with the same entityRef
+          // and watch it take over. Then delete the location in the UI, then it becomes unmanaged until re-processing resets
+          // the initial provider entity takes over.
+          class MockEntityProvider implements EntityProvider {
+            connection: EntityProviderConnection | undefined;
+            getProviderName(): string {
+              return 'MockProvider';
+            }
+            async connect(connection: EntityProviderConnection): Promise<void> {
+              this.connection = connection;
+            }
+
+            async run() {
+              await this.connection?.applyMutation({
+                type: 'full',
+                entities: [
+                  {
+                    entity: {
+                      apiVersion: 'backstage.io/v1alpha1',
+                      kind: 'Component',
+                      metadata: {
+                        name: 'test-component',
+                        namespace: 'default',
+                        annotations: {
+                          'backstage.io/managed-by-location':
+                            'url:test-location',
+                          'backstage.io/managed-by-origin-location':
+                            'url:test-origin-location',
+                        },
+                      },
+
+                      spec: {
+                        type: 'service',
+                        owner: 'guest',
+                        lifecycle: 'production',
+                        ownerRelations: [],
+                        system: 'backstage',
+                        description: 'A mock component',
+                        tags: [],
+                        providesApis: [],
+                        consumesApis: [],
+                        dependsOn: [],
+                      },
+                    },
+                  },
+                ],
+              });
+            }
+          }
+
+          const provider = new MockEntityProvider();
+          processing.addEntityProvider(provider);
+
+          lifecycle.addStartupHook(() => {
+            provider.run();
+          });
+        },
+      });
+    },
+  }),
+);
+// backend.add(import('@backstage/plugin-devtools-backend'));
+// backend.add(import('@backstage/plugin-entity-feedback-backend'));
+// backend.add(import('@backstage/plugin-jenkins-backend'));
+// backend.add(import('@backstage/plugin-kubernetes-backend/alpha'));
+// backend.add(import('@backstage/plugin-lighthouse-backend'));
+// backend.add(import('@backstage/plugin-linguist-backend'));
+// backend.add(import('@backstage/plugin-playlist-backend'));
+// backend.add(import('@backstage/plugin-nomad-backend'));
 backend.add(
   import('@backstage/plugin-permission-backend-module-allow-all-policy'),
 );
 backend.add(import('@backstage/plugin-permission-backend/alpha'));
-backend.add(import('@backstage/plugin-proxy-backend/alpha'));
-backend.add(import('@backstage/plugin-scaffolder-backend/alpha'));
-backend.add(import('@backstage/plugin-scaffolder-backend-module-github'));
-backend.add(import('@backstage/plugin-search-backend-module-catalog/alpha'));
-backend.add(import('@backstage/plugin-search-backend-module-explore/alpha'));
-backend.add(import('@backstage/plugin-search-backend-module-techdocs/alpha'));
-backend.add(
-  import('@backstage/plugin-catalog-backend-module-backstage-openapi'),
-);
-backend.add(import('@backstage/plugin-search-backend/alpha'));
-backend.add(import('@backstage/plugin-techdocs-backend/alpha'));
-backend.add(import('@backstage/plugin-todo-backend'));
-backend.add(import('@backstage/plugin-sonarqube-backend'));
-backend.add(import('@backstage/plugin-signals-backend'));
-backend.add(import('@backstage/plugin-notifications-backend'));
+// backend.add(import('@backstage/plugin-proxy-backend/alpha'));
+// backend.add(import('@backstage/plugin-scaffolder-backend/alpha'));
+// backend.add(import('@backstage/plugin-scaffolder-backend-module-github'));
+// backend.add(import('@backstage/plugin-search-backend-module-catalog/alpha'));
+// backend.add(import('@backstage/plugin-search-backend-module-explore/alpha'));
+// backend.add(import('@backstage/plugin-search-backend-module-techdocs/alpha'));
+// backend.add(
+//   import('@backstage/plugin-catalog-backend-module-backstage-openapi'),
+// );
+// backend.add(import('@backstage/plugin-search-backend/alpha'));
+// backend.add(import('@backstage/plugin-techdocs-backend/alpha'));
+// backend.add(import('@backstage/plugin-todo-backend'));
+// backend.add(import('@backstage/plugin-sonarqube-backend'));
+// backend.add(import('@backstage/plugin-signals-backend'));
+// backend.add(import('@backstage/plugin-notifications-backend'));
 
 backend.start();
