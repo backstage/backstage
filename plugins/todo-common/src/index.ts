cccdeaf82e5c057418db86b23662c9aec031275e
aramissennyeydd <aramis.sennyey@doordash.com>
2024-04-08 22:18:15 -0400

    update search + todo
    Signed-off-by: aramissennyeydd <aramis.sennyey@doordash.com>

---
+++ plugins/todo-common/src/index.ts
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2024 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Common functionalities for the todo plugin.
+ *
+ * @packageDocumentation
+ */
+
+/**
+ * In this package you might for example declare types that are common
+ * between the frontend and backend plugin packages.
+ */
+export type CommonType = {
+  field: string;
+};
+
+/**
+ * Or you might declare some common constants.
+ */
+export const COMMON_CONSTANT = 1;
