8f25c79a834aafcc51a5f4b47ea759256d15dd06
npiyush97 <npiyush35@gmail.com>
2024-04-16 14:12:20 +0530

    prettier:fix
    Signed-off-by: npiyush97 <npiyush35@gmail.com>

--- microsite/docusaurus.config.ts
+++ microsite/docusaurus.config.ts
@@ -24,7 +24,7 @@ import { Config } from '@docusaurus/types';
 const backstageTheme = themes.vsDark;
 backstageTheme.plain.backgroundColor = '#232323';
 
-const config:Config = {
+const config: Config = {
   title: 'Backstage Software Catalog and Developer Platform',
   tagline: 'An open source framework for building developer portals',
   url: 'https://backstage.io',
@@ -176,176 +176,174 @@ const config:Config = {
       },
     ],
   ],
-  themeConfig:
-    {
-      colorMode: {
-        defaultMode: 'dark',
-        disableSwitch: true,
+  themeConfig: {
+    colorMode: {
+      defaultMode: 'dark',
+      disableSwitch: true,
+    },
+    navbar: {
+      logo: {
+        alt: 'Backstage Software Catalog and Developer Platform',
+        src: 'img/logo.svg',
       },
-      navbar: {
-        logo: {
-          alt: 'Backstage Software Catalog and Developer Platform',
-          src: 'img/logo.svg',
+      items: [
+        {
+          href: 'https://github.com/backstage/backstage',
+          label: 'GitHub',
+          position: 'left',
         },
-        items: [
-          {
-            href: 'https://github.com/backstage/backstage',
-            label: 'GitHub',
-            position: 'left',
-          },
-          {
-            to: 'docs/overview/what-is-backstage',
-            label: 'Docs',
-            position: 'left',
-          },
-          {
-            to: '/plugins',
-            label: 'Plugins',
-            position: 'left',
-          },
-          {
-            to: '/blog',
-            label: 'Blog',
-            position: 'left',
-          },
-          {
-            to: 'docs/releases/v1.25.0',
-            label: 'Releases',
-            position: 'left',
-          },
-          {
-            to: '/demos',
-            label: 'Demos',
-            position: 'left',
-          },
-          {
-            to: '/community',
-            label: 'Community',
-            position: 'left',
-          },
-        ],
-      },
-      image: 'img/sharing-opengraph.png',
-      footer: {
-        links: [
-          {
-            items: [
-              {
-                html: `
+        {
+          to: 'docs/overview/what-is-backstage',
+          label: 'Docs',
+          position: 'left',
+        },
+        {
+          to: '/plugins',
+          label: 'Plugins',
+          position: 'left',
+        },
+        {
+          to: '/blog',
+          label: 'Blog',
+          position: 'left',
+        },
+        {
+          to: 'docs/releases/v1.25.0',
+          label: 'Releases',
+          position: 'left',
+        },
+        {
+          to: '/demos',
+          label: 'Demos',
+          position: 'left',
+        },
+        {
+          to: '/community',
+          label: 'Community',
+          position: 'left',
+        },
+      ],
+    },
+    image: 'img/sharing-opengraph.png',
+    footer: {
+      links: [
+        {
+          items: [
+            {
+              html: `
                 <a href="/" aria-label="Backstage Home">
                   <div class="footerLogo"></div>
                 </a>`,
-              },
-            ],
-          },
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'What is Backstage?',
-                to: 'docs/overview/what-is-backstage',
-              },
-              {
-                label: 'Getting started',
-                to: 'docs/getting-started/',
-              },
-              {
-                label: 'Software Catalog',
-                to: 'docs/features/software-catalog/',
-              },
-              {
-                label: 'Create a Plugin',
-                to: 'docs/plugins/create-a-plugin',
-              },
-              {
-                label: 'Designing for Backstage',
-                to: 'docs/dls/design',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Support chatroom',
-                to: 'https://discord.gg/backstage-687207715902193673',
-              },
-              {
-                label: 'Contributing',
-                to: 'https://github.com/backstage/backstage/blob/master/CONTRIBUTING.md',
-              },
-              {
-                label: 'Adopting',
-                to: 'https://backstage.spotify.com',
-              },
-              {
-                label: 'Subscribe to our newsletter',
-                to: 'https://info.backstage.spotify.com/newsletter_subscribe',
-              },
-              {
-                label: 'CNCF Incubation',
-                to: 'https://www.cncf.io/projects/',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Open Source @ Spotify',
-                to: 'https://spotify.github.io/',
-              },
-              {
-                label: 'Spotify Engineering Blog',
-                to: 'https://engineering.atspotify.com/',
-              },
-              {
-                label: 'Spotify for Developers',
-                to: 'https://developer.spotify.com/',
-              },
-              {
-                label: 'GitHub',
-                to: 'https://github.com/backstage/',
-              },
-            ],
-          },
-        ],
-        copyright:
-          `<p style="text-align:center"><a href="https://spotify.github.io/">Made with ❤️ at Spotify</a></p><p class="copyright">Copyright © ${new Date().getFullYear()} Backstage Project Authors. All rights reserved. The Linux Foundation has registered trademarks and uses trademarks. For a list of trademarks of The Linux Foundation, please see our Trademark Usage page: https://www.linuxfoundation.org/trademark-usage</p>`,
-      },
-      algolia: {
-        apiKey: '1f0ba86672ccfc3576faa94583e5b318',
-        indexName: 'crawler_Backstage Docusaurus 2',
-        appId: 'JCMFNHCHI8',
-        searchParameters: {},
-      },
-      prism: {
-        theme: backstageTheme,
-        // Supported languages: https://prismjs.com/#supported-languages
-        // Default languages: https://github.com/FormidableLabs/prism-react-renderer/blob/master/packages/generate-prism-languages/index.ts#L9-L23
-        additionalLanguages: ['docker', 'bash'],
-        magicComments: [
-          // Extend the default highlight class name
-          {
-            className: 'code-block-highlight-line',
-            line: 'highlight-next-line',
-            block: { start: 'highlight-start', end: 'highlight-end' },
-          },
-          {
-            className: 'code-block-add-line',
-            line: 'highlight-add-next-line',
-            block: { start: 'highlight-add-start', end: 'highlight-add-end' },
-          },
-          {
-            className: 'code-block-remove-line',
-            line: 'highlight-remove-next-line',
-            block: {
-              start: 'highlight-remove-start',
-              end: 'highlight-remove-end',
             },
+          ],
+        },
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'What is Backstage?',
+              to: 'docs/overview/what-is-backstage',
+            },
+            {
+              label: 'Getting started',
+              to: 'docs/getting-started/',
+            },
+            {
+              label: 'Software Catalog',
+              to: 'docs/features/software-catalog/',
+            },
+            {
+              label: 'Create a Plugin',
+              to: 'docs/plugins/create-a-plugin',
+            },
+            {
+              label: 'Designing for Backstage',
+              to: 'docs/dls/design',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Support chatroom',
+              to: 'https://discord.gg/backstage-687207715902193673',
+            },
+            {
+              label: 'Contributing',
+              to: 'https://github.com/backstage/backstage/blob/master/CONTRIBUTING.md',
+            },
+            {
+              label: 'Adopting',
+              to: 'https://backstage.spotify.com',
+            },
+            {
+              label: 'Subscribe to our newsletter',
+              to: 'https://info.backstage.spotify.com/newsletter_subscribe',
+            },
+            {
+              label: 'CNCF Incubation',
+              to: 'https://www.cncf.io/projects/',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Open Source @ Spotify',
+              to: 'https://spotify.github.io/',
+            },
+            {
+              label: 'Spotify Engineering Blog',
+              to: 'https://engineering.atspotify.com/',
+            },
+            {
+              label: 'Spotify for Developers',
+              to: 'https://developer.spotify.com/',
+            },
+            {
+              label: 'GitHub',
+              to: 'https://github.com/backstage/',
+            },
+          ],
+        },
+      ],
+      copyright: `<p style="text-align:center"><a href="https://spotify.github.io/">Made with ❤️ at Spotify</a></p><p class="copyright">Copyright © ${new Date().getFullYear()} Backstage Project Authors. All rights reserved. The Linux Foundation has registered trademarks and uses trademarks. For a list of trademarks of The Linux Foundation, please see our Trademark Usage page: https://www.linuxfoundation.org/trademark-usage</p>`,
+    },
+    algolia: {
+      apiKey: '1f0ba86672ccfc3576faa94583e5b318',
+      indexName: 'crawler_Backstage Docusaurus 2',
+      appId: 'JCMFNHCHI8',
+      searchParameters: {},
+    },
+    prism: {
+      theme: backstageTheme,
+      // Supported languages: https://prismjs.com/#supported-languages
+      // Default languages: https://github.com/FormidableLabs/prism-react-renderer/blob/master/packages/generate-prism-languages/index.ts#L9-L23
+      additionalLanguages: ['docker', 'bash'],
+      magicComments: [
+        // Extend the default highlight class name
+        {
+          className: 'code-block-highlight-line',
+          line: 'highlight-next-line',
+          block: { start: 'highlight-start', end: 'highlight-end' },
+        },
+        {
+          className: 'code-block-add-line',
+          line: 'highlight-add-next-line',
+          block: { start: 'highlight-add-start', end: 'highlight-add-end' },
+        },
+        {
+          className: 'code-block-remove-line',
+          line: 'highlight-remove-next-line',
+          block: {
+            start: 'highlight-remove-start',
+            end: 'highlight-remove-end',
           },
-        ],
-      },
-    } satisfies Preset.ThemeConfig,
+        },
+      ],
+    },
+  } satisfies Preset.ThemeConfig,
 };
-export default config
+export default config;
