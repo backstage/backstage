c27808698f689fca403056be2007df23b6c9f7ca
Nitin Ramnani <nitin.ramnani@infosys.com>
2024-05-03 18:32:54 +0530

    Added stories for multiple components
    Signed-off-by: Nitin Ramnani <nitin.ramnani@infosys.com>

--- packages/core-components/src/layout/Content/Content.stories.tsx
+++ packages/core-components/src/layout/Content/Content.stories.tsx
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import React, { ReactNode } from 'react';
+import React from 'react';
 import { Content } from './Content';
 
 export default {
