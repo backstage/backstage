df3c5351739d4e066ee35b6ac91e2b3343e47960
Dave Ashby <dave.ashby@potomacsi.com>
2024-05-24 18:33:25 -0400

    remove unnecessary Map Icon import
    Signed-off-by: Dave Ashby <dave.ashby@potomacsi.com>

--- packages/create-app/templates/default-app/packages/app/src/components/Root/Root.tsx
+++ packages/create-app/templates/default-app/packages/app/src/components/Root/Root.tsx
@@ -2,7 +2,6 @@ import React, { PropsWithChildren } from 'react';
 import { makeStyles } from '@material-ui/core';
 import HomeIcon from '@material-ui/icons/Home';
 import ExtensionIcon from '@material-ui/icons/Extension';
-import MapIcon from '@material-ui/icons/MyLocation';
 import LibraryBooks from '@material-ui/icons/LibraryBooks';
 import CreateComponentIcon from '@material-ui/icons/AddCircleOutline';
 import LogoFull from './LogoFull';
