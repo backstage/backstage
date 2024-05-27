3331ffb480d997ede3040a59391df2255ed0e942
John Redwood <john.r.k.redwood@gmail.com>
2024-05-27 22:35:49 +0000

    review: code cleanup from review
    Signed-off-by: John Redwood <john.r.k.redwood@gmail.com>

--- plugins/scaffolder-backend-module-gitlab/src/actions/gitlabIssueCreate.ts
+++ plugins/scaffolder-backend-module-gitlab/src/actions/gitlabIssueCreate.ts
@@ -139,11 +139,7 @@ export const createGitlabIssueAction = (options: {
         let isEpicScoped = false;
 
         if (epicId) {
-          isEpicScoped = await checkEpicScope(
-            api as any as InstanceType<typeof Gitlab>,
-            projectId,
-            epicId,
-          );
+          isEpicScoped = await checkEpicScope(api, projectId, epicId);
 
           if (isEpicScoped) {
             ctx.logger.info('Epic is within Project Scope');
