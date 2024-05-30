a77e6a436915e98176c2d7abd44cdcbea59ee268
Dave Ashby <dave.ashby@potomacsi.com>
2024-05-30 18:45:31 -0400

    tweak spacing
    Signed-off-by: Dave Ashby <dave.ashby@potomacsi.com>

--- packages/create-app/templates/default-app/packages/app/src/components/Root/Root.tsx
+++ packages/create-app/templates/default-app/packages/app/src/components/Root/Root.tsx
@@ -71,7 +71,7 @@ export const Root = ({ children }: PropsWithChildren<{}>) => (
         {/* End global nav */}
         <SidebarDivider />
         <SidebarScrollWrapper>
-          { /* Items in this group will be scrollable if they run out of space */ }
+        { /* Items in this group will be scrollable if they run out of space */ }
         </SidebarScrollWrapper>
       </SidebarGroup>
       <SidebarSpace />
