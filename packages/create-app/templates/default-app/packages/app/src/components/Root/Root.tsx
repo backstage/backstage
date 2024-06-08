780d994544de0045e8aae211a9ebe85957f3ea26
Andre Wanlin <awanlin@spotify.com>
2024-06-07 19:35:15 -0500

    Added `MyGroupsSidebarItem` to the sidebar in the `create-app` template
    Signed-off-by: Andre Wanlin <awanlin@spotify.com>

--- packages/create-app/templates/default-app/packages/app/src/components/Root/Root.tsx
+++ packages/create-app/templates/default-app/packages/app/src/components/Root/Root.tsx
@@ -25,6 +25,8 @@ import {
 } from '@backstage/core-components';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
+import { MyGroupsSidebarItem } from '@backstage/plugin-org';
+import GroupIcon from '@material-ui/icons/People';
 
 const useSidebarLogoStyles = makeStyles({
   root: {
@@ -65,6 +67,11 @@ export const Root = ({ children }: PropsWithChildren<{}>) => (
       <SidebarGroup label="Menu" icon={<MenuIcon />}>
         {/* Global nav, not org-specific */}
         <SidebarItem icon={HomeIcon} to="catalog" text="Home" />
+        <MyGroupsSidebarItem
+          singularTitle="My Group"
+          pluralTitle="My Groups"
+          icon={GroupIcon}
+        />
         <SidebarItem icon={ExtensionIcon} to="api-docs" text="APIs" />
         <SidebarItem icon={LibraryBooks} to="docs" text="Docs" />
         <SidebarItem icon={CreateComponentIcon} to="create" text="Create..." />
