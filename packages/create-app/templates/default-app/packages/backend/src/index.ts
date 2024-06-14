c0a705d44298c5274f1d94b230d642a86cada2c9
Andre Wanlin <awanlin@spotify.com>
2024-06-14 10:49:04 -0500

    Added the Kubernetes plugin to `create-app`
    Signed-off-by: Andre Wanlin <awanlin@spotify.com>

--- packages/create-app/templates/default-app/packages/backend/src/index.ts
+++ packages/create-app/templates/default-app/packages/backend/src/index.ts
@@ -41,4 +41,7 @@ backend.add(import('@backstage/plugin-search-backend/alpha'));
 backend.add(import('@backstage/plugin-search-backend-module-catalog/alpha'));
 backend.add(import('@backstage/plugin-search-backend-module-techdocs/alpha'));
 
+// kubernetes
+backend.add(import('@backstage/plugin-kubernetes-backend/alpha'));
+
 backend.start();
