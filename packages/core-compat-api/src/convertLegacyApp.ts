966cca176bc0506034499ce2cb622ebdffe2ca1b
blam <ben@blam.sh>
2024-06-26 16:31:25 +0200

    chore: tidying up Signed-off-by: blam <ben@blam.sh>
    Signed-off-by: blam <ben@blam.sh>

--- packages/core-compat-api/src/convertLegacyApp.ts
+++ packages/core-compat-api/src/convertLegacyApp.ts
@@ -26,7 +26,6 @@ import {
   coreExtensionData,
   createExtension,
   createExtensionInput,
-  createExtensionKind,
   createExtensionOverrides,
 } from '@backstage/frontend-plugin-api';
 import { getComponentData } from '@backstage/core-plugin-api';
@@ -129,19 +128,13 @@ export function convertLegacyApp(
       };
     },
   });
-
-  const CoreNavOverride = CurrentCoreNav.override({
-    // namespace: 'app',
-    // name: 'nav',
-    // attachTo: { id: 'app/layout', input: 'nav' },
-    // output: {},
+  const CoreNavOverride = createExtension({
+    namespace: 'app',
+    name: 'nav',
+    attachTo: { id: 'app/layout', input: 'nav' },
+    output: {},
     factory: () => ({}),
     disabled: true,
-  });
-
-  createExtensionOverride({
-    extension: CurrentCoreNav,
-    factory: () => null,
   });
 
   const collectedRoutes = collectLegacyRoutes(routesEl);
@@ -149,60 +142,7 @@ export function convertLegacyApp(
   return [
     ...collectedRoutes,
     createExtensionOverrides({
-      extensions: [CoreNavOverride, CoreNavOverride],
+      extensions: [CoreLayoutOverride, CoreNavOverride],
     }),
   ];
 }
-
-const EntityCardExtension = createExtensionKind({
-  kind: 'entity-card',
-  attachTo: { id: 'entity-card', input: 'default' },
-  inputs: {
-    loader: createExtensionInput({
-      element: coreExtensionData.reactElement,
-    }),
-  },
-  output: {
-    element: coreExtensionData.reactElement,
-  },
-  factory({ inputs }, props: { title: string }) {
-    console.log(inputs.loader);
-    return {
-      element: React.createElement('h1'),
-    };
-  },
-});
-
-const GithubCard = EntityCardExtension.new({
-  props: {
-    title: 'GitHub Card',
-  },
-  factory({ inputs: { loader } }) {
-    console.log(loader);
-    return {
-      element: React.createElement('h2'),
-    };
-  },
-});
-
-GithubCard.override({
-  attachTo: { id: 'entity-card', input: 'github' },
-  inputs: {
-    loader: createExtensionInput(
-      {
-        element: coreExtensionData.reactElement,
-      },
-      { singleton: false },
-    ),
-    loader2: createExtensionInput({
-      element: coreExtensionData.reactElement,
-    }),
-  },
-  factory({ originalFactory, inputs }) {
-    inputs.loader2;
-    inputs.loader;
-    return originalFactory({
-      inputsOverride: inputs,
-    });
-  },
-});
