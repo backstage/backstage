51046f5d27ac31307025cfe4b6b74b5877373d25
Patrik Oldsberg <poldsberg@gmail.com>
2024-08-27 13:07:54 +0200

    core-compat-api: update compat wrapper test snapshot
    Signed-off-by: Patrik Oldsberg <poldsberg@gmail.com>

--- packages/core-compat-api/src/compatWrapper/compatWrapper.test.tsx
+++ packages/core-compat-api/src/compatWrapper/compatWrapper.test.tsx
@@ -52,7 +52,7 @@ describe('BackwardsCompatProvider', () => {
               const app = useApp();
               return (
                 <div data-testid="ctx">
-                  plugins:
+                  plugins:{' '}
                   {app
                     .getPlugins()
                     .map(p => p.getId())
@@ -74,7 +74,7 @@ describe('BackwardsCompatProvider', () => {
     );
 
     expect(screen.getByTestId('ctx').textContent).toMatchInlineSnapshot(`
-      "plugins:
+      "plugins: app
       components: NotFoundErrorPage, BootErrorPage, Progress, Router, ErrorBoundaryFallback
       icons: brokenImage, catalog, scaffolder, techdocs, search, chat, dashboard, docs, email, github, group, help, kind:api, kind:component, kind:domain, kind:group, kind:location, kind:system, kind:user, kind:resource, kind:template, user, warning"
     `);
