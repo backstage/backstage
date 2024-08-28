3e9b1a439349d2dd189c81c83341af3abe6d3fd8
Fredrik Adelöw <freben@gmail.com>
2024-08-28 09:24:11 +0200

    add name to the extension
    Signed-off-by: Fredrik Adelöw <freben@gmail.com>

--- plugins/signals/src/plugin.ts
+++ plugins/signals/src/plugin.ts
@@ -46,6 +46,7 @@ export const signalsPlugin = createPlugin({
 /** @public */
 export const SignalsDisplay = signalsPlugin.provide(
   createComponentExtension({
+    name: 'SignalsDisplay',
     component: {
       // No-op for now, this is just a placeholder to avoid the need for an explicit plugin installation
       sync: () => null,
