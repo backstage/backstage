9f8d888594e790d10e15b02169e066b5d4e04edd
Łukasz Jernaś <lukasz.jernas@allegro.com>
2024-09-04 14:40:09 +0200

    fix(techdocs-node): Allow for dictionaries in pymdx plugin settings
    Signed-off-by: Łukasz Jernaś <lukasz.jernas@allegro.com>

---
+++ .changeset/happy-numbers-press.md
@@ -0,0 +1,5 @@
+---
+'@backstage/plugin-techdocs-node': minor
+---
+
+Fixed issue in techdocs yaml parser which didn't allow for dictionaries/mappings in pymdx plugin configuration.
--- plugins/techdocs-node/src/stages/generate/helpers.ts
+++ plugins/techdocs-node/src/stages/generate/helpers.ts
@@ -149,6 +149,14 @@ export const MKDOCS_SCHEMA = DEFAULT_SCHEMA.extend([
     instanceOf: UnknownTag,
     construct: (data: string, type?: string) => new UnknownTag(data, type),
   }),
+  new Type('', {
+    kind: 'mapping',
+    multi: true,
+    representName: o => (o as UnknownTag).type,
+    represent: o => (o as UnknownTag).data ?? '',
+    instanceOf: UnknownTag,
+    construct: (data: string, type?: string) => new UnknownTag(data, type),
+  }),
 ]);
 
 /**
