33de5b0feadb43a140a63443feb1859dd43c755b
Heikki Hellgren <heikki.hellgren@op.fi>
2024-09-17 17:19:45 +0300

    fix: skip trust proxy validation in rate limiting
    Signed-off-by: Heikki Hellgren <heikki.hellgren@op.fi>

--- packages/backend-defaults/src/entrypoints/rootHttpRouter/rootHttpRouterServiceFactory.ts
+++ packages/backend-defaults/src/entrypoints/rootHttpRouter/rootHttpRouterServiceFactory.ts
@@ -88,9 +88,6 @@ const rootHttpRouterServiceFactoryWithOptions = (
       const trustProxy = config.getOptional<boolean | number>(
         'backend.trustProxy',
       );
-      if (trustProxy) {
-        app.set('trust proxy', trustProxy);
-      }
 
       const router = DefaultRootHttpRouter.create({ indexPath });
       const middleware = MiddlewareFactory.create({ config, logger });
@@ -115,6 +112,9 @@ const rootHttpRouterServiceFactoryWithOptions = (
         applyDefaults() {
           if (process.env.NODE_ENV === 'development') {
             app.set('json spaces', 2);
+          }
+          if (trustProxy) {
+            app.set('trust proxy', trustProxy);
           }
           app.use(middleware.rateLimit());
           app.use(middleware.helmet());
