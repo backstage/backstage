e86e4a5ff4437a447388e038dab8c0d427d46d7d
Johan Haals <johan.haals@gmail.com>
2024-09-18 15:00:58 +0200

    Remove unused imports
    Signed-off-by: Johan Haals <johan.haals@gmail.com>

--- plugins/scaffolder/src/components/ActionsPage/ActionsPage.tsx
+++ plugins/scaffolder/src/components/ActionsPage/ActionsPage.tsx
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import React, { Fragment, useCallback, useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import useAsync from 'react-use/esm/useAsync';
 import {
   ActionExample,
