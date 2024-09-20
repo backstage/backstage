6af800ff7a397f7df1579b2d7b2c2391e5f8c759
Min Kim <minkimcello@gmail.com>
2024-09-19 20:13:44 -0400

    Create yaml files for existing templates
    Signed-off-by: Min Kim <minkimcello@gmail.com>

---
+++ packages/cli/templates/alpha/all-default-templates.ts
@@ -0,0 +1,38 @@
+export default [
+  {
+    id: 'backend-module',
+    target: '@backstage/cli/templates/alpha/default-backend-module.yaml',
+  },
+  {
+    id: 'backend-plugin',
+    target: '@backstage/cli/templates/alpha/default-backend-plugin.yaml',
+  },
+  {
+    id: 'plugin-common',
+    target: '@backstage/cli/templates/alpha/default-common-plugin-package.yaml',
+  },
+  {
+    id: 'plugin-node',
+    target: '@backstage/cli/templates/alpha/default-node-plugin-package.yaml',
+  },
+  {
+    id: 'frontend-plugion', // changed from 'plugin'
+    target: '@backstage/cli/templates/alpha/default-plugin.yaml',
+  },
+  {
+    id: 'plugin-react',
+    target: '@backstage/cli/templates/alpha/default-react-plugin-package.yaml',
+  },
+  {
+    id: 'node-library',
+    target: '@backstage/cli/templates/alpha/node-library-package.yaml',
+  },
+  {
+    id: 'scaffolder-module',
+    target: '@backstage/cli/templates/alpha/scaffolder-module.yaml',
+  },
+  {
+    id: 'web-library',
+    target: '@backstage/cli/templates/alpha/web-library-package.yaml',
+  },
+];
