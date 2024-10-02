79c0d69cdc1c1055503fd9670fd8f5e9f7d6be5e
Jasper Boeijenga <jboeijenga@gmail.com>
2024-10-02 11:39:44 +0200

    Removed unused import
    Signed-off-by: Jasper Boeijenga <jboeijenga@gmail.com>

--- plugins/scaffolder-react/src/next/components/Stepper/utils.test.ts
+++ plugins/scaffolder-react/src/next/components/Stepper/utils.test.ts
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { first } from 'lodash';
+
 import { type ParsedTemplateSchema } from '../../hooks/useTemplateSchema';
 import {
   getInitialFormState,
