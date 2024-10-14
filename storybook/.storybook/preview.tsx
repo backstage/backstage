28d3d82b425d9dd9b34de6c5e569a94e6252207d
Charles de Dreuille <charles.dedreuille@gmail.com>
2024-10-14 21:39:57 +0100

    Update Storybook to v8
    Signed-off-by: Charles de Dreuille <charles.dedreuille@gmail.com>

---
+++ storybook/.storybook/preview.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { TestApiProvider } from '@backstage/test-utils';
+import { Content, AlertDisplay } from '@backstage/core-components';
+import { lightTheme, darkTheme } from '@backstage/theme';
+import { CssBaseline, ThemeProvider } from '@material-ui/core';
+import { useDarkMode } from 'storybook-dark-mode';
+import { apis } from './apis';
+
+import type { Preview } from '@storybook/react';
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    darkMode: {
+      current: 'light',
+    },
+    layout: 'fullscreen',
+  },
+  decorators: [
+    Story => (
+      <TestApiProvider apis={apis}>
+        <ThemeProvider theme={useDarkMode() ? darkTheme : lightTheme}>
+          <CssBaseline>
+            <AlertDisplay />
+            <Content>
+              <Story />
+            </Content>
+          </CssBaseline>
+        </ThemeProvider>
+      </TestApiProvider>
+    ),
+  ],
+};
+
+export default preview;
+
+export const parameters = {
+  options: {
+    storySort: {
+      order: ['Plugins', 'Layout', 'Navigation'],
+    },
+  },
+};
