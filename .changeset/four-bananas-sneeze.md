11b001cf4dad381d3d6717482112dacafbdbf729
Matt Benson <gudnabrsam@gmail.com>
2024-10-18 14:09:51 -0500

    Add proxyEndpointsExtensionPoint
    Signed-off-by: Matt Benson <gudnabrsam@gmail.com>

---
+++ .changeset/four-bananas-sneeze.md
@@ -0,0 +1,6 @@
+---
+'@backstage/plugin-proxy-backend': minor
+'@backstage/plugin-proxy-node': minor
+---
+
+Add proxyEndpointsExtensionPoint
--- plugins/proxy-backend/src/plugin.ts
+++ plugins/proxy-backend/src/plugin.ts
@@ -20,6 +20,8 @@ import {
   coreServices,
 } from '@backstage/backend-plugin-api';
 import { createRouterInternal } from './service/router';
+import { proxyEndpointsExtensionPoint } from '@backstage/plugin-proxy-node/alpha';
+import { JsonObject } from '@backstage/types';
 
 /**
  * The proxy backend plugin.
@@ -29,6 +31,13 @@ import { createRouterInternal } from './service/router';
 export const proxyPlugin = createBackendPlugin({
   pluginId: 'proxy',
   register(env) {
+    const additionalEndpoints = {};
+
+    env.registerExtensionPoint(proxyEndpointsExtensionPoint, {
+      addProxyEndpoints(endpoints: JsonObject) {
+        Object.assign(additionalEndpoints, endpoints);
+      },
+    });
     env.registerInit({
       deps: {
         config: coreServices.rootConfig,
@@ -42,6 +51,7 @@ export const proxyPlugin = createBackendPlugin({
           discovery,
           logger: loggerToWinstonLogger(logger),
           httpRouterService: httpRouter,
+          additionalEndpoints,
         });
       },
     });
