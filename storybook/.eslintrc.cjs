f59c10ff6567a0a1ce0ef59a266d07b8bd55db29
Charles de Dreuille <charles.dedreuille@gmail.com>
2024-10-20 18:42:24 +0100

    Update imports of Storybook to other packages
    Signed-off-by: Charles de Dreuille <charles.dedreuille@gmail.com>

---
+++ storybook/.eslintrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  extends: ['plugin:storybook/recommended'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  env: {
+    es6: true,
+    node: true,
+  },
+};
