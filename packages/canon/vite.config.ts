4f4e864053ba5fd00008da184fbf76bf4350dfdb
Charles de Dreuille <charles.dedreuille@gmail.com>
2024-11-14 09:00:01 +0000

    Remove Vite test support
    Signed-off-by: Charles de Dreuille <charles.dedreuille@gmail.com>

--- packages/canon/vite.config.ts
+++ packages/canon/vite.config.ts
@@ -1,16 +1,29 @@
+/*
+ * Copyright 2024 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'node:path';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import dts from 'vite-plugin-dts';
-import yaml from '@modyfi/vite-plugin-yaml';
 
 export default defineConfig({
   plugins: [
     react(),
     dts({ exclude: ['**/*.stories.ts', '**/*.stories.tsx', '**/*.css.ts'] }),
     vanillaExtractPlugin(),
-    yaml(),
   ],
   build: {
     lib: {
