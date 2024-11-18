f1647440369b9e97f3d1f40be9e92094e1697e20
Juan Escalada <juanescalada175@gmail.com>
2024-11-18 16:40:54 +0900

    Add customFilters hook
    Signed-off-by: Juan Escalada <juanescalada175@gmail.com>

---
+++ plugins/scaffolder-react/src/hooks/useCustomFilters.ts
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2024 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { useElementFilter } from '@backstage/core-plugin-api';
+import { FILTERS_KEY, FILTERS_WRAPPER_KEY } from '../filters/keys';
+import { FilterOptions } from '../filters';
+
+/**
+ * Hook that returns all custom field extensions from the current outlet.
+ * @public
+ */
+export const useCustomFilters = <TComponentDataType = FilterOptions>(
+  outlet: React.ReactNode,
+) => {
+  return useElementFilter(outlet, elements =>
+    elements
+      .selectByComponentData({
+        key: FILTERS_WRAPPER_KEY,
+      })
+      .findComponentData<TComponentDataType>({
+        key: FILTERS_KEY,
+      }),
+  );
+};
