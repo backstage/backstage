96cb012cb056354bb84da485e6225e2250c54432
Javier Prieto <javier.accounts@fiml.co>
2024-11-20 10:06:35 -0500

    feat(home-plugin-i18n-integration):  add translations to AddWidgetDialog
    Signed-off-by: Javier Prieto <javier.accounts@fiml.co>

--- plugins/home/src/components/CustomHomepage/AddWidgetDialog.tsx
+++ plugins/home/src/components/CustomHomepage/AddWidgetDialog.tsx
@@ -24,6 +24,8 @@ import AddIcon from '@material-ui/icons/Add';
 import ListItemText from '@material-ui/core/ListItemText';
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import { useTranslationRef } from '@backstage/frontend-plugin-api';
+import { homePageTranslationRef } from '../../translation';
 
 interface AddWidgetDialogProps {
   widgets: Widget[];
@@ -36,9 +38,13 @@ const getTitle = (widget: Widget) => {
 
 export const AddWidgetDialog = (props: AddWidgetDialogProps) => {
   const { widgets, handleAdd } = props;
+  const { t } = useTranslationRef(homePageTranslationRef);
+
   return (
     <>
-      <DialogTitle>Add new widget to dashboard</DialogTitle>
+      <DialogTitle>
+        {t('components.customHomepage.addWidgetDialog.title')}
+      </DialogTitle>
       <DialogContent>
         <List dense>
           {widgets.map(widget => {
