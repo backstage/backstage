0df3c5ddf779c40204cb4084a1096e4e1bc58237
Cptn Fizzbin <code@cptnfizzbin.ca>
2024-11-20 10:59:56 -0500

    add rules for template entities
    Signed-off-by: Cptn Fizzbin <code@cptnfizzbin.ca>

---
+++ plugins/scaffolder-backend/src/service/rules/scaffolderTemplateEntityRules.test.ts
@@ -0,0 +1,586 @@
+/*
+ * Copyright 2024 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { JsonObject, JsonPrimitive } from '@backstage/types';
+import { RESOURCE_TYPE_SCAFFOLDER_TEMPLATE_ENTITY } from '@backstage/plugin-scaffolder-common/alpha';
+import { AuthorizeResult } from '@backstage/plugin-permission-common';
+import { createConditionAuthorizer } from '@backstage/plugin-permission-node';
+import {
+  hasAction,
+  hasActionWithBooleanProperty,
+  hasActionWithNumberProperty,
+  hasActionWithProperty,
+  hasActionWithStringProperty,
+} from './scaffolderTemplateEntityRules';
+import {
+  TemplateEntityStepV1beta3,
+  TemplateEntityV1beta3,
+  TemplateParametersV1beta3,
+} from '@backstage/plugin-scaffolder-common';
+
+function templateFactory(data: {
+  parameters?: TemplateParametersV1beta3 | TemplateParametersV1beta3[];
+  steps?: TemplateEntityStepV1beta3[];
+}): TemplateEntityV1beta3 {
+  return {
+    apiVersion: 'scaffolder.backstage.io/v1beta3',
+    kind: 'Template',
+    metadata: {
+      name: 'example-template',
+    },
+    spec: {
+      type: 'example',
+      parameters: data.parameters || [],
+      steps: data.steps || [],
+    },
+  };
+}
+
+describe('hasAction', () => {
+  describe('apply', () => {
+    it('returns false when actionId is not matched', () => {
+      expect(
+        hasAction.apply(
+          templateFactory({
+            steps: [{ action: 'fetch:files' }],
+          }),
+          {
+            actionId: 'not-matched',
+          },
+        ),
+      ).toEqual(false);
+    });
+
+    it('returns true when actionId is matched', () => {
+      expect(
+        hasAction.apply(
+          templateFactory({
+            steps: [{ action: 'fetch:files' }],
+          }),
+          {
+            actionId: 'fetch:files',
+          },
+        ),
+      ).toEqual(true);
+    });
+  });
+});
+
+const input: JsonObject = {
+  propwithstring: '1',
+  propwithnumber: 2,
+  propwithobject: {},
+  propwithnull: null,
+  propwithfalse: false,
+  propwithtrue: true,
+  propwitharray: ['item', 0, true, false],
+  nested: { propwithstring: '1', nested: { propwithnumber: 1 } },
+};
+
+describe('hasActionWithProperty', () => {
+  describe('apply', () => {
+    it.each([
+      'foo',
+      'bar',
+      'prop.prop',
+      'nested.nonexisting',
+      '',
+      'propwitharray.100',
+    ])(`returns false when a property doesn't exist in the input`, key => {
+      expect(
+        hasActionWithProperty.apply(
+          templateFactory({
+            steps: [{ action: 'action', input }],
+          }),
+          { actionId: 'action', key },
+        ),
+      ).toEqual(false);
+    });
+
+    it.each([
+      'propwithstring',
+      'propwithnumber',
+      'propwithobject',
+      'propwithnull',
+      'propwithfalse',
+      'propwithtrue',
+      'propwitharray',
+      'propwitharray.1',
+      'nested.propwithstring',
+      'nested.nested',
+      'nested.nested.propwithnumber',
+    ])(`returns true when a property exists, property=%s`, key => {
+      expect(
+        hasActionWithProperty.apply(
+          templateFactory({
+            steps: [{ action: 'action', input }],
+          }),
+          { actionId: 'action', key },
+        ),
+      ).toEqual(true);
+    });
+
+    it.each([
+      ['propwithstring', 1],
+      ['propwithnumber', '2'],
+      ['propwithnumber', true],
+      ['propwithobject', [{}]],
+      ['propwithnull', false],
+      ['propwithfalse', true],
+      ['propwithtrue', null],
+      ['propwitharray', 'nonexistingitem'],
+      ['propwitharray.0', 'nonmatchingitem'],
+      ['nested.propwithstring', 'x'],
+      ['nested.nested', '1'],
+      ['nested.nested.propwithnumber', 'ops'],
+    ])(
+      `returns false when a property exists but the value doesn't match, key=%s value=%o`,
+      (key, value) => {
+        expect(
+          hasActionWithProperty.apply(
+            templateFactory({
+              steps: [{ action: 'action', input }],
+            }),
+            { actionId: 'action', key, value: value as JsonPrimitive },
+          ),
+        ).toEqual(false);
+      },
+    );
+
+    it.each([
+      ['propwithstring', '1'],
+      ['propwithnumber', 2],
+      ['propwithnull', null],
+      ['propwithfalse', false],
+      ['propwithtrue', true],
+      ['propwitharray.0', 'item'],
+      ['nested.propwithstring', '1'],
+      ['nested.nested.propwithnumber', 1],
+    ])(
+      `returns true when a property exists and the value matches, key=%s value=%o`,
+      (key, value) => {
+        expect(
+          hasActionWithProperty.apply(
+            templateFactory({
+              steps: [{ action: 'action', input }],
+            }),
+            { actionId: 'action', key, value: value as JsonPrimitive },
+          ),
+        ).toEqual(true);
+      },
+    );
+
+    it('throws if params are invalid', () => {
+      const isActionAuthorized = createConditionAuthorizer([
+        hasActionWithProperty,
+      ]);
+
+      expect(() =>
+        isActionAuthorized(
+          {
+            resourceType: RESOURCE_TYPE_SCAFFOLDER_TEMPLATE_ENTITY,
+            pluginId: 'scaffolder',
+            result: AuthorizeResult.CONDITIONAL,
+            conditions: {
+              resourceType: RESOURCE_TYPE_SCAFFOLDER_TEMPLATE_ENTITY,
+              rule: 'HAS_ACTION_WITH_PROPERTY',
+              params: {
+                key: 1,
+              },
+            },
+          },
+          templateFactory({
+            steps: [{ action: 'action', input: {} }],
+          }),
+        ),
+      ).toThrow();
+
+      expect(() =>
+        isActionAuthorized(
+          {
+            resourceType: RESOURCE_TYPE_SCAFFOLDER_TEMPLATE_ENTITY,
+            pluginId: 'scaffolder',
+            result: AuthorizeResult.CONDITIONAL,
+            conditions: {
+              resourceType: RESOURCE_TYPE_SCAFFOLDER_TEMPLATE_ENTITY,
+              rule: 'HAS_ACTION_WITH_PROPERTY',
+              params: {},
+            },
+          },
+          templateFactory({
+            steps: [{ action: 'action', input: {} }],
+          }),
+        ),
+      ).toThrow();
+    });
+
+    it('should not throw if params are valid', () => {
+      const isActionAuthorized = createConditionAuthorizer([
+        hasActionWithProperty,
+      ]);
+
+      expect(() =>
+        isActionAuthorized(
+          {
+            resourceType: RESOURCE_TYPE_SCAFFOLDER_TEMPLATE_ENTITY,
+            pluginId: 'scaffolder',
+            result: AuthorizeResult.CONDITIONAL,
+            conditions: {
+              resourceType: RESOURCE_TYPE_SCAFFOLDER_TEMPLATE_ENTITY,
+              rule: 'HAS_ACTION_WITH_PROPERTY',
+              params: {
+                action: 'action',
+                key: 'key',
+              },
+            },
+          },
+          templateFactory({
+            steps: [{ action: 'action', input: {} }],
+          }),
+        ),
+      ).not.toThrow();
+
+      expect(() =>
+        isActionAuthorized(
+          {
+            resourceType: RESOURCE_TYPE_SCAFFOLDER_TEMPLATE_ENTITY,
+            pluginId: 'scaffolder',
+            result: AuthorizeResult.CONDITIONAL,
+            conditions: {
+              resourceType: RESOURCE_TYPE_SCAFFOLDER_TEMPLATE_ENTITY,
+              rule: 'HAS_ACTION_WITH_PROPERTY',
+              params: {
+                key: 'key',
+                value: 'value',
+              },
+            },
+          },
+          templateFactory({
+            steps: [{ action: 'action', input: {} }],
+          }),
+        ),
+      ).not.toThrow();
+    });
+  });
+});
+
+describe('hasActionWithBooleanProperty', () => {
+  describe('apply', () => {
+    it.each(['foo', 'bar', 'prop.prop', 'nested.nonexisting', ''])(
+      `returns false when a property doesn't exist in the input`,
+      key => {
+        expect(
+          hasActionWithBooleanProperty.apply(
+            templateFactory({
+              steps: [{ action: 'action', input }],
+            }),
+            { actionId: 'action', key },
+          ),
+        ).toEqual(false);
+      },
+    );
+
+    it.each([
+      'propwithstring',
+      'propwithnumber',
+      'propwithobject',
+      'propwithnull',
+      'propwitharray',
+      'propwitharray.0',
+      'nested.propwithstring',
+      'nested.nested',
+      'nested.nested.propwithnumber',
+    ])(
+      `returns false when a property exists and is not a boolean, property=%s`,
+      key => {
+        expect(
+          hasActionWithBooleanProperty.apply(
+            templateFactory({
+              steps: [{ action: 'action', input }],
+            }),
+            { actionId: 'action', key },
+          ),
+        ).toEqual(false);
+      },
+    );
+
+    it.each([
+      'propwithfalse',
+      'propwithtrue',
+      'propwitharray.2',
+      'propwitharray.3',
+    ])(`returns true when a property exists, property=%s`, key => {
+      expect(
+        hasActionWithBooleanProperty.apply(
+          templateFactory({
+            steps: [{ action: 'action', input }],
+          }),
+          { actionId: 'action', key },
+        ),
+      ).toEqual(true);
+    });
+
+    it.each([
+      ['propwithstring', true],
+      ['propwithnumber', true],
+      ['propwithnumber', true],
+      ['propwithobject', true],
+      ['propwithnull', true],
+      ['propwithfalse', true],
+      ['propwithtrue', false],
+      ['propwitharray', true],
+      ['propwitharray.2', false],
+      ['propwitharray.3', true],
+      ['nested.propwithstring', true],
+      ['nested.nested', true],
+      ['nested.nested.propwithnumber', true],
+    ])(
+      `returns false when a property exists but the value doesn't match, key=%s value=%o`,
+      (key, value) => {
+        expect(
+          hasActionWithBooleanProperty.apply(
+            templateFactory({
+              steps: [{ action: 'action', input }],
+            }),
+            { actionId: 'action', key, value },
+          ),
+        ).toEqual(false);
+      },
+    );
+
+    it.each([
+      ['propwithfalse', false],
+      ['propwithtrue', true],
+      ['propwitharray.2', true],
+      ['propwitharray.3', false],
+    ])(
+      `returns true when a property exists and the value matches, key=%s value=%o`,
+      (key, value) => {
+        expect(
+          hasActionWithBooleanProperty.apply(
+            templateFactory({
+              steps: [{ action: 'action', input }],
+            }),
+            { actionId: 'action', key, value },
+          ),
+        ).toEqual(true);
+      },
+    );
+  });
+});
+
+describe('hasActionWithNumberProperty', () => {
+  describe('apply', () => {
+    it.each(['foo', 'bar', 'prop.prop', 'nested.nonexisting', ''])(
+      `returns false when a property doesn't exist in the input`,
+      key => {
+        expect(
+          hasActionWithNumberProperty.apply(
+            templateFactory({
+              steps: [{ action: 'action', input }],
+            }),
+            { actionId: 'action', key },
+          ),
+        ).toEqual(false);
+      },
+    );
+
+    it.each([
+      'propwithstring',
+      'propwithobject',
+      'propwithnull',
+      'propwithfalse',
+      'propwithtrue',
+      'propwitharray',
+      'propwitharray.0',
+      'nested.propwithstring',
+      'nested.nested',
+    ])(
+      `returns false when a property exists and is not a number, property=%s`,
+      key => {
+        expect(
+          hasActionWithNumberProperty.apply(
+            templateFactory({
+              steps: [{ action: 'action', input }],
+            }),
+            { actionId: 'action', key },
+          ),
+        ).toEqual(false);
+      },
+    );
+
+    it.each([
+      'propwithnumber',
+      'nested.nested.propwithnumber',
+      'propwitharray.1',
+    ])(`returns true when a property exists, property=%s`, key => {
+      expect(
+        hasActionWithNumberProperty.apply(
+          templateFactory({
+            steps: [{ action: 'action', input }],
+          }),
+          { actionId: 'action', key },
+        ),
+      ).toEqual(true);
+    });
+
+    it.each([
+      ['propwithstring', 1],
+      ['propwithnumber', 1000],
+      ['propwithnumber', 101],
+      ['propwithobject', 1],
+      ['propwithnull', 1],
+      ['propwithfalse', 1],
+      ['propwithtrue', 1],
+      ['propwitharray', 1],
+      ['propwitharray.2', 1],
+      ['propwitharray.3', 1],
+      ['nested.propwithstring', 1],
+      ['nested.nested', 1],
+      ['nested.nested.propwithnumber', 100],
+    ])(
+      `returns false when a property exists but the value doesn't match, key=%s value=%o`,
+      (key, value) => {
+        expect(
+          hasActionWithNumberProperty.apply(
+            templateFactory({
+              steps: [{ action: 'action', input }],
+            }),
+            { actionId: 'action', key, value },
+          ),
+        ).toEqual(false);
+      },
+    );
+
+    it.each([
+      ['propwithnumber', 2],
+      ['nested.nested.propwithnumber', 1],
+      ['propwitharray.1', 0],
+    ])(
+      `returns true when a property exists and the value matches, key=%s value=%o`,
+      (key, value) => {
+        expect(
+          hasActionWithNumberProperty.apply(
+            templateFactory({
+              steps: [{ action: 'action', input }],
+            }),
+            { actionId: 'action', key, value },
+          ),
+        ).toEqual(true);
+      },
+    );
+  });
+});
+
+describe('hasActionWithStringProperty', () => {
+  describe('apply', () => {
+    it.each(['foo', 'bar', 'prop.prop', 'nested.nonexisting', ''])(
+      `returns false when a property doesn't exist in the input`,
+      key => {
+        expect(
+          hasActionWithStringProperty.apply(
+            templateFactory({
+              steps: [{ action: 'action', input }],
+            }),
+            { actionId: 'action', key },
+          ),
+        ).toEqual(false);
+      },
+    );
+
+    it.each([
+      'propwithnumber',
+      'propwithobject',
+      'propwithnull',
+      'propwithfalse',
+      'propwithtrue',
+      'propwitharray',
+      'propwitharray.1',
+      'nested.nested.propwithnumber',
+      'nested.nested',
+    ])(
+      `returns false when a property exists and is not a string, property=%s`,
+      key => {
+        expect(
+          hasActionWithStringProperty.apply(
+            templateFactory({
+              steps: [{ action: 'action', input }],
+            }),
+            { actionId: 'action', key },
+          ),
+        ).toEqual(false);
+      },
+    );
+
+    it.each(['propwithstring', 'nested.propwithstring', 'propwitharray.0'])(
+      `returns true when a property exists, property=%s`,
+      key => {
+        expect(
+          hasActionWithStringProperty.apply(
+            templateFactory({
+              steps: [{ action: 'action', input }],
+            }),
+            { actionId: 'action', key },
+          ),
+        ).toEqual(true);
+      },
+    );
+
+    it.each([
+      ['propwithstring', 'nonmatchingstring'],
+      ['propwithnumber', 's'],
+      ['propwithnumber', 's'],
+      ['propwithobject', 's'],
+      ['propwithnull', 's'],
+      ['propwithfalse', 's'],
+      ['propwithtrue', 's'],
+      ['propwitharray', 's'],
+      ['propwitharray.2', 's'],
+      ['propwitharray.3', 's'],
+      ['nested.nested', 's'],
+      ['nested.nested.propwithnumber', 's'],
+    ])(
+      `returns false when a property exists but the value doesn't match, key=%s value=%o`,
+      (key, value) => {
+        expect(
+          hasActionWithStringProperty.apply(
+            templateFactory({
+              steps: [{ action: 'action', input }],
+            }),
+            { actionId: 'action', key, value },
+          ),
+        ).toEqual(false);
+      },
+    );
+
+    it.each([
+      ['propwithstring', '1'],
+      ['nested.propwithstring', '1'],
+      ['propwitharray.0', 'item'],
+    ])(
+      `returns true when a property exists and the value matches, key=%s value=%o`,
+      (key, value) => {
+        expect(
+          hasActionWithStringProperty.apply(
+            templateFactory({
+              steps: [{ action: 'action', input }],
+            }),
+            { actionId: 'action', key, value },
+          ),
+        ).toEqual(true);
+      },
+    );
+  });
+});
---
+++ plugins/scaffolder-backend/src/service/rules/scaffolderTemplateEntityRules.ts
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2024 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { makeCreatePermissionRule } from '@backstage/plugin-permission-node';
+import { TemplateEntityV1beta3 } from '@backstage/plugin-scaffolder-common';
+import { z } from 'zod';
+import { RESOURCE_TYPE_SCAFFOLDER_TEMPLATE_ENTITY } from '@backstage/plugin-scaffolder-common/alpha';
+import { get } from 'lodash';
+import { JsonPrimitive } from '@backstage/types';
+
+export const createTemplateEntityPermissionRule = makeCreatePermissionRule<
+  TemplateEntityV1beta3,
+  {},
+  typeof RESOURCE_TYPE_SCAFFOLDER_TEMPLATE_ENTITY
+>();
+
+export const hasAction = createTemplateEntityPermissionRule({
+  name: 'HAS_ACTION',
+  resourceType: RESOURCE_TYPE_SCAFFOLDER_TEMPLATE_ENTITY,
+  description: `Match templates with that use one an action`,
+  paramsSchema: z.object({
+    actionId: z.string().describe('The ID of an action to match on'),
+  }),
+  apply: (resource, { actionId }) => {
+    const actions = resource.spec.steps;
+    return !!actions.find(action => action.action === actionId);
+  },
+  toQuery: () => ({}),
+});
+
+function buildHasActionProperty<Schema extends z.ZodType<JsonPrimitive>>({
+  name,
+  valueSchema,
+  validateProperty = true,
+}: {
+  name: string;
+  valueSchema: Schema;
+  validateProperty?: boolean;
+}) {
+  return createTemplateEntityPermissionRule({
+    name,
+    description: `Allow actions with the specified property`,
+    resourceType: RESOURCE_TYPE_SCAFFOLDER_TEMPLATE_ENTITY,
+    paramsSchema: z.object({
+      actionId: z.string().describe('The ID of an action to match on'),
+      key: z
+        .string()
+        .describe(`Property within the action parameters to match on`),
+      value: valueSchema
+        .optional()
+        .describe(`Value of the given property to match on`),
+    }) as unknown as z.ZodType<{
+      actionId: string;
+      key: string;
+      value?: z.infer<Schema>;
+    }>,
+    apply: (resource, { actionId, key, value }) => {
+      const matched = resource.spec.steps
+        .filter(action => action.action === actionId)
+        .find(action => {
+          const foundValue = get(action.input, key);
+
+          if (validateProperty && !valueSchema.safeParse(foundValue).success) {
+            return false;
+          }
+
+          if (value !== undefined) {
+            if (valueSchema.safeParse(value).success) {
+              return value === foundValue;
+            }
+            return false;
+          }
+
+          return foundValue !== undefined;
+        });
+
+      return !!matched;
+    },
+    toQuery: () => ({}),
+  });
+}
+
+export const hasActionWithProperty = buildHasActionProperty({
+  name: 'HAS_ACTION_WITH_PROPERTY',
+  valueSchema: z.union([z.string(), z.number(), z.boolean(), z.null()]),
+  validateProperty: false,
+});
+
+export const hasActionWithBooleanProperty = buildHasActionProperty({
+  name: 'HAS_ACTION_WITH_BOOLEAN_PROPERTY',
+  valueSchema: z.boolean(),
+});
+
+export const hasActionWithNumberProperty = buildHasActionProperty({
+  name: 'HAS_ACTION_WITH_NUMBER_PROPERTY',
+  valueSchema: z.number(),
+});
+
+export const hasActionWithStringProperty = buildHasActionProperty({
+  name: 'HAS_ACTION_WITH_STRING_PROPERTY',
+  valueSchema: z.string(),
+});
+
+export const scaffolderTemplateEntityRules = {
+  hasAction,
+  hasActionWithProperty,
+  hasActionWithBooleanProperty,
+  hasActionWithNumberProperty,
+  hasActionWithStringProperty,
+};
