ca4a287b1b4524a5c3c064046a2cab307ab27eb6
Tyler Davis <tylerd@canva.com>
2024-11-21 16:00:44 +1100

    rebase
    Signed-off-by: Tyler Davis <tylerd@canva.com>

--- plugins/catalog/src/components/CatalogTable/OffsetPaginatedCatalogTable.tsx
+++ plugins/catalog/src/components/CatalogTable/OffsetPaginatedCatalogTable.tsx
@@ -27,7 +27,7 @@ import { CatalogTableToolbar } from './CatalogTableToolbar';
 export function OffsetPaginatedCatalogTable(
   props: TableProps<CatalogTableRow>,
 ) {
-  const { columns, data, options, ...restProps } = props;
+  const { title, columns, data, options, ...restProps } = props;
   const { setLimit, setOffset, limit, totalItems, offset } = useEntityList();
 
   const [page, setPage] = React.useState(
@@ -44,6 +44,7 @@ export function OffsetPaginatedCatalogTable(
 
   return (
     <Table
+      title={title}
       columns={columns}
       data={data}
       options={{
