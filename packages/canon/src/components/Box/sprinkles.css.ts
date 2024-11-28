d0d6c54ca89707e2364a4eb2f49856f3ef7464ae
Charles de Dreuille <charles.dedreuille@gmail.com>
2024-11-28 14:27:02 +0000

    Fix Box component
    Signed-off-by: Charles de Dreuille <charles.dedreuille@gmail.com>

--- packages/canon/src/components/Box/sprinkles.css.ts
+++ packages/canon/src/components/Box/sprinkles.css.ts
@@ -21,28 +21,7 @@ import {
   ConditionalValue,
   createMapValueFn,
 } from '@vanilla-extract/sprinkles';
-
-/** @public */
-export const breakpoints = {
-  xs: {},
-  sm: { '@media': 'screen and (min-width: 640px)' },
-  md: { '@media': 'screen and (min-width: 768px)' },
-  lg: { '@media': 'screen and (min-width: 1024px)' },
-  xl: { '@media': 'screen and (min-width: 1280px)' },
-  '2xl': { '@media': 'screen and (min-width: 1536px)' },
-};
-
-/** @public */
-export const space = {
-  none: 0,
-  xxs: 'var(--canon-space-xxs)',
-  xs: 'var(--canon-space-xs)',
-  sm: 'var(--canon-space-sm)',
-  md: 'var(--canon-space-md)',
-  lg: 'var(--canon-space-lg)',
-  xl: 'var(--canon-space-xl)',
-  xxl: 'var(--canon-space-xxl)',
-};
+import { breakpoints, space } from './properties';
 
 /** @public */
 export const responsiveProperties = defineProperties({
