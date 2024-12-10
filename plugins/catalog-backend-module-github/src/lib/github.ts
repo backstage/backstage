b92790e6951e868031b6f003efd7cfcd37f6d2fe
sonikro <jonathan@nagayoshi.com.br>
2024-12-10 20:28:29 +0000

    feat(GitHubOrgEntityProvider): adds throttling and rate limit to GithubOrgEntityProvider
    Signed-off-by: sonikro <jonathan@nagayoshi.com.br>

--- plugins/catalog-backend-module-github/src/lib/github.ts
+++ plugins/catalog-backend-module-github/src/lib/github.ts
@@ -27,7 +27,9 @@ import {
 import { withLocations } from './withLocations';
 
 import { DeferredEntity } from '@backstage/plugin-catalog-node';
-
+import { Octokit } from '@octokit/core';
+import { LoggerService } from '@backstage/backend-plugin-api';
+import { throttling } from '@octokit/plugin-throttling';
 // Graphql types
 
 export type QueryResponse = {
@@ -710,3 +712,61 @@ export const createReplaceEntitiesOperation =
       added: entitiesToReplace,
     };
   };
+
+/**
+ * Creates a GraphQL Client with Throttling
+ */
+export const createGraphqlClient = (args: {
+  headers:
+    | {
+        [name: string]: string;
+      }
+    | undefined;
+  baseUrl: string;
+  logger: LoggerService;
+}): typeof graphql => {
+  const { headers, baseUrl, logger } = args;
+  const MyOctokit = Octokit.plugin(throttling);
+  const octokit = new MyOctokit({
+    throttle: {
+      onRateLimit: (retryAfter, rateLimitOptions, _, retryCount) => {
+        const rateLimitData = rateLimitOptions as any;
+        logger.warn(
+          `Request quota exhausted for request ${rateLimitData?.method} ${rateLimitData?.url}`,
+        );
+
+        if (retryCount < 2) {
+          logger.warn(
+            `Retrying after ${retryAfter} seconds for the ${retryCount} time due to Rate Limit!`,
+          );
+          return true;
+        }
+
+        return false;
+      },
+      onSecondaryRateLimit: (retryAfter, rateLimitOptions, _, retryCount) => {
+        const rateLimitData = rateLimitOptions as any;
+        logger.warn(
+          `Secondary Rate Limit Exhausted for request ${rateLimitData?.method} ${rateLimitData?.url}`,
+        );
+
+        if (retryCount < 2) {
+          logger.warn(
+            `Retrying after ${retryAfter} seconds for the ${retryCount} time due to Secondary Rate Limit!`,
+          );
+          return true;
+        }
+
+        return false;
+      },
+    },
+  });
+
+  const client = octokit.graphql.defaults({
+    headers,
+    baseUrl,
+  });
+
+  // Since the project has multiple versions of @octokit/graphql, we need to cast the client to the correct type
+  return client as unknown as typeof graphql;
+};
