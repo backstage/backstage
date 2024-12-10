9a4bd3908dee36505b4b85c0c029cc97050b19f1
solimant <solimant@users.noreply.github.com>
2024-12-10 22:01:36 +0000

    Generate Scaffolder OpenAPI client
    Signed-off-by: solimant <solimant@users.noreply.github.com>

--- plugins/scaffolder-common/.eslintrc.js
+++ plugins/scaffolder-common/.eslintrc.js
@@ -1,1 +1,10 @@
-module.exports = require('@backstage/cli/config/eslint-factory')(__dirname);
+module.exports = require('@backstage/cli/config/eslint-factory')(__dirname, {
+  overrides: [
+    {
+      files: ['client/src/schema/openapi/generated/models/*.model.ts'],
+      rules: {
+        '@typescript-eslint/no-redeclare': 'off',
+      },
+    },
+  ],
+});
