913dcf5fb0397b2ecb7ac7e1b38a87ba58128121
solimant <solimant@users.noreply.github.com>
2024-12-10 21:56:28 +0000

    Generate Scaffolder OpenAPI server
    Signed-off-by: solimant <solimant@users.noreply.github.com>

--- plugins/scaffolder-backend/src/service/router.ts
+++ plugins/scaffolder-backend/src/service/router.ts
@@ -85,7 +85,6 @@ import {
 } from '@backstage/plugin-scaffolder-node/alpha';
 import { HumanDuration, JsonObject, JsonValue } from '@backstage/types';
 import express from 'express';
-import Router from 'express-promise-router';
 import { validate } from 'jsonschema';
 import { Duration } from 'luxon';
 import { pathToFileURL } from 'url';
@@ -101,6 +100,7 @@ import {
 import { createDryRunner } from '../scaffolder/dryrun';
 import { StorageTaskBroker } from '../scaffolder/tasks/StorageTaskBroker';
 import { InternalTaskSecrets } from '../scaffolder/tasks/types';
+import { createOpenApiRouter } from '../schema/openapi';
 import { checkPermission } from '../util/checkPermissions';
 import {
   findTemplate,
@@ -276,7 +276,7 @@ const readDuration = (
 export async function createRouter(
   options: RouterOptions,
 ): Promise<express.Router> {
-  const router = Router();
+  const router = await createOpenApiRouter();
   // Be generous in upload size to support a wide range of templates in dry-run mode.
   router.use(express.json({ limit: '10MB' }));
 
