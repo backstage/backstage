7fc3cc68da92fb3e86ca8f09ad19d06d65589559
John Philip <johnphilip283@gmail.com>
2024-12-10 15:14:24 -0500

    Revert "[Feature] Add Pagination to Tech Docs Table"
    Signed-off-by: John Philip <jphilip@spotify.com>

--- plugins/techdocs/src/home/components/DefaultTechDocsHome.tsx
+++ plugins/techdocs/src/home/components/DefaultTechDocsHome.tsx
@@ -46,13 +46,7 @@ export type DefaultTechDocsHomeProps = TechDocsIndexPageProps;
  * @public
  */
 export const DefaultTechDocsHome = (props: TechDocsIndexPageProps) => {
-  const {
-    initialFilter = 'owned',
-    columns,
-    actions,
-    ownerPickerMode,
-    pagination,
-  } = props;
+  const { initialFilter = 'owned', columns, actions, ownerPickerMode } = props;
   return (
     <TechDocsPageWrapper>
       <Content>
@@ -61,7 +55,7 @@ export const DefaultTechDocsHome = (props: TechDocsIndexPageProps) => {
             Discover documentation in your ecosystem.
           </SupportButton>
         </ContentHeader>
-        <EntityListProvider pagination={pagination}>
+        <EntityListProvider>
           <CatalogFilterLayout>
             <CatalogFilterLayout.Filters>
               <TechDocsPicker />
