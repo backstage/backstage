ee4b3f342a40520cb8e4f8411642df765170bbf4
JounQin <admin@1stg.me>
2024-12-11 18:07:38 +0800

    feat: add i18n support for catalog-import plugin
    Signed-off-by: JounQin <admin@1stg.me>

--- plugins/catalog-import/src/components/StepPrepareCreatePullRequest/StepPrepareCreatePullRequest.tsx
+++ plugins/catalog-import/src/components/StepPrepareCreatePullRequest/StepPrepareCreatePullRequest.tsx
@@ -17,6 +17,7 @@
 import { Entity } from '@backstage/catalog-model';
 import { errorApiRef, useApi } from '@backstage/core-plugin-api';
 import { assertError } from '@backstage/errors';
+import { useTranslationRef } from '@backstage/frontend-plugin-api';
 import {
   catalogApiRef,
   humanizeEntityRef,
@@ -30,8 +31,10 @@ import React, { useCallback, useEffect, useState } from 'react';
 import { UnpackNestedValue, UseFormReturn } from 'react-hook-form';
 import useAsync from 'react-use/esm/useAsync';
 import YAML from 'yaml';
+
 import { AnalyzeResult, catalogImportApiRef } from '../../api';
 import { useCatalogFilename } from '../../hooks';
+import { catalogImportTranslationRef } from '../../translation';
 import { PartialEntity } from '../../types';
 import { BackButton, NextButton } from '../Buttons';
 import { PrepareResult } from '../useImportState';
@@ -111,6 +114,7 @@ export const StepPrepareCreatePullRequest = (
 ) => {
   const { analyzeResult, onPrepare, onGoBack, renderFormFields } = props;
 
+  const { t } = useTranslationRef(catalogImportTranslationRef);
   const classes = useStyles();
   const catalogApi = useApi(catalogApiRef);
   const catalogImportApi = useApi(catalogImportApiRef);
@@ -236,7 +240,9 @@ export const StepPrepareCreatePullRequest = (
               })}
 
               <Box marginTop={2}>
-                <Typography variant="h6">Preview Pull Request</Typography>
+                <Typography variant="h6">
+                  {t('stepPrepareCreatePullRequest.previewPr.title')}
+                </Typography>
               </Box>
 
               <PreviewPullRequestComponent
@@ -249,7 +255,9 @@ export const StepPrepareCreatePullRequest = (
               />
 
               <Box marginTop={2} marginBottom={1}>
-                <Typography variant="h6">Preview Entities</Typography>
+                <Typography variant="h6">
+                  {t('stepPrepareCreatePullRequest.previewCatalogInfo.title')}
+                </Typography>
               </Box>
 
               <PreviewCatalogInfoComponent
@@ -280,7 +288,7 @@ export const StepPrepareCreatePullRequest = (
                   )}
                   loading={submitted}
                 >
-                  Create PR
+                  {t('stepPrepareCreatePullRequest.nextButtonText')}
                 </NextButton>
               </Grid>
             </>
