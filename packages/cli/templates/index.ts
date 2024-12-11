1932e1ee17ff9fb9ce9f36a3f37cdd6d209abcb8
Min Kim <minkimcello@gmail.com>
2024-12-10 21:07:30 -0500

    Have default templates read from root node_modules
    Signed-off-by: Min Kim <minkimcello@gmail.com>
    Signed-off-by: Sayyid Yofa <sayyidyofa@gmail.com>

--- packages/cli/templates/index.ts
+++ packages/cli/templates/index.ts
@@ -1,38 +1,40 @@
+import { paths } from '../src/lib/paths';
+
 export default [
   {
     id: 'backend-module',
-    target: '@backstage/cli/templates/alpha/default-backend-module.yaml',
+    target: paths.resolveTargetRoot('node_modules/@backstage/cli/templates/alpha/default-backend-module.yaml'),
   },
   {
     id: 'backend-plugin',
-    target: '@backstage/cli/templates/alpha/default-backend-plugin.yaml',
+    target: paths.resolveTargetRoot('node_modules/@backstage/cli/templates/alpha/default-backend-plugin.yaml'),
   },
   {
     id: 'plugin-common',
-    target: '@backstage/cli/templates/alpha/default-common-plugin-package.yaml',
+    target: paths.resolveTargetRoot('node_modules/@backstage/cli/templates/alpha/default-common-plugin-package.yaml'),
   },
   {
     id: 'plugin-node',
-    target: '@backstage/cli/templates/alpha/default-node-plugin-package.yaml',
+    target: paths.resolveTargetRoot('node_modules/@backstage/cli/templates/alpha/default-node-plugin-package.yaml'),
   },
   {
     id: 'frontend-plugin', // changed from 'plugin'
-    target: '@backstage/cli/templates/alpha/default-plugin.yaml',
+    target: paths.resolveTargetRoot('node_modules/@backstage/cli/templates/alpha/default-plugin.yaml'),
   },
   {
     id: 'plugin-react',
-    target: '@backstage/cli/templates/alpha/default-react-plugin-package.yaml',
+    target: paths.resolveTargetRoot('node_modules/@backstage/cli/templates/alpha/default-react-plugin-package.yaml'),
   },
   {
     id: 'node-library',
-    target: '@backstage/cli/templates/alpha/node-library-package.yaml',
+    target: paths.resolveTargetRoot('node_modules/@backstage/cli/templates/alpha/node-library-package.yaml'),
   },
   {
     id: 'scaffolder-module',
-    target: '@backstage/cli/templates/alpha/scaffolder-module.yaml',
+    target: paths.resolveTargetRoot('node_modules/@backstage/cli/templates/alpha/scaffolder-module.yaml'),
   },
   {
     id: 'web-library',
-    target: '@backstage/cli/templates/alpha/web-library-package.yaml',
+    target: paths.resolveTargetRoot('node_modules/@backstage/cli/templates/alpha/web-library-package.yaml'),
   },
 ];
