a539b127987e61c99cd0e7f0164ed1f330ec1184
Charles de Dreuille <charles.dedreuille@gmail.com>
2024-12-17 11:51:50 +0000

    Fix button component
    Signed-off-by: Charles de Dreuille <charles.dedreuille@gmail.com>

--- packages/canon/src/components/Button/Button.stories.tsx
+++ packages/canon/src/components/Button/Button.stories.tsx
@@ -46,28 +46,34 @@ export const Primary: Story = {
   },
 };
 
-export const Secondary: Story = {
+export const Variants: Story = {
   args: {
-    children: 'Secondary button',
-    variant: 'secondary',
+    children: 'Button',
   },
-};
-
-export const Tertiary: Story = {
-  args: {
-    children: 'Tertiary button',
-    variant: 'tertiary',
+  parameters: {
+    argTypes: {
+      variant: {
+        control: false,
+      },
+    },
   },
+  render: () => (
+    <Inline alignY="center">
+      <Button variant="primary">Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="tertiary">Tertiary</Button>
+    </Inline>
+  ),
 };
 
 export const Sizes: Story = {
   args: {
     children: 'Button',
   },
-  render: args => (
+  render: () => (
     <Inline alignY="center">
-      <Button {...args} size="medium" />
-      <Button {...args} size="small" />
+      <Button size="medium">Medium</Button>
+      <Button size="small">Small</Button>
     </Inline>
   ),
 };
