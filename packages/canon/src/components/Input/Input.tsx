e1561c2ade32994baf2f9198f15b4756513c28fc
Charles de Dreuille <charles.dedreuille@gmail.com>
2024-12-17 18:22:23 +0000

    Add Field + Input components
    Signed-off-by: Charles de Dreuille <charles.dedreuille@gmail.com>

--- packages/canon/src/components/Input/Input.tsx
+++ packages/canon/src/components/Input/Input.tsx
@@ -13,16 +13,21 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import React from 'react';
 
-/* Components */
-@import '../components/Button/styles.css';
-@import '../components/Stack/styles.css';
-@import '../components/Inline/styles.css';
-@import '../components/Grid/styles.css';
-@import '../components/Container/styles.css';
-@import '../components/Icon/styles.css';
-@import '../components/Checkbox/styles.css';
-@import '../components/Table/styles.css';
-@import '../components/Text/styles.css';
-@import '../components/Heading/styles.css';
-@import '../components/Field/styles.css';
+import { Input as InputPrimitive } from '@base-ui-components/react/input';
+import clsx from 'clsx';
+
+const Input = React.forwardRef<
+  React.ElementRef<typeof InputPrimitive>,
+  React.ComponentPropsWithoutRef<typeof InputPrimitive>
+>(({ className, ...props }, ref) => (
+  <InputPrimitive
+    ref={ref}
+    className={clsx('canon-input', className)}
+    {...props}
+  />
+));
+Input.displayName = InputPrimitive.displayName;
+
+export { Input };
