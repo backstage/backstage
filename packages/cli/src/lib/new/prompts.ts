3ba6a9667a12a8d842ef9dc4cdf4c5d947b98343
Min Kim <minkimcello@gmail.com>
2024-12-19 16:45:43 -0500

    Move all prompt-related helpers into one file
    Signed-off-by: Min Kim <minkimcello@gmail.com>

--- packages/cli/src/lib/new/prompts.ts
+++ packages/cli/src/lib/new/prompts.ts
@@ -14,7 +14,8 @@
  * limitations under the License.
  */
 
-import { Prompt } from './types';
+import inquirer from 'inquirer';
+import { Prompt, ConfigurablePrompt } from './types';
 import { parseOwnerIds } from '../codeowners';
 
 export function pluginIdPrompt(): Prompt<{ id: string }> {
@@ -86,4 +87,66 @@ export function ownerPrompt(
       return true;
     },
   };
+}
+
+export async function promptOptions({
+  prompts,
+  globals,
+  codeOwnersFilePath,
+}: {
+  prompts: ConfigurablePrompt[];
+  globals: Record<string, string>;
+  codeOwnersFilePath: string | undefined;
+}): Promise<Record<string, string>> {
+  const answers = await inquirer.prompt(
+    prompts.map((prompt: ConfigurablePrompt) => {
+      if (typeof prompt === 'string') {
+        switch (prompt) {
+          case 'id':
+            return pluginIdPrompt();
+          case 'moduleid':
+            return moduleIdIdPrompt();
+          case 'npmregistry':
+            return npmRegistryPrompt();
+          case 'owner':
+            return ownerPrompt(codeOwnersFilePath);
+          default:
+            throw new Error(
+              `There is no built-in prompt with the following id: ${prompt}`,
+            );
+        }
+      }
+      return {
+        type: 'input',
+        name: prompt.id,
+        message: prompt.prompt,
+        default:
+          globals[prompt.id] !== undefined
+            ? globals[prompt.id]
+            : prompt.default,
+        validate: (value: string) => {
+          if (!value) {
+            return `Please provide a value for ${prompt.id}`;
+          } else if (prompt.validate) {
+            let valid: boolean;
+            let message: string;
+            switch (prompt.validate) {
+              case 'backstage-id':
+                valid = /^[a-z0-9]+(-[a-z0-9]+)*$/.test(value);
+                message =
+                  'Value must be lowercase and contain only letters, digits, and dashes.';
+                break;
+              default:
+                throw new Error(
+                  `There is no built-in validator with the following id: ${prompt.validate}`,
+                );
+            }
+            return valid || message;
+          }
+          return true;
+        },
+      };
+    }),
+  );
+  return { ...globals, ...answers };
 }
--- packages/cli/src/lib/new/utils.ts
+++ packages/cli/src/lib/new/utils.ts
@@ -19,16 +19,10 @@ import { parse } from 'yaml';
 import fs from 'fs-extra';
 
 import { paths } from '../paths';
-import {
-  pluginIdPrompt,
-  moduleIdIdPrompt,
-  npmRegistryPrompt,
-  ownerPrompt,
-} from './prompts';
 
 import defaultTemplates from '../../../templates/all-default-templates';
 
-import { Template, TemplateLocation, ConfigurablePrompt } from './types';
+import { Template, TemplateLocation } from './types';
 
 export async function readCliConfig(
   cliConfig:
@@ -100,68 +94,6 @@ export async function verifyTemplate({
     throw new Error(`Your template, ${id}, is missing a targetPath`);
   }
   return { id, templatePath, ...template };
-}
-
-export async function promptOptions({
-  prompts,
-  globals,
-  codeOwnersFilePath,
-}: {
-  prompts: ConfigurablePrompt[];
-  globals: Record<string, string>;
-  codeOwnersFilePath: string | undefined;
-}): Promise<Record<string, string>> {
-  const answers = await inquirer.prompt(
-    prompts.map((prompt: ConfigurablePrompt) => {
-      if (typeof prompt === 'string') {
-        switch (prompt) {
-          case 'id':
-            return pluginIdPrompt();
-          case 'moduleid':
-            return moduleIdIdPrompt();
-          case 'npmregistry':
-            return npmRegistryPrompt();
-          case 'owner':
-            return ownerPrompt(codeOwnersFilePath);
-          default:
-            throw new Error(
-              `There is no built-in prompt with the following id: ${prompt}`,
-            );
-        }
-      }
-      return {
-        type: 'input',
-        name: prompt.id,
-        message: prompt.prompt,
-        default:
-          globals[prompt.id] !== undefined
-            ? globals[prompt.id]
-            : prompt.default,
-        validate: (value: string) => {
-          if (!value) {
-            return `Please provide a value for ${prompt.id}`;
-          } else if (prompt.validate) {
-            let valid: boolean;
-            let message: string;
-            switch (prompt.validate) {
-              case 'backstage-id':
-                valid = /^[a-z0-9]+(-[a-z0-9]+)*$/.test(value);
-                message =
-                  'Value must be lowercase and contain only letters, digits, and dashes.';
-                break;
-              default:
-                throw new Error(
-                  `There is no built-in validator with the following id: ${prompt.validate}`,
-                );
-            }
-            return valid || message;
-          }
-          return true;
-        },
-      };
-    }),
-  );
-  return { ...globals, ...answers };
 }
 
 interface Options extends Record<string, string | boolean> {
