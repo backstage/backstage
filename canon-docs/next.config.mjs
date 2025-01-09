61a600758818ef279884edde8a3223bfccac2627
Charles de Dreuille <charles.dedreuille@gmail.com>
2025-01-09 16:53:24 +0000

    Improve static export
    Signed-off-by: Charles de Dreuille <charles.dedreuille@gmail.com>

--- canon-docs/next.config.mjs
+++ canon-docs/next.config.mjs
@@ -2,18 +2,8 @@ import createMDX from '@next/mdx';
 
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-  /**
-   * Enable static exports for the App Router.
-   *
-   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-   */
   output: 'export',
-  /**
-   * Disable server-based image optimization. Next.js does not support
-   * dynamic features with static exports.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-   */
+  assetPrefix: '/',
   images: {
     unoptimized: true,
   },
