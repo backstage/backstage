dc8ef86e456bb8e03dc73c5abbd3a3626934a137
Gasan Guseinov <gasan.guseinov@ing.com>
2025-01-23 18:39:27 +0100

    remove unwanted changes
    Signed-off-by: Gasan Guseinov <gasan.guseinov@ing.com>

--- packages/cli-module-test-jest/src/commands/repo/test.ts
+++ packages/cli-module-test-jest/src/commands/repo/test.ts
@@ -437,8 +437,5 @@ export default async ({ args, info }: CliCommandContext) => {
     };
   }
 
-  console.log('retry');
-  console.log(`args: ${JSON.stringify(args)}`);
-
   await runJest(args);
 };
