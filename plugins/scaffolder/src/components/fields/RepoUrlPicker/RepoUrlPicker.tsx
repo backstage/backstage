3959084f87aeb083f8ec6a3ccf75103bf3b29bbe
Severin Wischmann <severinwischmann@nianticlabs.com>
2025-01-29 15:12:10 +0100

    Use Gitlab path rather than project name
    Signed-off-by: Severin Wischmann <severinwischmann@nianticlabs.com>
    Signed-off-by: Sayyid Yofa <sayyidyofa@gmail.com>

--- plugins/scaffolder/src/components/fields/RepoUrlPicker/RepoUrlPicker.tsx
+++ plugins/scaffolder/src/components/fields/RepoUrlPicker/RepoUrlPicker.tsx
@@ -248,7 +248,7 @@ export const RepoUrlPicker = (
         onChange={repo =>
           setState(prevState => ({
             ...prevState,
-            repoName: repo.name,
+            repoName: repo.path || repo.name,
             id: repo.id || '',
           }))
         }
