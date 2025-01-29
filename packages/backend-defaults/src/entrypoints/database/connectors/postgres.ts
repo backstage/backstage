c3f249c28434f3efcd138d5ec6a0df5d827d3d13
Mike Schenk <mike_schenk@yahoo.com>
2025-01-29 09:35:01 -0700

    Defend error message creation against null/undefined
    Co-authored-by: Fredrik Adelöw <freben@gmail.com>
    Signed-off-by: Mike Schenk <mike_schenk@yahoo.com>

--- packages/backend-defaults/src/entrypoints/database/connectors/postgres.ts
+++ packages/backend-defaults/src/entrypoints/database/connectors/postgres.ts
@@ -218,7 +218,7 @@ export async function buildAzurePgConfig(config: any, sanitizedConfig: any) {
       default:
         throw new Error(
           `Unknown token credential type: ${
-            (tokenCredentialConfig as any).type
+            (tokenCredentialConfig as any)?.type
           }`,
         );
     }
