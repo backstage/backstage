be52964d9de2b9afbd9a23a903ba593eb11c85cc
Paul Schultz <pschultz@pobox.com>
2025-01-30 10:09:42 -0600

    docs: add new JSX Transform guide
    Signed-off-by: Paul Schultz <pschultz@pobox.com>

--- microsite/sidebars.js
+++ microsite/sidebars.js
@@ -514,6 +514,7 @@ module.exports = {
           'tutorials/yarn-migration',
           'tutorials/migrate-to-mui5',
           'tutorials/auth-service-migration',
+          'tutorials/jsx-transform-migration'
         ],
       },
     ],
