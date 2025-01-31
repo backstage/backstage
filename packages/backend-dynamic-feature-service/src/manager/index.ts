92a56f6b9c58dad068064a03cec2184d0d11b0af
Patrik Oldsberg <poldsberg@gmail.com>
2025-01-31 10:54:30 +0100

    backend-plugin-api: removed the deprecated featureDiscoveryServiceRef
    Signed-off-by: Patrik Oldsberg <poldsberg@gmail.com>

--- packages/backend-dynamic-feature-service/src/manager/index.ts
+++ packages/backend-dynamic-feature-service/src/manager/index.ts
@@ -32,7 +32,6 @@ export type {
 
 export {
   DynamicPluginManager,
-  dynamicPluginsFeatureDiscoveryServiceFactory,
   dynamicPluginsServiceFactory,
   dynamicPluginsServiceFactoryWithOptions,
   dynamicPluginsServiceRef,
