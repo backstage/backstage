e65d2d9d51ca3c9ddc5b05b39806ce8b375ac2da
Phil Kuang <pkuang@factset.com>
2025-02-05 16:24:00 -0500

    refactor(CatalogTable): use builtin material toolbar for client side paginated impl
    Signed-off-by: Phil Kuang <pkuang@factset.com>
    Signed-off-by: Sayyid Yofa <sayyidyofa@gmail.com>

--- plugins/catalog/src/components/CatalogTable/OffsetPaginatedCatalogTable.tsx
+++ plugins/catalog/src/components/CatalogTable/OffsetPaginatedCatalogTable.tsx
@@ -27,7 +27,7 @@ import { CatalogTableToolbar } from './CatalogTableToolbar';
 export function OffsetPaginatedCatalogTable(
   props: TableProps<CatalogTableRow>,
 ) {
-  const { columns, components, data, options, ...restProps } = props;
+  const { columns, data, options, ...restProps } = props;
   const { setLimit, setOffset, limit, totalItems, offset } = useEntityList();
 
   const [page, setPage] = React.useState(
@@ -55,7 +55,6 @@ export function OffsetPaginatedCatalogTable(
       }}
       components={{
         Toolbar: CatalogTableToolbar,
-        ...(components ?? {}),
       }}
       page={page}
       onPageChange={newPage => {
