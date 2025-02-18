25300cb802ca482790619be5980166bf6c98d2a4
Lex Felix <112618115+lexfelixpost@users.noreply.github.com>
2025-02-18 11:15:44 +0100

    fix(core-components): SimpleStepper back button works with activeStep set higher than 0
    Signed-off-by: Lex Felix <112618115+lexfelixpost@users.noreply.github.com>

--- packages/core-components/src/components/SimpleStepper/SimpleStepper.tsx
+++ packages/core-components/src/components/SimpleStepper/SimpleStepper.tsx
@@ -32,6 +32,7 @@ type InternalState = {
 };
 
 const noop = () => {};
+
 export const VerticalStepperContext = React.createContext<InternalState>({
   stepperLength: 0,
   stepIndex: 0,
@@ -50,7 +51,17 @@ export interface StepperProps {
 export function SimpleStepper(props: PropsWithChildren<StepperProps>) {
   const { children, elevated, onStepChange, activeStep = 0 } = props;
   const [stepIndex, setStepIndex] = useState<number>(activeStep);
-  const [stepHistory, setStepHistory] = useState<number[]>([0]);
+  /*
+  Recreates the stepHistory array based on the activeStep
+  to make sure the handleBack function of the Footer works when activeStep is higher than 0
+  */
+  const inOrderRecreatedStepHistory = Array.from(
+    { length: activeStep + 1 },
+    (_, i) => i,
+  );
+  const [stepHistory, setStepHistory] = useState<number[]>(
+    inOrderRecreatedStepHistory,
+  );
 
   useEffect(() => {
     setStepIndex(activeStep);
