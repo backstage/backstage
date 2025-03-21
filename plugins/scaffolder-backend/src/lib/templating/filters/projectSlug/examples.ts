95b352ebaccf53883f3b4cb6625546787315ef0d
benjdlambert <ben@blam.sh>
2025-03-21 11:08:53 +0100

    chore: update changeset Signed-off-by: benjdlambert <ben@blam.sh>
    Signed-off-by: benjdlambert <ben@blam.sh>

--- plugins/scaffolder-backend/src/lib/templating/filters/projectSlug/examples.ts
+++ plugins/scaffolder-backend/src/lib/templating/filters/projectSlug/examples.ts
@@ -1,5 +1,3 @@
-import { TemplateFilterExample } from '@backstage/plugin-scaffolder-node/alpha';
-
 /*
  * Copyright 2025 The Backstage Authors
  *
@@ -15,6 +13,8 @@ import { TemplateFilterExample } from '@backstage/plugin-scaffolder-node/alpha';
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import { TemplateFilterExample } from '@backstage/plugin-scaffolder-node/alpha';
+
 export const examples: TemplateFilterExample[] = [
   {
     example: `\
