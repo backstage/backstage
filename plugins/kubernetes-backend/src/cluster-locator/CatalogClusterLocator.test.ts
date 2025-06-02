3c5061423b0026b710425f576f31da1bc22e4e91
Florian JUDITH <florian.judith.b@gmail.com>
2025-06-02 15:30:18 -0400

    Fixed tests
    Signed-off-by: Florian JUDITH <florian.judith.b@gmail.com>

--- plugins/kubernetes-backend/src/cluster-locator/CatalogClusterLocator.test.ts
+++ plugins/kubernetes-backend/src/cluster-locator/CatalogClusterLocator.test.ts
@@ -79,30 +79,27 @@ const entities: Entity[] = [
       type: 'kubernetes-cluster',
     },
   },
-];
-
-const catalogLocatorConfig = mockServices
-  .rootConfig({
-    data: {
-      kubernetes: {
-        clusterLocatorMethods: [{ type: 'catalog' }],
+  {
+    apiVersion: 'backstage.io/v1alpha1',
+    kind: 'Resource',
+    metadata: {
+      annotations: {
+        'kubernetes.io/api-server': 'https://apiserver.com',
+        'kubernetes.io/api-server-certificate-authority': 'caData',
+        [ANNOTATION_KUBERNETES_AUTH_PROVIDER]: 'microsoft',
+        [ANNOTATION_KUBERNETES_MICROSOFT_ENTRA_ID_SCOPE]:
+          'microsoft-entra-id/scope.verb',
+        'kubernetes.io/dashboard-url': 'my-url',
+        'kubernetes.io/dashboard-app': 'my-app',
       },
+      name: 'owned',
+      namespace: 'default',
     },
-  })
-  .getConfigArray('kubernetes.clusterLocatorMethods')[0];
-
-function createLocator(entitiesList: Entity[] = entities) {
-  const logger = mockServices.logger.mock();
-  return {
-    logger,
-    clusterSupplier: CatalogClusterLocator.fromConfig(
-      catalogServiceMock({ entities: entitiesList }),
-      mockServices.auth(),
-      catalogLocatorConfig,
-      logger,
-    ),
-  };
-}
+    spec: {
+      type: 'kubernetes-cluster',
+    },
+  },
+];
 
 describe('CatalogClusterLocator', () => {
   beforeEach(() => {
@@ -123,7 +120,7 @@ describe('CatalogClusterLocator', () => {
     const { clusterSupplier } = createLocator();
 
     const result = await clusterSupplier.getClusters({ credentials });
-    expect(result).toHaveLength(2);
+    expect(result).toHaveLength(3);
     expect(result[0]).toMatchSnapshot();
     expect(result[0].authMetadata.serviceAccountToken).toBeUndefined();
   });
@@ -133,123 +130,19 @@ describe('CatalogClusterLocator', () => {
     const { clusterSupplier } = createLocator();
 
     const result = await clusterSupplier.getClusters({ credentials });
-    expect(result).toHaveLength(2);
+    expect(result).toHaveLength(3);
     expect(result[1]).toMatchSnapshot();
   });
 
-  it('ignores clusters with private API server URLs', async () => {
+  it('returns the microsoft authenticated cluster details provided by annotations', async () => {
     const credentials = mockCredentials.user();
-    const { logger, clusterSupplier } = createLocator([
-      {
-        ...entities[0],
-        metadata: {
-          ...entities[0].metadata,
-          name: 'private-cluster',
-          annotations: {
-            ...entities[0].metadata.annotations!,
-            'kubernetes.io/api-server': 'https://127.0.0.1:6443',
-          },
-        },
-      },
-    ]);
-
-    const warn = jest.spyOn(logger, 'warn');
-    const result = await clusterSupplier.getClusters({ credentials });
-    expect(result).toHaveLength(0);
-    expect(warn).toHaveBeenCalledWith(
-      expect.stringContaining('private-cluster'),
-    );
-  });
-
-  it('accepts loopback API server URLs when dangerouslyAllowClusterUrls is configured', async () => {
-    const credentials = mockCredentials.user();
-    const logger = mockServices.logger.mock();
-    const config = mockServices.rootConfig({
-      data: {
-        kubernetes: {
-          clusterLocatorMethods: [
-            {
-              type: 'catalog',
-              dangerouslyAllowClusterUrls: ['127.0.0.1'],
-            },
-          ],
-        },
-      },
-    });
     const clusterSupplier = CatalogClusterLocator.fromConfig(
-      catalogServiceMock({
-        entities: [
-          {
-            ...entities[0],
-            metadata: {
-              ...entities[0].metadata,
-              name: 'local-cluster',
-              annotations: {
-                ...entities[0].metadata.annotations!,
-                'kubernetes.io/api-server': 'http://127.0.0.1:6443',
-              },
-            },
-          },
-        ],
-      }),
+      catalogServiceMock({ entities }),
       mockServices.auth(),
-      config.getConfigArray('kubernetes.clusterLocatorMethods')[0],
-      logger,
     );
 
     const result = await clusterSupplier.getClusters({ credentials });
-    expect(result).toHaveLength(1);
-    expect(result[0].url).toBe('http://127.0.0.1:6443');
-  });
-
-  it('ignores serviceAccount auth provider clusters', async () => {
-    const credentials = mockCredentials.user();
-    const { logger, clusterSupplier } = createLocator([
-      {
-        ...entities[0],
-        metadata: {
-          ...entities[0].metadata,
-          name: 'sa-cluster',
-          annotations: {
-            ...entities[0].metadata.annotations!,
-            [ANNOTATION_KUBERNETES_AUTH_PROVIDER]: 'serviceAccount',
-          },
-        },
-      },
-    ]);
-
-    const warn = jest.spyOn(logger, 'warn');
-    const result = await clusterSupplier.getClusters({ credentials });
-    expect(result).toHaveLength(0);
-    expect(warn).toHaveBeenCalledWith(
-      expect.stringContaining('serviceAccount auth provider'),
-    );
-  });
-
-  it('honors skip TLS verify only when dangerouslyAllowSkipTLSVerify is enabled', async () => {
-    const credentials = mockCredentials.user();
-    const logger = mockServices.logger.mock();
-    const config = mockServices.rootConfig({
-      data: {
-        kubernetes: {
-          clusterLocatorMethods: [
-            {
-              type: 'catalog',
-              dangerouslyAllowSkipTLSVerify: true,
-            },
-          ],
-        },
-      },
-    });
-    const clusterSupplier = CatalogClusterLocator.fromConfig(
-      catalogServiceMock({ entities: [entities[0]] }),
-      mockServices.auth(),
-      config.getConfigArray('kubernetes.clusterLocatorMethods')[0],
-      logger,
-    );
-
-    const result = await clusterSupplier.getClusters({ credentials });
-    expect(result).toHaveLength(1);
-    expect(result[0].skipTLSVerify).toBe(true);
+    expect(result).toHaveLength(3);
+    expect(result[2]).toMatchSnapshot();
   });
 });
