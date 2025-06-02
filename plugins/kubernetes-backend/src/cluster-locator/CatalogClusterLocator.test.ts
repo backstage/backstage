94478178c8c2de4821cf0ca91532ccd570f97fba
Florian JUDITH <florian.judith.b@gmail.com>
2025-06-02 15:30:18 -0400

    Fixed tests
    Signed-off-by: Florian JUDITH <florian.judith.b@gmail.com>

--- plugins/kubernetes-backend/src/cluster-locator/CatalogClusterLocator.test.ts
+++ plugins/kubernetes-backend/src/cluster-locator/CatalogClusterLocator.test.ts
@@ -69,6 +69,26 @@ const entities: Entity[] = [
       type: 'kubernetes-cluster',
     },
   },
+  {
+    apiVersion: 'backstage.io/v1alpha1',
+    kind: 'Resource',
+    metadata: {
+      annotations: {
+        'kubernetes.io/api-server': 'https://apiserver.com',
+        'kubernetes.io/api-server-certificate-authority': 'caData',
+        [ANNOTATION_KUBERNETES_AUTH_PROVIDER]: 'microsoft',
+        [ANNOTATION_KUBERNETES_MICROSOFT_ENTRA_ID_SCOPE]:
+          'microsoft-entra-id/scope.verb',
+        'kubernetes.io/dashboard-url': 'my-url',
+        'kubernetes.io/dashboard-app': 'my-app',
+      },
+      name: 'owned',
+      namespace: 'default',
+    },
+    spec: {
+      type: 'kubernetes-cluster',
+    },
+  },
 ];
 
 describe('CatalogClusterLocator', () => {
@@ -92,7 +112,7 @@ describe('CatalogClusterLocator', () => {
     );
 
     const result = await clusterSupplier.getClusters({ credentials });
-    expect(result).toHaveLength(2);
+    expect(result).toHaveLength(3);
     expect(result[0]).toMatchSnapshot();
   });
 
@@ -104,7 +124,19 @@ describe('CatalogClusterLocator', () => {
     );
 
     const result = await clusterSupplier.getClusters({ credentials });
-    expect(result).toHaveLength(2);
+    expect(result).toHaveLength(3);
     expect(result[1]).toMatchSnapshot();
+  });
+
+  it('returns the microsoft authenticated cluster details provided by annotations', async () => {
+    const credentials = mockCredentials.user();
+    const clusterSupplier = CatalogClusterLocator.fromConfig(
+      catalogServiceMock({ entities }),
+      mockServices.auth(),
+    );
+
+    const result = await clusterSupplier.getClusters({ credentials });
+    expect(result).toHaveLength(3);
+    expect(result[2]).toMatchSnapshot();
   });
 });
