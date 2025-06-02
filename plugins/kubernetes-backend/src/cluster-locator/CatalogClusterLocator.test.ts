aacd03b12a5a14ef8778d951f592abb2e573dae2
Florian JUDITH <florian.judith.b@gmail.com>
2025-06-02 15:30:18 -0400

    Fixed tests
    Signed-off-by: Florian JUDITH <florian.judith.b@gmail.com>

--- plugins/kubernetes-backend/src/cluster-locator/CatalogClusterLocator.test.ts
+++ plugins/kubernetes-backend/src/cluster-locator/CatalogClusterLocator.test.ts
@@ -61,6 +61,24 @@ const entities: Entity[] = [
         [ANNOTATION_KUBERNETES_AWS_ASSUME_ROLE]: 'my-role',
         [ANNOTATION_KUBERNETES_AWS_EXTERNAL_ID]: 'my-id',
         [ANNOTATION_KUBERNETES_OIDC_TOKEN_PROVIDER]: 'google',
+        'kubernetes.io/dashboard-url': 'my-url',
+        'kubernetes.io/dashboard-app': 'my-app',
+      },
+      name: 'owned',
+      namespace: 'default',
+    },
+    spec: {
+      type: 'kubernetes-cluster',
+    },
+  },
+  {
+    apiVersion: 'backstage.io/v1alpha1',
+    kind: 'Resource',
+    metadata: {
+      annotations: {
+        'kubernetes.io/api-server': 'https://apiserver.com',
+        'kubernetes.io/api-server-certificate-authority': 'caData',
+        [ANNOTATION_KUBERNETES_AUTH_PROVIDER]: 'microsoft',
         [ANNOTATION_KUBERNETES_MICROSOFT_ENTRA_ID_SCOPE]:
           'microsoft-entra-id/scope.verb',
         'kubernetes.io/dashboard-url': 'my-url',
@@ -96,7 +114,7 @@ describe('CatalogClusterLocator', () => {
     );
 
     const result = await clusterSupplier.getClusters({ credentials });
-    expect(result).toHaveLength(2);
+    expect(result).toHaveLength(3);
     expect(result[0]).toMatchSnapshot();
   });
 
@@ -108,7 +126,19 @@ describe('CatalogClusterLocator', () => {
     );
 
     const result = await clusterSupplier.getClusters({ credentials });
-    expect(result).toHaveLength(2);
+    expect(result).toHaveLength(3);
     expect(result[1]).toMatchSnapshot();
+  });
+
+  it('returns the microsoft authenticated cluster details provided by annotations', async () => {
+    const credentials = mockCredentials.user();
+    const clusterSupplier = CatalogClusterLocator.fromConfig(
+      catalogServiceMock({ entities }),
+      mockServices.auth(),
+    );
+
+    const result = await clusterSupplier.getClusters({ credentials });
+    expect(result).toHaveLength(3);
+    expect(result[2]).toMatchSnapshot();
   });
 });
