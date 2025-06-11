424610a6ab0273758e8b881e3780dd338c4c2283
Joseph <josephevanspublic@gmail.com>
2025-06-11 19:42:55 +0100

    Scaffolder audit Log now includes taskId and createdBy
    Signed-off-by: Joseph <josephevanspublic@gmail.com>

--- plugins/scaffolder-backend/src/scaffolder/tasks/StorageTaskBroker.ts
+++ plugins/scaffolder-backend/src/scaffolder/tasks/StorageTaskBroker.ts
@@ -105,6 +105,10 @@ export class TaskManager implements TaskContext {
     private readonly auth?: AuthService,
   ) {}
 
+  get taskId() {
+    return this.task.taskId;
+  }
+
   get spec() {
     return this.task.spec;
   }
