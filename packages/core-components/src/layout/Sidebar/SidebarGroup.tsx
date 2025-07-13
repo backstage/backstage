8c4880d04c580f935e9e906eb55281fae48ef4c3
nehaaaa8 <n47845149@gmail.com>
2025-07-13 12:53:12 +0530

    Sidebar/SidebarGroup.tsx

--- packages/core-components/src/layout/Sidebar/SidebarGroup.tsx
+++ packages/core-components/src/layout/Sidebar/SidebarGroup.tsx
@@ -1,30 +1,22 @@
-/* eslint-disable @typescript-eslint/no-shadow */
-/*
- * Copyright 2020 The Backstage Authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 
 import BottomNavigationAction, {
   BottomNavigationActionProps,
 } from '@material-ui/core/BottomNavigationAction';
 import { Theme, makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Collapse from '@material-ui/core/Collapse';
 import { ReactNode, ChangeEvent, useContext } from 'react';
 import { useLocation } from 'react-router-dom';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { Link } from '../../components/Link/Link';
 import { SidebarConfig, SidebarConfigContext } from './config';
 import { MobileSidebarContext } from './MobileSidebarContext';
 import { useSidebarPinState } from './SidebarPinStateContext';
+import { SidebarContext } from './SidebarContext';
 
 /**
  * Props for the `SidebarGroup`
@@ -36,15 +28,12 @@ export interface SidebarGroupProps extends BottomNavigationActionProps {
    * If the `SidebarGroup` should be a `Link`, `to` should be a pathname to that location
    */
   to?: string;
-  /**
-   * If the `SidebarGroup`s should be in a different order than in the normal `Sidebar`, you can provide
-   * each `SidebarGroup` it's own priority to reorder them.
-   */
+
   priority?: number;
-  /**
-   * React children
-   */
+ 
   children?: ReactNode;
+
+  id?: string;
 }
 
 const useStyles = makeStyles<Theme, { sidebarConfig: SidebarConfig }>(
@@ -54,15 +43,47 @@ const useStyles = makeStyles<Theme, { sidebarConfig: SidebarConfig }>(
       margin: theme.spacing(0, 2),
       color: theme.palette.navigation.color,
     },
-
     selected: props => ({
       color: `${theme.palette.navigation.selectedColor}!important`,
       borderTop: `solid ${props.sidebarConfig.selectedIndicatorWidth}px ${theme.palette.navigation.indicator}`,
       marginTop: '-1px',
     }),
-
     label: {
       display: 'none',
+    },
+ 
+    expandableRoot: {
+      width: '100%',
+    },
+    header: {
+      display: 'flex',
+      alignItems: 'center',
+      cursor: 'pointer',
+      padding: theme.spacing(1, 2),
+      '&:hover': {
+        backgroundColor: theme.palette.navigation.navItem?.hoverBackground ?? '#404040',
+      },
+    },
+    headerText: {
+      flex: 1,
+      fontWeight: 'bold',
+      whiteSpace: 'nowrap',
+      overflow: 'hidden',
+      textOverflow: 'ellipsis',
+    },
+    expandIcon: {
+      color: theme.palette.navigation.color,
+      opacity: 0.7,
+      transition: 'transform 200ms',
+    },
+    expandIconExpanded: {
+      transform: 'rotate(180deg)',
+    },
+    nested: {
+      paddingLeft: theme.spacing(6),
+    },
+    nestedList: {
+      padding: 0,
     },
   }),
 );
@@ -113,21 +134,78 @@ const MobileSidebarGroup = (props: SidebarGroupProps) => {
 };
 
 /**
+ * Expandable sidebar group component for desktop view
+ * @internal
+ */
+const DesktopSidebarGroup = (props: SidebarGroupProps) => {
+  const { children, icon, label, id } = props;
+  const { sidebarConfig } = useContext(SidebarConfigContext);
+  const classes = useStyles({ sidebarConfig });
+  const { isCollapsed, expandedGroups, onGroupToggle } = useContext(SidebarContext);
+  
+  const groupId = id || label || 'unknown';
+  const isExpanded = expandedGroups?.[groupId] ?? false;
+
+  const handleToggle = () => {
+    onGroupToggle?.(groupId, !isExpanded);
+  };
+
+  if (isCollapsed) {
+    // When collapsed, just show the icon with a tooltip
+    return (
+      <ListItem button className={classes.header} title={label}>
+        <ListItemIcon>{icon}</ListItemIcon>
+      </ListItem>
+    );
+  }
+
+  return (
+    <div className={classes.expandableRoot}>
+      <ListItem button className={classes.header} onClick={handleToggle}>
+        <ListItemIcon>{icon}</ListItemIcon>
+        <ListItemText primary={label} className={classes.headerText} />
+        <ExpandMoreIcon 
+          className={`${classes.expandIcon} ${isExpanded ? classes.expandIconExpanded : ''}`}
+        />
+      </ListItem>
+      
+      <Collapse in={isExpanded} timeout="auto" unmountOnExit>
+        <List component="div" className={classes.nestedList}>
+          {children}
+        </List>
+      </Collapse>
+    </div>
+  );
+};
+
+/**
  * Groups items of the `Sidebar` together.
  *
  * @remarks
- * On bigger screens, this won't have any effect at the moment.
+ * On bigger screens, this creates an expandable navigation group.
  * On small screens, it will add an action to the bottom navigation - either triggering an overlay menu or acting as a link
  *
  * @public
  */
 export const SidebarGroup = (props: SidebarGroupProps) => {
-  const { children, to, label, icon, value } = props;
+  const { children, to, label, icon, value, id } = props;
   const { isMobile } = useSidebarPinState();
 
+  // If there's a `to` prop, this is a simple link group (not expandable)
+  if (to) {
+    return isMobile ? (
+      <MobileSidebarGroup to={to} label={label} icon={icon} value={value} />
+    ) : (
+      <>{children}</>
+    );
+  }
+
+  // This is an expandable group
   return isMobile ? (
     <MobileSidebarGroup to={to} label={label} icon={icon} value={value} />
   ) : (
-    <>{children}</>
+    <DesktopSidebarGroup icon={icon} label={label} id={id}>
+      {children}
+    </DesktopSidebarGroup>
   );
 };
