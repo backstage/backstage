cb116a2012dfded01b7fc32eff5bd5dc07f8455f
awanlin <67169551+awanlin@users.noreply.github.com>
2025-07-17 19:19:05 +0000

    Deploying to gh-pages from @ backstage/backstage@02e3cd555b451abb19c067d239792d3233e81c30 🚀

---
+++ storybook/assets/Breadcrumbs.stories-BcR6qcz7.js
@@ -0,0 +1,98 @@
+import{j as e}from"./jsx-runtime-Cw0GR0a5.js";import{i as k}from"./interopRequireDefault-Y9pwbXtE.js";import{r as P,i as v}from"./createSvgIcon-rCELOQ8q.js";import{r as s}from"./index-CTjT7uj6.js";import{d as I}from"./ExpandMore-DJEUZydb.js";import{B as o,H as E}from"./Header-C8Z6XcxN.js";import{M as b}from"./index-w6SBqnNd.js";import{T as r}from"./Typography-CUBppVl0.js";import{P as C}from"./Page-TGYZWQPM.js";import{L as t}from"./Link-Bp-Lt7-P.js";import{B}from"./Box-BZcLdGyY.js";import{P as S}from"./Popover-BpMibsVW.js";import{L as _}from"./List-BslH4zsa.js";import{L as i}from"./ListItem-CUB3wWpf.js";import"./capitalize-CjHL08xv.js";import"./defaultTheme-U8IXQtr7.js";import"./withStyles-Dj_puyu8.js";import"./hoist-non-react-statics.cjs-DzIEFHQI.js";import"./createChainedFunction-Da-WpsAN.js";import"./createSvgIcon-CAWH9WqG.js";import"./debounce-DtXjJkxj.js";import"./isMuiElement-B_4ddUuK.js";import"./ownerWindow-C3iVrxHF.js";import"./useIsFocusVisible-BQk2_Vhe.js";import"./index-DwHHXP4W.js";import"./useControlled-B47E2WMp.js";import"./unstable_useId-B3Hiq1YI.js";import"./Helmet-DPVyO7__.js";import"./index-BRV0Se7Z.js";import"./makeStyles-3WuthtJ7.js";import"./Grid-Cd4CaOSn.js";import"./Breadcrumbs-CwAuT6EW.js";import"./react-is.production.min-D0tnNtx9.js";import"./ButtonBase-C1iu_4vV.js";import"./TransitionGroupContext-BtzQ-Cv7.js";import"./Page-MYhfovj0.js";import"./ApiRef-CqkoWjZn.js";import"./ConfigApi-D1qiBdfc.js";import"./useMediaQuery-DtB-IR2v.js";import"./Tooltip-D1xsFmJ6.js";import"./useTheme-hfNS2WFw.js";import"./Popper-CRCF8N5R.js";import"./Portal-BcgI5KAA.js";import"./Grow-Bw-3CPgf.js";import"./utils-ClB-4IsE.js";import"./index-Cqve-NHl.js";import"./ThemeProvider-HGIy1WSf.js";import"./lodash-CoGan1YB.js";import"./useAnalytics-DVyBXs_0.js";import"./typography-hVTC7Hfk.js";import"./Modal-CkYXz1UB.js";import"./classCallCheck-BNzALLS0.js";import"./Paper-BZKq1osr.js";import"./ListContext-DydK1sOh.js";var p={},w=k,H=v;Object.defineProperty(p,"__esModule",{value:!0});var L=p.default=void 0,M=H(s),O=w(P()),R=(0,O.default)(M.createElement("path",{d:"M12 8l-6 6 1.41 1.41L12 10.83l4.59 4.58L18 14z"}),"ExpandLess");L=p.default=R;const Ae={title:"Layout/Breadcrumbs",component:o},a=()=>e.jsxs(b,{children:[e.jsx("h2",{children:"Standard breadcrumbs"}),e.jsx(r,{paragraph:!0,children:"Underlined pages are links. This should show a hierarchical relationship."}),e.jsx(C,{themeId:"other",children:e.jsx(E,{title:"Current Page",type:"General Page",typeLink:"/"})})]}),n=()=>{const[l,h]=s.useState(null),f=T=>{h(T.currentTarget)},j=()=>{h(null)},c=!!l;return e.jsxs(b,{children:[e.jsx(r,{paragraph:!0,children:"It might be the case that you want to keep your breadcrumbs outside of the header. In that case, they should be positioned above the title of the page."}),e.jsx("h2",{children:"Standard breadcrumbs"}),e.jsx(r,{paragraph:!0,children:"Underlined pages are links. This should show a hierarchical relationship."}),e.jsx(o,{color:"primaryText"}),e.jsxs(o,{color:"primaryText",children:[e.jsx(t,{to:"/",children:"General Page"}),e.jsx(t,{to:"/",children:"Second Page"}),e.jsx(r,{children:"Current page"})]}),e.jsx("h2",{children:"Hidden breadcrumbs"}),e.jsx(r,{paragraph:!0,children:"Use this when you have more than three breadcrumbs. When user clicks on ellipses, expand the breadcrumbs out."}),e.jsxs(o,{color:"primaryText",children:[e.jsx(t,{to:"/",children:"General Page"}),e.jsx(t,{to:"/",children:"Second Page"}),e.jsx(t,{to:"/",children:"Third Page"}),e.jsx(t,{to:"/",children:"Fourth Page"}),e.jsx(r,{children:"Current page"})]}),e.jsx("h2",{children:"Layered breadcrumbs"}),e.jsx(r,{paragraph:!0,children:"Use this when you want to show alternative breadcrumbs on the same hierarchical level."}),e.jsxs(s.Fragment,{children:[e.jsxs(o,{color:"primaryText",children:[e.jsx(t,{to:"/",children:"General Page"}),e.jsx(t,{to:"/",onClick:f,children:e.jsxs(B,{display:"flex",alignItems:"center",children:[e.jsx(r,{component:"span",children:"Second Page"}),c?e.jsx(L,{}):e.jsx(I,{})]})}),e.jsx(r,{children:"Current page"})]}),e.jsx(S,{open:c,onClose:j,anchorEl:l,anchorOrigin:{vertical:"bottom",horizontal:"left"},transformOrigin:{vertical:"top",horizontal:"left"},children:e.jsxs(_,{children:[e.jsx(i,{button:!0,style:{textDecoration:"underline"},children:"Parallel second page"}),e.jsx(i,{button:!0,style:{textDecoration:"underline"},children:"Another parallel second page"}),e.jsx(i,{button:!0,style:{textDecoration:"underline"},children:"Yet another, parallel second page"})]})})]})]})};a.__docgenInfo={description:"",methods:[],displayName:"InHeader"};n.__docgenInfo={description:"",methods:[],displayName:"OutsideOfHeader"};var d,m,u;a.parameters={...a.parameters,docs:{...(d=a.parameters)==null?void 0:d.docs,source:{originalSource:`() => <MemoryRouter>
+    <h2>Standard breadcrumbs</h2>
+    <Typography paragraph>
+      Underlined pages are links. This should show a hierarchical relationship.
+    </Typography>
+
+    <Page themeId="other">
+      <Header title="Current Page" type="General Page" typeLink="/" />
+    </Page>
+  </MemoryRouter>`,...(u=(m=a.parameters)==null?void 0:m.docs)==null?void 0:u.source}}};var g,y,x;n.parameters={...n.parameters,docs:{...(g=n.parameters)==null?void 0:g.docs,source:{originalSource:`() => {
+  const [anchorEl, setAnchorEl] = useState<HTMLAnchorElement | null>(null);
+  const handleClick = (event: MouseEvent<HTMLAnchorElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const open = Boolean(anchorEl);
+  return <MemoryRouter>
+      <Typography paragraph>
+        It might be the case that you want to keep your breadcrumbs outside of
+        the header. In that case, they should be positioned above the title of
+        the page.
+      </Typography>
+
+      <h2>Standard breadcrumbs</h2>
+      <Typography paragraph>
+        Underlined pages are links. This should show a hierarchical
+        relationship.
+      </Typography>
+
+      <Breadcrumbs color="primaryText" />
+
+      <Breadcrumbs color="primaryText">
+        <Link to="/">General Page</Link>
+        <Link to="/">Second Page</Link>
+        <Typography>Current page</Typography>
+      </Breadcrumbs>
+
+      <h2>Hidden breadcrumbs</h2>
+      <Typography paragraph>
+        Use this when you have more than three breadcrumbs. When user clicks on
+        ellipses, expand the breadcrumbs out.
+      </Typography>
+
+      <Breadcrumbs color="primaryText">
+        <Link to="/">General Page</Link>
+        <Link to="/">Second Page</Link>
+        <Link to="/">Third Page</Link>
+        <Link to="/">Fourth Page</Link>
+        <Typography>Current page</Typography>
+      </Breadcrumbs>
+
+      <h2>Layered breadcrumbs</h2>
+      <Typography paragraph>
+        Use this when you want to show alternative breadcrumbs on the same
+        hierarchical level.
+      </Typography>
+
+      <Fragment>
+        <Breadcrumbs color="primaryText">
+          <Link to="/">General Page</Link>
+          <Link to="/" onClick={handleClick}>
+            <Box display="flex" alignItems="center">
+              <Typography component="span">Second Page</Typography>
+              {open ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+            </Box>
+          </Link>
+          <Typography>Current page</Typography>
+        </Breadcrumbs>
+        <Popover open={open} onClose={handleClose} anchorEl={anchorEl} anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'left'
+      }} transformOrigin={{
+        vertical: 'top',
+        horizontal: 'left'
+      }}>
+          <List>
+            <ListItem button style={{
+            textDecoration: 'underline'
+          }}>
+              Parallel second page
+            </ListItem>
+            <ListItem button style={{
+            textDecoration: 'underline'
+          }}>
+              Another parallel second page
+            </ListItem>
+            <ListItem button style={{
+            textDecoration: 'underline'
+          }}>
+              Yet another, parallel second page
+            </ListItem>
+          </List>
+        </Popover>
+      </Fragment>
+    </MemoryRouter>;
+}`,...(x=(y=n.parameters)==null?void 0:y.docs)==null?void 0:x.source}}};const De=["InHeader","OutsideOfHeader"];export{a as InHeader,n as OutsideOfHeader,De as __namedExportsOrder,Ae as default};
--- storybook/assets/LinkButton-DK-fVnZm.js
+++
@@ -1,4 +0,0 @@
-import{j as n}from"./jsx-runtime-CvpxdxdE.js";import{r}from"./index-DSHF18-l.js";import{L as i}from"./Link-DmlXjQke.js";import{B as s}from"./Button-CCF23O8k.js";const p=r.forwardRef((o,t)=>n.jsx(i,{ref:t,...o,color:"initial"})),m=r.forwardRef((o,t)=>n.jsx(s,{ref:t,component:p,...o}));m.__docgenInfo={description:`Thin wrapper on top of material-ui's {@link https://v4.mui.com/components/buttons/ | Button} component
-
-@public
-@remarks`,methods:[],displayName:"LinkButton"};export{m as L};
