f927cf0a19d5d013f51b9cfc60a86cd583d76f12
Rugvip <poldsberg@gmail.com>
2025-08-07 13:54:55 +0000

    Deploying to gh-pages from @ backstage/backstage@abe4d879f8e538546e8340e8e20194d58dc2b7dd 🚀

--- storybook/assets/Close-9ox07WbL.js
+++
@@ -1,1 +0,0 @@
-import{r as o}from"./index-CTjT7uj6.js";import{c as r}from"./createSvgIcon-CL6P1I3F.js";const a=r(o.createElement("path",{d:"M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"}));export{a as C};
---
+++ storybook/assets/GaugeCard.stories-ULlGch9x.js
@@ -0,0 +1,123 @@
+import{j as e}from"./jsx-runtime-Cw0GR0a5.js";import{G as r}from"./GaugeCard-Cx1Qy8xD.js";import{d as m}from"./Info-yr8fJ-BC.js";import{S as s}from"./Grid-Cd4CaOSn.js";import{T as u}from"./Tooltip-D1xsFmJ6.js";import{M as A}from"./index-w6SBqnNd.js";import"./index-CTjT7uj6.js";import"./Gauge-CNw3jOs_.js";import"./index-Cqve-NHl.js";import"./defaultTheme-U8IXQtr7.js";import"./objectSpread2-SEv4dr8R.js";import"./makeStyles-3WuthtJ7.js";import"./useTheme-hfNS2WFw.js";import"./Box-BZcLdGyY.js";import"./typography-hVTC7Hfk.js";import"./hoist-non-react-statics.cjs-DzIEFHQI.js";import"./InfoCard-D6r2XHWf.js";import"./capitalize-CjHL08xv.js";import"./withStyles-Dj_puyu8.js";import"./CardActions-B1gbxntd.js";import"./CardContent-7S1EI-5r.js";import"./Paper-BZKq1osr.js";import"./ErrorBoundary-DzlmMc_B.js";import"./translation-f08mCW_U.js";import"./TranslationApi-DhmNHZQM.js";import"./ApiRef-CqkoWjZn.js";import"./ErrorPanel-Bc_i8B-c.js";import"./WarningPanel-D1axctXz.js";import"./interopRequireDefault-Y9pwbXtE.js";import"./createSvgIcon-rCELOQ8q.js";import"./createChainedFunction-Da-WpsAN.js";import"./createSvgIcon-CAWH9WqG.js";import"./debounce-DtXjJkxj.js";import"./isMuiElement-B_4ddUuK.js";import"./ownerWindow-C3iVrxHF.js";import"./useIsFocusVisible-BQk2_Vhe.js";import"./index-DwHHXP4W.js";import"./useControlled-B47E2WMp.js";import"./unstable_useId-B3Hiq1YI.js";import"./ExpandMore-DJEUZydb.js";import"./AccordionDetails-dncjf4Z1.js";import"./toArray-QeYAVC82.js";import"./react-is.production.min-D0tnNtx9.js";import"./Collapse-DeBi3gal.js";import"./utils-ClB-4IsE.js";import"./TransitionGroupContext-BtzQ-Cv7.js";import"./ButtonBase-C1iu_4vV.js";import"./IconButton-BxJ-nFiT.js";import"./Typography-CUBppVl0.js";import"./MarkdownContent-B5sMJgSx.js";import"./index-BRV0Se7Z.js";import"./CodeSnippet-Pc1I8vNw.js";import"./iframe-CDQt0QCi.js";import"./objectWithoutProperties-Et-85jJS.js";import"./classCallCheck-BNzALLS0.js";import"./inherits-Cm41Z5uw.js";import"./toConsumableArray-BEwiObev.js";import"./CopyTextButton-Mm8GD1wS.js";import"./useCopyToClipboard-BkLYi_DT.js";import"./useMountedState-DkESzBh4.js";import"./useTranslationRef-Bfx90Ud1.js";import"./List-BslH4zsa.js";import"./ListContext-DydK1sOh.js";import"./ListItem-CUB3wWpf.js";import"./ListItemText-CjIJj2RO.js";import"./LinkButton-DhIs5zOZ.js";import"./Link-Bp-Lt7-P.js";import"./lodash-CoGan1YB.js";import"./useAnalytics-DVyBXs_0.js";import"./ConfigApi-D1qiBdfc.js";import"./Button-Cwg5hjTf.js";import"./CardHeader-BfI3cg9q.js";import"./Divider-BRx2RnpI.js";import"./BottomLink-B58khoVX.js";import"./ArrowForward-KwKdGpo_.js";import"./Popper-CRCF8N5R.js";import"./Portal-BcgI5KAA.js";import"./Grow-Bw-3CPgf.js";const h={title:"Go to XYZ Location",link:"#"},t=({children:c})=>e.jsx(A,{children:e.jsx(s,{container:!0,spacing:2,children:c})}),pr={title:"Data Display/Progress Card",component:r},i=()=>e.jsxs(t,{children:[e.jsx(s,{item:!0,children:e.jsx(r,{title:"Progress",progress:.3})}),e.jsx(s,{item:!0,children:e.jsx(r,{title:"Progress",progress:.57})}),e.jsx(s,{item:!0,children:e.jsx(r,{title:"Progress",progress:.89})}),e.jsx(s,{item:!0,children:e.jsx(r,{title:"Progress",inverse:!0,progress:.2})})]}),o=()=>e.jsxs(t,{children:[e.jsx(s,{item:!0,children:e.jsx(r,{title:"Progress",subheader:"With a subheader",progress:.3})}),e.jsx(s,{item:!0,children:e.jsx(r,{title:"Progress",subheader:"With a subheader",progress:.57})}),e.jsx(s,{item:!0,children:e.jsx(r,{title:"Progress",subheader:"With a subheader",progress:.89})}),e.jsx(s,{item:!0,children:e.jsx(r,{title:"Progress",subheader:"With a subheader",inverse:!0,progress:.2})})]}),a=()=>e.jsxs(t,{children:[e.jsx(s,{item:!0,children:e.jsx(r,{title:"Progress",deepLink:h,progress:.3})}),e.jsx(s,{item:!0,children:e.jsx(r,{title:"Progress",deepLink:h,progress:.57})}),e.jsx(s,{item:!0,children:e.jsx(r,{title:"Progress",deepLink:h,progress:.89})}),e.jsx(s,{item:!0,children:e.jsx(r,{title:"Progress",deepLink:h,inverse:!0,progress:.2})})]}),n=()=>e.jsxs(t,{children:[e.jsx(s,{item:!0,children:e.jsx(r,{getColor:()=>"#f00",title:"Red",progress:.5})}),e.jsx(s,{item:!0,children:e.jsx(r,{getColor:()=>"#0f0",title:"Green",progress:.5})}),e.jsx(s,{item:!0,children:e.jsx(r,{getColor:()=>"#00f",title:"Blue",progress:.5})}),e.jsx(s,{item:!0,children:e.jsx(r,{getColor:({palette:c})=>c.status.error,title:"palette.status.error",progress:.5})})]}),d=()=>e.jsxs(t,{children:[e.jsx(s,{item:!0,children:e.jsx(r,{title:"Progress",subheader:"With a subheader",progress:.3,icon:e.jsx(u,{title:"Info Message",arrow:!0,children:e.jsx(m,{style:{float:"right"}})})})}),e.jsx(s,{item:!0,children:e.jsx(r,{title:"Progress",subheader:"With a subheader",progress:.57,icon:e.jsx(u,{title:"Info Message",arrow:!0,children:e.jsx(m,{style:{float:"right"}})})})}),e.jsx(s,{item:!0,children:e.jsx(r,{title:"Progress",subheader:"With a subheader",progress:.89,icon:e.jsx(u,{title:"Info Message",arrow:!0,children:e.jsx(m,{style:{float:"right"}})})})}),e.jsx(s,{item:!0,children:e.jsx(r,{title:"Progress",subheader:"With a subheader",inverse:!0,progress:.2,icon:e.jsx(u,{title:"Info Message",arrow:!0,children:e.jsx(m,{style:{float:"right"}})})})})]}),l=()=>e.jsxs(t,{children:[e.jsx(s,{item:!0,children:e.jsx(r,{variant:"fullHeight",alignGauge:"bottom",title:"Progress",subheader:"With a subheader",progress:.3})}),e.jsx(s,{item:!0,children:e.jsx(r,{variant:"fullHeight",alignGauge:"bottom",title:"Progress",subheader:"With a subheader",progress:.57})}),e.jsx(s,{item:!0,children:e.jsx(r,{variant:"fullHeight",alignGauge:"bottom",title:"Progress with longer title",subheader:"With a subheader",progress:.89})}),e.jsx(s,{item:!0,children:e.jsx(r,{variant:"fullHeight",alignGauge:"bottom",title:"Progress",subheader:"With a subheader",inverse:!0,progress:.2})})]}),g=()=>e.jsxs(t,{children:[e.jsx(s,{item:!0,children:e.jsx(r,{variant:"fullHeight",alignGauge:"bottom",size:"small",title:"Progress",progress:.3})}),e.jsx(s,{item:!0,children:e.jsx(r,{variant:"fullHeight",alignGauge:"bottom",size:"small",title:"Progress",subheader:"With a subheader",progress:.57})}),e.jsx(s,{item:!0,children:e.jsx(r,{variant:"fullHeight",alignGauge:"bottom",size:"small",title:"Progress, longer title",progress:.89})}),e.jsx(s,{item:!0,children:e.jsx(r,{variant:"fullHeight",alignGauge:"bottom",size:"small",title:"Progress",inverse:!0,progress:.2})})]}),p=()=>e.jsxs(t,{children:[e.jsx(s,{item:!0,children:e.jsx(r,{title:"Progress",progress:.3,description:"Hover Message"})}),e.jsx(s,{item:!0,children:e.jsx(r,{title:"Progress",progress:.57,description:"Hover Message"})}),e.jsx(s,{item:!0,children:e.jsx(r,{title:"Progress",progress:.89,description:"Hover Message"})}),e.jsx(s,{item:!0,children:e.jsx(r,{title:"Progress",inverse:!0,progress:.2,description:"Hover Message"})})]});i.__docgenInfo={description:"",methods:[],displayName:"Default"};o.__docgenInfo={description:"",methods:[],displayName:"Subhead"};a.__docgenInfo={description:"",methods:[],displayName:"LinkInFooter"};n.__docgenInfo={description:"",methods:[],displayName:"StaticColor"};d.__docgenInfo={description:"",methods:[],displayName:"InfoMessage"};l.__docgenInfo={description:"",methods:[],displayName:"AlignedBottom"};g.__docgenInfo={description:"",methods:[],displayName:"Small"};p.__docgenInfo={description:"",methods:[],displayName:"HoverMessage"};var G,x,j;i.parameters={...i.parameters,docs:{...(G=i.parameters)==null?void 0:G.docs,source:{originalSource:`() => <Wrapper>
+    <Grid item>
+      <GaugeCard title="Progress" progress={0.3} />
+    </Grid>
+    <Grid item>
+      <GaugeCard title="Progress" progress={0.57} />
+    </Grid>
+    <Grid item>
+      <GaugeCard title="Progress" progress={0.89} />
+    </Grid>
+    <Grid item>
+      <GaugeCard title="Progress" inverse progress={0.2} />
+    </Grid>
+  </Wrapper>`,...(j=(x=i.parameters)==null?void 0:x.docs)==null?void 0:j.source}}};var b,f,P;o.parameters={...o.parameters,docs:{...(b=o.parameters)==null?void 0:b.docs,source:{originalSource:`() => <Wrapper>
+    <Grid item>
+      <GaugeCard title="Progress" subheader="With a subheader" progress={0.3} />
+    </Grid>
+    <Grid item>
+      <GaugeCard title="Progress" subheader="With a subheader" progress={0.57} />
+    </Grid>
+    <Grid item>
+      <GaugeCard title="Progress" subheader="With a subheader" progress={0.89} />
+    </Grid>
+    <Grid item>
+      <GaugeCard title="Progress" subheader="With a subheader" inverse progress={0.2} />
+    </Grid>
+  </Wrapper>`,...(P=(f=o.parameters)==null?void 0:f.docs)==null?void 0:P.source}}};var C,W,v;a.parameters={...a.parameters,docs:{...(C=a.parameters)==null?void 0:C.docs,source:{originalSource:`() => <Wrapper>
+    <Grid item>
+      <GaugeCard title="Progress" deepLink={linkInfo} progress={0.3} />
+    </Grid>
+    <Grid item>
+      <GaugeCard title="Progress" deepLink={linkInfo} progress={0.57} />
+    </Grid>
+    <Grid item>
+      <GaugeCard title="Progress" deepLink={linkInfo} progress={0.89} />
+    </Grid>
+    <Grid item>
+      <GaugeCard title="Progress" deepLink={linkInfo} inverse progress={0.2} />
+    </Grid>
+  </Wrapper>`,...(v=(W=a.parameters)==null?void 0:W.docs)==null?void 0:v.source}}};var I,H,M;n.parameters={...n.parameters,docs:{...(I=n.parameters)==null?void 0:I.docs,source:{originalSource:`() => <Wrapper>
+    <Grid item>
+      <GaugeCard getColor={() => '#f00'} title="Red" progress={0.5} />
+    </Grid>
+    <Grid item>
+      <GaugeCard getColor={() => '#0f0'} title="Green" progress={0.5} />
+    </Grid>
+    <Grid item>
+      <GaugeCard getColor={() => '#00f'} title="Blue" progress={0.5} />
+    </Grid>
+    <Grid item>
+      <GaugeCard getColor={({
+      palette
+    }) => palette.status.error} title="palette.status.error" progress={0.5} />
+    </Grid>
+  </Wrapper>`,...(M=(H=n.parameters)==null?void 0:H.docs)==null?void 0:M.source}}};var _,y,S;d.parameters={...d.parameters,docs:{...(_=d.parameters)==null?void 0:_.docs,source:{originalSource:`() => <Wrapper>
+    <Grid item>
+      <GaugeCard title="Progress" subheader="With a subheader" progress={0.3} icon={<Tooltip title="Info Message" arrow>
+            <Info style={{
+        float: 'right'
+      }} />
+          </Tooltip>} />
+    </Grid>
+    <Grid item>
+      <GaugeCard title="Progress" subheader="With a subheader" progress={0.57} icon={<Tooltip title="Info Message" arrow>
+            <Info style={{
+        float: 'right'
+      }} />
+          </Tooltip>} />
+    </Grid>
+    <Grid item>
+      <GaugeCard title="Progress" subheader="With a subheader" progress={0.89} icon={<Tooltip title="Info Message" arrow>
+            <Info style={{
+        float: 'right'
+      }} />
+          </Tooltip>} />
+    </Grid>
+    <Grid item>
+      <GaugeCard title="Progress" subheader="With a subheader" inverse progress={0.2} icon={<Tooltip title="Info Message" arrow>
+            <Info style={{
+        float: 'right'
+      }} />
+          </Tooltip>} />
+    </Grid>
+  </Wrapper>`,...(S=(y=d.parameters)==null?void 0:y.docs)==null?void 0:S.source}}};var k,L,w;l.parameters={...l.parameters,docs:{...(k=l.parameters)==null?void 0:k.docs,source:{originalSource:`() => <Wrapper>
+    <Grid item>
+      <GaugeCard variant="fullHeight" alignGauge="bottom" title="Progress" subheader="With a subheader" progress={0.3} />
+    </Grid>
+    <Grid item>
+      <GaugeCard variant="fullHeight" alignGauge="bottom" title="Progress" subheader="With a subheader" progress={0.57} />
+    </Grid>
+    <Grid item>
+      <GaugeCard variant="fullHeight" alignGauge="bottom" title="Progress with longer title" subheader="With a subheader" progress={0.89} />
+    </Grid>
+    <Grid item>
+      <GaugeCard variant="fullHeight" alignGauge="bottom" title="Progress" subheader="With a subheader" inverse progress={0.2} />
+    </Grid>
+  </Wrapper>`,...(w=(L=l.parameters)==null?void 0:L.docs)==null?void 0:w.source}}};var T,z,N;g.parameters={...g.parameters,docs:{...(T=g.parameters)==null?void 0:T.docs,source:{originalSource:`() => <Wrapper>
+    <Grid item>
+      <GaugeCard variant="fullHeight" alignGauge="bottom" size="small" title="Progress" progress={0.3} />
+    </Grid>
+    <Grid item>
+      <GaugeCard variant="fullHeight" alignGauge="bottom" size="small" title="Progress" subheader="With a subheader" progress={0.57} />
+    </Grid>
+    <Grid item>
+      <GaugeCard variant="fullHeight" alignGauge="bottom" size="small" title="Progress, longer title" progress={0.89} />
+    </Grid>
+    <Grid item>
+      <GaugeCard variant="fullHeight" alignGauge="bottom" size="small" title="Progress" inverse progress={0.2} />
+    </Grid>
+  </Wrapper>`,...(N=(z=g.parameters)==null?void 0:z.docs)==null?void 0:N.source}}};var B,D,R;p.parameters={...p.parameters,docs:{...(B=p.parameters)==null?void 0:B.docs,source:{originalSource:`() => <Wrapper>
+    <Grid item>
+      <GaugeCard title="Progress" progress={0.3} description="Hover Message" />
+    </Grid>
+    <Grid item>
+      <GaugeCard title="Progress" progress={0.57} description="Hover Message" />
+    </Grid>
+    <Grid item>
+      <GaugeCard title="Progress" progress={0.89} description="Hover Message" />
+    </Grid>
+    <Grid item>
+      <GaugeCard title="Progress" inverse progress={0.2} description="Hover Message" />
+    </Grid>
+  </Wrapper>`,...(R=(D=p.parameters)==null?void 0:D.docs)==null?void 0:R.source}}};const mr=["Default","Subhead","LinkInFooter","StaticColor","InfoMessage","AlignedBottom","Small","HoverMessage"];export{l as AlignedBottom,i as Default,p as HoverMessage,d as InfoMessage,a as LinkInFooter,g as Small,n as StaticColor,o as Subhead,mr as __namedExportsOrder,pr as default};
