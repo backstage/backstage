c19b2e7cee958c8f79ac1ce85f36343bd0a6bf7c
cdedreuille <charles.dedreuille@gmail.com>
2025-08-17 10:11:18 +0000

    Deploying to gh-pages from @ backstage/backstage@f9cd11f62228830f1b65664c1e73474c2657c7f0 🚀

---
+++ storybook/assets/Card.stories-h6-oITDk.js
@@ -0,0 +1,76 @@
+import{j as r}from"./jsx-runtime-hv06LKfz.js";import{C as a,a as d,b as l,c as p}from"./Menu-AHtoomqu.js";import"./provider-DYpvHhpv.js";import"./Box-B2EcAVRr.js";import"./Grid-DRAgwYOA.js";import"./Flex-CoduicHc.js";import"./Container-DJRXq03e.js";import"./Button-DujfZ8CP.js";import"./Collapsible-ClUSU77R.js";import"./FieldLabel-5PdQNUsa.js";import"./ButtonIcon-BsVswHzm.js";import"./ButtonLink-p7Nyyxfr.js";import"./RadioGroup-CifUS7F7.js";import"./index-D8-PC79C.js";import"./Tabs-E-xgLsWy.js";import{T as o}from"./Text-CQXdJK5k.js";import"./TextField-BWnGClvN.js";import"./Tooltip-B9ZJKhsq.js";import"./ScrollArea-B_S57z8L.js";import"./SearchField-kxTQB0WD.js";import"./Link-BkhPVLun.js";import"./Select-BlEAeDGs.js";import"./Skeleton-B62qU-xc.js";import"./Switch-_DNy3hPq.js";import"./clsx-B-dksMZM.js";import"./useStyles-DaPhFVFa.js";import"./useBaseUiId-D_SK3tu4.js";import"./Link-BYO7Me1T.js";import"./utils-SVxEJA3c.js";import"./useFocusRing-CSBfGNH9.js";import"./usePress-C0W0oizE.js";import"./index-DXvUqTe6.js";import"./index-BITTEREo.js";import"./index-B7KODvs-.js";import"./Button-DjvYHEK_.js";import"./Hidden-Bl3CD3Sw.js";import"./Collection-C6PdQmfC.js";import"./FocusScope-CQlMwp1s.js";import"./context-C8UuisDZ.js";import"./useControlledState-hFzvQclK.js";import"./SearchField-CXr1fdwq.js";import"./FieldError-Bv1neE71.js";import"./useLabels-CXdioV2U.js";import"./Input-BdpedIEL.js";import"./useFormReset-JKupIHyW.js";import"./Label-x6hg8m87.js";import"./VisuallyHidden-C9OzMNB-.js";import"./OverlayArrow-jSjk85tW.js";import"./spacing.props-m9PQeFPu.js";import"./TextField-Cy-x5SL9.js";const dr={title:"Backstage UI/Card",component:p,subcomponents:{CardHeader:l,CardBody:d,CardFooter:a}},t={render:h=>r.jsxs(p,{...h,children:[r.jsx(l,{children:"Header"}),r.jsx(d,{children:"Body"}),r.jsx(a,{children:"Footer"})]})},i={args:{style:{width:"300px",height:"200px"}},render:t.render},n={render:()=>r.jsxs(p,{style:{width:"300px",height:"200px"},children:[r.jsx(l,{children:r.jsx(o,{children:"Header"})}),r.jsxs(d,{children:[r.jsx(o,{children:"This is the first paragraph of a long body text that demonstrates how the Card component handles extensive content. The card should adjust accordingly to display all the text properly while maintaining its structure."}),r.jsx(o,{children:"Here's a second paragraph that adds more content to our card body. Having multiple paragraphs helps to visualize how spacing works within the card component."}),r.jsx(o,{children:"This third paragraph continues to add more text to ensure we have a proper demonstration of a card with significant content. This makes it easier to test scrolling behavior and overall layout when content exceeds the initial view."})]}),r.jsx(a,{children:r.jsx(o,{children:"Footer"})})]})},e=({children:h})=>r.jsx("div",{style:{height:40,width:"100%",backgroundColor:"var(--bui-gray-3)",display:"flex",alignItems:"center",paddingInline:"var(--bui-space-3)",borderRadius:"var(--bui-radius-2)",fontSize:"var(--bui-font-size-3)",marginBottom:"var(--bui-space-1)"},children:h}),s={render:()=>r.jsxs(p,{style:{width:"300px",height:"200px"},children:[r.jsx(l,{children:r.jsx(o,{children:"Header"})}),r.jsxs(d,{children:[r.jsx(e,{children:"Hello world"}),r.jsx(e,{children:"Hello world"}),r.jsx(e,{children:"Hello world"}),r.jsx(e,{children:"Hello world"}),r.jsx(e,{children:"Hello world"}),r.jsx(e,{children:"Hello world"}),r.jsx(e,{children:"Hello world"}),r.jsx(e,{children:"Hello world"}),r.jsx(e,{children:"Hello world"}),r.jsx(e,{children:"Hello world"}),r.jsx(e,{children:"Hello world"}),r.jsx(e,{children:"Hello world"}),r.jsx(e,{children:"Hello world"}),r.jsx(e,{children:"Hello world"}),r.jsx(e,{children:"Hello world"}),r.jsx(e,{children:"Hello world"})]}),r.jsx(a,{children:r.jsx(o,{children:"Footer"})})]})};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  render: args => <Card {...args}>
+      <CardHeader>Header</CardHeader>
+      <CardBody>Body</CardBody>
+      <CardFooter>Footer</CardFooter>
+    </Card>
+}`,...t.parameters?.docs?.source}}};i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  args: {
+    style: {
+      width: '300px',
+      height: '200px'
+    }
+  },
+  render: Default.render
+}`,...i.parameters?.docs?.source}}};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  render: () => <Card style={{
+    width: '300px',
+    height: '200px'
+  }}>
+      <CardHeader>
+        <Text>Header</Text>
+      </CardHeader>
+      <CardBody>
+        <Text>
+          This is the first paragraph of a long body text that demonstrates how
+          the Card component handles extensive content. The card should adjust
+          accordingly to display all the text properly while maintaining its
+          structure.
+        </Text>
+        <Text>
+          Here's a second paragraph that adds more content to our card body.
+          Having multiple paragraphs helps to visualize how spacing works within
+          the card component.
+        </Text>
+        <Text>
+          This third paragraph continues to add more text to ensure we have a
+          proper demonstration of a card with significant content. This makes it
+          easier to test scrolling behavior and overall layout when content
+          exceeds the initial view.
+        </Text>
+      </CardBody>
+      <CardFooter>
+        <Text>Footer</Text>
+      </CardFooter>
+    </Card>
+}`,...n.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  render: () => <Card style={{
+    width: '300px',
+    height: '200px'
+  }}>
+      <CardHeader>
+        <Text>Header</Text>
+      </CardHeader>
+      <CardBody>
+        <ListRow>Hello world</ListRow>
+        <ListRow>Hello world</ListRow>
+        <ListRow>Hello world</ListRow>
+        <ListRow>Hello world</ListRow>
+        <ListRow>Hello world</ListRow>
+        <ListRow>Hello world</ListRow>
+        <ListRow>Hello world</ListRow>
+        <ListRow>Hello world</ListRow>
+        <ListRow>Hello world</ListRow>
+        <ListRow>Hello world</ListRow>
+        <ListRow>Hello world</ListRow>
+        <ListRow>Hello world</ListRow>
+        <ListRow>Hello world</ListRow>
+        <ListRow>Hello world</ListRow>
+        <ListRow>Hello world</ListRow>
+        <ListRow>Hello world</ListRow>
+      </CardBody>
+      <CardFooter>
+        <Text>Footer</Text>
+      </CardFooter>
+    </Card>
+}`,...s.parameters?.docs?.source}}};const lr=["Default","CustomSize","WithLongBody","WithListRow"];export{i as CustomSize,t as Default,s as WithListRow,n as WithLongBody,lr as __namedExportsOrder,dr as default};
