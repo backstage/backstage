077dee1902fb0c967bbf728497011db5f29c85af
Patrik Oldsberg <poldsberg@gmail.com>
2025-09-12 18:53:05 +0200

    plugins: scaffold bui-themer plugin
    Signed-off-by: Patrik Oldsberg <poldsberg@gmail.com>

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -73,6 +73,7 @@ import {
 } from '@backstage/plugin-notifications';
 import { CustomizableHomePage } from './components/home/CustomizableHomePage';
 import { HomePage } from './components/home/HomePage';
+import { BuiThemerPage } from '@backstage/plugin-bui-themer';
 
 const app = createApp({
   apis,
@@ -208,6 +209,7 @@ const routes = (
       {customDevToolsPage}
     </Route>
     <Route path="/notifications" element={<NotificationsPage />} />
+    <Route path="/bui-themer" element={<BuiThemerPage />} />
   </FlatRoutes>
 );
 
