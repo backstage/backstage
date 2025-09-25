c890dc6edbea7dc5e85df0c3d1dae37e78117486
Patrik Oldsberg <poldsberg@gmail.com>
2025-09-25 11:58:22 +0200

    bui-themer: remove unnecessary matchMedia mocks
    Signed-off-by: Patrik Oldsberg <poldsberg@gmail.com>

--- plugins/bui-themer/src/components/BuiThemerPage/ThemeContent.test.tsx
+++ plugins/bui-themer/src/components/BuiThemerPage/ThemeContent.test.tsx
@@ -30,22 +30,6 @@ jest.mock('./convertMuiToBuiTheme', () => ({
 describe('ThemeContent', () => {
   const muiTheme = createTheme();
 
-  beforeAll(() => {
-    Object.defineProperty(window, 'matchMedia', {
-      writable: true,
-      value: jest.fn().mockImplementation(query => ({
-        matches: false,
-        media: query,
-        onchange: null,
-        addListener: jest.fn(),
-        removeListener: jest.fn(),
-        addEventListener: jest.fn(),
-        removeEventListener: jest.fn(),
-        dispatchEvent: jest.fn(),
-      })),
-    });
-  });
-
   it('renders title, variant and tabs', async () => {
     await renderInTestApp(
       <ThemeContent
