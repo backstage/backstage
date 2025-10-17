926389b38c4d5bd0bc47ebddecec95838c3abb1e
Patrik Oldsberg <poldsberg@gmail.com>
2025-10-17 19:40:36 +0200

    eslint-plugin: add lint rule to ensure BUI CSS is not imported in plugins
    Signed-off-by: Patrik Oldsberg <poldsberg@gmail.com>

--- packages/dev-utils/src/devApp/BuiCss.tsx
+++ packages/dev-utils/src/devApp/BuiCss.tsx
@@ -14,6 +14,8 @@
  * limitations under the License.
  */
 
+// This ensures that dev apps always have the BUI CSS loaded.
+// eslint-disable-next-line @backstage/no-ui-css-imports-in-non-frontend
 import '@backstage/ui/css/styles.css';
 
 /**
