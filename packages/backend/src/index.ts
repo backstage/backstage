3c6161d3ae3416122ff9612427155222e4c125d1
aramissennyeydd <aramis.sennyey@doordash.com>
2025-11-12 15:17:35 -0500

    delete system metadata custom plugin
    Signed-off-by: aramissennyeydd <aramis.sennyey@doordash.com>

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -70,7 +70,6 @@ backend.add(searchLoader);
 backend.add(import('@backstage/plugin-techdocs-backend'));
 backend.add(import('@backstage/plugin-signals-backend'));
 backend.add(import('@backstage/plugin-notifications-backend'));
-backend.add(import('./systemMetadataPlugin'));
 backend.add(rootSystemMetadataServiceFactory);
 
 backend.add(import('@backstage/plugin-events-backend-module-google-pubsub'));
