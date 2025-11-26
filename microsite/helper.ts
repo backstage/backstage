989742dc77b7e7ce05a7cbb3f381e42cf6093639
gyan <gyan@BLRKEC691785L.ad.infosys.com>
2025-11-26 11:48:52 +0530

    do suggested changes in sidebar.ts and helper.ts
    Signed-off-by: gyan <gyan@BLRKEC691785L.ad.infosys.com>

--- microsite/helper.ts
+++ microsite/helper.ts
@@ -1,8 +1,39 @@
-export function tryToLoadCustomSidebar(ref: string): any[] {
-  try {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    return require(ref);
-  } catch (e) {
-    return [];
-  }
+export function makeGeneratedIndexCategory(
+  label: string,
+  slug: string,
+  items: (string | object)[],
+  title: string = label,
+) {
+  return {
+    type: 'category',
+    label,
+    link: { type: 'generated-index', title, slug: `/${slug}` },
+    items,
+  };
+}
+
+export function makeApiCategory(
+  featureName: string,
+  sidebar: (string | object)[],
+) {
+  const slug = `/category/${featureName
+    .toLowerCase()
+    .replace(/\s+/g, '-')}-api`;
+  return {
+    type: 'category',
+    label: 'API',
+    link:
+      sidebar && sidebar.length > 0
+        ? { type: 'generated-index', title: `${featureName} API`, slug }
+        : { type: 'doc', id: 'openapi/generated-docs/404' },
+    items: sidebar ?? [],
+  };
+}
+
+export function makeProviderCategory(
+  label: string,
+  slug: string,
+  items: (string | object)[],
+) {
+  return makeGeneratedIndexCategory(label, slug, items, label);
 }
--- microsite/sidebars.ts
+++ microsite/sidebars.ts
@@ -1,5 +1,13 @@
 import { releases } from './releases';
-import { tryToLoadCustomSidebar } from './helper';
+import { makeGeneratedIndexCategory, makeApiCategory } from './helper';
+
+const tryToLoadCustomSidebar = (ref: string): any[] => {
+  try {
+    return require(ref);
+  } catch (e) {
+    return [];
+  }
+};
 
 const catalogSidebar = tryToLoadCustomSidebar(
   '../docs/features/software-catalog/api/sidebar.ts',
@@ -13,296 +21,231 @@ const scaffolderSidebar = tryToLoadCustomSidebar(
 
 export default {
   docs: [
-    {
-      type: 'category',
-      label: 'Overview',
-      link: { type: 'generated-index', title: 'Overview', slug: '/overview' },
-      items: [
-        'overview/what-is-backstage',
-        'overview/technical-overview',
-        'overview/architecture-overview',
-        'overview/roadmap',
-        'overview/threat-model',
-        'overview/versioning-policy',
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Getting Started',
-      link: {
-        type: 'generated-index',
-        title: 'Getting Started',
-        slug: '/getting-started',
+    makeGeneratedIndexCategory('Overview', 'overview', [
+      'overview/what-is-backstage',
+      'overview/technical-overview',
+      'overview/architecture-overview',
+      'overview/roadmap',
+      'overview/threat-model',
+      'overview/versioning-policy',
+    ]),
+    ,
+    makeGeneratedIndexCategory('Getting Started', 'getting-started', [
+      'getting-started/index',
+      makeGeneratedIndexCategory(
+        'Configuring Backstage',
+        'configuring-backstage',
+        [
+          'getting-started/config/database',
+          'getting-started/config/authentication',
+          'getting-started/configure-app-with-plugins',
+          'getting-started/homepage',
+        ],
+      ),
+      makeGeneratedIndexCategory('Deploying Backstage', 'deploying-backstage', [
+        'deployment/index',
+        'deployment/scaling',
+        'deployment/docker',
+        'deployment/k8s',
+      ]),
+      makeGeneratedIndexCategory('Using Backstage', 'using-backstage', [
+        'getting-started/logging-in',
+        'getting-started/register-a-component',
+        'getting-started/create-a-component',
+      ]),
+      'overview/support',
+      'getting-started/keeping-backstage-updated',
+    ]),
+    makeGeneratedIndexCategory('Core Features', 'core-features', [
+      makeGeneratedIndexCategory('Auth and Identity', 'auth-and-identity', [
+        'auth/index',
+        {
+          type: 'category',
+          label: 'Included providers',
+          items: [
+            'auth/auth0/provider',
+            'auth/atlassian/provider',
+            'auth/aws-alb/provider',
+            'auth/microsoft/provider',
+            'auth/microsoft/easy-auth',
+            'auth/bitbucket/provider',
+            'auth/bitbucketServer/provider',
+            'auth/cloudflare/provider',
+            'auth/github/provider',
+            'auth/gitlab/provider',
+            'auth/google/provider',
+            'auth/google/gcp-iap-auth',
+            'auth/guest/provider',
+            'auth/okta/provider',
+            'auth/oauth2-proxy/provider',
+            'auth/onelogin/provider',
+            'auth/vmware-cloud/provider',
+          ],
+        },
+        'auth/identity-resolver',
+        'auth/oauth',
+        'auth/oidc',
+        'auth/add-auth-provider',
+        'auth/service-to-service-auth',
+        'auth/autologout',
+        'auth/troubleshooting',
+      ]),
+
+      {
+        type: 'category',
+        label: 'Kubernetes',
+        items: [
+          'features/kubernetes/overview',
+          'features/kubernetes/installation',
+          'features/kubernetes/configuration',
+          'features/kubernetes/authentication',
+          'features/kubernetes/authentication-strategies',
+          'features/kubernetes/troubleshooting',
+          'features/kubernetes/proxy',
+        ],
       },
-      items: [
-        'getting-started/index',
-        {
-          type: 'category',
-          label: 'Configuring Backstage',
-          link: {
-            type: 'generated-index',
-            title: 'Configuring Backstage',
-            slug: '/configuring-backstage',
-          },
-          items: [
-            'getting-started/config/database',
-            'getting-started/config/authentication',
-            'getting-started/configure-app-with-plugins',
-            'getting-started/homepage',
-          ],
-        },
-        {
-          type: 'category',
-          label: 'Deploying Backstage',
-          link: {
-            type: 'generated-index',
-            title: 'Deploying Backstage',
-            slug: '/deploying-backstage',
-          },
-          items: [
-            'deployment/index',
-            'deployment/scaling',
-            'deployment/docker',
-            'deployment/k8s',
-          ],
-        },
-        {
-          type: 'category',
-          label: 'Using Backstage',
-          link: {
-            type: 'generated-index',
-            title: 'Using Backstage',
-            slug: '/using-backstage',
-          },
-          items: [
-            'getting-started/logging-in',
-            'getting-started/register-a-component',
-            'getting-started/create-a-component',
-          ],
-        },
-        'overview/support',
-        'getting-started/keeping-backstage-updated',
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Core Features',
-      link: {
-        type: 'generated-index',
-        title: 'Core Features',
-        slug: '/core-features',
+
+      {
+        type: 'category',
+        label: 'Notifications',
+        items: [
+          'notifications/index',
+          'notifications/processors',
+          'notifications/usage',
+        ],
       },
-      items: [
-        {
-          type: 'category',
-          label: 'Auth and Identity',
-          link: {
-            type: 'generated-index',
-            title: 'Auth and Identity',
-            slug: '/auth-and-identity',
+
+      {
+        type: 'category',
+        label: 'Permissions',
+        items: [
+          'permissions/overview',
+          'permissions/concepts',
+          'permissions/getting-started',
+          'permissions/writing-a-policy',
+          'permissions/frontend-integration',
+          'permissions/custom-rules',
+          {
+            type: 'category',
+            label: 'Tutorial: using Permissions in your plugin',
+            items: [
+              'permissions/plugin-authors/01-setup',
+              'permissions/plugin-authors/02-adding-a-basic-permission-check',
+              'permissions/plugin-authors/03-adding-a-resource-permission-check',
+              'permissions/plugin-authors/04-authorizing-access-to-paginated-data',
+              'permissions/plugin-authors/05-frontend-authorization',
+            ],
           },
-          items: [
-            'auth/index',
-            {
-              type: 'category',
-              label: 'Included providers',
-              items: [
-                'auth/auth0/provider',
-                'auth/atlassian/provider',
-                'auth/aws-alb/provider',
-                'auth/microsoft/provider',
-                'auth/microsoft/easy-auth',
-                'auth/bitbucket/provider',
-                'auth/bitbucketServer/provider',
-                'auth/cloudflare/provider',
-                'auth/github/provider',
-                'auth/gitlab/provider',
-                'auth/google/provider',
-                'auth/google/gcp-iap-auth',
-                'auth/guest/provider',
-                'auth/okta/provider',
-                'auth/oauth2-proxy/provider',
-                'auth/onelogin/provider',
-                'auth/vmware-cloud/provider',
-              ],
-            },
-            'auth/identity-resolver',
-            'auth/oauth',
-            'auth/oidc',
-            'auth/add-auth-provider',
-            'auth/service-to-service-auth',
-            'auth/autologout',
-            'auth/troubleshooting',
-          ],
-        },
+        ],
+      },
 
-        {
-          type: 'category',
-          label: 'Kubernetes',
-          items: [
-            'features/kubernetes/overview',
-            'features/kubernetes/installation',
-            'features/kubernetes/configuration',
-            'features/kubernetes/authentication',
-            'features/kubernetes/authentication-strategies',
-            'features/kubernetes/troubleshooting',
-            'features/kubernetes/proxy',
-          ],
-        },
+      {
+        type: 'category',
+        label: 'Search',
+        items: [
+          'features/search/search-overview',
+          'features/search/getting-started',
+          'features/search/concepts',
+          makeApiCategory('Search', searchSidebar),
+          ,
+          'features/search/architecture',
+          'features/search/search-engines',
+          'features/search/collators',
+          'features/search/how-to-guides',
+        ],
+      },
 
-        {
-          type: 'category',
-          label: 'Notifications',
-          items: [
-            'notifications/index',
-            'notifications/processors',
-            'notifications/usage',
-          ],
-        },
+      {
+        type: 'category',
+        label: 'Software Catalog',
+        items: [
+          'features/software-catalog/software-catalog-overview',
+          'features/software-catalog/life-of-an-entity',
+          'features/software-catalog/configuration',
+          'features/software-catalog/system-model',
+          'features/software-catalog/descriptor-format',
+          'features/software-catalog/references',
+          'features/software-catalog/well-known-annotations',
+          'features/software-catalog/well-known-relations',
+          'features/software-catalog/well-known-statuses',
+          'features/software-catalog/extending-the-model',
+          'features/software-catalog/external-integrations',
+          'features/software-catalog/catalog-customization',
+          {
+            type: 'category',
+            label: 'API',
+            link:
+              catalogSidebar.length > 0
+                ? {
+                    type: 'generated-index',
+                    title: 'Catalog API',
+                    slug: '/category/catalog-api',
+                  }
+                : { type: 'doc', id: 'openapi/generated-docs/404' },
+            items: catalogSidebar,
+          },
+          'features/software-catalog/creating-the-catalog-graph',
+          'features/software-catalog/faq',
+        ],
+      },
 
-        {
-          type: 'category',
-          label: 'Permissions',
-          items: [
-            'permissions/overview',
-            'permissions/concepts',
-            'permissions/getting-started',
-            'permissions/writing-a-policy',
-            'permissions/frontend-integration',
-            'permissions/custom-rules',
-            {
-              type: 'category',
-              label: 'Tutorial: using Permissions in your plugin',
-              items: [
-                'permissions/plugin-authors/01-setup',
-                'permissions/plugin-authors/02-adding-a-basic-permission-check',
-                'permissions/plugin-authors/03-adding-a-resource-permission-check',
-                'permissions/plugin-authors/04-authorizing-access-to-paginated-data',
-                'permissions/plugin-authors/05-frontend-authorization',
-              ],
-            },
-          ],
-        },
+      {
+        type: 'category',
+        label: 'Software Templates',
+        items: [
+          'features/software-templates/software-templates-index',
+          'features/software-templates/configuration',
+          'features/software-templates/adding-templates',
+          'features/software-templates/writing-templates',
+          'features/software-templates/input-examples',
+          'features/software-templates/builtin-actions',
+          'features/software-templates/writing-custom-actions',
+          'features/software-templates/writing-tests-for-actions',
+          'features/software-templates/writing-custom-field-extensions',
+          'features/software-templates/writing-custom-step-layouts',
+          'features/software-templates/authorizing-scaffolder-template-details',
+          'features/software-templates/migrating-to-rjsf-v5',
+          'features/software-templates/migrating-from-v1beta2-to-v1beta3',
+          'features/software-templates/dry-run-testing',
+          'features/software-templates/experimental',
+          'features/software-templates/templating-extensions',
+          {
+            type: 'category',
+            label: 'API',
+            link:
+              scaffolderSidebar.length > 0
+                ? {
+                    type: 'generated-index',
+                    title: 'Scaffolder API',
+                    slug: '/category/scaffolder-api',
+                  }
+                : { type: 'doc', id: 'openapi/generated-docs/404' },
+            items: scaffolderSidebar,
+          },
+        ],
+      },
 
-        {
-          type: 'category',
-          label: 'Search',
-          items: [
-            'features/search/search-overview',
-            'features/search/getting-started',
-            'features/search/concepts',
-            {
-              type: 'category',
-              label: 'API',
-              link:
-                searchSidebar.length > 0
-                  ? {
-                      type: 'generated-index',
-                      title: 'Search API',
-                      slug: '/category/search-api',
-                    }
-                  : { type: 'doc', id: 'openapi/generated-docs/404' },
-              items: searchSidebar,
-            },
-            'features/search/architecture',
-            'features/search/search-engines',
-            'features/search/collators',
-            'features/search/how-to-guides',
-          ],
-        },
-
-        {
-          type: 'category',
-          label: 'Software Catalog',
-          items: [
-            'features/software-catalog/software-catalog-overview',
-            'features/software-catalog/life-of-an-entity',
-            'features/software-catalog/configuration',
-            'features/software-catalog/system-model',
-            'features/software-catalog/descriptor-format',
-            'features/software-catalog/references',
-            'features/software-catalog/well-known-annotations',
-            'features/software-catalog/well-known-relations',
-            'features/software-catalog/well-known-statuses',
-            'features/software-catalog/extending-the-model',
-            'features/software-catalog/external-integrations',
-            'features/software-catalog/catalog-customization',
-            {
-              type: 'category',
-              label: 'API',
-              link:
-                catalogSidebar.length > 0
-                  ? {
-                      type: 'generated-index',
-                      title: 'Catalog API',
-                      slug: '/category/catalog-api',
-                    }
-                  : { type: 'doc', id: 'openapi/generated-docs/404' },
-              items: catalogSidebar,
-            },
-            'features/software-catalog/creating-the-catalog-graph',
-            'features/software-catalog/faq',
-          ],
-        },
-
-        {
-          type: 'category',
-          label: 'Software Templates',
-          items: [
-            'features/software-templates/software-templates-index',
-            'features/software-templates/configuration',
-            'features/software-templates/adding-templates',
-            'features/software-templates/writing-templates',
-            'features/software-templates/input-examples',
-            'features/software-templates/builtin-actions',
-            'features/software-templates/writing-custom-actions',
-            'features/software-templates/writing-tests-for-actions',
-            'features/software-templates/writing-custom-field-extensions',
-            'features/software-templates/writing-custom-step-layouts',
-            'features/software-templates/authorizing-scaffolder-template-details',
-            'features/software-templates/migrating-to-rjsf-v5',
-            'features/software-templates/migrating-from-v1beta2-to-v1beta3',
-            'features/software-templates/dry-run-testing',
-            'features/software-templates/experimental',
-            'features/software-templates/templating-extensions',
-            {
-              type: 'category',
-              label: 'API',
-              link:
-                scaffolderSidebar.length > 0
-                  ? {
-                      type: 'generated-index',
-                      title: 'Scaffolder API',
-                      slug: '/category/scaffolder-api',
-                    }
-                  : { type: 'doc', id: 'openapi/generated-docs/404' },
-              items: scaffolderSidebar,
-            },
-          ],
-        },
-
-        {
-          type: 'category',
-          label: 'TechDocs',
-          items: [
-            'features/techdocs/techdocs-overview',
-            'features/techdocs/getting-started',
-            'features/techdocs/concepts',
-            'features/techdocs/addons',
-            'features/techdocs/architecture',
-            'features/techdocs/extensions',
-            'features/techdocs/creating-and-publishing',
-            'features/techdocs/configuration',
-            'features/techdocs/using-cloud-storage',
-            'features/techdocs/configuring-ci-cd',
-            'features/techdocs/cli',
-            'features/techdocs/how-to-guides',
-            'features/techdocs/troubleshooting',
-            'features/techdocs/faqs',
-          ],
-        },
-      ],
-    },
+      {
+        type: 'category',
+        label: 'TechDocs',
+        items: [
+          'features/techdocs/techdocs-overview',
+          'features/techdocs/getting-started',
+          'features/techdocs/concepts',
+          'features/techdocs/addons',
+          'features/techdocs/architecture',
+          'features/techdocs/extensions',
+          'features/techdocs/creating-and-publishing',
+          'features/techdocs/configuration',
+          'features/techdocs/using-cloud-storage',
+          'features/techdocs/configuring-ci-cd',
+          'features/techdocs/cli',
+          'features/techdocs/how-to-guides',
+          'features/techdocs/troubleshooting',
+          'features/techdocs/faqs',
+        ],
+      },
+    ]),
     {
       type: 'category',
       label: 'Integrations',
