3a23b411f6ae13557184dee3678adddd7d6eba3f
Yi Cai <yicai@redhat.com>
2025-12-03 12:25:52 -0500

    updated import to use new dependency
    Signed-off-by: Yi Cai <yicai@redhat.com>

--- plugins/notifications/src/translation.ts
+++ plugins/notifications/src/translation.ts
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import { createTranslationRef } from '@backstage/core-plugin-api/alpha';
+import { createTranslationRef } from '@backstage/frontend-plugin-api';
 
 /** @alpha */
 export const notificationsTranslationRef = createTranslationRef({
