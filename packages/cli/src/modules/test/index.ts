f176eed93256990e5f4c9922a22c7fd42ab6c1b5
Gabriel Dugny <gabriel.dugny@believe.com>
2026-01-13 00:49:02 +0100

    chore: update dependency commander to v14, remove old @types/commander
    Signed-off-by: Gabriel Dugny <gabriel.dugny@believe.com>

--- packages/cli/src/modules/test/index.ts
+++ packages/cli/src/modules/test/index.ts
@@ -27,6 +27,7 @@ export default createCliPlugin({
       execute: async ({ args }) => {
         const command = new Command();
         command.allowUnknownOption(true);
+        command.allowExcessArguments(true);
         command.option(
           '--since <ref>',
           'Only test packages that changed since the specified ref',
@@ -53,7 +54,8 @@ export default createCliPlugin({
         const command = new Command();
 
         command.allowUnknownOption(true);
-        command.helpOption(', --backstage-cli-help');
+        command.allowExcessArguments(true);
+        command.helpOption('--backstage-cli-help');
         command.action(
           lazy(() => import('./commands/package/test'), 'default'),
         );
