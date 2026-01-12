971b11936651d7a37b70a844d2dd57304141f52e
Johan Persson <johanopersson@gmail.com>
2026-01-12 15:49:43 +0100

    refactor(ui): migrate Box to use useDefinition hook
    Signed-off-by: Johan Persson <johanopersson@gmail.com>

--- packages/ui/src/components/Box/types.ts
+++ packages/ui/src/components/Box/types.ts
@@ -14,23 +14,27 @@
  * limitations under the License.
  */
 
-import type { SpaceProps, Responsive, Surface } from '../../types';
+import type { ReactNode, CSSProperties } from 'react';
+import type { Responsive, Surface, SpaceProps } from '../../types';
 
 /** @public */
-export interface BoxProps extends SpaceProps {
-  display?: Responsive<'none' | 'flex' | 'block' | 'inline'>;
+export type BoxOwnProps = {
   as?: keyof JSX.IntrinsicElements;
+  surface?: Responsive<Surface>;
+  display?: Responsive<'none' | 'flex' | 'block' | 'inline'>;
+  position?: Responsive<
+    'static' | 'relative' | 'absolute' | 'fixed' | 'sticky'
+  >;
   width?: Responsive<string>;
   minWidth?: Responsive<string>;
   maxWidth?: Responsive<string>;
   height?: Responsive<string>;
   minHeight?: Responsive<string>;
   maxHeight?: Responsive<string>;
-  position?: Responsive<
-    'static' | 'relative' | 'absolute' | 'fixed' | 'sticky'
-  >;
-  children?: React.ReactNode;
+  children?: ReactNode;
   className?: string;
-  style?: React.CSSProperties;
-  surface?: Responsive<Surface>;
-}
+  style?: CSSProperties;
+};
+
+/** @public */
+export interface BoxProps extends SpaceProps, BoxOwnProps {}
