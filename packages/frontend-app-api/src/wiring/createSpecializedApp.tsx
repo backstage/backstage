bdf05eac2fa82396cf73429478df9b89ce52fed5
Patrik Oldsberg <poldsberg@gmail.com>
2026-01-29 10:08:27 +0100

    Revert "frontend-app-api: switch new API conflict error to be a warning"
    This reverts commit 17e0eb3e5f0c76558c0f4d1c2686ac5861892d14.
    
    Signed-off-by: Patrik Oldsberg <poldsberg@gmail.com>

--- packages/frontend-app-api/src/wiring/createSpecializedApp.tsx
+++ packages/frontend-app-api/src/wiring/createSpecializedApp.tsx
@@ -428,14 +428,13 @@ function createApiFactories(options: {
             existingPluginId: acceptedPluginId,
           },
         });
-        if (!shouldReplace) {
-          // eslint-disable-next-line no-console
-          console.warn(
-            `DEPRECATION WARNING: Plugin '${rejectedPluginId}' is overriding API '${apiRefId}' ` +
-              `from plugin '${acceptedPluginId}'. This will be blocked in a future release. ` +
-              `Please use a module for plugin '${acceptedPluginId}' instead.`,
-          );
+        if (shouldReplace) {
+          factoriesById.set(apiRefId, {
+            pluginId,
+            factory: apiFactory,
+          });
         }
+        continue;
       }
 
       factoriesById.set(apiRefId, { pluginId, factory: apiFactory });
