6015f0f9cfaa81833f793ee86d2a49789ab714db
Patrik Oldsberg <poldsberg@gmail.com>
2026-02-16 13:35:35 +0100

    snapshot fixes for tests
    Signed-off-by: Patrik Oldsberg <poldsberg@gmail.com>
    Signed-off-by: ferin79 <ferinpatel79@gmail.com>

--- packages/frontend-defaults/src/createApp.test.tsx
+++ packages/frontend-defaults/src/createApp.test.tsx
@@ -388,11 +388,13 @@ describe('createApp', () => {
               <component:app/core-progress out=[core.swappableComponent] />
               <component:app/core-not-found-error-page out=[core.swappableComponent] />
               <component:app/core-error-display out=[core.swappableComponent] />
+              <component:app/core-page-layout out=[core.swappableComponent] />
             ]
           </api:app/swappable-components>
           <api:app/icons out=[core.api.factory] />
           <api:app/feature-flags out=[core.api.factory] />
           <api:app/plugin-wrapper out=[core.api.factory] />
+          <api:app/header-actions out=[core.api.factory] />
           <api:app/translations out=[core.api.factory] />
           <api:app/components out=[core.api.factory] />
         ]
@@ -408,7 +410,7 @@ describe('createApp', () => {
                     content [
                       <app/routes out=[core.reactElement]>
                         routes [
-                          <page:my-plugin out=[core.routing.path, core.reactElement] />
+                          <page:my-plugin out=[core.routing.path, core.reactElement, core.title] />
                         ]
                       </app/routes>
                     ]
