df6b7ea0c2ab7e9abf1f143d9033f8e45daef248
benjdlambert <ben@blam.sh>
2026-02-18 08:05:33 +0100

    feat(cli): add auth commands for OIDC login
    Signed-off-by: benjdlambert <ben@blam.sh>

---
+++ packages/cli/src/modules/auth/commands/show.ts
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2025 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import yargs from 'yargs';
+import { httpJson } from '../lib/http';
+import { getSelectedInstance } from '../lib/storage';
+import { accessTokenNeedsRefresh, refreshAccessToken } from '../lib/auth';
+import { getSecretStore } from '../lib/secretStore';
+
+export async function show(argv: string[]) {
+  const parsed = await yargs(argv)
+    .option('instance', {
+      type: 'string',
+      desc: 'Name of the instance to show',
+    })
+    .parse();
+
+  let instance = await getSelectedInstance(parsed.instance);
+
+  if (accessTokenNeedsRefresh(instance)) {
+    instance = await refreshAccessToken(instance.name);
+  }
+  const authBase = new URL('/api/auth', instance.baseUrl)
+    .toString()
+    .replace(/\/$/, '');
+
+  const secretStore = await getSecretStore();
+  const service = `backstage-cli:instance:${instance.name}`;
+  const accessToken = await secretStore.get(service, 'accessToken');
+  if (!accessToken) {
+    throw new Error('No access token found. Run "auth login" to authenticate.');
+  }
+
+  const userinfo = await httpJson<{ claims: { sub: string; ent: string[] } }>(
+    `${authBase}/v1/userinfo`,
+    {
+      headers: { Authorization: `Bearer ${accessToken}` },
+      signal: AbortSignal.timeout(30_000),
+    },
+  );
+
+  process.stdout.write(`User: ${userinfo.claims.sub}\n`);
+  process.stdout.write(`\n`);
+  process.stdout.write(`Ownership:\n`);
+  for (const ent of userinfo.claims.ent ?? []) {
+    process.stdout.write(`  - ${ent}\n`);
+  }
+}
