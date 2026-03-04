0c97f8b14eb18ca7d277c8705f0d1bc3360a4fe2
Johan Persson <johanopersson@gmail.com>
2026-03-04 15:16:28 +0100

    feat(ui): add analytics types
    Signed-off-by: Johan Persson <johanopersson@gmail.com>

---
+++ packages/ui/src/analytics/types.ts
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2026 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Key-value metadata attached to analytics events.
+ * @public
+ */
+export type AnalyticsEventAttributes = {
+  [key: string]: string | boolean | number;
+};
+
+/**
+ * A tracker that captures analytics events. The signature matches
+ * Backstage's AnalyticsTracker for direct pass-through wiring.
+ * @public
+ */
+export type AnalyticsTracker = {
+  captureEvent: (
+    action: string,
+    subject: string,
+    options?: {
+      value?: number;
+      attributes?: AnalyticsEventAttributes;
+    },
+  ) => void;
+};
+
+/**
+ * A hook function that returns an AnalyticsTracker.
+ * Provided via context by the consumer (e.g. a Backstage app).
+ * @public
+ */
+export type UseAnalyticsFn = () => AnalyticsTracker;
