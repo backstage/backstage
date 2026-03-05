a733fd1a68ee47314567713b1e3510eb69bea84a
Johan Persson <johanopersson@gmail.com>
2026-03-05 09:19:09 +0100

    feat(ui): support native HTML attributes on Flex component
    Extend FlexProps to accept standard HTML div attributes (e.g. onClick,
    id, aria-*) by inheriting from React.HTMLAttributes<HTMLDivElement>.
    The rest props are spread onto the underlying div element.
    
    Signed-off-by: Johan Persson <johanopersson@gmail.com>

--- packages/ui/src/components/Flex/Flex.tsx
+++ packages/ui/src/components/Flex/Flex.tsx
@@ -21,7 +21,7 @@ import { FlexDefinition } from './definition';
 
 /** @public */
 export const Flex = forwardRef<HTMLDivElement, FlexProps>((props, ref) => {
-  const { ownProps, dataAttributes, utilityStyle } = useDefinition(
+  const { ownProps, dataAttributes, utilityStyle, restProps } = useDefinition(
     FlexDefinition,
     { gap: '4', ...props },
   );
@@ -33,6 +33,7 @@ export const Flex = forwardRef<HTMLDivElement, FlexProps>((props, ref) => {
       className={classes.root}
       style={{ ...utilityStyle, ...ownProps.style }}
       {...dataAttributes}
+      {...restProps}
     >
       {childrenWithBgProvider}
     </div>
