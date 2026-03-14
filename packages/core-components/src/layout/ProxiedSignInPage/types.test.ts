ccfbc7b10d081c8795b4a60cdaedef02030c3f82
Gabriel Dugny <gabriel.dugny@believe.com>
2026-03-14 11:38:12 +0100

    chore: Switch some zod imports to type only
    Signed-off-by: Gabriel Dugny <gabriel.dugny@believe.com>

--- packages/core-components/src/layout/ProxiedSignInPage/types.test.ts
+++ packages/core-components/src/layout/ProxiedSignInPage/types.test.ts
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import { TypeOf } from 'zod/v3';
+import type { TypeOf } from 'zod/v3';
 import { ProxiedSession, proxiedSessionSchema } from './types';
 
 describe('types', () => {
