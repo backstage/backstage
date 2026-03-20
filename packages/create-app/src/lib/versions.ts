85c9ad373fe5529cc16d6d12763805f85e3338bf
Github patch release workflow <noreply@backstage.io>
2026-03-20 15:42:57 +0000

    Patch from PR #33413
    Signed-off-by: Github patch release workflow <noreply@backstage.io>

--- packages/create-app/src/lib/versions.ts
+++ packages/create-app/src/lib/versions.ts
@@ -39,7 +39,6 @@ import { version as catalogClient } from '../../../catalog-client/package.json';
 import { version as catalogModel } from '../../../catalog-model/package.json';
 import { version as cli } from '../../../cli/package.json';
 import { version as cliDefaults } from '../../../cli-defaults/package.json';
-import { version as cliModuleNew } from '../../../cli-module-new/package.json';
 import { version as config } from '../../../config/package.json';
 import { version as coreAppApi } from '../../../core-app-api/package.json';
 import { version as coreCompatApi } from '../../../core-compat-api/package.json';
@@ -110,7 +109,6 @@ export const packageVersions = {
   '@backstage/catalog-model': catalogModel,
   '@backstage/cli': cli,
   '@backstage/cli-defaults': cliDefaults,
-  '@backstage/cli-module-new': cliModuleNew,
   '@backstage/config': config,
   '@backstage/core-app-api': coreAppApi,
   '@backstage/core-compat-api': coreCompatApi,
