14e944fc5c7080f1462e23cc095b06440605fce9
Estehsan <estehsaan@gmail.com>
2026-04-04 15:30:51 +0200

    chore: remove subtree, switching to git submodule

--- plugins/backstage-plugin-onboarding/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('eslint').Linter.Config} */
-module.exports = {
-  root: true,
-  rules: {
-    'no-warning-comments': ['error', { terms: ['FIXME'], location: 'start' }],
-  },
-};
--- plugins/backstage-plugin-onboarding/workspaces/onboarding/plugins/onboarding-common/.eslintrc.js
+++
@@ -1,3 +0,0 @@
-module.exports = require('@backstage/cli/config/eslint-factory')(__dirname, {
-  settings: { jest: { version: 30 } },
-});
