f87bb8adffdd5da043a055d16c63cabc50e15e5d
Thomas Cardonne <thomas.cardonne@adevinta.com>
2026-06-01 19:05:58 +0200

    feat(scaffolder-backend-module-gerrit): make description parameter optional
    Signed-off-by: Thomas Cardonne <thomas.cardonne@adevinta.com>

--- plugins/scaffolder-backend-module-gerrit/src/actions/gerrit.test.ts
+++ plugins/scaffolder-backend-module-gerrit/src/actions/gerrit.test.ts
@@ -291,6 +291,55 @@ describe('publish:gerrit', () => {
     );
   });
 
+  it('can correctly create a new project without description', async () => {
+    expect.assertions(5);
+    server.use(
+      rest.put('https://gerrithost.org/a/projects/repo', (req, res, ctx) => {
+        expect(req.headers.get('Authorization')).toBe(
+          'Basic Z2Vycml0dXNlcjp1c2VydG9rZW4=',
+        );
+        expect(req.body).toEqual({
+          branches: ['master'],
+          create_empty_commit: false,
+          owners: [],
+          parent: 'workspace',
+        });
+        return res(
+          ctx.status(201),
+          ctx.set('Content-Type', 'application/json'),
+          ctx.json({}),
+        );
+      }),
+    );
+
+    await action.handler({
+      ...mockContext,
+      input: {
+        repoUrl: 'gerrithost.org?workspace=workspace&repo=repo',
+        sourcePath: 'repository/',
+      },
+    });
+
+    expect(initRepoAndPush).toHaveBeenCalledWith({
+      dir: `${mockContext.workspacePath}${path.sep}repository${path.sep}`,
+      remoteUrl: 'https://gerrithost.org/a/repo',
+      defaultBranch: 'master',
+      auth: { username: 'gerrituser', password: 'usertoken' },
+      logger: mockContext.logger,
+      commitMessage: expect.stringContaining('initial commit\n\nChange-Id:'),
+      gitAuthorInfo: {},
+    });
+
+    expect(mockContext.output).toHaveBeenCalledWith(
+      'remoteUrl',
+      'https://gerrithost.org/a/repo',
+    );
+    expect(mockContext.output).toHaveBeenCalledWith(
+      'repoContentsUrl',
+      'https://gerrithost.org/gitiles/repo/+/refs/heads/master',
+    );
+  });
+
   it('should not create new projects on dryRun', async () => {
     await action.handler({
       ...mockContext,
