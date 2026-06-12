1f8b5d5d2c916784c881b2d14173e579c11cf670
evhnn29 <eva.gustavsson@t-online.de>
2026-06-12 16:37:23 +0200

    Update due to change in ID
    Signed-off-by: evhnn29 <eva.gustavsson@t-online.de>

--- microsite/sidebars.ts
+++ microsite/sidebars.ts
@@ -591,7 +591,7 @@ export default {
                 'frontend-system/building-apps/plugin-conversion',
                 'frontend-system/building-apps/module-federation',
                 'frontend-system/building-apps/migrating',
-                'frontend-system/building-apps/troubleshooting-app-visualizer',
+                'frontend-system/building-apps/troubleshooting-visualizer',
               ],
             ),
             sidebarElementWithIndex(
