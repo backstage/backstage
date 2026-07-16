7b538da36adb5e50544061bd52c033d201efa6ce
Shruti2110-coder <jainshruti1021@gmail.com>
2026-07-16 17:35:40 +0530

    chore: remove accidentally committed nested backstage app scaffold
    Signed-off-by: Shruti2110-coder <jainshruti1021@gmail.com>

--- backstage/packages/app/src/modules/nav/index.ts
+++
@@ -1,7 +0,0 @@
-import { createFrontendModule } from '@backstage/frontend-plugin-api';
-import { SidebarContent } from './Sidebar';
-
-export const navModule = createFrontendModule({
-  pluginId: 'app',
-  extensions: [SidebarContent],
-});
