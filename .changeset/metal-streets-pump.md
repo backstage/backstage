115b02e6046d5f73c51fc87744a2e04e12e92f5c
Nikshaan <shettynikshaan@gmail.com>
2026-08-11 21:08:52 +0530

    fix(scaffolder): show loading progress and bound concurrency in template editor file tree
    Signed-off-by: Nikshaan <shettynikshaan@gmail.com>

---
+++ .changeset/metal-streets-pump.md
@@ -0,0 +1,5 @@
+---
+'@backstage/plugin-scaffolder': patch
+---
+
+Show loading progress in the template editor's file tree, and load files in bounded concurrent batches instead of all at once.
--- plugins/scaffolder/src/alpha/components/TemplateEditorPage/DirectoryEditorContext.tsx
+++ plugins/scaffolder/src/alpha/components/TemplateEditorPage/DirectoryEditorContext.tsx
@@ -24,6 +24,7 @@ import {
 
 const MAX_SIZE = 1024 * 1024;
 const MAX_SIZE_MESSAGE = 'This file is too large to be displayed';
+const FILE_READ_CONCURRENCY = 6;
 
 interface DirectoryEditorFile {
   /** The path of the file relative to the root directory */
@@ -38,12 +39,19 @@ interface DirectoryEditorFile {
   /** Save the staged content of the file to disk */
   save(): Promise<void>;
   /** Reload the staged content of the file from disk */
-  reload(): Promise<void>;
+  reload(options?: { silent?: boolean }): Promise<void>;
 }
 
 interface DirectoryEditor {
   /** A list of all files in the edited directory */
   files: Array<DirectoryEditorFile>;
+
+  /** Whether the directory is being loaded from disk */
+  loading: boolean;
+  /** Number of files loaded so far during the current reload */
+  loadedFileCount: number;
+  /** Total number of files to load during the current reload */
+  totalFileCount: number;
 
   /** The currently selected file */
   selectedFile: DirectoryEditorFile | undefined;
@@ -98,13 +106,15 @@ class DirectoryEditorFileManager implements DirectoryEditorFile {
     }
   }
 
-  async reload(): Promise<void> {
+  async reload(options?: { silent?: boolean }): Promise<void> {
     const file = await this.#access.file();
     if (file.size > MAX_SIZE) {
       if (this.#content !== undefined) {
         this.#content = undefined;
         this.#savedContent = undefined;
-        this.#signalUpdate();
+        if (!options?.silent) {
+          this.#signalUpdate();
+        }
       }
       return;
     }
@@ -113,7 +123,9 @@ class DirectoryEditorFileManager implements DirectoryEditorFile {
     if (this.#content !== content) {
       this.#content = content;
       this.#savedContent = content;
-      this.#signalUpdate();
+      if (!options?.silent) {
+        this.#signalUpdate();
+      }
     }
   }
 }
@@ -124,6 +136,9 @@ class DirectoryEditorManager implements DirectoryEditor {
 
   #files: DirectoryEditorFile[] = [];
   #selectedFile: DirectoryEditorFile | undefined;
+  #loading = false;
+  #loadedFileCount = 0;
+  #totalFileCount = 0;
 
   constructor(access: TemplateDirectoryAccess) {
     this.#access = access;
@@ -131,6 +146,18 @@ class DirectoryEditorManager implements DirectoryEditor {
 
   get files() {
     return this.#files;
+  }
+
+  get loading() {
+    return this.#loading;
+  }
+
+  get loadedFileCount() {
+    return this.#loadedFileCount;
+  }
+
+  get totalFileCount() {
+    return this.#totalFileCount;
   }
 
   get selectedFile() {
@@ -155,22 +182,42 @@ class DirectoryEditorManager implements DirectoryEditor {
   }
 
   async reload(): Promise<void> {
+    if (this.#loading) {
+      return;
+    }
+
     const selectedPath = this.#selectedFile?.path;
 
-    const files = await this.#access.listFiles();
-    const fileManagers = await Promise.all(
-      files.map(async file => {
-        const manager = new DirectoryEditorFileManager(
-          file,
-          this.#signalUpdate,
-        );
-        await manager.reload();
-        return manager;
-      }),
-    );
-    this.#files.length = 0;
-    this.#files.push(...fileManagers);
+    this.#loading = true;
+    this.#loadedFileCount = 0;
+    this.#totalFileCount = 0;
+    this.#signalUpdate();
 
+    const fileAccesses = await this.#access.listFiles();
+
+    this.#files.length = 0;
+    this.#totalFileCount = fileAccesses.length;
+    this.#signalUpdate();
+
+    for (let i = 0; i < fileAccesses.length; i += FILE_READ_CONCURRENCY) {
+      const chunk = fileAccesses.slice(i, i + FILE_READ_CONCURRENCY);
+      const managers = await Promise.all(
+        chunk.map(async fileAccess => {
+          const manager = new DirectoryEditorFileManager(
+            fileAccess,
+            this.#signalUpdate,
+          );
+          await manager.reload({ silent: true });
+          this.#loadedFileCount++;
+          this.#signalUpdate();
+          return manager;
+        }),
+      );
+      this.#files.push(...managers);
+      this.#signalUpdate();
+    }
+
+    this.#loading = false;
     this.setSelectedFile(selectedPath);
     this.#signalUpdate();
   }
