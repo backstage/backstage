49f3e9aa276de7413107f07bc6e15e27950fcfd6
Patrik Oldsberg <poldsberg@gmail.com>
2026-08-22 11:39:20 +0200

    Test template methods with prepared context
    Signed-off-by: Patrik Oldsberg <poldsberg@gmail.com>

--- plugins/scaffolder-backend/src/lib/templating/Nunjitsu.test.ts
+++ plugins/scaffolder-backend/src/lib/templating/Nunjitsu.test.ts
@@ -35,20 +35,26 @@ describe('Nunjitsu', () => {
   });
 
   it('supports intrinsic methods', () => {
-    const { render } = createTemplateRenderer();
-    const context = {
+    const renderer = createTemplateRenderer();
+    const context = renderer.prepareContext({
       text: 'backstage',
       number: 12.345,
       array: ['catalog', 'scaffolder'],
       map: new Map([['plugin', 'scaffolder']]),
       set: new Set(['catalog', 'scaffolder']),
-    };
+    });
 
-    expect(render('${{ text.toUpperCase() }}', context)).toBe('BACKSTAGE');
-    expect(render('${{ number.toFixed(2) }}', context)).toBe('12.35');
-    expect(render('${{ array.includes("scaffolder") }}', context)).toBe('true');
-    expect(render('${{ map.get("plugin") }}', context)).toBe('scaffolder');
-    expect(render('${{ set.has("catalog") }}', context)).toBe('true');
+    expect(renderer.render('${{ text.toUpperCase() }}', context)).toBe(
+      'BACKSTAGE',
+    );
+    expect(renderer.render('${{ number.toFixed(2) }}', context)).toBe('12.35');
+    expect(
+      renderer.render('${{ array.includes("scaffolder") }}', context),
+    ).toBe('true');
+    expect(renderer.render('${{ map.get("plugin") }}', context)).toBe(
+      'scaffolder',
+    );
+    expect(renderer.render('${{ set.has("catalog") }}', context)).toBe('true');
   });
 
   it('preserves native values for sole interpolations', () => {
