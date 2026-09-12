75e1fd94e0d61ff4a0348047eeff6f030ae0c9ec
Swayam Shinde <shindeswayam2004@gmail.com>
2026-09-12 10:10:32 +0530

    test(integration): correctly normalize mocked Octokit pagination
    Signed-off-by: Swayam Shinde <shindeswayam2004@gmail.com>

--- packages/integration/src/github/SingleInstanceGithubCredentialsProvider.test.ts
+++ packages/integration/src/github/SingleInstanceGithubCredentialsProvider.test.ts
@@ -17,7 +17,14 @@
 import { GithubCredentialsProvider } from './types';
 
 const octokit = {
-  paginate: async (fn: any) => (await fn()).data,
+  paginate: async <TItems extends object[]>(
+    fn: () =>
+      | { data: TItems | { repositories: TItems } }
+      | Promise<{ data: TItems | { repositories: TItems } }>,
+  ): Promise<TItems> => {
+    const { data } = await fn();
+    return Array.isArray(data) ? data : data.repositories;
+  },
   apps: {
     listInstallations: jest.fn(),
     listReposAccessibleToInstallation: jest.fn(),
@@ -271,8 +278,10 @@ describe('SingleInstanceGithubCredentialsProvider tests', () => {
     } as RestEndpointMethodTypes['apps']['createInstallationAccessToken']['response']);
 
     octokit.apps.listReposAccessibleToInstallation.mockReturnValue({
-      data: [{ name: 'some-repo' }],
-    } as unknown as RestEndpointMethodTypes['apps']['listReposAccessibleToInstallation']['response']);
+      data: {
+        repositories: [{ name: 'some-repo' }],
+      },
+    } as RestEndpointMethodTypes['apps']['listReposAccessibleToInstallation']['response']);
 
     const { token, headers } = await github.getCredentials({
       url: 'https://github.com/Backstage',
@@ -307,8 +316,10 @@ describe('SingleInstanceGithubCredentialsProvider tests', () => {
     } as RestEndpointMethodTypes['apps']['createInstallationAccessToken']['response']);
 
     octokit.apps.listReposAccessibleToInstallation.mockReturnValue({
-      data: [{ name: 'some-repo' }],
-    } as unknown as RestEndpointMethodTypes['apps']['listReposAccessibleToInstallation']['response']);
+      data: {
+        repositories: [{ name: 'some-repo' }],
+      },
+    } as RestEndpointMethodTypes['apps']['listReposAccessibleToInstallation']['response']);
 
     const { token, headers } = await github.getCredentials({
       url: 'https://github.com/backstage',
@@ -465,8 +476,10 @@ describe('SingleInstanceGithubCredentialsProvider tests', () => {
     } as RestEndpointMethodTypes['apps']['createInstallationAccessToken']['response']);
 
     octokit.apps.listReposAccessibleToInstallation.mockReturnValue({
-      data: [{ name: repoName }],
-    } as unknown as RestEndpointMethodTypes['apps']['listReposAccessibleToInstallation']['response']);
+      data: {
+        repositories: [{ name: repoName }],
+      },
+    } as RestEndpointMethodTypes['apps']['listReposAccessibleToInstallation']['response']);
 
     await expect(
       github.getCredentials({
@@ -501,8 +514,10 @@ describe('SingleInstanceGithubCredentialsProvider tests', () => {
     } as RestEndpointMethodTypes['apps']['createInstallationAccessToken']['response']);
 
     octokit.apps.listReposAccessibleToInstallation.mockReturnValue({
-      data: [{ name: repoName }],
-    } as unknown as RestEndpointMethodTypes['apps']['listReposAccessibleToInstallation']['response']);
+      data: {
+        repositories: [{ name: repoName }],
+      },
+    } as RestEndpointMethodTypes['apps']['listReposAccessibleToInstallation']['response']);
 
     await expect(
       github.getCredentials({
