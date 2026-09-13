01407bf4bd3171113bb8ef83984e5fc4448aa68e
Stefan Baramov <sbaramov@gmail.com>
2026-09-13 18:38:32 -0400

    fix(integration): add support for bearer authentication for Bitbucket Cloud (#35566)

--- packages/integration/src/bitbucketCloud/core.ts
+++ packages/integration/src/bitbucketCloud/core.ts
@@ -213,7 +213,8 @@ export function getBitbucketCloudFileFetchUrl(
 /**
  * Gets the request options necessary to make requests to a given provider.
  * Returns headers for authenticating with Bitbucket Cloud.
- * Supports OAuth (clientId/clientSecret), username/token, and username/appPassword auth.
+ * Supports OAuth (clientId/clientSecret), username/token, username/appPassword,
+ * and Bearer token auth.
  *
  * @param config - The relevant provider config
  * @public
@@ -244,6 +245,11 @@ export async function getBitbucketCloudRequestOptions(
       'utf8',
     );
     headers.Authorization = `Basic ${buffer.toString('base64')}`;
+  }
+
+  // Using Bearer authentication (token only)
+  if (config.token && !config.username) {
+    headers.Authorization = `Bearer ${config.token}`;
   }
 
   return { headers };
