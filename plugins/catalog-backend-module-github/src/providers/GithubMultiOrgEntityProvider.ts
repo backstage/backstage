68df247a9f929ab3265a0b9478b99605d59796b9
Hitoshi Kamezaki <kamezaki@ap-com.co.jp>
2026-09-22 08:10:38 +0900

    The namespace specification in `spec.members` is applied only for Group entities.
    Signed-off-by: Hitoshi Kamezaki <kamezaki@ap-com.co.jp>

--- plugins/catalog-backend-module-github/src/providers/GithubMultiOrgEntityProvider.ts
+++ plugins/catalog-backend-module-github/src/providers/GithubMultiOrgEntityProvider.ts
@@ -1054,6 +1054,7 @@ export class GithubMultiOrgEntityProvider implements EntityProvider {
       }
 
       if (isGroupEntity(result) && result.spec.members) {
+        // Group `spec.members` inherits the namespace of it's group so need to explicitly specify refs here
         result.spec.members = result.spec.members.map(m => {
           const { namespace, name } = parseEntityRef(m, {
             defaultKind: 'user',
@@ -1061,11 +1062,6 @@ export class GithubMultiOrgEntityProvider implements EntityProvider {
           });
           return `${namespace}/${name}`;
         });
-      } else if (result.spec && !this.options.teamTransformer) {
-        // Group `spec.members` inherits the namespace of its group so need to explicitly specify refs here
-        result.spec.members = team.members.map(
-          user => `${DEFAULT_NAMESPACE}/${user.login}`,
-        );
       }
     }
 
